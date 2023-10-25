f4c42f92a3be79799b203472861546cb2cf43c76
gballigand <gballigand@users.noreply.github.com>
2023-10-25 08:44:07 +0000

    deploy: c59c226e310371c5102292d5904ea5413a896fee

---
+++ assets/Button.stories-421f8da6.js
@@ -0,0 +1,22 @@
+import{B as y}from"./Button-272d3c80.js";import"./vue.esm-bundler-7b0c4c4e.js";import"./_plugin-vue_export-helper-c27b6911.js";const z={title:"Foundation/Button",component:y,tags:["autodocs"],argTypes:{size:{control:"select",options:["small","medium","large"]},backgroundColor:{control:"color"},onClick:{action:"clicked"}},args:{primary:!1}},r={args:{primary:!0,label:"Button"}},a={args:{primary:!1,label:"Button"}},e={args:{label:"Button",size:"large"}},o={args:{label:"Button",size:"small"}};var s,t,n;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Button'
+  }
+}`,...(n=(t=r.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};var l,c,m;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    primary: false,
+    label: 'Button'
+  }
+}`,...(m=(c=a.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var i,p,u;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    size: 'large'
+  }
+}`,...(u=(p=e.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};var d,g,B;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    size: 'small'
+  }
+}`,...(B=(g=o.parameters)==null?void 0:g.docs)==null?void 0:B.source}}};const k=["Primary","Secondary","Large","Small"];export{e as Large,r as Primary,a as Secondary,o as Small,k as __namedExportsOrder,z as default};
+//# sourceMappingURL=Button.stories-421f8da6.js.map
