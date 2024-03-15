2ea995501be6a905c71794d8b927cb2c1d275055
rduteil <rduteil@users.noreply.github.com>
2024-03-15 08:20:41 +0000

    deploy: d02c66f6d812a9da7ddba8a486cf6ff7e0efaed4

---
+++ assets/Card.stories-847fb4ee.js
@@ -0,0 +1,104 @@
+import{_ as n}from"./FSButton-5b06f77e.js";import{F as a}from"./FSCard-2d7fb699.js";import{F as i}from"./FSText-fa675dd3.js";import{_ as d}from"./FSRow-f7e9ea33.js";import{V as m}from"./VDivider-f90b9c24.js";import"./vue.esm-bundler-0a1af505.js";import"./vue-router-f60daad6.js";import"./useColors-1745df70.js";import"./_commonjsHelpers-725317a4.js";import"./index-a0e5e2a3.js";import"./theme-8d83670f.js";import"./css-403c87e6.js";import"./_plugin-vue_export-helper-c27b6911.js";import"./FSSpan-50615fe3.js";import"./useSlots-de2ef136.js";import"./FSIcon-61d2f8ac.js";import"./VIcon-4501998f.js";import"./color-84472fbe.js";import"./useRender-76465e3d.js";import"./tag-6ffe4ba6.js";import"./FSCol-0a1a151b.js";const V={title:"Foundation/Shared/Card",component:a,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},t={render:()=>({components:{VDivider:m,FSCard:a,FSText:i,FSRow:d,FSButton:n},template:`
+    <div style="display: flex; flex-direction: column; gap: 10px;">
+        <div style="display: flex; gap: 10px;">
+            <FSCard>
+                <template #header>
+                    <FSText font="text-button">Obiwan kenobi</FSText>
+                </template>
+                <template #body>
+                    <FSText> - Hello there!</FSText>
+                </template>
+            </FSCard>
+        </div>
+        <div style="display: flex; gap: 10px;">
+            <FSCard>
+                <template #header>
+                    <FSText font="text-button">General Grievous</FSText>
+                </template>
+                <template #body>
+                    <FSText> - General Kenobi!</FSText>
+                </template>
+                <template #footer>
+                    <FSText font="text-underline">(suprised)</FSText>
+                </template>
+            </FSCard>
+            <FSCard>
+                <template #header>
+                    <FSText font="text-button">General Grievous</FSText>
+                </template>
+                <template #body>
+                    <FSText> - You are a bold one.</FSText>
+                </template>
+            </FSCard>
+        </div>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <div style="display: flex; gap: 10px;">
+            <FSCard color="primary">
+                <FSRow>
+                    <FSButton color="primary" variant="icon" icon="mdi-format-header-1" />
+                    <FSButton color="primary" variant="icon" icon="mdi-format-header-2" />
+                    <FSButton color="primary" variant="icon" icon="mdi-format-header-3" />
+                    <v-divider vertical />
+                    <FSButton color="primary" variant="icon" icon="mdi-format-bold" />
+                    <FSButton color="primary" variant="icon" icon="mdi-format-italic" />
+                </FSRow>
+            </FSCard>
+        </div>
+    </div>`})};var e,o,r;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      VDivider,
+      FSCard,
+      FSText,
+      FSRow,
+      FSButton
+    },
+    template: \`
+    <div style="display: flex; flex-direction: column; gap: 10px;">
+        <div style="display: flex; gap: 10px;">
+            <FSCard>
+                <template #header>
+                    <FSText font="text-button">Obiwan kenobi</FSText>
+                </template>
+                <template #body>
+                    <FSText> - Hello there!</FSText>
+                </template>
+            </FSCard>
+        </div>
+        <div style="display: flex; gap: 10px;">
+            <FSCard>
+                <template #header>
+                    <FSText font="text-button">General Grievous</FSText>
+                </template>
+                <template #body>
+                    <FSText> - General Kenobi!</FSText>
+                </template>
+                <template #footer>
+                    <FSText font="text-underline">(suprised)</FSText>
+                </template>
+            </FSCard>
+            <FSCard>
+                <template #header>
+                    <FSText font="text-button">General Grievous</FSText>
+                </template>
+                <template #body>
+                    <FSText> - You are a bold one.</FSText>
+                </template>
+            </FSCard>
+        </div>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <div style="display: flex; gap: 10px;">
+            <FSCard color="primary">
+                <FSRow>
+                    <FSButton color="primary" variant="icon" icon="mdi-format-header-1" />
+                    <FSButton color="primary" variant="icon" icon="mdi-format-header-2" />
+                    <FSButton color="primary" variant="icon" icon="mdi-format-header-3" />
+                    <v-divider vertical />
+                    <FSButton color="primary" variant="icon" icon="mdi-format-bold" />
+                    <FSButton color="primary" variant="icon" icon="mdi-format-italic" />
+                </FSRow>
+            </FSCard>
+        </div>
+    </div>\`
+  })
+}`,...(r=(o=t.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const k=["Variations"];export{t as Variations,k as __namedExportsOrder,V as default};
---
+++ assets/DateField.stories-41118628.js
@@ -0,0 +1,155 @@
+import{F as l}from"./FSDateField-8b63e65d.js";import{D as u,F as g}from"./FSForm-a81c5c76.js";import{_ as p}from"./FSCol-0a1a151b.js";import{_ as F}from"./FSRow-f7e9ea33.js";import"./vue.esm-bundler-0a1af505.js";import"./FSTextField-579f05ad.js";import"./FSSpan-50615fe3.js";import"./useSlots-de2ef136.js";import"./_plugin-vue_export-helper-c27b6911.js";import"./VField-96becc41.js";import"./index-9b6ba610.js";import"./useRender-76465e3d.js";import"./theme-8d83670f.js";import"./transition-afc70ddd.js";import"./VInput-4e45c526.js";import"./locale-d3181d48.js";import"./proxiedModel-350eff8d.js";import"./VIcon-4501998f.js";import"./color-84472fbe.js";import"./tag-6ffe4ba6.js";import"./density-cfa18558.js";import"./loader-780645af.js";import"./rounded-caec20b0.js";import"./forwardRefs-e658ad70.js";import"./useColors-1745df70.js";import"./_commonjsHelpers-725317a4.js";import"./index-a0e5e2a3.js";import"./VSpacer-638ccd4e.js";import"./VTextField-4fd69d43.js";import"./index-411e5c5c.js";import"./FSCalendar-5ff691b5.js";import"./FSButton-5b06f77e.js";import"./vue-router-f60daad6.js";import"./FSCard-2d7fb699.js";import"./css-403c87e6.js";import"./FSIcon-61d2f8ac.js";import"./useTimeZone-9d6731ed.js";import"./useTranslations-c047d5be.js";import"./VLocaleProvider-07b8d61d.js";import"./date-be665ad9.js";import"./VDefaultsProvider-76561179.js";import"./VBtn-bc3cd0df.js";import"./border-70ae5555.js";import"./elevation-68e8c545.js";import"./group-197554d7.js";import"./dimensions-41d36b25.js";import"./router-1b110d23.js";import"./index-ee92f8e8.js";import"./VProgressCircular-67340a4b.js";import"./resizeObserver-366ccb2e.js";import"./VMenu-35d64e8e.js";import"./VOverlay-5bb0ade1.js";import"./display-58dfd5ea.js";import"./lazy-2c8cbded.js";const ve={title:"Foundation/Shared/Input fields/DateField",component:l,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},e={args:{args:{value1:null,value2:1701234e6,value3:1701234e6}},render:(t,{argTypes:o})=>({components:{FSDateField:l,FSCol:p},props:Object.keys(o),setup(){return{...t}},template:`
+    <FSCol>
+      <FSDateField
+        label="Date & time, primary color"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSDateField
+        label="Required date & time, warning color, with description"
+        description="Description for this field"
+        color="warning"
+        :required="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSDateField
+        label="Uneditable date & time, error color, with description"
+        description="Uneditable description"
+        color="error"
+        :editable="false"
+        v-model="args.value3"
+      />
+    </FSCol>`})},r={args:{args:{valid:!1,value1:null,value2:null,value3:null,rules:u}},render:(t,{argTypes:o})=>({components:{FSForm:g,FSDateField:l,FSCol:p,FSRow:F},props:Object.keys(o),setup(){return{...t}},template:`
+    <FSForm v-model="args.valid">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: before 2024-01-31 00:00"
+          :rules="[args.rules.max(1706738400000)]"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: required & before 2024-01-31 00:00 & after 2024-01-02 00:00"
+          :rules="[args.rules.required(), args.rules.max(1706738400000), args.rules.min(1704146400000)]"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>`})};var i,a,n;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      value2: 1701234000000,
+      value3: 1701234000000
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSDateField,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSDateField
+        label="Date & time, primary color"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSDateField
+        label="Required date & time, warning color, with description"
+        description="Description for this field"
+        color="warning"
+        :required="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSDateField
+        label="Uneditable date & time, error color, with description"
+        description="Uneditable description"
+        color="error"
+        :editable="false"
+        v-model="args.value3"
+      />
+    </FSCol>\`
+  })
+}`,...(n=(a=e.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};var s,d,m;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    args: {
+      valid: false,
+      value1: null,
+      value2: null,
+      value3: null,
+      rules: DateRules
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSForm,
+      FSDateField,
+      FSCol,
+      FSRow
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSForm v-model="args.valid">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: before 2024-01-31 00:00"
+          :rules="[args.rules.max(1706738400000)]"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: required & before 2024-01-31 00:00 & after 2024-01-02 00:00"
+          :rules="[args.rules.required(), args.rules.max(1706738400000), args.rules.min(1704146400000)]"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>\`
+  })
+}`,...(m=(d=r.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};const ce=["Variations","Rules"];export{r as Rules,e as Variations,ce as __namedExportsOrder,ve as default};
