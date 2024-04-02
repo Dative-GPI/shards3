5f3b62a1d3e2e51bf7434339ec0a055864e0ba16
rduteil <rduteil@users.noreply.github.com>
2024-04-02 13:19:55 +0000

    deploy: ae6f0b7608209da2526272a50c51697e101ab455

---
+++ assets/ToggleSet.stories-370c0948.js
@@ -0,0 +1,229 @@
+import{F as l}from"./FSToggleSet-3a7aef15.js";import{_ as o}from"./FSButton-147099af.js";import{F as s}from"./FSText-13835655.js";import"./vue.esm-bundler-37de1696.js";import"./FSSlideGroup-889f024f.js";import"./FSButtonNextIcon-803ed288.js";import"./useColors-ba22af06.js";import"./_commonjsHelpers-725317a4.js";import"./index-a0e5e2a3.js";import"./theme-07d11e72.js";import"./_plugin-vue_export-helper-c27b6911.js";import"./useSlots-df9def22.js";import"./css-cb27a64f.js";import"./VSlideGroup-62b1ce2b.js";import"./index-3a20b1dd.js";import"./useRender-ccb83440.js";import"./display-62cdc35d.js";import"./group-bc028d0d.js";import"./proxiedModel-e8e26ca7.js";import"./tag-9d688242.js";import"./locale-ee313da6.js";import"./resizeObserver-8d36419f.js";import"./VIcon-122293c7.js";import"./color-efc02365.js";import"./FSWrapGroup-603b7e9b.js";import"./vue-router-f501b7d2.js";import"./FSCard-61ed401b.js";import"./FSCol-927ba931.js";import"./FSRow-632ce14e.js";import"./VProgressCircular-aa80f032.js";import"./intersectionObserver-12fc3323.js";import"./FSSpan-ce1ed5e8.js";import"./FSIcon-486fd767.js";const H={title:"Foundation/Shared/ToggleSet",component:l,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},t={args:{args:{values1:[...Array(10).keys()].map(e=>({id:e.toString(),label:`Choice number ${e+1}`})),value1:[],values2:[{id:"1",prependIcon:"mdi-home-thermometer-outline",label:"Keep at room temperature"},{id:"2",prependIcon:"mdi-pool-thermometer",label:"Keep just nice for skinny dipping"},{id:"3",prependIcon:"mdi-snowflake-thermometer",label:"Keep cool"},{id:"4",prependIcon:"mdi-sun-thermometer-outline",label:"Keep at full power of the sun"}],value2:null,values3:[...Array(10).keys()].map(e=>({id:e.toString(),label:`Skier ${e+1}`})),value3:[],value4:"0"}},render:(e,{argTypes:i})=>({components:{FSToggleSet:l,FSButton:o,FSText:s},props:Object.keys(i),setup(){return{...e}},template:`
+    <div style="display: flex; flex-direction: column; gap: 30px;">
+      <FSText> Props only, wrap, single selection, not required </FSText>
+      <FSToggleSet
+        variant="wrap"
+        buttonVariant="standard"
+        activeVariant="standard"
+        buttonColor="light"
+        activeColor="primary"
+        :multiple="false"
+        :required="false"
+        :values="args.values1"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Props only, slide, single selection, required </FSText>
+      <FSToggleSet
+        variant="slide"
+        buttonVariant="standard"
+        activeVariant="full"
+        buttonColor="warning"
+        activeColor="success"
+        :multiple="false"
+        :required="true"
+        :values="args.values2"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Props & item template, slide, multiple selection, required </FSText>
+      <FSToggleSet
+        variant="slide"
+        buttonVariant="standard"
+        activeVariant="standard"
+        buttonColor="dark"
+        activeColor="light"
+        :multiple="true"
+        :required="true"
+        :values="args.values3"
+        v-model="args.value3"
+      >
+        <template #item>
+          <FSButton
+            appendIcon="mdi-ski-water"
+          />
+        </template>
+      </FSToggleSet>
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Default template, slide, single selection, not required </FSText>
+      <FSToggleSet
+        gap="24px"
+        :modelValue="args.value4"
+      >
+        <FSButton
+          variant="icon"
+          icon="mdi-wifi-strength-off-outline"
+          :color="(args.value4 === '0') ? 'primary' : 'light'"
+          @click="() => args.value4 === '0' ? args.value4 = null : args.value4 = '0'"
+        />
+        <FSButton
+          variant="icon"
+          icon="mdi-wifi-strength-outline"
+          :color="(args.value4 === '1') ? 'primary' : 'light'"
+          @click="() => args.value4 === '1' ? args.value4 = null : args.value4 = '1'"
+        />
+        <FSButton
+          variant="icon"
+          icon="mdi-wifi-strength-1"
+          :color="(args.value4 === '2') ? 'primary' : 'light'"
+          @click="() => args.value4 === '2' ? args.value4 = null : args.value4 = '2'"
+        />
+        <FSButton
+          variant="icon"
+          icon="mdi-wifi-strength-2"
+          :color="(args.value4 === '3') ? 'primary' : 'light'"
+          @click="() => args.value4 === '3' ? args.value4 = null : args.value4 = '3'"
+        />
+        <FSButton
+          variant="icon"
+          icon="mdi-wifi-strength-3"
+          :color="(args.value4 === '4') ? 'primary' : 'light'"
+          @click="() => args.value4 === '4' ? args.value4 = null : args.value4 = '4'"
+        />
+        <FSButton
+          variant="icon"
+          icon="mdi-wifi-strength-4"
+          :color="(args.value4 === '5') ? 'primary' : 'light'"
+          @click="() => args.value4 === '5' ? args.value4 = null : args.value4 = '5'"
+        />
+      </FSToggleSet>
+    </div>`})};var r,n,a;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    args: {
+      values1: [...Array(10).keys()].map(i => ({
+        id: i.toString(),
+        label: \`Choice number \${i + 1}\`
+      })),
+      value1: [],
+      values2: [{
+        id: "1",
+        prependIcon: "mdi-home-thermometer-outline",
+        label: "Keep at room temperature"
+      }, {
+        id: "2",
+        prependIcon: "mdi-pool-thermometer",
+        label: "Keep just nice for skinny dipping"
+      }, {
+        id: "3",
+        prependIcon: "mdi-snowflake-thermometer",
+        label: "Keep cool"
+      }, {
+        id: "4",
+        prependIcon: "mdi-sun-thermometer-outline",
+        label: "Keep at full power of the sun"
+      }],
+      value2: null,
+      values3: [...Array(10).keys()].map(i => ({
+        id: i.toString(),
+        label: \`Skier \${i + 1}\`
+      })),
+      value3: [],
+      value4: "0"
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSToggleSet,
+      FSButton,
+      FSText
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <div style="display: flex; flex-direction: column; gap: 30px;">
+      <FSText> Props only, wrap, single selection, not required </FSText>
+      <FSToggleSet
+        variant="wrap"
+        buttonVariant="standard"
+        activeVariant="standard"
+        buttonColor="light"
+        activeColor="primary"
+        :multiple="false"
+        :required="false"
+        :values="args.values1"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Props only, slide, single selection, required </FSText>
+      <FSToggleSet
+        variant="slide"
+        buttonVariant="standard"
+        activeVariant="full"
+        buttonColor="warning"
+        activeColor="success"
+        :multiple="false"
+        :required="true"
+        :values="args.values2"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Props & item template, slide, multiple selection, required </FSText>
+      <FSToggleSet
+        variant="slide"
+        buttonVariant="standard"
+        activeVariant="standard"
+        buttonColor="dark"
+        activeColor="light"
+        :multiple="true"
+        :required="true"
+        :values="args.values3"
+        v-model="args.value3"
+      >
+        <template #item>
+          <FSButton
+            appendIcon="mdi-ski-water"
+          />
+        </template>
+      </FSToggleSet>
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Default template, slide, single selection, not required </FSText>
+      <FSToggleSet
+        gap="24px"
+        :modelValue="args.value4"
+      >
+        <FSButton
+          variant="icon"
+          icon="mdi-wifi-strength-off-outline"
+          :color="(args.value4 === '0') ? 'primary' : 'light'"
+          @click="() => args.value4 === '0' ? args.value4 = null : args.value4 = '0'"
+        />
+        <FSButton
+          variant="icon"
+          icon="mdi-wifi-strength-outline"
+          :color="(args.value4 === '1') ? 'primary' : 'light'"
+          @click="() => args.value4 === '1' ? args.value4 = null : args.value4 = '1'"
+        />
+        <FSButton
+          variant="icon"
+          icon="mdi-wifi-strength-1"
+          :color="(args.value4 === '2') ? 'primary' : 'light'"
+          @click="() => args.value4 === '2' ? args.value4 = null : args.value4 = '2'"
+        />
+        <FSButton
+          variant="icon"
+          icon="mdi-wifi-strength-2"
+          :color="(args.value4 === '3') ? 'primary' : 'light'"
+          @click="() => args.value4 === '3' ? args.value4 = null : args.value4 = '3'"
+        />
+        <FSButton
+          variant="icon"
+          icon="mdi-wifi-strength-3"
+          :color="(args.value4 === '4') ? 'primary' : 'light'"
+          @click="() => args.value4 === '4' ? args.value4 = null : args.value4 = '4'"
+        />
+        <FSButton
+          variant="icon"
+          icon="mdi-wifi-strength-4"
+          :color="(args.value4 === '5') ? 'primary' : 'light'"
+          @click="() => args.value4 === '5' ? args.value4 = null : args.value4 = '5'"
+        />
+      </FSToggleSet>
+    </div>\`
+  })
+}`,...(a=(n=t.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const J=["Variations"];export{t as Variations,J as __namedExportsOrder,H as default};
