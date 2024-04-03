c0c2438380b3a4362bffd0cd9ad48abaaa77b090
rduteil <rduteil@users.noreply.github.com>
2024-04-03 13:05:22 +0000

    deploy: 13e01a0600c89855de090bcd7dd75bb768a96888

---
+++ assets/OptionGroup.stories-2b63432a.js
@@ -0,0 +1,176 @@
+import{F as o,a as l}from"./FSOptionGroup-6ac0d35c.js";import{F as s}from"./FSText-13835655.js";import"./vue.esm-bundler-37de1696.js";import"./FSButton-6343e5ff.js";import"./vue-router-f501b7d2.js";import"./FSCard-61ed401b.js";import"./FSCol-927ba931.js";import"./css-cb27a64f.js";import"./_plugin-vue_export-helper-c27b6911.js";import"./FSRow-632ce14e.js";import"./useColors-ba22af06.js";import"./_commonjsHelpers-725317a4.js";import"./index-a0e5e2a3.js";import"./theme-07d11e72.js";import"./VProgressCircular-aa80f032.js";import"./color-efc02365.js";import"./useRender-ccb83440.js";import"./intersectionObserver-12fc3323.js";import"./resizeObserver-8d36419f.js";import"./VIcon-122293c7.js";import"./tag-9d688242.js";import"./FSSpan-ce1ed5e8.js";import"./useSlots-df9def22.js";import"./FSIcon-486fd767.js";import"./FSSlideGroup-4ee12493.js";import"./FSButtonNextIcon-bdc4940b.js";import"./VSlideGroup-62b1ce2b.js";import"./index-3a20b1dd.js";import"./display-62cdc35d.js";import"./group-bc028d0d.js";import"./proxiedModel-e8e26ca7.js";import"./locale-ee313da6.js";import"./FSWrapGroup-603b7e9b.js";const J={title:"Foundation/Shared/OptionGroup",component:o,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},r={args:{args:{values1:[...Array(10).keys()].map(e=>({id:e.toString(),icon:`mdi-numeric-${e+1}`})),value1:[],values2:[...Array(6).keys()].map(e=>({id:e.toString(),icon:`mdi-dice-${e+1}-outline`})),value2:null,values3:[...Array(6).keys()].map(e=>({id:e.toString(),icon:`mdi-hexagon-slice-${e+1}`})),value3:[0],value4:"0"}},render:(e,{argTypes:a})=>({components:{FSOptionGroup:o,FSOptionItem:l,FSText:s},props:Object.keys(a),setup(){return{...e}},template:`
+    <div style="display: flex; flex-direction: column; gap: 30px;">
+      <FSText> Props only, wrap, single selection, not required </FSText>
+      <FSOptionGroup
+        :required="false"
+        :values="args.values1"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Props only, slide, single selection, required </FSText>
+      <FSOptionGroup
+        variant="slide"
+        activeColor="success"
+        :values="args.values2"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Props & item template, slide, multiple selection, required </FSText>
+      <FSOptionGroup
+        variant="slide"
+        activeColor="dark"
+        :multiple="true"
+        :values="args.values3"
+        v-model="args.value3"
+      >
+        <template #item>
+          <FSOptionItem
+            appendIcon="mdi-ski-water"
+          />
+        </template>
+      </FSOptionGroup>
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Default template, slide, single selection, not required </FSText>
+      <FSOptionGroup
+        padding="8px"
+        gap="8px"
+        :modelValue="args.value4"
+      >
+        <FSOptionItem
+          icon="mdi-wifi-strength-off-outline"
+          :color="(args.value4 === '0') ? 'success' : 'error'"
+          @click="() => args.value4 === '0' ? args.value4 = null : args.value4 = '0'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-outline"
+          :color="(args.value4 === '1') ? 'success' : 'error'"
+          @click="() => args.value4 === '1' ? args.value4 = null : args.value4 = '1'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-1"
+          :color="(args.value4 === '2') ? 'success' : 'error'"
+          @click="() => args.value4 === '2' ? args.value4 = null : args.value4 = '2'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-2"
+          :color="(args.value4 === '3') ? 'success' : 'error'"
+          @click="() => args.value4 === '3' ? args.value4 = null : args.value4 = '3'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-3"
+          :color="(args.value4 === '4') ? 'success' : 'error'"
+          @click="() => args.value4 === '4' ? args.value4 = null : args.value4 = '4'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-4"
+          :color="(args.value4 === '5') ? 'success' : 'error'"
+          @click="() => args.value4 === '5' ? args.value4 = null : args.value4 = '5'"
+        />
+      </FSOptionGroup>
+    </div>`})};var t,i,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    args: {
+      values1: [...Array(10).keys()].map(i => ({
+        id: i.toString(),
+        icon: \`mdi-numeric-\${i + 1}\`
+      })),
+      value1: [],
+      values2: [...Array(6).keys()].map(i => ({
+        id: i.toString(),
+        icon: \`mdi-dice-\${i + 1}-outline\`
+      })),
+      value2: null,
+      values3: [...Array(6).keys()].map(i => ({
+        id: i.toString(),
+        icon: \`mdi-hexagon-slice-\${i + 1}\`
+      })),
+      value3: [0],
+      value4: "0"
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSOptionGroup,
+      FSOptionItem,
+      FSText
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <div style="display: flex; flex-direction: column; gap: 30px;">
+      <FSText> Props only, wrap, single selection, not required </FSText>
+      <FSOptionGroup
+        :required="false"
+        :values="args.values1"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Props only, slide, single selection, required </FSText>
+      <FSOptionGroup
+        variant="slide"
+        activeColor="success"
+        :values="args.values2"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Props & item template, slide, multiple selection, required </FSText>
+      <FSOptionGroup
+        variant="slide"
+        activeColor="dark"
+        :multiple="true"
+        :values="args.values3"
+        v-model="args.value3"
+      >
+        <template #item>
+          <FSOptionItem
+            appendIcon="mdi-ski-water"
+          />
+        </template>
+      </FSOptionGroup>
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Default template, slide, single selection, not required </FSText>
+      <FSOptionGroup
+        padding="8px"
+        gap="8px"
+        :modelValue="args.value4"
+      >
+        <FSOptionItem
+          icon="mdi-wifi-strength-off-outline"
+          :color="(args.value4 === '0') ? 'success' : 'error'"
+          @click="() => args.value4 === '0' ? args.value4 = null : args.value4 = '0'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-outline"
+          :color="(args.value4 === '1') ? 'success' : 'error'"
+          @click="() => args.value4 === '1' ? args.value4 = null : args.value4 = '1'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-1"
+          :color="(args.value4 === '2') ? 'success' : 'error'"
+          @click="() => args.value4 === '2' ? args.value4 = null : args.value4 = '2'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-2"
+          :color="(args.value4 === '3') ? 'success' : 'error'"
+          @click="() => args.value4 === '3' ? args.value4 = null : args.value4 = '3'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-3"
+          :color="(args.value4 === '4') ? 'success' : 'error'"
+          @click="() => args.value4 === '4' ? args.value4 = null : args.value4 = '4'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-4"
+          :color="(args.value4 === '5') ? 'success' : 'error'"
+          @click="() => args.value4 === '5' ? args.value4 = null : args.value4 = '5'"
+        />
+      </FSOptionGroup>
+    </div>\`
+  })
+}`,...(n=(i=r.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const K=["Variations"];export{r as Variations,K as __namedExportsOrder,J as default};
