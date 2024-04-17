e53608cf1ca49835b6a42f1c74835329265f7859
rduteil <rduteil@users.noreply.github.com>
2024-04-17 09:41:30 +0000

    deploy: 9c5e93b2a6a8017981f2d3d1c13fc34c1aa0a0fe

--- assets/Dialog.stories-fa69eaee.js
+++ assets/Dialog.stories-fa69eaee.js
@@ -1,4 +1,4 @@
-import{F as G}from"./FSDateRangeField-b5975794.js";import{F as N}from"./FSDateField-b7cc687c.js";import{F as c,b as $}from"./FSCalendarTwin-cdc4e7ae.js";import{j as H,e as b,H as I,I as K,J as P,Y as z,K as t,x as A,z as o,P as i,Q as s}from"./vue.esm-bundler-9b0e8103.js";import{_ as O}from"./FSIcon-42763d21.js";import{_ as n}from"./FSSpan-66c77817.js";import{_ as l}from"./FSCol-417d1c42.js";import{_ as r}from"./FSRow-efd9bf42.js";import{u as L}from"./css-a20776b9.js";import{C as J}from"./useColors-c8cdc69f.js";import{u as M}from"./useTranslations-8945c1d2.js";import{_ as Q}from"./_plugin-vue_export-helper-c27b6911.js";import{V as U}from"./VProgressCircular-a5b71e87.js";import{_ as g}from"./FSButton-8289abd7.js";import{F as d}from"./FSCard-bd4b7ce0.js";import"./FSTextField-458b2a19.js";import"./useRules-1f7652b9.js";import"./useSlots-bfff66e3.js";import"./VSpacer-fd2fc595.js";import"./useRender-7c6c7a38.js";import"./theme-eb9d1a1f.js";import"./VTextField-abdb6d44.js";import"./VField-0cfa6d9e.js";import"./index-f5f914d5.js";import"./transition-6fc66ff9.js";import"./VLabel-2c7c7f15.js";import"./VInput-9743ddde.js";import"./locale-e5242025.js";import"./proxiedModel-bfd89816.js";import"./VIcon-dbd6f8e0.js";import"./color-8a9bca19.js";import"./tag-6bef4934.js";import"./density-104762f9.js";import"./loader-ce3d0f3e.js";import"./intersectionObserver-569479cd.js";import"./rounded-f08b035c.js";import"./VDefaultsProvider-b110751b.js";import"./forwardRefs-e658ad70.js";import"./index-d737aecc.js";import"./useAppTimeZone-7e26f50a.js";import"./FSCalendar-e6d2fd7c.js";import"./VLocaleProvider-d1053338.js";import"./date-2780c143.js";import"./VBtn-6366fd39.js";import"./border-e6ad3d9a.js";import"./elevation-9129e0f4.js";import"./group-76451b20.js";import"./dimensions-a3779498.js";import"./router-cb82541f.js";import"./index-5b7905d8.js";import"./VMenu-38668fe2.js";import"./VOverlay-b685a01d.js";import"./display-cd82cc66.js";import"./lazy-9297b748.js";import"./_commonjsHelpers-725317a4.js";import"./index-a0e5e2a3.js";import"./resizeObserver-d1864ccd.js";import"./FSClickable-154d7a9c.js";const j=H({name:"FSRemoveDialog",components:{FSSubmitDialog:c,FSIcon:O,FSSpan:n,FSCol:l,FSRow:r},props:{modelValue:{type:Boolean,required:!1,default:!1},removing:{type:Boolean,required:!1,default:!1},removeTotal:{type:Number,required:!0},removeCurrent:{type:Number,required:!1,default:0}},emits:["update:modelValue"],setup(e){const{isMobileSized:a}=L(),{$tr:F}=M(),v=b(()=>e.removeTotal>1?F("ui.remove-dialog.remove-plural","Remove {0} entities",e.removeTotal.toString()):F("ui.remove-dialog.remove-singular","Remove an entity")),f=b(()=>a.value?"36px":"40px");return{ColorEnum:J,footerHeight:f,title:v}}});function Y(e,a,F,v,f,X){const q=I("FSSubmitDialog");return K(),P(q,A({rightButtonLabel:e.$tr("ui.button.remove","Remove"),rightButtonColor:e.ColorEnum.Error,title:e.title,modelValue:e.$props.modelValue,"onUpdate:modelValue":a[0]||(a[0]=E=>e.$emit("update:modelValue",E))},e.$attrs),z({body:t(()=>[o(l,{gap:"16px"},{default:t(()=>[o(r,null,{default:t(()=>[o(O,{color:e.ColorEnum.Error},{default:t(()=>[i(" mdi-alert-outline ")]),_:1},8,["color"]),o(r,{gap:"2px"},{default:t(()=>[o(n,null,{default:t(()=>[i(s(e.$tr("ui.remove-dialog.body-regular","This action is")),1)]),_:1}),o(n,{font:"text-button"},{default:t(()=>[i(s(e.$tr("ui.remove-dialog.body-bold","definitive")),1)]),_:1})]),_:1})]),_:1}),o(n,null,{default:t(()=>[i(s(e.$tr("ui.remove-dialog.final-warning","Once removed, entities won't be retrievable")),1)]),_:1})]),_:1})]),_:2},[e.$props.removing?{name:"footer",fn:t(()=>[o(r,{align:"center-right",height:e.footerHeight},{default:t(()=>[o(n,null,{default:t(()=>[i(s(e.$props.removeCurrent)+" / "+s(e.$props.removeTotal),1)]),_:1}),o(U,{color:e.ColorEnum.Dark,indeterminate:!0},null,8,["color"])]),_:1},8,["height"])]),key:"0"}:void 0]),1040,["rightButtonLabel","rightButtonColor","title","modelValue"])}const W=Q(j,[["render",Y]]);j.__docgenInfo={displayName:"FSRemoveDialog",exportName:"default",description:"",tags:{},props:[{name:"modelValue",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}},{name:"removing",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}},{name:"removeTotal",type:{name:"number"},required:!0},{name:"removeCurrent",type:{name:"number"},required:!1,defaultValue:{func:!1,value:"0"}}],events:[{name:"update:modelValue"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/shared/foundation-shared-components/components/FSRemoveDialog.vue"]};const oa={title:"Foundation/Shared/Dialog",component:$,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},m={args:{args:{value1:!1,value2:!1,value3:!1}},render:(e,{argTypes:a})=>({components:{FSDialog:$,FSButton:g,FSCard:d,FSSpan:n,FSCol:l,FSRow:r},props:Object.keys(a),setup(){return{...e}},template:`
+import{F as G}from"./FSDateRangeField-2012936c.js";import{F as N}from"./FSDateField-b7cc687c.js";import{F as A}from"./FSToggleSet-7213f538.js";import{F as v,b as $}from"./FSCalendarTwin-1ac17b30.js";import{j as I,e as b,H,I as K,J as P,Y as z,K as t,x as L,z as o,P as i,Q as s}from"./vue.esm-bundler-9b0e8103.js";import{_ as q}from"./FSIcon-42763d21.js";import{_ as n}from"./FSSpan-66c77817.js";import{_ as l}from"./FSCol-417d1c42.js";import{_ as r}from"./FSRow-efd9bf42.js";import{u as J}from"./css-a20776b9.js";import{C as M}from"./useColors-c8cdc69f.js";import{u as Q}from"./useTranslations-8945c1d2.js";import{_ as U}from"./_plugin-vue_export-helper-c27b6911.js";import{V as Y}from"./VProgressCircular-a5b71e87.js";import{_ as S}from"./FSButton-8289abd7.js";import{F as d}from"./FSCard-bd4b7ce0.js";import"./FSTextField-458b2a19.js";import"./useRules-1f7652b9.js";import"./useSlots-bfff66e3.js";import"./VSpacer-fd2fc595.js";import"./useRender-7c6c7a38.js";import"./theme-eb9d1a1f.js";import"./VTextField-abdb6d44.js";import"./VField-0cfa6d9e.js";import"./index-f5f914d5.js";import"./transition-6fc66ff9.js";import"./VLabel-2c7c7f15.js";import"./VInput-9743ddde.js";import"./locale-e5242025.js";import"./proxiedModel-bfd89816.js";import"./VIcon-dbd6f8e0.js";import"./color-8a9bca19.js";import"./tag-6bef4934.js";import"./density-104762f9.js";import"./loader-ce3d0f3e.js";import"./intersectionObserver-569479cd.js";import"./rounded-f08b035c.js";import"./VDefaultsProvider-b110751b.js";import"./forwardRefs-e658ad70.js";import"./index-d737aecc.js";import"./useAppTimeZone-7e26f50a.js";import"./FSCalendar-e6d2fd7c.js";import"./VLocaleProvider-d1053338.js";import"./date-2780c143.js";import"./VBtn-6366fd39.js";import"./border-e6ad3d9a.js";import"./elevation-9129e0f4.js";import"./group-76451b20.js";import"./dimensions-a3779498.js";import"./router-cb82541f.js";import"./index-5b7905d8.js";import"./VMenu-38668fe2.js";import"./VOverlay-b685a01d.js";import"./display-cd82cc66.js";import"./lazy-9297b748.js";import"./FSSlideGroup-18d14808.js";import"./FSButtonNextIcon-090fb355.js";import"./VSlideGroup-ffd03f93.js";import"./resizeObserver-d1864ccd.js";import"./FSWrapGroup-0cd3a162.js";import"./_commonjsHelpers-725317a4.js";import"./index-a0e5e2a3.js";import"./FSClickable-154d7a9c.js";const O=I({name:"FSRemoveDialog",components:{FSSubmitDialog:v,FSIcon:q,FSSpan:n,FSCol:l,FSRow:r},props:{modelValue:{type:Boolean,required:!1,default:!1},removing:{type:Boolean,required:!1,default:!1},removeTotal:{type:Number,required:!0},removeCurrent:{type:Number,required:!1,default:0}},emits:["update:modelValue"],setup(e){const{isMobileSized:a}=J(),{$tr:c}=Q(),F=b(()=>e.removeTotal>1?c("ui.remove-dialog.remove-plural","Remove {0} entities",e.removeTotal.toString()):c("ui.remove-dialog.remove-singular","Remove an entity")),f=b(()=>a.value?"36px":"40px");return{ColorEnum:M,footerHeight:f,title:F}}});function W(e,a,c,F,f,Z){const j=H("FSSubmitDialog");return K(),P(j,L({rightButtonLabel:e.$tr("ui.button.remove","Remove"),rightButtonColor:e.ColorEnum.Error,title:e.title,modelValue:e.$props.modelValue,"onUpdate:modelValue":a[0]||(a[0]=E=>e.$emit("update:modelValue",E))},e.$attrs),z({body:t(()=>[o(l,{gap:"16px"},{default:t(()=>[o(r,null,{default:t(()=>[o(q,{color:e.ColorEnum.Error},{default:t(()=>[i(" mdi-alert-outline ")]),_:1},8,["color"]),o(r,{gap:"2px"},{default:t(()=>[o(n,null,{default:t(()=>[i(s(e.$tr("ui.remove-dialog.body-regular","This action is")),1)]),_:1}),o(n,{font:"text-button"},{default:t(()=>[i(s(e.$tr("ui.remove-dialog.body-bold","definitive")),1)]),_:1})]),_:1})]),_:1}),o(n,null,{default:t(()=>[i(s(e.$tr("ui.remove-dialog.final-warning","Once removed, entities won't be retrievable")),1)]),_:1})]),_:1})]),_:2},[e.$props.removing?{name:"footer",fn:t(()=>[o(r,{align:"center-right",height:e.footerHeight},{default:t(()=>[o(n,null,{default:t(()=>[i(s(e.$props.removeCurrent)+" / "+s(e.$props.removeTotal),1)]),_:1}),o(Y,{color:e.ColorEnum.Dark,indeterminate:!0},null,8,["color"])]),_:1},8,["height"])]),key:"0"}:void 0]),1040,["rightButtonLabel","rightButtonColor","title","modelValue"])}const X=U(O,[["render",W]]);O.__docgenInfo={displayName:"FSRemoveDialog",exportName:"default",description:"",tags:{},props:[{name:"modelValue",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}},{name:"removing",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}},{name:"removeTotal",type:{name:"number"},required:!0},{name:"removeCurrent",type:{name:"number"},required:!1,defaultValue:{func:!1,value:"0"}}],events:[{name:"update:modelValue"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/shared/foundation-shared-components/components/FSRemoveDialog.vue"]};const ma={title:"Foundation/Shared/Dialog",component:$,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},m={args:{args:{buttons1:[...Array(5).keys()].map(e=>({id:e.toString(),label:`Choice number ${e+1}`})),selected1:[],value1:!1,value2:!1,value3:!1}},render:(e,{argTypes:a})=>({components:{FSDialog:$,FSButton:S,FSCard:d,FSToggleSet:A,FSSpan:n,FSCol:l,FSRow:r},props:Object.keys(a),setup(){return{...e}},template:`
     <div style="display: flex; gap: 10px;">
       <FSButton
         color="primary"
@@ -6,14 +6,25 @@ import{F as G}from"./FSDateRangeField-b5975794.js";import{F as N}from"./FSDateFi
         @click="() => args.value1 = true"
       />
       <FSDialog
+        width="90%"
         color="primary"
         v-model="args.value1"
       >
         <template #header>
-          <FSSpan font="text-button">Obiwan kenobi</FSSpan>
+          <FSSpan font="text-button">Toggle set</FSSpan>
         </template>
         <template #body>
-          <FSSpan> - Hello there!</FSSpan>
+          <FSToggleSet
+            variant="wrap"
+            buttonVariant="standard"
+            activeVariant="standard"
+            buttonColor="light"
+            activeColor="primary"
+            :multiple="false"
+            :required="false"
+            :values="args.buttons1"
+            v-model="args.selected1"
+          />
         </template>
       </FSDialog>
       <FSButton
@@ -58,7 +69,7 @@ import{F as G}from"./FSDateRangeField-b5975794.js";import{F as N}from"./FSDateFi
           </FSRow>
         </template>
       </FSDialog>
-    </div>`})},p={args:{args:{value1:!1,value2:!1,value3:!1}},render:(e,{argTypes:a})=>({components:{FSSubmitDialog:c,FSButton:g,FSCard:d,FSSpan:n,FSCol:l,FSRow:r},props:Object.keys(a),setup(){return{...e}},template:`
+    </div>`})},p={args:{args:{value1:!1,value2:!1,value3:!1}},render:(e,{argTypes:a})=>({components:{FSSubmitDialog:v,FSButton:S,FSCard:d,FSSpan:n,FSCol:l,FSRow:r},props:Object.keys(a),setup(){return{...e}},template:`
     <div style="display: flex; gap: 10px;">
       <FSButton
         color="primary"
@@ -106,7 +117,7 @@ import{F as G}from"./FSDateRangeField-b5975794.js";import{F as N}from"./FSDateFi
           </FSRow>
         </template>
       </FSSubmitDialog>
-    </div>`})},u={args:{args:{value1:!1,removing1:!1,value2:!1,removing2:!1,value3:!1,removing3:!1}},render:(e,{argTypes:a})=>({components:{FSRemoveDialog:W,FSButton:g,FSCard:d,FSSpan:n,FSCol:l,FSRow:r},props:Object.keys(a),setup(){return{...e}},template:`
+    </div>`})},u={args:{args:{value1:!1,removing1:!1,value2:!1,removing2:!1,value3:!1,removing3:!1}},render:(e,{argTypes:a})=>({components:{FSRemoveDialog:X,FSButton:S,FSCard:d,FSSpan:n,FSCol:l,FSRow:r},props:Object.keys(a),setup(){return{...e}},template:`
     <div style="display: flex; gap: 10px;">
       <FSButton
         label="Dialog - singular"
@@ -138,7 +149,7 @@ import{F as G}from"./FSDateRangeField-b5975794.js";import{F as N}from"./FSDateFi
         @click:rightButton="() => args.removing3 = true"
         v-model="args.value3"
       />
-    </div>`})},S={args:{args:{value1:!1,date1:null,date2:null}},render:(e,{argTypes:a})=>({components:{FSSubmitDialog:c,FSButton:g,FSDateField:N,FSDateRangeField:G,FSSpan:n,FSCard:d,FSCol:l,FSRow:r},props:Object.keys(a),setup(){return{...e}},template:`
+    </div>`})},g={args:{args:{value1:!1,date1:null,date2:null}},render:(e,{argTypes:a})=>({components:{FSSubmitDialog:v,FSButton:S,FSDateField:N,FSDateRangeField:G,FSSpan:n,FSCard:d,FSCol:l,FSRow:r},props:Object.keys(a),setup(){return{...e}},template:`
     <div style="display: flex; gap: 10px;">
       <FSButton
         color="primary"
@@ -176,6 +187,11 @@ import{F as G}from"./FSDateRangeField-b5975794.js";import{F as N}from"./FSDateFi
     </div>`})};var y,D,h;m.parameters={...m.parameters,docs:{...(y=m.parameters)==null?void 0:y.docs,source:{originalSource:`{
   args: {
     args: {
+      buttons1: [...Array(5).keys()].map(i => ({
+        id: i.toString(),
+        label: \`Choice number \${i + 1}\`
+      })),
+      selected1: [],
       value1: false,
       value2: false,
       value3: false
@@ -188,6 +204,7 @@ import{F as G}from"./FSDateRangeField-b5975794.js";import{F as N}from"./FSDateFi
       FSDialog,
       FSButton,
       FSCard,
+      FSToggleSet,
       FSSpan,
       FSCol,
       FSRow
@@ -206,14 +223,25 @@ import{F as G}from"./FSDateRangeField-b5975794.js";import{F as N}from"./FSDateFi
         @click="() => args.value1 = true"
       />
       <FSDialog
+        width="90%"
         color="primary"
         v-model="args.value1"
       >
         <template #header>
-          <FSSpan font="text-button">Obiwan kenobi</FSSpan>
+          <FSSpan font="text-button">Toggle set</FSSpan>
         </template>
         <template #body>
-          <FSSpan> - Hello there!</FSSpan>
+          <FSToggleSet
+            variant="wrap"
+            buttonVariant="standard"
+            activeVariant="standard"
+            buttonColor="light"
+            activeColor="primary"
+            :multiple="false"
+            :required="false"
+            :values="args.buttons1"
+            v-model="args.selected1"
+          />
         </template>
       </FSDialog>
       <FSButton
@@ -260,7 +288,7 @@ import{F as G}from"./FSDateRangeField-b5975794.js";import{F as N}from"./FSDateFi
       </FSDialog>
     </div>\`
   })
-}`,...(h=(D=m.parameters)==null?void 0:D.docs)==null?void 0:h.source}}};var k,B,R;p.parameters={...p.parameters,docs:{...(k=p.parameters)==null?void 0:k.docs,source:{originalSource:`{
+}`,...(h=(D=m.parameters)==null?void 0:D.docs)==null?void 0:h.source}}};var k,B,C;p.parameters={...p.parameters,docs:{...(k=p.parameters)==null?void 0:k.docs,source:{originalSource:`{
   args: {
     args: {
       value1: false,
@@ -335,7 +363,7 @@ import{F as G}from"./FSDateRangeField-b5975794.js";import{F as N}from"./FSDateFi
       </FSSubmitDialog>
     </div>\`
   })
-}`,...(R=(B=p.parameters)==null?void 0:B.docs)==null?void 0:R.source}}};var C,w,x;u.parameters={...u.parameters,docs:{...(C=u.parameters)==null?void 0:C.docs,source:{originalSource:`{
+}`,...(C=(B=p.parameters)==null?void 0:B.docs)==null?void 0:C.source}}};var R,w,T;u.parameters={...u.parameters,docs:{...(R=u.parameters)==null?void 0:R.docs,source:{originalSource:`{
   args: {
     args: {
       value1: false,
@@ -397,7 +425,7 @@ import{F as G}from"./FSDateRangeField-b5975794.js";import{F as N}from"./FSDateFi
       />
     </div>\`
   })
-}`,...(x=(w=u.parameters)==null?void 0:w.docs)==null?void 0:x.source}}};var T,_,V;S.parameters={...S.parameters,docs:{...(T=S.parameters)==null?void 0:T.docs,source:{originalSource:`{
+}`,...(T=(w=u.parameters)==null?void 0:w.docs)==null?void 0:T.source}}};var x,_,V;g.parameters={...g.parameters,docs:{...(x=g.parameters)==null?void 0:x.docs,source:{originalSource:`{
   args: {
     args: {
       value1: false,
@@ -461,4 +489,4 @@ import{F as G}from"./FSDateRangeField-b5975794.js";import{F as N}from"./FSDateFi
       </FSSubmitDialog>
     </div>\`
   })
-}`,...(V=(_=S.parameters)==null?void 0:_.docs)==null?void 0:V.source}}};const na=["Variations","Submit","Remove","ChainedDialogs"];export{S as ChainedDialogs,u as Remove,p as Submit,m as Variations,na as __namedExportsOrder,oa as default};
+}`,...(V=(_=g.parameters)==null?void 0:_.docs)==null?void 0:V.source}}};const pa=["Variations","Submit","Remove","ChainedDialogs"];export{g as ChainedDialogs,u as Remove,p as Submit,m as Variations,pa as __namedExportsOrder,ma as default};
