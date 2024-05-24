5bd32123eabdc43f2f82ba41d3e84a6fb76bfac5
rduteil <rduteil@users.noreply.github.com>
2024-05-24 08:26:05 +0000

    deploy: eaed910ec7e90c05147055480e8d7710914c63f2

---
+++ assets/AutocompleteManufacturer.stories-d9429ecd.js
@@ -0,0 +1,73 @@
+import{j as b,e as M,I as h,J as y,K as A,B as C}from"./vue.esm-bundler-01006032.js";import{F as V}from"./FSAutocompleteField-c1dfb31d.js";import{C as _}from"./base-d6256dbd.js";import{S as w}from"./serviceFactory-7552fe10.js";import{C as U}from"./composableFactory-25aa3ef9.js";import{u as $}from"./useAutocomplete-3cb054e4.js";import{_ as R}from"./_plugin-vue_export-helper-c27b6911.js";import{_ as D}from"./FSCol-c97fad61.js";import"./FSToggleSet-be653781.js";import"./FSSlideGroup-8aec8bc5.js";import"./FSButtonNextIcon-3107391f.js";import"./FSButton-be94d44f.js";import"./FSClickable-a0c1e9ef.js";import"./FSCard-8fcd8e1e.js";import"./FSRow-83e75060.js";import"./css-279a0029.js";import"./useColors-13ed6ee6.js";import"./_commonjsHelpers-725317a4.js";import"./index-a0e5e2a3.js";import"./theme-a26d2375.js";import"./VProgressCircular-f05fef1e.js";import"./color-8c62b342.js";import"./useRender-4bcbb6ac.js";import"./resizeObserver-308bb976.js";import"./VIcon-17be8ae5.js";import"./tag-1f5f74bd.js";import"./FSSpan-6e79caf3.js";import"./useSlots-e0d7ab7c.js";import"./FSIcon-15b738c8.js";import"./VSlideGroup-e0f01fc0.js";import"./index-491d6eb9.js";import"./display-77f43728.js";import"./goto-9bab4060.js";import"./locale-903ef8d8.js";import"./proxiedModel-69023448.js";import"./group-e9c3615d.js";import"./FSWrapGroup-83a13764.js";import"./VInput-50bb839f.js";import"./transition-9c7eb302.js";import"./density-d09a1492.js";import"./dimensions-4232d0fa.js";import"./VField-f1a9daf9.js";import"./VSpacer-78b81ef3.js";import"./VLabel-f27c70ae.js";import"./loader-878147ed.js";import"./anchor-1cd9d9ab.js";import"./rounded-9001edc0.js";import"./VDefaultsProvider-d99c6ff5.js";import"./forwardRefs-e658ad70.js";import"./FSCheckbox-40e0c55f.js";import"./useRules-54978858.js";import"./VSelectionControl-3e964160.js";import"./index-89394051.js";import"./FSLoader-639650a7.js";import"./elevation-28c6fd5f.js";import"./VSelect-450b500f.js";import"./VTextField-f4d9b05c.js";import"./index-4a977ff0.js";import"./VOverlay-d4d3a71f.js";import"./lazy-d0bac168.js";import"./router-5e751bda.js";import"./scopeId-3678c7b9.js";import"./VMenu-4c05ddd6.js";import"./ssrBoot-b1480e9f.js";import"./border-10554549.js";import"./VImg-f5b3db66.js";import"./VDivider-10bc2e69.js";import"./filter-9717b913.js";import"./useAppOrganisationId-2267e850.js";import"./lodash-569b8a6d.js";import"./useDebounce-ff82ad1d.js";class s{constructor(t){this.id=t.id,this.imageId=t.imageId,this.label=t.label}}class k extends s{constructor(t){super(t),this.description=t.description}}const c=()=>`${_()}/manufacturers`,q=e=>`${c()}/${encodeURIComponent(e)}`,I=new w("manufacturer",k).create(e=>e.build(e.addGet(q),e.addGetMany(c,s),e.addNotify())),N=U.getMany(I),d=b({name:"FSAutocompleteManufacturer",components:{FSAutocompleteField:V},props:{manufacturerFilters:{type:Object,required:!1,default:null},modelValue:{type:[Array,String],required:!1,default:null},toggleSetDisabled:{type:Boolean,required:!1,default:!1}},emits:["update:modelValue"],setup(e,{emit:t}){const{getMany:l,fetching:n,entities:r}=N(),u=v=>l({...e.manufacturerFilters,search:v??void 0}),{toggleSet:a,init:g,onUpdate:F}=$(r,[()=>e.manufacturerFilters],t,u),S=M(()=>g.value&&n.value);return{manufacturers:r,toggleSet:a,loading:S,onUpdate:F}}});function O(e,t,l,n,r,u){const a=h("FSAutocompleteField");return y(),A(a,C({toggleSet:!e.$props.toggleSetDisabled&&e.toggleSet,items:e.manufacturers,loading:e.loading,modelValue:e.$props.modelValue,"onUpdate:modelValue":e.onUpdate},e.$attrs),null,16,["toggleSet","items","loading","modelValue","onUpdate:modelValue"])}const f=R(d,[["render",O]]);d.__docgenInfo={displayName:"FSAutocompleteManufacturer",exportName:"default",description:"",tags:{},props:[{name:"manufacturerFilters",type:{name:"ManufacturerFilters"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"modelValue",type:{name:"string[] | string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"toggleSetDisabled",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}}],events:[{name:"update:modelValue"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/core/foundation-core-components/components/autocompletes/FSAutocompleteManufacturer.vue"]};const et={title:"Foundation/Core/Autocompletes/AutocompleteManufacturer",component:f,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},o={args:{args:{value1:null,value2:null,value3:null,value4:null}},render:(e,{argTypes:t})=>({components:{FSAutocompleteManufacturer:f,FSCol:D},props:Object.keys(t),setup(){return{...e}},template:`
+    <FSCol>
+      <FSAutocompleteManufacturer
+        label="Manufacturer"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteManufacturer
+        label="Manufacturer with toggleset disabled"
+        :toggleSetDisabled="true"
+        v-model="args.value2"
+      />
+      <FSAutocompleteManufacturer
+        label="Manufacturer with multiple selection"
+        :multiple="true"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteManufacturer
+        label="Manufacturer with toggleset disabled and multiple selection"
+        :multiple="true"
+        :toggleSetDisabled="true"
+        v-model="args.value4"
+      />
+    </FSCol>`})};var i,m,p;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      value2: null,
+      value3: null,
+      value4: null
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSAutocompleteManufacturer,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSAutocompleteManufacturer
+        label="Manufacturer"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteManufacturer
+        label="Manufacturer with toggleset disabled"
+        :toggleSetDisabled="true"
+        v-model="args.value2"
+      />
+      <FSAutocompleteManufacturer
+        label="Manufacturer with multiple selection"
+        :multiple="true"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteManufacturer
+        label="Manufacturer with toggleset disabled and multiple selection"
+        :multiple="true"
+        :toggleSetDisabled="true"
+        v-model="args.value4"
+      />
+    </FSCol>\`
+  })
+}`,...(p=(m=o.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const tt=["Variations"];export{o as Variations,tt as __namedExportsOrder,et as default};
--- assets/WithTooltip-V3YHNWJZ-8ca2b70b.js
+++
@@ -1,1 +0,0 @@
-import{W as T,W as e,a as h}from"./DocsRenderer-NNNQARDV-41d5cf56.js";import"./iframe-bc539b46.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./index-8c3ac41d.js";import"./index-41dadfd5.js";import"./index-356e4a49.js";export{T as WithToolTipState,e as WithTooltip,h as WithTooltipPure};
