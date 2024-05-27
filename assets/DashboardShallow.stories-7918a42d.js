69be8a3e4e04df6530fbc3f887e9957cb6a8e810
rduteil <rduteil@users.noreply.github.com>
2024-05-27 06:28:23 +0000

    deploy: 2c21d0a59340ac959e6b885b75638469148cde26

---
+++ assets/DashboardShallow.stories-7918a42d.js
@@ -0,0 +1,40 @@
+import{f,g as C,I as d,J as n,K as h,B as S,o as E,w as $,N as V}from"./vue.esm-bundler-cd768bc3.js";import{F as U}from"./FSSimpleIconTileUI-bdda81e4.js";import{C as m}from"./useColors-91ab292c.js";import{_ as g}from"./_plugin-vue_export-helper-c27b6911.js";import{F as R}from"./FSLoadTile-767d5692.js";import{C as A}from"./base-a00c89a9.js";import{W as k}from"./widgetInfos-913cb8f0.js";import{P as L}from"./pathCrumb-3dbc85d5.js";import{S as O}from"./serviceFactory-6fc44bfb.js";import{C as _}from"./composableFactory-c82cc213.js";import"./FSCard-0de7b634.js";import"./FSCol-ff457bc3.js";import"./css-c2ea3b68.js";import"./FSRow-1d7c2545.js";import"./FSIcon-203715fb.js";import"./VIcon-3b9ab711.js";import"./color-4e667524.js";import"./theme-33f5ac1a.js";import"./useRender-b3fd6f8b.js";import"./tag-bc86e39f.js";import"./FSText-92e9b97f.js";import"./useSlots-2af30a3d.js";import"./FSTile-1d7371a7.js";import"./FSClickable-95c940f4.js";import"./VProgressCircular-f6ca2848.js";import"./resizeObserver-054c2f9a.js";import"./FSCheckbox-0f33b66c.js";import"./FSSpan-da6dde8e.js";import"./useRules-bb9e9594.js";import"./VSelectionControl-e9dab1c2.js";import"./density-80e773e2.js";import"./proxiedModel-80cf7d0e.js";import"./index-f4fa4c36.js";import"./VLabel-a834020c.js";import"./VInput-5fe9e616.js";import"./locale-d1b7e37d.js";import"./index-055d6469.js";import"./transition-2948fa2b.js";import"./dimensions-9d1d90d7.js";import"./_commonjsHelpers-725317a4.js";import"./index-a0e5e2a3.js";import"./FSLoader-c57e8f50.js";import"./elevation-7537bf82.js";import"./useAppOrganisationId-7c704676.js";import"./lodash-569b8a6d.js";class y{constructor(e){this.id=e.id,this.organisationId=e.organisationId,this.dashboardOrganisationTypeId=e.dashboardOrganisationTypeId,this.dashboardOrganisationTypeLabel=e.dashboardOrganisationTypeLabel,this.folderId=e.folderId,this.folderLabel=e.folderLabel,this.folderIcon=e.folderIcon,this.label=e.label,this.code=e.code,this.icon=e.icon,this.tags=e.tags.slice(),this.colors=e.colors.slice(),this.dashboardId=e.dashboardId,this.scope=e.scope}}class G extends y{constructor(e){super(e),this.path=e.path.map(t=>new L(t)).sort((t,a)=>a.index-t.index),this.overrideSingleEntity=e.overrideSingleEntity,this.overrideDynamicEntities=e.overrideDynamicEntities,this.overrideGlobalSelectedEntities=e.overrideGlobalSelectedEntities,this.overrideGlobalEntitiesIds=e.overrideGlobalEntitiesIds?e.overrideGlobalEntitiesIds.slice():null,this.overrideDynamicDates=e.overrideDynamicDates,this.overrideGlobalStartDate=e.overrideGlobalStartDate,this.overrideGlobalEndDate=e.overrideGlobalEndDate,this.overrideUseAutoRefresh=e.overrideUseAutoRefresh,this.overrideAutoRefresh=e.overrideAutoRefresh,this.singleEntity=e.singleEntity,this.dynamicEntities=e.dynamicEntities,this.globalSelectedEntities=e.globalSelectedEntities,this.globalEntitiesIds=e.globalEntitiesIds.slice(),this.dynamicDates=e.dynamicDates,this.globalStartDate=e.globalStartDate,this.globalEndDate=e.globalEndDate,this.useAutoRefresh=e.useAutoRefresh,this.autoRefresh=e.autoRefresh,this.widgets=e.widgets.map(t=>new k(t))}}const u=()=>`${A()}/dashboard-shallows`,s=o=>`${u()}/${encodeURIComponent(o)}`,q=new O("dashboardShallow",G).create(o=>o.build(o.addGet(s),o.addGetMany(u,y),o.addCreate(u),o.addUpdate(s),o.addRemove(s),o.addNotify())),B=_.get(q),w=f({name:"FSDashboardShallowTileUI",props:{bottomColor:{type:[Array,String],required:!1,default:m.Primary},icon:{type:String,required:!1,default:"mdi-view-dashboard"}},components:{FSSimpleIconTileUI:U},setup(o){return{color:C(()=>Array.isArray(o.bottomColor)&&!o.bottomColor.length?m.Primary:o.bottomColor)}}});function x(o,e,t,a,D,F){const l=d("FSSimpleIconTileUI");return n(),h(l,S({bottomColor:o.color,icon:o.$props.icon},o.$attrs),null,16,["bottomColor","icon"])}const N=g(w,[["render",x]]);w.__docgenInfo={displayName:"FSDashboardShallowTileUI",exportName:"default",description:"",tags:{},props:[{name:"bottomColor",type:{name:"ColorBase | ColorBase[]"},required:!1,defaultValue:{func:!1,value:"ColorEnum.Primary"}},{name:"icon",type:{name:"string"},required:!1,defaultValue:{func:!1,value:'"mdi-view-dashboard"'}}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/shared/foundation-shared-components/components/tiles/FSDashboardShallowTileUI.vue"]};const v=f({name:"FSDashboardShallowTile",components:{FSDashboardShallowTileUI:N,FSLoadTile:R},props:{dashboardShallowId:{type:String,required:!0},modelValue:{type:Boolean,required:!1,default:!1},editable:{type:Boolean,required:!1,default:!0}},setup(o){const{get:e,getting:t,entity:a}=B();return E(()=>{e(o.dashboardShallowId)}),$(()=>o.dashboardShallowId,()=>{e(o.dashboardShallowId)}),{getting:t,entity:a}}});function P(o,e,t,a,D,F){const l=d("FSLoadTile"),T=d("FSDashboardShallowTileUI");return o.getting?(n(),h(l,{key:0,editable:o.$props.editable,modelValue:o.$props.modelValue,"onUpdate:modelValue":e[0]||(e[0]=r=>o.$emit("update:modelValue",r))},null,8,["editable","modelValue"])):o.entity?(n(),h(T,S({key:1,label:o.entity.label,code:o.entity.code,bottomColor:o.entity.colors,icon:o.entity.icon,editable:o.$props.editable,modelValue:o.$props.modelValue,"onUpdate:modelValue":e[1]||(e[1]=r=>o.$emit("update:modelValue",r))},o.$attrs),null,16,["label","code","bottomColor","icon","editable","modelValue"])):V("",!0)}const I=g(v,[["render",P]]);v.__docgenInfo={displayName:"FSDashboardShallowTile",exportName:"default",description:"",tags:{},props:[{name:"dashboardShallowId",type:{name:"string"},required:!0},{name:"modelValue",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}},{name:"editable",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"true"}}],events:[{name:"update:modelValue"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/core/foundation-core-components/components/tiles/FSDashboardShallowTile.vue"]};const Le={title:"Foundation/Core/Tiles/DashboardShallow",component:I,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},i={args:{args:{values:["1","2","3"],selected:[!1,!1,!1]}},render:(o,{argTypes:e})=>({components:{FSDashboardShallowTile:I},props:Object.keys(e),setup(){return{...o}},template:`
+    <div style="display: flex; gap: 10px; flex-wrap: wrap; width: 100vw;">
+      <FSDashboardShallowTile
+        v-for="(dashboardShallowId, index) in args.values"
+        :key="index"
+        :dashboardShallowId="dashboardShallowId"
+        @auxclick="args.values.pop()"
+        v-model="args.selected[index]"
+      />
+    </div>`})};var p,c,b;i.parameters={...i.parameters,docs:{...(p=i.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    args: {
+      values: ["1", "2", "3"],
+      selected: [false, false, false]
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSDashboardShallowTile
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <div style="display: flex; gap: 10px; flex-wrap: wrap; width: 100vw;">
+      <FSDashboardShallowTile
+        v-for="(dashboardShallowId, index) in args.values"
+        :key="index"
+        :dashboardShallowId="dashboardShallowId"
+        @auxclick="args.values.pop()"
+        v-model="args.selected[index]"
+      />
+    </div>\`
+  })
+}`,...(b=(c=i.parameters)==null?void 0:c.docs)==null?void 0:b.source}}};const Oe=["Variations"];export{i as Variations,Oe as __namedExportsOrder,Le as default};
---
+++ assets/TimeZones.stories-955a6499.js
@@ -0,0 +1,29 @@
+import{f as Z,g as T,o as V,I as p,J as s,K as a,L as C,B as y,V as h,N as k}from"./vue.esm-bundler-cd768bc3.js";import{F as v}from"./FSSelectField-b12274a3.js";import{F as _}from"./FSChip-0b35e4ee.js";import{u as $}from"./useTimeZones-29217f78.js";import{C as b}from"./useColors-91ab292c.js";import{_ as N}from"./_plugin-vue_export-helper-c27b6911.js";import"./FSDialogMenu-72c7cda0.js";import"./FSCard-0de7b634.js";import"./FSCol-ff457bc3.js";import"./css-c2ea3b68.js";import"./FSRow-1d7c2545.js";import"./VDialog-c1d28576.js";import"./VOverlay-43a7a638.js";import"./forwardRefs-e658ad70.js";import"./useRender-b3fd6f8b.js";import"./theme-33f5ac1a.js";import"./proxiedModel-80cf7d0e.js";import"./anchor-9469ffd7.js";import"./color-4e667524.js";import"./dimensions-9d1d90d7.js";import"./display-23d3bc4a.js";import"./lazy-07480c2b.js";import"./locale-d1b7e37d.js";import"./router-12edcf18.js";import"./scopeId-7a393669.js";import"./transition-2948fa2b.js";import"./VDefaultsProvider-22eb0e57.js";import"./FSRadioGroup-55d3a9e5.js";import"./FSIcon-203715fb.js";import"./VIcon-3b9ab711.js";import"./tag-bc86e39f.js";import"./FSSpan-da6dde8e.js";import"./useSlots-2af30a3d.js";import"./useRules-bb9e9594.js";import"./VSelectionControl-e9dab1c2.js";import"./density-80e773e2.js";import"./index-f4fa4c36.js";import"./VLabel-a834020c.js";import"./VField-d090f013.js";import"./VSpacer-0c52efc7.js";import"./index-055d6469.js";import"./VInput-5fe9e616.js";import"./loader-3bcfb6cd.js";import"./VProgressCircular-f6ca2848.js";import"./resizeObserver-054c2f9a.js";import"./rounded-c9c22222.js";import"./FSCheckbox-0f33b66c.js";import"./FSFadeOut-ad56c427.js";import"./useDebounce-8f204f15.js";import"./FSButton-ecc11eb7.js";import"./FSClickable-95c940f4.js";import"./FSTextField-8af1deda.js";import"./VTextField-d2eba149.js";import"./index-cc35ebf5.js";import"./VSelect-faa1eb31.js";import"./VMenu-501341cc.js";import"./ssrBoot-a9e4f1d0.js";import"./border-d3eb62c5.js";import"./elevation-7537bf82.js";import"./VImg-1d5e27a6.js";import"./VDivider-1fe66aeb.js";import"./VSlideGroup-61bc8bc9.js";import"./goto-fb9b6e06.js";import"./group-9e3f1093.js";import"./base-0de1729e.js";import"./serviceFactory-6fc44bfb.js";import"./_commonjsHelpers-725317a4.js";import"./lodash-569b8a6d.js";import"./composableFactory-c82cc213.js";import"./index-a0e5e2a3.js";const c=Z({name:"FSSelectTimeZone",components:{FSSelectField:v,FSChip:_},props:{modelValue:{type:String,required:!1,default:null}},emits:["update:modelValue"],setup(e){const{getMany:o,entities:r}=$(),n=T(()=>{var m;return(m=r.value.find(i=>i.id===e.modelValue))==null?void 0:m.offset});return V(()=>{o()}),{ColorEnum:b,timeZones:r,offset:n}}});function B(e,o,r,n,m,i){const S=p("FSChip"),g=p("FSSelectField");return s(),a(g,y({itemTitle:"id",items:e.timeZones,modelValue:e.$props.modelValue,"onUpdate:modelValue":o[0]||(o[0]=F=>e.$emit("update:modelValue",F))},e.$attrs),{"append-inner":C(()=>[h(e.$slots,"append-inner",{},()=>[e.offset?(s(),a(S,{key:0,variant:"standard",color:e.ColorEnum.Dark,label:e.offset},null,8,["color","label"])):k("",!0)])]),_:3},16,["items","modelValue"])}const f=N(c,[["render",B]]);c.__docgenInfo={displayName:"FSSelectTimeZone",exportName:"default",description:"",tags:{},props:[{name:"modelValue",type:{name:"string | null"},required:!1,defaultValue:{func:!1,value:"null"}}],events:[{name:"update:modelValue"}],slots:[{name:"append-inner"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/shared/foundation-shared-components/components/selects/FSSelectTimeZone.vue"]};const He={title:"Foundation/Shared/Selectors/SelectTimeZone",component:f,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},t={args:{args:{timeZone1:null}},render:(e,{argTypes:o})=>({components:{FSSelectTimeZone:f},props:Object.keys(o),setup(){return{...e}},template:`
+        <FSSelectTimeZone
+          v-model="args.timeZone1"
+        />
+    `})};var l,d,u;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    args: {
+      timeZone1: null
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSSelectTimeZone
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+        <FSSelectTimeZone
+          v-model="args.timeZone1"
+        />
+    \`
+  })
+}`,...(u=(d=t.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};const Qe=["Standard"];export{t as Standard,Qe as __namedExportsOrder,He as default};
