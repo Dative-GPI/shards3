e0a2d69bd197bbd34c9916741d819f2b4e6587a3
rduteil <rduteil@users.noreply.github.com>
2024-05-29 13:41:01 +0000

    deploy: 10b5d9cd947ab4de3f652d69bee6e2ec4f6a3d37

--- assets/DateField.stories-70a06411.js
+++ assets/DateField.stories-70a06411.js
@@ -1,11 +1,11 @@
-import{F as i}from"./FSDateTimeField-a54ca086.js";import{F as u}from"./FSForm-25bb07d2.js";import{_ as p}from"./FSCol-ff457bc3.js";import{_ as g}from"./FSRow-1d7c2545.js";import{D as F}from"./rules-fd87a465.js";import"./vue.esm-bundler-cd768bc3.js";import"./FSDialogMenu-72c7cda0.js";import"./FSCard-0de7b634.js";import"./useColors-91ab292c.js";import"./_commonjsHelpers-725317a4.js";import"./index-a0e5e2a3.js";import"./theme-33f5ac1a.js";import"./css-c2ea3b68.js";import"./_plugin-vue_export-helper-c27b6911.js";import"./VDialog-c1d28576.js";import"./VOverlay-43a7a638.js";import"./forwardRefs-e658ad70.js";import"./useRender-b3fd6f8b.js";import"./proxiedModel-80cf7d0e.js";import"./anchor-9469ffd7.js";import"./color-4e667524.js";import"./dimensions-9d1d90d7.js";import"./display-23d3bc4a.js";import"./lazy-07480c2b.js";import"./locale-d1b7e37d.js";import"./router-12edcf18.js";import"./scopeId-7a393669.js";import"./transition-2948fa2b.js";import"./VDefaultsProvider-22eb0e57.js";import"./FSTextField-8af1deda.js";import"./VField-d090f013.js";import"./FSSpan-da6dde8e.js";import"./useSlots-2af30a3d.js";import"./VSpacer-0c52efc7.js";import"./index-055d6469.js";import"./VLabel-a834020c.js";import"./VInput-5fe9e616.js";import"./VIcon-3b9ab711.js";import"./tag-bc86e39f.js";import"./density-80e773e2.js";import"./loader-3bcfb6cd.js";import"./VProgressCircular-f6ca2848.js";import"./resizeObserver-054c2f9a.js";import"./rounded-c9c22222.js";import"./FSButton-ecc11eb7.js";import"./FSClickable-95c940f4.js";import"./FSIcon-203715fb.js";import"./useRules-bb9e9594.js";import"./VTextField-d2eba149.js";import"./index-cc35ebf5.js";import"./FSCalendar-af6cbf9b.js";import"./useAppTimeZone-d6a8f46f.js";import"./useTranslations-3c37761e.js";import"./VLocaleProvider-d5cd746f.js";import"./date-e2599fc4.js";import"./VBtn-c02fe116.js";import"./border-d3eb62c5.js";import"./elevation-7537bf82.js";import"./group-9e3f1093.js";import"./index-f4fa4c36.js";import"./FSWindow-ff92c6fb.js";import"./ssrBoot-a9e4f1d0.js";import"./FSClock-0f4ebbae.js";import"./FSSlider-4d2074dd.js";import"./VSlider-5ea44ebd.js";import"./VMenu-501341cc.js";const xe={title:"Foundation/Shared/Input fields/DateTimeField",component:i,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},e={args:{args:{value1:null,value2:1701234e6,value3:1701234e6}},render:(t,{argTypes:o})=>({components:{FSDateTimeField:i,FSCol:p},props:Object.keys(o),setup(){return{...t}},template:`
+import{F as i}from"./FSDateField-9fa8f3cb.js";import{F as u}from"./FSForm-d3dd6a93.js";import{_ as p}from"./FSCol-ff457bc3.js";import{_ as g}from"./FSRow-1d7c2545.js";import{D as F}from"./rules-fd87a465.js";import"./vue.esm-bundler-cd768bc3.js";import"./FSDialogMenu-72c7cda0.js";import"./FSCard-0de7b634.js";import"./useColors-91ab292c.js";import"./_commonjsHelpers-725317a4.js";import"./index-a0e5e2a3.js";import"./theme-33f5ac1a.js";import"./css-c2ea3b68.js";import"./_plugin-vue_export-helper-c27b6911.js";import"./VDialog-c1d28576.js";import"./VOverlay-43a7a638.js";import"./forwardRefs-e658ad70.js";import"./useRender-b3fd6f8b.js";import"./proxiedModel-80cf7d0e.js";import"./anchor-9469ffd7.js";import"./color-4e667524.js";import"./dimensions-9d1d90d7.js";import"./display-23d3bc4a.js";import"./lazy-07480c2b.js";import"./locale-d1b7e37d.js";import"./router-12edcf18.js";import"./scopeId-7a393669.js";import"./transition-2948fa2b.js";import"./VDefaultsProvider-22eb0e57.js";import"./FSTextField-8af1deda.js";import"./VField-d090f013.js";import"./FSSpan-da6dde8e.js";import"./useSlots-2af30a3d.js";import"./VSpacer-0c52efc7.js";import"./index-055d6469.js";import"./VLabel-a834020c.js";import"./VInput-5fe9e616.js";import"./VIcon-3b9ab711.js";import"./tag-bc86e39f.js";import"./density-80e773e2.js";import"./loader-3bcfb6cd.js";import"./VProgressCircular-f6ca2848.js";import"./resizeObserver-054c2f9a.js";import"./rounded-c9c22222.js";import"./FSButton-ecc11eb7.js";import"./FSClickable-95c940f4.js";import"./FSIcon-203715fb.js";import"./useRules-bb9e9594.js";import"./VTextField-d2eba149.js";import"./index-cc35ebf5.js";import"./FSCalendar-af6cbf9b.js";import"./useAppTimeZone-d6a8f46f.js";import"./useTranslations-3c37761e.js";import"./VLocaleProvider-d5cd746f.js";import"./date-e2599fc4.js";import"./VBtn-c02fe116.js";import"./border-d3eb62c5.js";import"./elevation-7537bf82.js";import"./group-9e3f1093.js";import"./index-f4fa4c36.js";import"./VMenu-501341cc.js";const fe={title:"Foundation/Shared/Input fields/DateField",component:i,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},e={args:{args:{value1:null,value2:1701234e6,value3:1701234e6}},render:(t,{argTypes:o})=>({components:{FSDateField:i,FSCol:p},props:Object.keys(o),setup(){return{...t}},template:`
     <FSCol>
-      <FSDateTimeField
+      <FSDateField
         label="Date & time, primary color"
         v-model="args.value1"
       />
       <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
-      <FSDateTimeField
+      <FSDateField
         label="Required date & time, warning color, with description"
         description="Description for this field"
         color="warning"
@@ -13,14 +13,14 @@ import{F as i}from"./FSDateTimeField-a54ca086.js";import{F as u}from"./FSForm-25
         v-model="args.value2"
       />
       <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
-      <FSDateTimeField
+      <FSDateField
         label="Uneditable date & time, error color, with description"
         description="Uneditable description"
         color="error"
         :editable="false"
         v-model="args.value3"
       />
-    </FSCol>`})},r={args:{args:{valid:!1,value1:null,value2:null,value3:null,rules:F}},render:(t,{argTypes:o})=>({components:{FSForm:u,FSDateTimeField:i,FSCol:p,FSRow:g},props:Object.keys(o),setup(){return{...t}},template:`
+    </FSCol>`})},r={args:{args:{valid:!1,value1:null,value2:null,value3:null,rules:F}},render:(t,{argTypes:o})=>({components:{FSForm:u,FSDateField:i,FSCol:p,FSRow:g},props:Object.keys(o),setup(){return{...t}},template:`
     <FSForm v-model="args.valid">
       <FSCol>
         <FSRow>
@@ -29,21 +29,21 @@ import{F as i}from"./FSDateTimeField-a54ca086.js";import{F as u}from"./FSForm-25
           </div>
         </FSRow>
         <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
-        <FSDateTimeField
+        <FSDateField
           label="Rules: required"
           :rules="[args.rules.required()]"
           :required="true"
           v-model="args.value1"
         />
         <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
-        <FSDateTimeField
+        <FSDateField
           label="Rules: before 2024-01-31 00:00"
           :rules="[args.rules.max(1706738400000)]"
           :required="true"
           v-model="args.value2"
         />
         <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
-        <FSDateTimeField
+        <FSDateField
           label="Rules: required & before 2024-01-31 00:00 & after 2024-01-02 00:00"
           :rules="[args.rules.required(), args.rules.max(1706738400000), args.rules.min(1704146400000)]"
           :required="true"
@@ -62,7 +62,7 @@ import{F as i}from"./FSDateTimeField-a54ca086.js";import{F as u}from"./FSForm-25
     argTypes
   }) => ({
     components: {
-      FSDateTimeField,
+      FSDateField,
       FSCol
     },
     props: Object.keys(argTypes),
@@ -73,12 +73,12 @@ import{F as i}from"./FSDateTimeField-a54ca086.js";import{F as u}from"./FSForm-25
     },
     template: \`
     <FSCol>
-      <FSDateTimeField
+      <FSDateField
         label="Date & time, primary color"
         v-model="args.value1"
       />
       <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
-      <FSDateTimeField
+      <FSDateField
         label="Required date & time, warning color, with description"
         description="Description for this field"
         color="warning"
@@ -86,7 +86,7 @@ import{F as i}from"./FSDateTimeField-a54ca086.js";import{F as u}from"./FSForm-25
         v-model="args.value2"
       />
       <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
-      <FSDateTimeField
+      <FSDateField
         label="Uneditable date & time, error color, with description"
         description="Uneditable description"
         color="error"
@@ -95,7 +95,7 @@ import{F as i}from"./FSDateTimeField-a54ca086.js";import{F as u}from"./FSForm-25
       />
     </FSCol>\`
   })
-}`,...(n=(a=e.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};var s,m,d;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+}`,...(n=(a=e.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};var s,d,m;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
   args: {
     args: {
       valid: false,
@@ -110,7 +110,7 @@ import{F as i}from"./FSDateTimeField-a54ca086.js";import{F as u}from"./FSForm-25
   }) => ({
     components: {
       FSForm,
-      FSDateTimeField,
+      FSDateField,
       FSCol,
       FSRow
     },
@@ -129,21 +129,21 @@ import{F as i}from"./FSDateTimeField-a54ca086.js";import{F as u}from"./FSForm-25
           </div>
         </FSRow>
         <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
-        <FSDateTimeField
+        <FSDateField
           label="Rules: required"
           :rules="[args.rules.required()]"
           :required="true"
           v-model="args.value1"
         />
         <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
-        <FSDateTimeField
+        <FSDateField
           label="Rules: before 2024-01-31 00:00"
           :rules="[args.rules.max(1706738400000)]"
           :required="true"
           v-model="args.value2"
         />
         <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
-        <FSDateTimeField
+        <FSDateField
           label="Rules: required & before 2024-01-31 00:00 & after 2024-01-02 00:00"
           :rules="[args.rules.required(), args.rules.max(1706738400000), args.rules.min(1704146400000)]"
           :required="true"
@@ -152,4 +152,4 @@ import{F as i}from"./FSDateTimeField-a54ca086.js";import{F as u}from"./FSForm-25
       </FSCol>
     </FSForm>\`
   })
-}`,...(d=(m=r.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};const Ce=["Variations","Rules"];export{r as Rules,e as Variations,Ce as __namedExportsOrder,xe as default};
+}`,...(m=(d=r.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};const De=["Variations","Rules"];export{r as Rules,e as Variations,De as __namedExportsOrder,fe as default};
