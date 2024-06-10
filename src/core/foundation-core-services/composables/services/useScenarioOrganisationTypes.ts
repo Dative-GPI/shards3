18a45dbb7c2d840a5f1f01956df88bebb590a1bf
gballigand <gballigand@dative-gpi.com>
2024-06-10 16:35:30 +0200

    add duplicate/publish and use createComplete

--- src/core/foundation-core-services/composables/services/useScenarioOrganisationTypes.ts
+++ src/core/foundation-core-services/composables/services/useScenarioOrganisationTypes.ts
@@ -6,14 +6,26 @@ const ScenarioOrganisationTypeServiceFactory = new ServiceFactory<ScenarioOrgani
     .create(factory => factory.build(
         factory.addGet(SCENARIO_ORGANISATION_TYPE_URL),
         factory.addGetMany<ScenarioOrganisationTypeInfosDTO, ScenarioOrganisationTypeInfos, ScenarioOrganisationTypeFilters>(SCENARIO_ORGANISATION_TYPES_URL, ScenarioOrganisationTypeInfos),
-        factory.addCreate<CreateScenarioOrganisationTypeDTO>(SCENARIO_ORGANISATION_TYPES_URL),
         factory.addUpdate<UpdateScenarioOrganisationTypeDTO>(SCENARIO_ORGANISATION_TYPE_URL),
         factory.addRemove(SCENARIO_ORGANISATION_TYPE_URL),
-        factory.addNotify()
+        factory.addRemove(SCENARIO_ORGANISATION_TYPE_URL),
+        factory.addNotify(notifyService => ({
+            ...ServiceFactory.addCustom("duplicate", (axios, scenarioOrganisationTypeId: string) => axios.patch(SCENARIO_ORGANISATION_TYPE_URL(scenarioOrganisationTypeId)), (dto: ScenarioOrganisationTypeDetailsDTO) => {
+              const result = new ScenarioOrganisationTypeDetails(dto);
+              notifyService.notify("update", result);
+              return result;
+            }),
+            ...ServiceFactory.addCustom("publish", (axios, payload: CreateScenarioOrganisationTypeDTO) => axios.post(SCENARIO_ORGANISATION_TYPES_URL(), payload), (dto: ScenarioOrganisationTypeDetailsDTO) => {
+              const result = new ScenarioOrganisationTypeDetails(dto);
+              notifyService.notify("add", result);
+              return result;
+            })
+        }))
     ));
 
 export const useScenarioOrganisationType = ComposableFactory.get(ScenarioOrganisationTypeServiceFactory);
 export const useScenarioOrganisationTypes = ComposableFactory.getMany(ScenarioOrganisationTypeServiceFactory);
-export const useCreateScenarioOrganisationType = ComposableFactory.create(ScenarioOrganisationTypeServiceFactory);
 export const useUpdateScenarioOrganisationType = ComposableFactory.update(ScenarioOrganisationTypeServiceFactory);
 export const useRemoveScenarioOrganisationType = ComposableFactory.remove(ScenarioOrganisationTypeServiceFactory);
+export const useDuplicateScenarioOrganisationType = ComposableFactory.custom(ScenarioOrganisationTypeServiceFactory.duplicate);
+export const usePublishScenarioOrganisationType = ComposableFactory.custom(ScenarioOrganisationTypeServiceFactory.publish);
--- src/core/foundation-core-services/composables/services/useScenarioOrganisations.ts
+++ src/core/foundation-core-services/composables/services/useScenarioOrganisations.ts
@@ -2,18 +2,24 @@ import { ComposableFactory, ServiceFactory } from "@dative-gpi/bones-ui/core";
 import { CreateScenarioOrganisationDTO, ScenarioOrganisationDetails, ScenarioOrganisationDetailsDTO, ScenarioOrganisationFilters, ScenarioOrganisationInfos, ScenarioOrganisationInfosDTO, UpdateScenarioOrganisationDTO } from "@dative-gpi/foundation-core-domain/models";
 import { SCENARIO_ORGANISATION_URL, SCENARIO_ORGANISATIONS_URL } from "../../config/urls/scenarioOrganisations";
 
+
 const ScenarioOrganisationServiceFactory = new ServiceFactory<ScenarioOrganisationDetailsDTO, ScenarioOrganisationDetails>("scenarioOrganisation", ScenarioOrganisationDetails)
+  .createComplete<ScenarioOrganisationInfos, ScenarioOrganisationInfosDTO, CreateScenarioOrganisationDTO, UpdateScenarioOrganisationDTO, ScenarioOrganisationFilters>(SCENARIO_ORGANISATIONS_URL, SCENARIO_ORGANISATION_URL, ScenarioOrganisationInfos);
+  
+const ScenarioOrganisationServiceFactoryIncomplete = new ServiceFactory<ScenarioOrganisationDetailsDTO, ScenarioOrganisationDetails>("scenarioOrganisation", ScenarioOrganisationDetails)
     .create(factory => factory.build(
-        factory.addGet(SCENARIO_ORGANISATION_URL),
-        factory.addGetMany<ScenarioOrganisationInfosDTO, ScenarioOrganisationInfos, ScenarioOrganisationFilters>(SCENARIO_ORGANISATIONS_URL, ScenarioOrganisationInfos),
-        factory.addCreate<CreateScenarioOrganisationDTO>(SCENARIO_ORGANISATIONS_URL),
-        factory.addUpdate<UpdateScenarioOrganisationDTO>(SCENARIO_ORGANISATION_URL),
-        factory.addRemove(SCENARIO_ORGANISATION_URL),
-        factory.addNotify()
-    ));
+        factory.addNotify(notifyService => ({
+            ...ServiceFactory.addCustom("duplicate", (axios, scenarioOrganisationId: string) => axios.patch(SCENARIO_ORGANISATION_URL(scenarioOrganisationId)), (dto: ScenarioOrganisationDetailsDTO) => {
+              const result = new ScenarioOrganisationDetails(dto);
+              notifyService.notify("add", result);
+              return result;
+            })
+        })
+    )));
 
 export const useScenarioOrganisation = ComposableFactory.get(ScenarioOrganisationServiceFactory);
 export const useScenarioOrganisations = ComposableFactory.getMany(ScenarioOrganisationServiceFactory);
 export const useCreateScenarioOrganisation = ComposableFactory.create(ScenarioOrganisationServiceFactory);
 export const useUpdateScenarioOrganisation = ComposableFactory.update(ScenarioOrganisationServiceFactory);
 export const useRemoveScenarioOrganisation = ComposableFactory.remove(ScenarioOrganisationServiceFactory);
+export const useDuplicateScenarioOrganisation = ComposableFactory.custom(ScenarioOrganisationServiceFactoryIncomplete.duplicate);
