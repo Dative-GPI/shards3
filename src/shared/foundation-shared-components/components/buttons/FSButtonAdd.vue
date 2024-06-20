40000b39186ccdf940ad8722328ec16845c83a1a
rduteil <rduteil@dative-gpi.com>
2024-06-20 14:57:19 +0200

    FSButtonAdd

---
+++ src/shared/foundation-shared-components/components/buttons/FSButtonAdd.vue
@@ -0,0 +1,28 @@
+<template>
+  <FSButton
+    prependIcon="mdi-plus-circle-outline"
+    :label="$tr('ui.button.add', 'Add')"
+    :color="ColorEnum.Primary"
+    v-bind="$attrs"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import { ColorEnum } from "@dative-gpi/foundation-shared-components/models";
+
+import FSButton from "../FSButton.vue";
+
+export default defineComponent({
+  name: "FSButtonAdd",
+  components: {
+    FSButton
+  },
+  setup() {
+    return {
+      ColorEnum
+    }
+  }
+});
+</script>
---
+++ src/shared/foundation-shared-components/components/buttons/FSButtonAddLabel.vue
@@ -0,0 +1,27 @@
+<template>
+  <FSButton
+    :label="$tr('ui.button.add', 'Add')"
+    :color="ColorEnum.Primary"
+    v-bind="$attrs"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import { ColorEnum } from "@dative-gpi/foundation-shared-components/models";
+
+import FSButton from "../FSButton.vue";
+
+export default defineComponent({
+  name: "FSButtonAddLabel",
+  components: {
+    FSButton
+  },
+  setup() {
+    return {
+      ColorEnum
+    }
+  }
+});
+</script>
