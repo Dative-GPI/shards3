274f4ceaac290a4df87043dc45e1244bb1070f56
SchroterQuentin <SchroterQuentin@users.noreply.github.com>
2024-06-26 12:51:47 +0000

    deploy: 25e0a93c3addf97e3d5bffceab1875bdd85fa8ce

---
+++ assets/AutocompleteField.stories-BTFZZC2s.js
@@ -0,0 +1,227 @@
+import{F as i}from"./FSAutocompleteField-CBJWUiM8.js";import{F as u}from"./FSForm-CZhuTBbT.js";import{_ as p}from"./FSCol-CA6NzDpV.js";import{_ as g}from"./FSRow-JJutUvlP.js";import{A as v}from"./rules-Dgz8ALDd.js";import"./vue.esm-bundler-DeSao-KJ.js";import"./FSSearchField-DwhyMBBv.js";import"./FSTextField-xG_kerPN.js";import"./VField-DEMHFDPm.js";import"./FSSpan-t6bMdSRn.js";import"./useSlots-DPxV8yUU.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./useColors-DL92kHTE.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-_Z9wfsFC.js";import"./theme-RQK62Xub.js";import"./VSpacer-CFNx9InD.js";import"./useRender-CIdmfI-I.js";import"./index-BIb3aONP.js";import"./transition-CTQG5_JK.js";import"./VLabel-CWiDHCZM.js";import"./VInput-DX4N6NPh.js";import"./locale-BTkT-VzI.js";import"./proxiedModel-B9H_VIYa.js";import"./VIcon-B0BI55vc.js";import"./color-XZPyE2lN.js";import"./tag-BL1d6r1f.js";import"./density-4sjlTnVP.js";import"./loader-CnGjHJ-m.js";import"./VProgressCircular-DA_NN_fl.js";import"./resizeObserver-euslJwVY.js";import"./anchor-LC_hEQAj.js";import"./rounded-BOBir3aK.js";import"./VDefaultsProvider-DHsFAklf.js";import"./forwardRefs-DWGaNmQL.js";import"./FSButton-CUXN7UlR.js";import"./FSClickable-7AMX5jTq.js";import"./FSCard-B1Oet1YO.js";import"./css-BnhLLynS.js";import"./FSIcon-DuYoAbGq.js";import"./useRules-DQ31e--d.js";import"./index-CrTEBV4I.js";import"./useTranslations-DAApg7Ob.js";import"./FSDialogMenu-D74RUSZ7.js";import"./VDialog--AywZFlO.js";import"./VOverlay-Dh7x6Oij.js";import"./dimensions-aXABR5Zz.js";import"./display-CUoCREED.js";import"./lazy-B9cZ4h7g.js";import"./router-Cr7df_Je.js";import"./FSRadioGroup-D4D4UEOO.js";import"./FSRadio-x2anfynz.js";import"./VSelectionControl-DWVZmIhS.js";import"./index-BWi4ltEp.js";import"./FSToggleSet-B3jJ7hmw.js";import"./FSSlideGroup-DGX0Yc4-.js";import"./uuid-DTaye2KM.js";import"./VSlideGroup-C7Ln-DYU.js";import"./group-BqOqgFvy.js";import"./FSWrapGroup-BNjss27o.js";import"./FSCheckbox-BfU8RDs1.js";import"./FSFadeOut-IHk7VvTz.js";import"./FSLoader-D-GHyjSx.js";import"./elevation-BBJMtRPf.js";import"./VSelect-DRB1q99h.js";import"./VList-CKDIvLuI.js";import"./ssrBoot-BXIJ_jLM.js";import"./border-IVtV0L-L.js";import"./VImg-BljrNAqA.js";import"./VDivider-CPb7S374.js";import"./VMenu-CbRbHjmn.js";import"./filter-De_Yj-mJ.js";import"./useAppTimeZone-B8cBFYr8.js";import"./time-Ba2sHI9J.js";const c=["Alpha","Beta","Charly","Delta","Echo","Foxtrot","Golf","Hotel","India","Juliett","Kilo","Lima","Mike","November","Oscar","Papa","Quebec","Romeo","Sierra","Tango","Uniform","Victor","Whiskey","X-ray","Yankee","Zulu"],Me={title:"Foundation/Shared/Input fields/AutocompleteField",component:i,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},t={args:{args:{items:Array.from(Array(30).keys()).map(e=>({id:e.toString(),label:`Option ${e+1}`})),value1:null,value2:["0","1","2"],value3:"2",value4:"2"}},render:(e,{argTypes:o})=>({components:{FSAutocompleteField:i,FSCol:p},props:Object.keys(o),setup(){return{...e}},template:`
+    <FSCol>
+      <FSAutocompleteField
+        label="Autocomplete"
+        :items="args.items"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteField
+        label="Multiple select"
+        :items="args.items"
+        :multiple="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteField
+        label="Required select, with description"
+        description="Description for this field"
+        :required="true"
+        :items="args.items"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteField
+        label="Uneditable select, with description"
+        description="Uneditable description"
+        :editable="false"
+        :items="args.items"
+        v-model="args.value4"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteField
+        label="With toggle set enabled"
+        :items="args.items"
+        :toggleSet="true"
+        v-model="args.value5"
+      />
+    </FSCol>`})},r={args:{args:{valid:!1,items:Array.from(Array(5).keys()).map(e=>({id:e.toString(),label:`Option ${c[e]}`})),value1:null,value2:null,value3:null,value4:null,rules:v}},render:(e,{argTypes:o})=>({components:{FSForm:u,FSAutocompleteField:i,FSCol:p,FSRow:g},props:Object.keys(o),setup(){return{...e}},template:`
+    <FSForm v-model="args.valid"
+      variant="standard">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+          Form validity: {{ args.valid ?? "false" }}
+          </div>
+          </FSRow>
+          <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+          <FSAutocompleteField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :items="args.items"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSAutocompleteField
+          label="Rules: min 2 options"
+          :rules="[args.rules.min(2)]"
+          :toggleSet="true"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSAutocompleteField
+          label="Rules: required & min 2 options & max 3 options"
+          :rules="[args.rules.required(), args.rules.min(2), args.rules.max(3)]"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value3"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSAutocompleteField
+          label="Rules: required & min 2 options & max 3 options"
+          :rules="[args.rules.required(), args.rules.min(2), args.rules.max(3)]"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value4"
+        />
+      </FSCol>
+    </FSForm>`})};var l,s,n;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    args: {
+      items: Array.from(Array(30).keys()).map(i => ({
+        id: i.toString(),
+        label: \`Option \${i + 1}\`
+      })),
+      value1: null,
+      value2: ["0", "1", "2"],
+      value3: "2",
+      value4: "2"
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSAutocompleteField,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSAutocompleteField
+        label="Autocomplete"
+        :items="args.items"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteField
+        label="Multiple select"
+        :items="args.items"
+        :multiple="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteField
+        label="Required select, with description"
+        description="Description for this field"
+        :required="true"
+        :items="args.items"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteField
+        label="Uneditable select, with description"
+        description="Uneditable description"
+        :editable="false"
+        :items="args.items"
+        v-model="args.value4"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteField
+        label="With toggle set enabled"
+        :items="args.items"
+        :toggleSet="true"
+        v-model="args.value5"
+      />
+    </FSCol>\`
+  })
+}`,...(n=(s=t.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var m,a,d;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    args: {
+      valid: false,
+      items: Array.from(Array(5).keys()).map(i => ({
+        id: i.toString(),
+        label: \`Option \${international[i]}\`
+      })),
+      value1: null,
+      value2: null,
+      value3: null,
+      value4: null,
+      rules: AutocompleteRules
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSForm,
+      FSAutocompleteField,
+      FSCol,
+      FSRow
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSForm v-model="args.valid"
+      variant="standard">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+          Form validity: {{ args.valid ?? "false" }}
+          </div>
+          </FSRow>
+          <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+          <FSAutocompleteField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :items="args.items"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSAutocompleteField
+          label="Rules: min 2 options"
+          :rules="[args.rules.min(2)]"
+          :toggleSet="true"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSAutocompleteField
+          label="Rules: required & min 2 options & max 3 options"
+          :rules="[args.rules.required(), args.rules.min(2), args.rules.max(3)]"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value3"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSAutocompleteField
+          label="Rules: required & min 2 options & max 3 options"
+          :rules="[args.rules.required(), args.rules.min(2), args.rules.max(3)]"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value4"
+        />
+      </FSCol>
+    </FSForm>\`
+  })
+}`,...(d=(a=r.parameters)==null?void 0:a.docs)==null?void 0:d.source}}};const Ve=["Variations","Rules"];export{r as Rules,t as Variations,Ve as __namedExportsOrder,Me as default};
---
+++ assets/Pagination.stories-CxwmQJPg.js
@@ -0,0 +1,66 @@
+import{d as F,c as u,z as o,A as x,B as w,Q as c,C as g,P as V,S as _,F as B}from"./vue.esm-bundler-DeSao-KJ.js";import{_ as b}from"./FSRow-JJutUvlP.js";import{C as n,u as C}from"./useColors-DL92kHTE.js";import{s as d}from"./css-BnhLLynS.js";import{_ as P}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{F as $}from"./FSButton-CUXN7UlR.js";import{F as q}from"./FSText-DFdak8tp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-_Z9wfsFC.js";import"./theme-RQK62Xub.js";import"./FSClickable-7AMX5jTq.js";import"./FSCard-B1Oet1YO.js";import"./FSCol-CA6NzDpV.js";import"./VProgressCircular-DA_NN_fl.js";import"./color-XZPyE2lN.js";import"./useRender-CIdmfI-I.js";import"./resizeObserver-euslJwVY.js";import"./VIcon-B0BI55vc.js";import"./tag-BL1d6r1f.js";import"./FSSpan-t6bMdSRn.js";import"./useSlots-DPxV8yUU.js";import"./FSIcon-DuYoAbGq.js";const v=F({name:"FSPagination",components:{FSRow:b},props:{width:{type:[Array,String,Number],required:!1,default:null},gap:{type:[Array,String,Number],required:!1,default:"4px"},pages:{type:Number,required:!1,default:1},modelValue:{type:Number,required:!1,default:0},color:{type:String,required:!1,default:n.Dark}},setup(e){const{getColors:a}=C(),s=u(()=>a(e.color)),i=a(n.Light),l=a(n.Dark);return{style:u(()=>{switch(e.color){case n.Light:case n.Dark:return{"--fs-pagination-page-width":`calc(100% / ${e.pages} - ${d(e.gap)}`,"--fs-pagination-background-color":i.dark,"--fs-pagination-active-background-color":l.base};default:return{"--fs-pagination-page-width":`calc(100% / ${e.pages} - ${d(e.gap)}`,"--fs-pagination-background-color":s.value.light,"--fs-pagination-active-background-color":s.value.base}}}),classes:t=>{const p=["fs-pagination-page"];return t<=e.modelValue&&p.push("fs-pagination-active-page"),p}}}});function N(e,a,s,i,l,S){return o(),x(b,{class:"fs-pagination",align:"center-center",width:e.$props.width,gap:e.$props.gap,style:c(e.style)},{default:w(()=>[(o(!0),g(B,null,V(e.$props.pages,(k,t)=>(o(),g("div",{class:_(e.classes(t)),style:c(e.style),key:t},null,6))),128))]),_:1},8,["width","gap","style"])}const h=P(v,[["render",N]]);v.__docgenInfo={displayName:"FSPagination",exportName:"default",description:"",tags:{},props:[{name:"width",type:{name:"string[] | number[] | string | number | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"gap",type:{name:"string[] | number[] | string | number | null"},required:!1,defaultValue:{func:!1,value:'"4px"'}},{name:"pages",type:{name:"number"},required:!1,defaultValue:{func:!1,value:"1"}},{name:"modelValue",type:{name:"number"},required:!1,defaultValue:{func:!1,value:"0"}},{name:"color",type:{name:"ColorBase"},required:!1,defaultValue:{func:!1,value:"ColorEnum.Dark"}}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/shared/foundation-shared-components/components/FSPagination.vue"]};const ae={title:"Foundation/Shared/Pagination",component:h,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},r={args:{args:{page1:0,pages1:3}},render:(e,{argTypes:a})=>({components:{FSPagination:h,FSButton:$,FSText:q},props:Object.keys(a),setup(){return{...e}},template:`
+      <div style="display: flex; flex-direction: column; gap: 30px;">
+        <div style="display: flex; width: 100%; flex-direction: column; gap: 8px; justify-content: center;">
+          <FSPagination
+            :modelValue="args.page1"
+            :pages="args.pages1"
+          />
+          <div style="display: flex; width: 100%; gap: 8px; justify-content: center;">
+            <FSButton
+              label="Previous"
+              :editable="args.page1 > 0"
+              @click="args.page1--"
+            />
+            <FSButton
+              label="Next"
+              :editable="args.page1 < args.pages1 - 1"
+              @click="args.page1++"
+            />
+          </div>
+        </div>
+      </div>
+    `})};var m,f,y;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    args: {
+      page1: 0,
+      pages1: 3
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSPagination,
+      FSButton,
+      FSText
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+      <div style="display: flex; flex-direction: column; gap: 30px;">
+        <div style="display: flex; width: 100%; flex-direction: column; gap: 8px; justify-content: center;">
+          <FSPagination
+            :modelValue="args.page1"
+            :pages="args.pages1"
+          />
+          <div style="display: flex; width: 100%; gap: 8px; justify-content: center;">
+            <FSButton
+              label="Previous"
+              :editable="args.page1 > 0"
+              @click="args.page1--"
+            />
+            <FSButton
+              label="Next"
+              :editable="args.page1 < args.pages1 - 1"
+              @click="args.page1++"
+            />
+          </div>
+        </div>
+      </div>
+    \`
+  })
+}`,...(y=(f=r.parameters)==null?void 0:f.docs)==null?void 0:y.source}}};const ne=["Variations"];export{r as Variations,ne as __namedExportsOrder,ae as default};
