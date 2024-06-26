611322035d4f5b47991a2072fb820874b890f683
dimitrilexi <dimitrilexi@users.noreply.github.com>
2024-06-26 07:56:08 +0000

    deploy: df06102aa0268368562fced90515e7a0df65278c

---
+++ assets/NumberField.stories-I25r6Dct.js
@@ -0,0 +1,151 @@
+import i from"./FSNumberField-D3lJRfa1.js";import{F as p}from"./FSForm-CZhuTBbT.js";import{_ as u}from"./FSCol-CA6NzDpV.js";import{_ as g}from"./FSRow-JJutUvlP.js";import{N as b}from"./rules-Dgz8ALDd.js";import"./vue.esm-bundler-DeSao-KJ.js";import"./FSTextField-xG_kerPN.js";import"./VField-DEMHFDPm.js";import"./FSSpan-t6bMdSRn.js";import"./useSlots-DPxV8yUU.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./useColors-DL92kHTE.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-_Z9wfsFC.js";import"./theme-RQK62Xub.js";import"./VSpacer-CFNx9InD.js";import"./useRender-CIdmfI-I.js";import"./index-BIb3aONP.js";import"./transition-CTQG5_JK.js";import"./VLabel-CWiDHCZM.js";import"./VInput-DX4N6NPh.js";import"./locale-BTkT-VzI.js";import"./proxiedModel-B9H_VIYa.js";import"./VIcon-B0BI55vc.js";import"./color-XZPyE2lN.js";import"./tag-BL1d6r1f.js";import"./density-4sjlTnVP.js";import"./loader-CnGjHJ-m.js";import"./VProgressCircular-DA_NN_fl.js";import"./resizeObserver-euslJwVY.js";import"./anchor-LC_hEQAj.js";import"./rounded-BOBir3aK.js";import"./VDefaultsProvider-DHsFAklf.js";import"./forwardRefs-DWGaNmQL.js";import"./FSButton-CUXN7UlR.js";import"./FSClickable-7AMX5jTq.js";import"./FSCard-B1Oet1YO.js";import"./css-BnhLLynS.js";import"./FSIcon-DuYoAbGq.js";import"./useRules-DQ31e--d.js";import"./index-CrTEBV4I.js";import"./useAppTimeZone-B8cBFYr8.js";import"./useTranslations-DAApg7Ob.js";import"./time-Ba2sHI9J.js";const le={title:"Foundation/Shared/Input fields/NumberField",component:i,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},e={args:{args:{value1:0,value2:500,value3:1500}},render:(t,{argTypes:o})=>({components:{FSNumberField:i,FSCol:u},props:Object.keys(o),setup(){return{...t}},template:`
+    <FSCol>
+      <FSNumberField
+        label="Number"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSNumberField
+        label="Required number, with description"
+        description="Description for this field"
+        :required="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSNumberField
+        label="Uneditable number, with description"
+        description="Uneditable description"
+        :editable="false"
+        v-model="args.value3"
+      />
+    </FSCol>`})},r={args:{args:{valid:!1,value1:0,value2:500,value3:1500,rules:b}},render:(t,{argTypes:o})=>({components:{FSForm:p,FSNumberField:i,FSCol:u,FSRow:g},props:Object.keys(o),setup(){return{...t}},template:`
+    <FSForm v-model="args.valid">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSNumberField
+          label="Rules: required & min 10"
+          :rules="[args.rules.required(), args.rules.min(10)]"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSNumberField
+          label="Rules: max 10"
+          :rules="[args.rules.max(10)]"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSNumberField
+          label="Rules: required & min 10 & integer"
+          :rules="[args.rules.required(), args.rules.min(10), args.rules.integer()]"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>`})};var l,n,s;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: 0,
+      value2: 500,
+      value3: 1500
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSNumberField,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSNumberField
+        label="Number"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSNumberField
+        label="Required number, with description"
+        description="Description for this field"
+        :required="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSNumberField
+        label="Uneditable number, with description"
+        description="Uneditable description"
+        :editable="false"
+        v-model="args.value3"
+      />
+    </FSCol>\`
+  })
+}`,...(s=(n=e.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};var d,a,m;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    args: {
+      valid: false,
+      value1: 0,
+      value2: 500,
+      value3: 1500,
+      rules: NumberRules
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSForm,
+      FSNumberField,
+      FSCol,
+      FSRow
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSForm v-model="args.valid">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSNumberField
+          label="Rules: required & min 10"
+          :rules="[args.rules.required(), args.rules.min(10)]"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSNumberField
+          label="Rules: max 10"
+          :rules="[args.rules.max(10)]"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSNumberField
+          label="Rules: required & min 10 & integer"
+          :rules="[args.rules.required(), args.rules.min(10), args.rules.integer()]"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>\`
+  })
+}`,...(m=(a=r.parameters)==null?void 0:a.docs)==null?void 0:m.source}}};const ne=["Variations","Rules"];export{r as Rules,e as Variations,ne as __namedExportsOrder,le as default};
