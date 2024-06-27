c90f890be62f4c9d6c2b8cda0b5f5534a909f835
rduteil <rduteil@users.noreply.github.com>
2024-06-27 06:29:13 +0000

    deploy: 308d313331910c08fd0025449b1800be7115c657

---
+++ assets/AutocompleteRole.stories-DHNMly4_.js
@@ -0,0 +1,53 @@
+import{F as p}from"./FSAutocompleteRole-ByjF7kc3.js";import{_ as l}from"./FSCol-CA6NzDpV.js";import"./vue.esm-bundler-DeSao-KJ.js";import"./FSAutocompleteField-BK95k7Ah.js";import"./FSSearchField-Bk9E08WQ.js";import"./FSTextField-BoGA7VT8.js";import"./VField-C-8g7gy8.js";import"./FSSpan-t6bMdSRn.js";import"./useSlots-DPxV8yUU.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FSRow-JJutUvlP.js";import"./css-BnhLLynS.js";import"./useColors-C3YrZIBy.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-_Z9wfsFC.js";import"./theme-RQK62Xub.js";import"./VSpacer-CFNx9InD.js";import"./useRender-CIdmfI-I.js";import"./index-BIb3aONP.js";import"./transition-CTQG5_JK.js";import"./VLabel-CWiDHCZM.js";import"./VInput-DX4N6NPh.js";import"./locale-BTkT-VzI.js";import"./proxiedModel-B9H_VIYa.js";import"./VIcon-B0BI55vc.js";import"./color-XZPyE2lN.js";import"./tag-BL1d6r1f.js";import"./density-4sjlTnVP.js";import"./loader-CnGjHJ-m.js";import"./VProgressCircular-DA_NN_fl.js";import"./resizeObserver-euslJwVY.js";import"./anchor-LC_hEQAj.js";import"./rounded-BOBir3aK.js";import"./VDefaultsProvider-DHsFAklf.js";import"./forwardRefs-DWGaNmQL.js";import"./FSButton-F4U-An3c.js";import"./FSClickable-CtyeoOHr.js";import"./FSCard-Cr3VWqvL.js";import"./FSIcon-DnAZkNNf.js";import"./useRules-DQ31e--d.js";import"./index-CrTEBV4I.js";import"./useTranslations-DAApg7Ob.js";import"./FSDialogMenu-CtAO0Sgc.js";import"./VDialog--AywZFlO.js";import"./VOverlay-Dh7x6Oij.js";import"./dimensions-aXABR5Zz.js";import"./display-CUoCREED.js";import"./lazy-B9cZ4h7g.js";import"./router-Cr7df_Je.js";import"./FSRadioGroup-F53nrXFK.js";import"./FSRadio-DlvvITIU.js";import"./VSelectionControl-DWVZmIhS.js";import"./index-BWi4ltEp.js";import"./FSToggleSet-BmzhnM61.js";import"./FSSlideGroup-BNPQrUcW.js";import"./uuid-DTaye2KM.js";import"./VSlideGroup-C7Ln-DYU.js";import"./group-BqOqgFvy.js";import"./FSWrapGroup-D1ZJoYu1.js";import"./FSCheckbox-Dba0sDeH.js";import"./FSFadeOut-D6QzK9Bb.js";import"./FSLoader-BG9q5Y9c.js";import"./elevation-BBJMtRPf.js";import"./VSelect-Bw07qUGf.js";import"./VList-CKDIvLuI.js";import"./ssrBoot-BXIJ_jLM.js";import"./border-IVtV0L-L.js";import"./VImg-BljrNAqA.js";import"./VDivider-CPb7S374.js";import"./VMenu-CbRbHjmn.js";import"./filter-De_Yj-mJ.js";import"./FSChip-9XbuP80h.js";import"./permissionInfos-BPDgTHQl.js";import"./base-Db9bJp6t.js";import"./useAppOrganisationId-JzV73dfQ.js";import"./serviceFactory-Bd5jifNi.js";import"./lodash-BiW_TGGX.js";import"./composableFactory-rrf4GM2L.js";import"./useAutocomplete-DIOO3j-W.js";const Eo={title:"Foundation/Core/Autocompletes/AutocompleteRole",component:p,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},o={args:{args:{value1:null,type1:0,value2:"11",type2:2}},render:(m,{argTypes:i})=>({components:{FSAutocompleteRole:p,FSCol:l},props:Object.keys(i),setup(){return{...m}},template:`
+    <FSCol>
+      <FSAutocompleteRole
+        label="Role"
+        v-model="args.value1"
+        v-model:type="args.type1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteRole
+        label="Role with toggleset disabled"
+        :toggleSetDisabled="true"
+        v-model="args.value2"
+        v-model:type="args.type2"
+      />
+    </FSCol>`})};var t,r,e;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      type1: 0,
+      value2: "11",
+      type2: 2
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSAutocompleteRole,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSAutocompleteRole
+        label="Role"
+        v-model="args.value1"
+        v-model:type="args.type1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteRole
+        label="Role with toggleset disabled"
+        :toggleSetDisabled="true"
+        v-model="args.value2"
+        v-model:type="args.type2"
+      />
+    </FSCol>\`
+  })
+}`,...(e=(r=o.parameters)==null?void 0:r.docs)==null?void 0:e.source}}};const qo=["Variations"];export{o as Variations,qo as __namedExportsOrder,Eo as default};
---
+++ assets/TagField.stories-ijCQa03a.js
@@ -0,0 +1,168 @@
+import{F as o}from"./FSTagField-ksG1xvA3.js";import{F as g}from"./FSForm-CZhuTBbT.js";import{_ as p}from"./FSCol-CA6NzDpV.js";import{_ as u}from"./FSRow-JJutUvlP.js";import{a as v}from"./rules-Dgz8ALDd.js";import"./vue.esm-bundler-DeSao-KJ.js";import"./FSTextField-BoGA7VT8.js";import"./VField-C-8g7gy8.js";import"./FSSpan-t6bMdSRn.js";import"./useSlots-DPxV8yUU.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./useColors-C3YrZIBy.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-_Z9wfsFC.js";import"./theme-RQK62Xub.js";import"./VSpacer-CFNx9InD.js";import"./useRender-CIdmfI-I.js";import"./index-BIb3aONP.js";import"./transition-CTQG5_JK.js";import"./VLabel-CWiDHCZM.js";import"./VInput-DX4N6NPh.js";import"./locale-BTkT-VzI.js";import"./proxiedModel-B9H_VIYa.js";import"./VIcon-B0BI55vc.js";import"./color-XZPyE2lN.js";import"./tag-BL1d6r1f.js";import"./density-4sjlTnVP.js";import"./loader-CnGjHJ-m.js";import"./VProgressCircular-DA_NN_fl.js";import"./resizeObserver-euslJwVY.js";import"./anchor-LC_hEQAj.js";import"./rounded-BOBir3aK.js";import"./VDefaultsProvider-DHsFAklf.js";import"./forwardRefs-DWGaNmQL.js";import"./FSButton-F4U-An3c.js";import"./FSClickable-CtyeoOHr.js";import"./FSCard-Cr3VWqvL.js";import"./css-BnhLLynS.js";import"./FSIcon-DnAZkNNf.js";import"./useRules-DQ31e--d.js";import"./index-CrTEBV4I.js";import"./FSTagGroup-DHlrbR20.js";import"./FSWrapGroup-D1ZJoYu1.js";import"./uuid-DTaye2KM.js";import"./VSlideGroup-C7Ln-DYU.js";import"./display-CUoCREED.js";import"./group-BqOqgFvy.js";import"./FSSlideGroup-BNPQrUcW.js";import"./FSTag-eLrJFb99.js";import"./VBtn-D2I7zlII.js";import"./border-IVtV0L-L.js";import"./elevation-BBJMtRPf.js";import"./dimensions-aXABR5Zz.js";import"./router-Cr7df_Je.js";import"./index-BWi4ltEp.js";import"./useAppTimeZone-B8cBFYr8.js";import"./useTranslations-DAApg7Ob.js";import"./time-Ba2sHI9J.js";const yr={title:"Foundation/Shared/Input fields/TagField",component:o,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},r={args:{args:{value1:["full","tag","variant"],value2:["standard","tag","variant"],value3:[],value4:["thou","shalt","not","edit"]}},render:(t,{argTypes:a})=>({components:{FSTagField:o,FSCol:p},props:Object.keys(a),setup(){return{...t}},template:`
+    <FSCol>
+      <FSTagField
+        label="Tag, full, primary color"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTagField
+        label="Tag, standard, primary color"
+        tagVariant="standard"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTagField
+        label="Required tag, full, warning color, with description"
+        description="Description for this field"
+        tagColor="warning"
+        :required="true"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTagField
+        label="Uneditable tag, full, error color, width description"
+        description="Uneditable description"
+        tagColor="error"
+        :editable="false"
+        v-model="args.value4"
+      />
+    </FSCol>`})},e={args:{args:{valid:!1,value1:[],value2:["a","b","c"],value3:[],rules:v}},render:(t,{argTypes:a})=>({components:{FSForm:g,FSTagField:o,FSCol:p,FSRow:u},props:Object.keys(a),setup(){return{...t}},template:`
+    <FSForm v-model="args.valid" variant="standard">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTagField
+          label="Rules: required & min 2 tags"
+          :rules="[args.rules.required(), args.rules.min(2)]"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTagField
+          label="Rules: max 5 tags"
+          :rules="[args.rules.max(5)]"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTagField
+          label="Rules: required & max 10 tags & min 5 tags"
+          :rules="[args.rules.required(), args.rules.min(5), args.rules.max(10)]"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>`})};var i,l,n;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: ["full", "tag", "variant"],
+      value2: ["standard", "tag", "variant"],
+      value3: [],
+      value4: ["thou", "shalt", "not", "edit"]
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSTagField,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSTagField
+        label="Tag, full, primary color"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTagField
+        label="Tag, standard, primary color"
+        tagVariant="standard"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTagField
+        label="Required tag, full, warning color, with description"
+        description="Description for this field"
+        tagColor="warning"
+        :required="true"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTagField
+        label="Uneditable tag, full, error color, width description"
+        description="Uneditable description"
+        tagColor="error"
+        :editable="false"
+        v-model="args.value4"
+      />
+    </FSCol>\`
+  })
+}`,...(n=(l=r.parameters)==null?void 0:l.docs)==null?void 0:n.source}}};var d,s,m;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    args: {
+      valid: false,
+      value1: [],
+      value2: ["a", "b", "c"],
+      value3: [],
+      rules: TagRules
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSForm,
+      FSTagField,
+      FSCol,
+      FSRow
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSForm v-model="args.valid" variant="standard">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTagField
+          label="Rules: required & min 2 tags"
+          :rules="[args.rules.required(), args.rules.min(2)]"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTagField
+          label="Rules: max 5 tags"
+          :rules="[args.rules.max(5)]"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTagField
+          label="Rules: required & max 10 tags & min 5 tags"
+          :rules="[args.rules.required(), args.rules.min(5), args.rules.max(10)]"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>\`
+  })
+}`,...(m=(s=e.parameters)==null?void 0:s.docs)==null?void 0:m.source}}};const hr=["Variations","Rules"];export{e as Rules,r as Variations,hr as __namedExportsOrder,yr as default};
