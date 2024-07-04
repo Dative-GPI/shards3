07616197ffaf6b2adbb6a37ccd8d841c51873681
rduteil <rduteil@users.noreply.github.com>
2024-07-04 14:51:36 +0000

    deploy: 3f02da6bfb45157aaa8972915efa08fb74162bb7

---
+++ assets/Link.stories-Bipk0os0.js
@@ -0,0 +1,37 @@
+import{d as h,c as r,y as S,z as u,C as L,J as p,O as b,A as q,B as C,m as F,D as $,E as f,G as d,K as V,L as w}from"./vue.esm-bundler-DC82FEWN.js";import{C as x,u as _}from"./useColors-vjSCjmKB.js";import{u as B}from"./useSlots-V2mVelz6.js";import{_ as N}from"./_plugin-vue_export-helper-DlAUqK2U.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-_Z9wfsFC.js";import"./theme-CDGAP9oX.js";const y=h({name:"FSLink",props:{label:{type:String,required:!1,default:null},to:{type:[String,Object],required:!1,default:null},href:{type:String,required:!1,default:null},font:{type:String,required:!1,default:"text-body"},lineClamp:{type:Number,required:!1,default:1},ellipsis:{type:Boolean,required:!1,default:!0},color:{type:String,required:!1,default:x.Primary},variant:{type:String,required:!1,default:"dark"}},setup(e){const{getColors:n}=_(),{slots:l}=B(),t=r(()=>n(e.color)),o=r(()=>{const a=["fs-link",e.font];return l.default||a.push("fs-span-pre-wrap"),e.lineClamp>1?a.push("fs-span-line-clamp"):e.ellipsis&&a.push("fs-span-ellipsis"),a}),i=r(()=>{switch(e.variant){case"base":return{"--fs-span-line-clamp":e.lineClamp.toString(),"--fs-link-color":t.value.base};case"light":return{"--fs-span-line-clamp":e.lineClamp.toString(),"--fs-link-color":t.value.light};case"dark":return{"--fs-span-line-clamp":e.lineClamp.toString(),"--fs-link-color":t.value.dark}}});return{classes:o,style:i}}}),R=["href"];function U(e,n,l,t,o,i){const a=S("router-link");return e.$props.href?(u(),L("a",{key:0,href:e.$props.href,style:b(e.style)},[p(e.$slots,"default",{},()=>[f(d(e.$props.label),1)])],12,R)):e.$props.to?(u(),q(a,F({key:1,to:e.$props.to,class:e.classes,style:e.style},e.$attrs),{default:C(v=>[p(e.$slots,"default",V(w(v)),()=>[f(d(e.$props.label),1)])]),_:3},16,["to","class","style"])):$("",!0)}const k=N(y,[["render",U]]);y.__docgenInfo={displayName:"FSLink",exportName:"default",description:"",tags:{},props:[{name:"label",type:{name:"string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"to",type:{name:"string | RouteLocation | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"href",type:{name:"string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"font",values:["text-h1","text-h2","text-h3","text-body","text-button","text-overline","text-underline"],type:{name:"string"},required:!1,defaultValue:{func:!1,value:'"text-body"'}},{name:"lineClamp",type:{name:"number"},required:!1,defaultValue:{func:!1,value:"1"}},{name:"ellipsis",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"true"}},{name:"color",type:{name:"ColorBase"},required:!1,defaultValue:{func:!1,value:"ColorEnum.Primary"}},{name:"variant",values:["base","light","dark"],type:{name:"string"},required:!1,defaultValue:{func:!1,value:'"dark"'}}],slots:[{name:"default",scoped:!0,bindings:[]}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/shared/foundation-shared-components/components/FSLink.vue"]};const G={title:"Foundation/Shared/Link",component:k,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},s={args:{args:{}},render:(e,{argTypes:n})=>({components:{FSLink:k},props:Object.keys(n),setup(){return{...e}},template:`
+      <div style="display: flex; flex-direction: column; gap: 30px;">
+        <FSLink :to="{ name: 'Home', params: { deviceId: '1' }, query: { search: 'Device' } }">
+          Using RouteLocation from vue-router
+        </FSLink>
+        <FSLink to="https://www.google.fr">
+          Using Google URL
+        </FSLink>
+      </div>
+    `})};var c,m,g;s.parameters={...s.parameters,docs:{...(c=s.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    args: {}
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSLink
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+      <div style="display: flex; flex-direction: column; gap: 30px;">
+        <FSLink :to="{ name: 'Home', params: { deviceId: '1' }, query: { search: 'Device' } }">
+          Using RouteLocation from vue-router
+        </FSLink>
+        <FSLink to="https://www.google.fr">
+          Using Google URL
+        </FSLink>
+      </div>
+    \`
+  })
+}`,...(g=(m=s.parameters)==null?void 0:m.docs)==null?void 0:g.source}}};const I=["Variations"];export{s as Variations,I as __namedExportsOrder,G as default};
---
+++ assets/MagicConfigField.stories-CvoTw4la.js
@@ -0,0 +1,137 @@
+import{F as o}from"./FSMagicConfigField-CXVWnrL7.js";import{_ as n}from"./FSCol-B7HQy3FB.js";import"./vue.esm-bundler-DC82FEWN.js";import"./useMagicFieldProvider-ClrdlFuQ.js";import"./iframe-DQ88L7RM.js";import"../sb-preview/runtime.js";import"./FSButtonAddIcon-coHTI7k4.js";import"./FSButton-uZjHfaK3.js";import"./FSClickable-fH0IP9Gi.js";import"./FSCard-C7Q5zpHU.js";import"./FSRow-DoCXWKDP.js";import"./css-DYOPUjjE.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./useColors-vjSCjmKB.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-_Z9wfsFC.js";import"./theme-CDGAP9oX.js";import"./VProgressCircular-D8KFt_en.js";import"./color-BhlhQtQT.js";import"./VIcon-uFUcYN4x.js";import"./FSSpan-CwfDYIua.js";import"./useSlots-V2mVelz6.js";import"./FSIcon-CH1L3cXE.js";import"./FSTranslateField-CmKGVJ4E.js";import"./FSDialogSubmit-Chfd6Al3.js";import"./FSFadeOut-DU52ZiYP.js";import"./uuid-DTaye2KM.js";import"./FSDialog-CkwT6Gx6.js";import"./FSText-CRWIpNZ3.js";import"./VDialog-B-ihhCeR.js";import"./VOverlay-HCh9BhQb.js";import"./forwardRefs-DWGaNmQL.js";import"./proxiedModel-CYw7-KgF.js";import"./anchor-CLTIcUx3.js";import"./dimensions-fR777bfb.js";import"./display-D6hZonHv.js";import"./lazy-e3yj3TcL.js";import"./locale-BC9z6YbT.js";import"./router-CRaPYJ7n.js";import"./transition-CJbyFvkq.js";import"./VDefaultsProvider-CTE9NJfn.js";import"./VSpacer-CMZ7Jymr.js";import"./useTranslations-DV-taHAL.js";import"./FSTextField-Bzrs8V-b.js";import"./VField-Tu0u6Fec.js";import"./index-DWl9tX7E.js";import"./VLabel-Cp9LN_v7.js";import"./VInput-BvcCh06W.js";import"./density-XYshZx8z.js";import"./loader-CTRePS7y.js";import"./rounded-TFXDyxjJ.js";import"./useRules-DS9e5-1Q.js";import"./index-yKNzK_fJ.js";import"./useLanguages-CGyTN8dQ.js";import"./base-BNnJ_ndD.js";import"./serviceFactory-Bd5jifNi.js";import"./lodash-BiW_TGGX.js";import"./composableFactory-J8cSLWf9.js";import"./rules-DlyQ2XjG.js";import"./useAppTimeZone-SgthjzoP.js";import"./time-xMV0WbLU.js";const bl={title:"Foundation/Shared/Input fields/MagicConfigField",component:o,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},l={args:{args:{labelDefault1:"NumberField",translations1:[],value1:"",labelDefault2:"Switch",translations2:[],value2:"",labelDefault3:"TextField",translations3:[],value3:"",labelDefault4:"DateTimeField",translations4:[],value4:"",labelDefault5:"IconField",translations5:[],value5:"",labelDefault6:"TimeField",translations6:[],value6:""}},render:(i,{argTypes:r})=>({components:{FSMagicConfigField:o,FSCol:n},props:Object.keys(r),setup(){return{...i}},template:`
+    <FSCol>
+      <FSMagicConfigField
+        label="Number field"
+        :type="1"
+        v-model:translations="args.translations1"
+        v-model:labelDefault="args.labelDefault1"
+        v-model:value="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Switch"
+        :type="2"
+        v-model:translations="args.translations2"
+        v-model:labelDefault="args.labelDefault2"
+        v-model:value="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Text field"
+        :type="3"
+        v-model:translations="args.translations3"
+        v-model:labelDefault="args.labelDefault3"
+        v-model:value="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Date time field"
+        :type="4"
+        v-model:translations="args.translations4"
+        v-model:labelDefault="args.labelDefault4"
+        v-model:value="args.value4"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Icon field"
+        :type="5"
+        v-model:translations="args.translations5"
+        v-model:labelDefault="args.labelDefault5"
+        v-model:value="args.value5"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Time field"
+        :type="6"
+        v-model:translations="args.translations6"
+        v-model:labelDefault="args.labelDefault6"
+        v-model:value="args.value6"
+      />
+    </FSCol>`})};var e,t,a;l.parameters={...l.parameters,docs:{...(e=l.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    args: {
+      labelDefault1: "NumberField",
+      translations1: [],
+      value1: "",
+      labelDefault2: "Switch",
+      translations2: [],
+      value2: "",
+      labelDefault3: "TextField",
+      translations3: [],
+      value3: "",
+      labelDefault4: "DateTimeField",
+      translations4: [],
+      value4: "",
+      labelDefault5: "IconField",
+      translations5: [],
+      value5: "",
+      labelDefault6: "TimeField",
+      translations6: [],
+      value6: ""
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSMagicConfigField,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSMagicConfigField
+        label="Number field"
+        :type="1"
+        v-model:translations="args.translations1"
+        v-model:labelDefault="args.labelDefault1"
+        v-model:value="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Switch"
+        :type="2"
+        v-model:translations="args.translations2"
+        v-model:labelDefault="args.labelDefault2"
+        v-model:value="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Text field"
+        :type="3"
+        v-model:translations="args.translations3"
+        v-model:labelDefault="args.labelDefault3"
+        v-model:value="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Date time field"
+        :type="4"
+        v-model:translations="args.translations4"
+        v-model:labelDefault="args.labelDefault4"
+        v-model:value="args.value4"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Icon field"
+        :type="5"
+        v-model:translations="args.translations5"
+        v-model:labelDefault="args.labelDefault5"
+        v-model:value="args.value5"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Time field"
+        :type="6"
+        v-model:translations="args.translations6"
+        v-model:labelDefault="args.labelDefault6"
+        v-model:value="args.value6"
+      />
+    </FSCol>\`
+  })
+}`,...(a=(t=l.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const fl=["Variations"];export{l as Variations,fl as __namedExportsOrder,bl as default};
