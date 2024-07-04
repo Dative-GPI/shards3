fdbbf75afaf99b130eeb4537eb6b6ce9251a6506
rduteil <rduteil@users.noreply.github.com>
2024-07-04 13:29:50 +0000

    deploy: d3c436f731f0b72b59162231fe0cf5dbde204990

---
+++ assets/MetaFormContent.stories-B9oapsWR.js
@@ -0,0 +1,150 @@
+import{d as A,y as m,z as r,A as d,B as n,C as y,N as f,I as c,E as M,G as x,F as g}from"./vue.esm-bundler-DC82FEWN.js";import{F as P}from"./FSText-CRWIpNZ3.js";import{F as _}from"./FSMetaField-CDdGAM05.js";import{_ as v}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{_ as p}from"./FSCol-B7HQy3FB.js";import{_ as L}from"./FSRow-DoCXWKDP.js";import{P as o}from"./customPropertyEnums-jIhOaYfs.js";import"./useColors-vjSCjmKB.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-_Z9wfsFC.js";import"./theme-CDGAP9oX.js";import"./useSlots-V2mVelz6.js";import"./FSDateTimeField-r-1iJLCF.js";import"./FSDialogMenu-_ixMOtNT.js";import"./FSCard-C7Q5zpHU.js";import"./css-DYOPUjjE.js";import"./VDialog-B-ihhCeR.js";import"./VOverlay-HCh9BhQb.js";import"./forwardRefs-DWGaNmQL.js";import"./color-BhlhQtQT.js";import"./proxiedModel-CYw7-KgF.js";import"./anchor-CLTIcUx3.js";import"./dimensions-fR777bfb.js";import"./display-D6hZonHv.js";import"./lazy-e3yj3TcL.js";import"./locale-BC9z6YbT.js";import"./router-CRaPYJ7n.js";import"./transition-CJbyFvkq.js";import"./VDefaultsProvider-CTE9NJfn.js";import"./FSTextField-Bzrs8V-b.js";import"./VField-Tu0u6Fec.js";import"./FSSpan-CwfDYIua.js";import"./VSpacer-CMZ7Jymr.js";import"./index-DWl9tX7E.js";import"./VLabel-Cp9LN_v7.js";import"./VInput-BvcCh06W.js";import"./VIcon-uFUcYN4x.js";import"./density-XYshZx8z.js";import"./loader-CTRePS7y.js";import"./VProgressCircular-D8KFt_en.js";import"./rounded-TFXDyxjJ.js";import"./FSButton-uZjHfaK3.js";import"./FSClickable-fH0IP9Gi.js";import"./FSIcon-CH1L3cXE.js";import"./useRules-DS9e5-1Q.js";import"./index-yKNzK_fJ.js";import"./FSCalendar-CjgkhGFT.js";import"./useAppTimeZone-SgthjzoP.js";import"./useTranslations-DV-taHAL.js";import"./VLocaleProvider-CCst0Z0f.js";import"./VBtn-BeQ1BOJB.js";import"./border-Csacwza1.js";import"./elevation-BoGrGvdU.js";import"./group-DaluXt-5.js";import"./index-ySUbTvsm.js";import"./FSWindow-CZV_0_Js.js";import"./ssrBoot-D90vJCps.js";import"./FSClock-C-LryTNQ.js";import"./FSSlider-hAOWrC5b.js";import"./VSlider-DzaF8Z2w.js";import"./VMenu-Dhrs_Dg2.js";import"./FSNumberField--0YrdG7o.js";import"./FSSelectField-BmBtJNJi.js";import"./FSRadioGroup-B1zDN-ex.js";import"./FSRadio-CMZuYlpC.js";import"./VSelectionControl-DLPOArCM.js";import"./FSCheckbox-B7KB3blX.js";import"./FSFadeOut-DU52ZiYP.js";import"./uuid-DTaye2KM.js";import"./VList-D_XNZRf5.js";import"./VImg-BLQ7D_3g.js";import"./VDivider-xUYR78_z.js";import"./VSelect-D7_mePYk.js";import"./VSlideGroup-CcTYsOIU.js";import"./FSIconField-DsL7APtq.js";import"./FSToggleSet-CmH1Z7Qc.js";import"./FSSlideGroup-G_yR414R.js";import"./FSWrapGroup-BRpaPdC9.js";import"./FSSwitch-BqyJI9N6.js";const S=A({name:"FSMetaFormContent",components:{FSMetaField:_,FSText:P},props:{customProperties:{type:[Object],required:!0},modelValue:{type:Object,required:!0},editable:{type:Boolean,required:!1,default:!0}},setup(e){return{categories:e.customProperties.reduce((t,a)=>(t[a.categoryLabel]===void 0&&(t[a.categoryLabel]=[]),t[a.categoryLabel].push(a),t),{})}}});function k(e,s,t,a,z,D){const F=m("FSText"),h=m("FSMetaField");return r(),d(p,{gap:24},{default:n(()=>[(r(!0),y(g,null,f(e.categories,(w,u)=>(r(),d(p,{key:u,gap:16},{default:n(()=>[c(L,null,{default:n(()=>[c(F,{font:"text-h3"},{default:n(()=>[M(x(u),1)]),_:2},1024)]),_:2},1024),(r(!0),y(g,null,f(w,(i,I)=>(r(),d(h,{customProperty:i,editable:e.editable,key:I,modelValue:e.modelValue[i.code],"onUpdate:modelValue":T=>e.$emit("update:modelValue",{...e.modelValue,[i.code]:T})},null,8,["customProperty","editable","modelValue","onUpdate:modelValue"]))),128))]),_:2},1024))),128))]),_:1})}const O=v(S,[["render",k]]);S.__docgenInfo={displayName:"FSMetaFormContent",exportName:"default",description:"",tags:{},props:[{name:"customProperties",type:{name:"CustomPropertyInfos[]"},required:!0},{name:"modelValue",type:{name:"{ [key: string]: string }"},required:!0},{name:"editable",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"true"}}],events:[{name:"update:modelValue"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/core/foundation-core-components/components/customProperties/FSMetaFormContent.vue"]};const io={title:"Foundation/Core/CustomProperties/MetaFormContent",component:O,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},l={args:{args:{customProperties:[{id:"1",label:"Selector",code:"selector",categoryLabel:"Category1",index:0,entity:1,dataType:o.String,addInInfos:!0,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!0,allowedValues:{1:"value1",2:"value 2",3:"value 3","key without value":null}},{id:"2",label:"Number",code:"number",categoryLabel:"Category1",index:0,entity:1,dataType:o.Number,addInInfos:!0,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!1,allowedValues:{}},{id:"3",label:"Boolean",code:"bool",categoryLabel:"Category1",index:0,entity:1,dataType:o.Boolean,addInInfos:!0,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!1,allowedValues:{}},{id:"4",label:"String",code:"string",categoryLabel:"Category2",index:0,entity:1,dataType:o.String,addInInfos:!0,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!1,allowedValues:{}},{id:"5",label:"Date",code:"date",categoryLabel:"Category2",index:0,entity:1,dataType:o.DateTime,addInInfos:!0,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!1,allowedValues:{}},{id:"6",label:"Icon",code:"icon",categoryLabel:"Category3",index:0,entity:1,dataType:o.Icon,addInInfos:!0,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!1,allowedValues:{}}],meta:{existing:"test"}}},render:(e,{argTypes:s})=>({components:{FSMetaFormContent:O,FSCol:p},props:Object.keys(s),setup(){return{...e}},template:`
+    <FSCol>
+      <FSMetaFormContent
+        :customProperties="args.customProperties"
+        v-model="args.meta" />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+    </FSCol>`})};var V,b,C;l.parameters={...l.parameters,docs:{...(V=l.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    args: {
+      customProperties: [{
+        id: "1",
+        label: "Selector",
+        code: "selector",
+        categoryLabel: "Category1",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.String,
+        addInInfos: true,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: true,
+        allowedValues: {
+          ["1"]: "value1",
+          ["2"]: "value 2",
+          ["3"]: "value 3",
+          ["key without value"]: null
+        }
+      }, {
+        id: "2",
+        label: "Number",
+        code: "number",
+        categoryLabel: "Category1",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.Number,
+        addInInfos: true,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: false,
+        allowedValues: {}
+      }, {
+        id: "3",
+        label: "Boolean",
+        code: "bool",
+        categoryLabel: "Category1",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.Boolean,
+        addInInfos: true,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: false,
+        allowedValues: {}
+      }, {
+        id: "4",
+        label: "String",
+        code: "string",
+        categoryLabel: "Category2",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.String,
+        addInInfos: true,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: false,
+        allowedValues: {}
+      }, {
+        id: "5",
+        label: "Date",
+        code: "date",
+        categoryLabel: "Category2",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.DateTime,
+        addInInfos: true,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: false,
+        allowedValues: {}
+      }, {
+        id: "6",
+        label: "Icon",
+        code: "icon",
+        categoryLabel: "Category3",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.Icon,
+        addInInfos: true,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: false,
+        allowedValues: {}
+      }],
+      meta: {
+        existing: "test"
+      }
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSMetaFormContent,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSMetaFormContent
+        :customProperties="args.customProperties"
+        v-model="args.meta" />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+    </FSCol>\`
+  })
+}`,...(C=(b=l.parameters)==null?void 0:b.docs)==null?void 0:C.source}}};const po=["Variations"];export{l as Variations,po as __namedExportsOrder,io as default};
