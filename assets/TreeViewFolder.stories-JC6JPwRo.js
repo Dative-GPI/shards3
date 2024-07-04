71cf401de481a2647718f041c37e7a34243f14eb
gballigand <gballigand@users.noreply.github.com>
2024-07-04 13:49:34 +0000

    deploy: 8f554c94571b8c7ecc5235b145c0660bf38661d6

---
+++ assets/TreeViewFolder.stories-JC6JPwRo.js
@@ -0,0 +1,49 @@
+import{d as v,y as S,z as m,A as s,B as d,m as w,E as y,G as T,D as _}from"./vue.esm-bundler-DC82FEWN.js";import{F as h}from"./FSTreeViewField-T806SPA0.js";import{_ as f}from"./FSIcon-CH1L3cXE.js";import{u as C}from"./useFolders-CU-QKePD.js";import{u as b}from"./useTreeView-tMByGffe.js";import{_ as k}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{_ as $}from"./FSCol-B7HQy3FB.js";import"./FSDialogMenu-_ixMOtNT.js";import"./FSCard-C7Q5zpHU.js";import"./FSRow-DoCXWKDP.js";import"./css-DYOPUjjE.js";import"./useColors-vjSCjmKB.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-_Z9wfsFC.js";import"./theme-CDGAP9oX.js";import"./VDialog-B-ihhCeR.js";import"./VOverlay-HCh9BhQb.js";import"./forwardRefs-DWGaNmQL.js";import"./color-BhlhQtQT.js";import"./proxiedModel-CYw7-KgF.js";import"./anchor-CLTIcUx3.js";import"./dimensions-fR777bfb.js";import"./display-D6hZonHv.js";import"./lazy-e3yj3TcL.js";import"./locale-BC9z6YbT.js";import"./router-CRaPYJ7n.js";import"./transition-CJbyFvkq.js";import"./VDefaultsProvider-CTE9NJfn.js";import"./FSTextField-Bzrs8V-b.js";import"./VField-Tu0u6Fec.js";import"./FSSpan-CwfDYIua.js";import"./useSlots-V2mVelz6.js";import"./VSpacer-CMZ7Jymr.js";import"./index-DWl9tX7E.js";import"./VLabel-Cp9LN_v7.js";import"./VInput-BvcCh06W.js";import"./VIcon-uFUcYN4x.js";import"./density-XYshZx8z.js";import"./loader-CTRePS7y.js";import"./VProgressCircular-D8KFt_en.js";import"./rounded-TFXDyxjJ.js";import"./FSButton-uZjHfaK3.js";import"./FSClickable-fH0IP9Gi.js";import"./useRules-DS9e5-1Q.js";import"./index-yKNzK_fJ.js";import"./FSCheckbox-B7KB3blX.js";import"./VSelectionControl-DLPOArCM.js";import"./index-ySUbTvsm.js";import"./FSFadeOut-DU52ZiYP.js";import"./uuid-DTaye2KM.js";import"./FSLoader-BPRTbLLe.js";import"./elevation-BoGrGvdU.js";import"./FSRadio-CMZuYlpC.js";import"./VList-D_XNZRf5.js";import"./ssrBoot-D90vJCps.js";import"./border-Csacwza1.js";import"./VImg-BLQ7D_3g.js";import"./VDivider-xUYR78_z.js";import"./VBtn-BeQ1BOJB.js";import"./group-DaluXt-5.js";import"./filter-DSaKTciw.js";import"./VMenu-Dhrs_Dg2.js";import"./pathCrumb-Db-cq5HI.js";import"./base-CMiH1YbJ.js";import"./useAppOrganisationId-CkwgOwhD.js";import"./serviceFactory-Bd5jifNi.js";import"./lodash-BiW_TGGX.js";import"./composableFactory-J8cSLWf9.js";const g=v({name:"FSTreeViewFolder",components:{FSTreeViewField:h,FSIcon:f},props:{folderFilters:{type:Object,required:!1,default:null},modelValue:{type:[Array,String],required:!1,default:null},multiple:{type:Boolean,required:!1,default:!1}},emits:["update:modelValue"],setup(e,{emit:o}){const{getMany:i,fetching:n,entities:t}=C(),a=()=>i({...e.folderFilters}),{onUpdate:l}=b(t,[()=>e.folderFilters],o,a);return{fetchingFolders:n,folders:t,onUpdate:l}}});function q(e,o,i,n,t,a){const l=S("FSTreeViewField");return m(),s(l,w({multiple:e.$props.multiple,loading:e.fetchingFolders,items:e.folders,modelValue:e.$props.modelValue,"onUpdate:modelValue":e.onUpdate},e.$attrs),{"menu-prepend":d(({item:p})=>[p.icon?(m(),s(f,{key:0},{default:d(()=>[y(T(p.icon),1)]),_:2},1024)):_("",!0)]),_:1},16,["multiple","loading","items","modelValue","onUpdate:modelValue"])}const V=k(g,[["render",q]]);g.__docgenInfo={displayName:"FSTreeViewFolder",exportName:"default",description:"",tags:{},props:[{name:"folderFilters",type:{name:"FolderFilters"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"modelValue",type:{name:"string[] | string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"multiple",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}}],events:[{name:"update:modelValue"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/core/foundation-core-components/components/treeviews/FSTreeViewFolder.vue"]};const He={title:"Foundation/Core/TreeViews/TreeViewFolder",component:V,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},r={args:{args:{value1:null,value2:null,value3:null,value4:null}},render:(e,{argTypes:o})=>({components:{FSTreeViewFolder:V,FSCol:$},props:Object.keys(o),setup(){return{...e}},template:`
+    <FSCol>
+      <FSTreeViewFolder
+        label="Folder"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewFolder
+        label="Folder with multiple selection"
+        :multiple="true"
+        v-model="args.value3"
+      />
+    </FSCol>`})};var u,c,F;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      value2: null,
+      value3: null,
+      value4: null
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSTreeViewFolder,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSTreeViewFolder
+        label="Folder"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewFolder
+        label="Folder with multiple selection"
+        :multiple="true"
+        v-model="args.value3"
+      />
+    </FSCol>\`
+  })
+}`,...(F=(c=r.parameters)==null?void 0:c.docs)==null?void 0:F.source}}};const Je=["Variations"];export{r as Variations,Je as __namedExportsOrder,He as default};
