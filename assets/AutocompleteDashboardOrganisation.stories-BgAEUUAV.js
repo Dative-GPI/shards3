a89e79cd1f3b1f0eb54d07b7d66e7825d465a7b1
peyronth <peyronth@users.noreply.github.com>
2024-07-04 12:56:43 +0000

    deploy: 35cd6ed1c25f3fd9366bc2c32102d479b70e3742

---
+++ assets/AutocompleteDashboardOrganisation.stories-BgAEUUAV.js
@@ -0,0 +1,73 @@
+import{d as V,c as C,y as k,z as r,A as n,B as t,m as $,E as l,G as i,D as u,I as g}from"./vue.esm-bundler-DC82FEWN.js";import{F as q}from"./FSAutocompleteField-9Z77q9u9.js";import{_ as c}from"./FSIcon-CqTsM4Lx.js";import{_ as b}from"./FSSpan-CwfDYIua.js";import{_ as h}from"./FSRow-DoCXWKDP.js";import{u as N}from"./useDashboardOrganisations-kGqGUzzT.js";import{u as B}from"./useAutocomplete-CCZg0XDc.js";import{_ as I}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{_ as T}from"./FSCol-B7HQy3FB.js";import"./FSSearchField-B1cVTZp_.js";import"./FSTextField-BVT64V1F.js";import"./VField-CQBU8LtE.js";import"./useColors-WxHDk9aB.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-3zl-tJp_.js";import"./theme-CDGAP9oX.js";import"./useSlots-V2mVelz6.js";import"./VSpacer-CMZ7Jymr.js";import"./color-BhlhQtQT.js";import"./index-DWl9tX7E.js";import"./transition-CJbyFvkq.js";import"./VLabel-Cp9LN_v7.js";import"./VInput-BvcCh06W.js";import"./locale-BC9z6YbT.js";import"./VIcon-uFUcYN4x.js";import"./density-XYshZx8z.js";import"./proxiedModel-CYw7-KgF.js";import"./loader-CTRePS7y.js";import"./VProgressCircular-D8KFt_en.js";import"./anchor-CLTIcUx3.js";import"./rounded-TFXDyxjJ.js";import"./VDefaultsProvider-CTE9NJfn.js";import"./forwardRefs-DWGaNmQL.js";import"./FSButton-cgjePGb6.js";import"./FSClickable-CvEctr0N.js";import"./FSCard-TPuCZi-6.js";import"./css-DYOPUjjE.js";import"./useRules-DS9e5-1Q.js";import"./index-yKNzK_fJ.js";import"./useTranslations-DV-taHAL.js";import"./FSDialogMenu-L2iVIZdg.js";import"./VDialog-B-ihhCeR.js";import"./VOverlay-HCh9BhQb.js";import"./dimensions-fR777bfb.js";import"./display-D6hZonHv.js";import"./lazy-e3yj3TcL.js";import"./router-CRaPYJ7n.js";import"./FSRadioGroup-CCDraVhx.js";import"./FSRadio-C2a3p5gK.js";import"./VSelectionControl-DLPOArCM.js";import"./index-ySUbTvsm.js";import"./FSToggleSet-OAzwv8eZ.js";import"./FSSlideGroup-_2N_53q2.js";import"./uuid-DTaye2KM.js";import"./VSlideGroup-CcTYsOIU.js";import"./group-DaluXt-5.js";import"./FSWrapGroup-CBQCrLfv.js";import"./FSCheckbox-Bb9obVvl.js";import"./FSFadeOut-DOYA3BXE.js";import"./FSLoader-Bx7UBqYr.js";import"./elevation-BoGrGvdU.js";import"./VSelect-BFSoAAL8.js";import"./VList-D_XNZRf5.js";import"./ssrBoot-D90vJCps.js";import"./border-Csacwza1.js";import"./VImg-BLQ7D_3g.js";import"./VDivider-xUYR78_z.js";import"./VMenu-Dhrs_Dg2.js";import"./filter-DSaKTciw.js";import"./dashboardTranslation-CGe8lPc2.js";import"./pathCrumb-Db-cq5HI.js";import"./base-CMiH1YbJ.js";import"./useAppOrganisationId-CkwgOwhD.js";import"./serviceFactory-Bd5jifNi.js";import"./lodash-BiW_TGGX.js";import"./composableFactory-J8cSLWf9.js";const y=V({name:"FSAutocompleteDashboard",components:{FSAutocompleteField:q,FSIcon:c,FSSpan:b,FSRow:h},props:{dashboardOrganisationFilters:{type:Object,required:!1,default:null},modelValue:{type:[Array,String],required:!1,default:null},toggleSetDisabled:{type:Boolean,required:!1,default:!1}},emits:["update:modelValue"],setup(e,{emit:a}){const{getMany:f,fetching:S,entities:p}=N(),F=C(()=>d.value&&S.value),m=A=>f({...e.dashboardOrganisationFilters,search:A??void 0}),{toggleSet:o,init:d,onUpdate:w}=B(p,[()=>e.dashboardOrganisationFilters],a,m);return{dashboardOrganisations:p,toggleSet:o,loading:F,onUpdate:w}}});function U(e,a,f,S,p,F){const m=k("FSAutocompleteField");return r(),n(m,$({toggleSet:!e.$props.toggleSetDisabled&&e.toggleSet,loading:e.loading,items:e.dashboardOrganisations,modelValue:e.$props.modelValue,"onUpdate:modelValue":e.onUpdate},e.$attrs),{"autocomplete-selection":t(({item:o})=>[e.$props.modelValue?(r(),n(h,{key:0,align:"center-center",wrap:!1},{default:t(()=>[o.raw.icon?(r(),n(c,{key:0},{default:t(()=>[l(i(o.raw.icon),1)]),_:2},1024)):u("",!0),g(b,null,{default:t(()=>[l(i(o.raw.label),1)]),_:2},1024)]),_:2},1024)):u("",!0)]),"item-label":t(({item:o,font:d})=>[g(h,{align:"center-left",wrap:!1},{default:t(()=>[o.raw.icon?(r(),n(c,{key:0},{default:t(()=>[l(i(o.raw.icon),1)]),_:2},1024)):u("",!0),g(b,{font:d},{default:t(()=>[l(i(o.raw.label),1)]),_:2},1032,["font"])]),_:2},1024)]),_:1},16,["toggleSet","loading","items","modelValue","onUpdate:modelValue"])}const _=I(y,[["render",U]]);y.__docgenInfo={displayName:"FSAutocompleteDashboard",exportName:"default",description:"",tags:{},props:[{name:"dashboardOrganisationFilters",type:{name:"DashboardOrganisationFilters"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"modelValue",type:{name:"string[] | string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"toggleSetDisabled",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}}],events:[{name:"update:modelValue"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/core/foundation-core-components/components/autocompletes/FSAutocompleteDashboardOrganisation.vue"]};const no={title:"Foundation/Core/Autocompletes/AutocompleteDashboardOrganisation",component:_,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},s={args:{args:{value1:null,value2:null,value3:null,value4:null}},render:(e,{argTypes:a})=>({components:{FSAutocompleteDashboardOrganisation:_,FSCol:T},props:Object.keys(a),setup(){return{...e}},template:`
+    <FSCol>
+      <FSAutocompleteDashboardOrganisation
+        label="DashboardOrganisation"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteDashboardOrganisation
+        label="DashboardOrganisation with toggleset disabled"
+        :toggleSetDisabled="true"
+        v-model="args.value2"
+      />
+      <FSAutocompleteDashboardOrganisation
+        label="DashboardOrganisation with multiple selection"
+        :multiple="true"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteDashboardOrganisation
+        label="DashboardOrganisation with toggleset disabled and multiple selection"
+        :multiple="true"
+        :toggleSetDisabled="true"
+        v-model="args.value4"
+      />
+    </FSCol>`})};var v,D,O;s.parameters={...s.parameters,docs:{...(v=s.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      value2: null,
+      value3: null,
+      value4: null
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSAutocompleteDashboardOrganisation,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSAutocompleteDashboardOrganisation
+        label="DashboardOrganisation"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteDashboardOrganisation
+        label="DashboardOrganisation with toggleset disabled"
+        :toggleSetDisabled="true"
+        v-model="args.value2"
+      />
+      <FSAutocompleteDashboardOrganisation
+        label="DashboardOrganisation with multiple selection"
+        :multiple="true"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteDashboardOrganisation
+        label="DashboardOrganisation with toggleset disabled and multiple selection"
+        :multiple="true"
+        :toggleSetDisabled="true"
+        v-model="args.value4"
+      />
+    </FSCol>\`
+  })
+}`,...(O=(D=s.parameters)==null?void 0:D.docs)==null?void 0:O.source}}};const lo=["Variations"];export{s as Variations,lo as __namedExportsOrder,no as default};
