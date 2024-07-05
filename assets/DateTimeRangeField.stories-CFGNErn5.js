d819d1546c2cbf71dfe46f25f5b7a32455b44356
gballigand <gballigand@users.noreply.github.com>
2024-07-05 09:52:50 +0000

    deploy: ca582d1a902c182973377dd0cae44181088f932f

---
+++ assets/DateTimeRangeField.stories-CFGNErn5.js
@@ -0,0 +1,155 @@
+import{F as i}from"./FSDateTimeRangeField-zh0DlMXs.js";import{F as u}from"./FSForm-A_xyKD4e.js";import{_ as p}from"./FSCol-B7HQy3FB.js";import{_ as g}from"./FSRow-DoCXWKDP.js";import{D as F}from"./rules-DlyQ2XjG.js";import"./vue.esm-bundler-DC82FEWN.js";import"./FSDialogSubmit-Chfd6Al3.js";import"./FSFadeOut-DU52ZiYP.js";import"./uuid-DTaye2KM.js";import"./css-DYOPUjjE.js";import"./useColors-vjSCjmKB.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-_Z9wfsFC.js";import"./theme-CDGAP9oX.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FSButton-uZjHfaK3.js";import"./FSClickable-fH0IP9Gi.js";import"./FSCard-C7Q5zpHU.js";import"./VProgressCircular-D8KFt_en.js";import"./color-BhlhQtQT.js";import"./VIcon-uFUcYN4x.js";import"./FSSpan-CwfDYIua.js";import"./useSlots-V2mVelz6.js";import"./FSIcon-CH1L3cXE.js";import"./FSDialog-CkwT6Gx6.js";import"./FSText-CRWIpNZ3.js";import"./VDialog-B-ihhCeR.js";import"./VOverlay-HCh9BhQb.js";import"./forwardRefs-DWGaNmQL.js";import"./proxiedModel-CYw7-KgF.js";import"./anchor-CLTIcUx3.js";import"./dimensions-fR777bfb.js";import"./display-D6hZonHv.js";import"./lazy-e3yj3TcL.js";import"./locale-BC9z6YbT.js";import"./router-CRaPYJ7n.js";import"./transition-CJbyFvkq.js";import"./VDefaultsProvider-CTE9NJfn.js";import"./VSpacer-CMZ7Jymr.js";import"./useTranslations-DV-taHAL.js";import"./FSCalendarTwin-DcEDk3bu.js";import"./useAppTimeZone-SgthjzoP.js";import"./VLocaleProvider-CCst0Z0f.js";import"./VBtn-BeQ1BOJB.js";import"./border-Csacwza1.js";import"./density-XYshZx8z.js";import"./elevation-BoGrGvdU.js";import"./rounded-TFXDyxjJ.js";import"./group-DaluXt-5.js";import"./loader-CTRePS7y.js";import"./index-ySUbTvsm.js";import"./FSTextField-Bzrs8V-b.js";import"./VField-Tu0u6Fec.js";import"./index-DWl9tX7E.js";import"./VLabel-Cp9LN_v7.js";import"./VInput-BvcCh06W.js";import"./useRules-DS9e5-1Q.js";import"./index-yKNzK_fJ.js";import"./FSClock-C-LryTNQ.js";import"./FSSlider-hAOWrC5b.js";import"./VSlider-DzaF8Z2w.js";import"./time-xMV0WbLU.js";const fe={title:"Foundation/Shared/Input fields/DateTimeRangeField",component:i,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},e={args:{args:{value1:null,value2:[17040672e5,17356032e5],value3:[17040672e5,17356032e5]}},render:(t,{argTypes:o})=>({components:{FSDateTimeRangeField:i,FSCol:p},props:Object.keys(o),setup(){return{...t}},template:`
+    <FSCol>
+      <FSDateTimeRangeField
+        label="Date & time range, primary color"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSDateTimeRangeField
+        label="Required date & time range, warning color, with description"
+        description="Description for this field"
+        color="warning"
+        :required="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSDateTimeRangeField
+        label="Uneditable date & time range, error color, with description"
+        description="Uneditable description"
+        color="error"
+        :editable="false"
+        v-model="args.value3"
+      />
+    </FSCol>`})},r={args:{args:{valid:!1,value1:null,value2:null,value3:null,rules:F}},render:(t,{argTypes:o})=>({components:{FSForm:u,FSDateTimeRangeField:i,FSCol:p,FSRow:g},props:Object.keys(o),setup(){return{...t}},template:`
+    <FSForm v-model="args.valid">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateTimeRangeField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateTimeRangeField
+          label="Rules: before 2024-01-31 00:00"
+          :rules="[args.rules.max(1706738400000)]"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateTimeRangeField
+          label="Rules: required & before 2024-01-31 00:00 & after 2024-01-02 00:00"
+          :rules="[args.rules.required(), args.rules.max(1706738400000), args.rules.min(1704146400000)]"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>`})};var l,a,n;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      value2: [1704067200000, 1735603200000],
+      value3: [1704067200000, 1735603200000]
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSDateTimeRangeField,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSDateTimeRangeField
+        label="Date & time range, primary color"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSDateTimeRangeField
+        label="Required date & time range, warning color, with description"
+        description="Description for this field"
+        color="warning"
+        :required="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSDateTimeRangeField
+        label="Uneditable date & time range, error color, with description"
+        description="Uneditable description"
+        color="error"
+        :editable="false"
+        v-model="args.value3"
+      />
+    </FSCol>\`
+  })
+}`,...(n=(a=e.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};var s,d,m;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    args: {
+      valid: false,
+      value1: null,
+      value2: null,
+      value3: null,
+      rules: DateRules
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSForm,
+      FSDateTimeRangeField,
+      FSCol,
+      FSRow
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSForm v-model="args.valid">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateTimeRangeField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateTimeRangeField
+          label="Rules: before 2024-01-31 00:00"
+          :rules="[args.rules.max(1706738400000)]"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateTimeRangeField
+          label="Rules: required & before 2024-01-31 00:00 & after 2024-01-02 00:00"
+          :rules="[args.rules.required(), args.rules.max(1706738400000), args.rules.min(1704146400000)]"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>\`
+  })
+}`,...(m=(d=r.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};const De=["Variations","Rules"];export{r as Rules,e as Variations,De as __namedExportsOrder,fe as default};
---
+++ assets/OptionGroup.stories-KkgBYnxQ.js
@@ -0,0 +1,176 @@
+import{F as o,a as l}from"./FSOptionGroup-BBsHcV5z.js";import{F as s}from"./FSText-CRWIpNZ3.js";import"./vue.esm-bundler-DC82FEWN.js";import"./FSClickable-fH0IP9Gi.js";import"./FSCard-C7Q5zpHU.js";import"./FSCol-B7HQy3FB.js";import"./css-DYOPUjjE.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FSRow-DoCXWKDP.js";import"./useColors-vjSCjmKB.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-_Z9wfsFC.js";import"./theme-CDGAP9oX.js";import"./VProgressCircular-D8KFt_en.js";import"./color-BhlhQtQT.js";import"./VIcon-uFUcYN4x.js";import"./FSSpan-CwfDYIua.js";import"./useSlots-V2mVelz6.js";import"./FSIcon-CH1L3cXE.js";import"./FSSlideGroup-G_yR414R.js";import"./uuid-DTaye2KM.js";import"./FSButton-uZjHfaK3.js";import"./VSlideGroup-CcTYsOIU.js";import"./index-DWl9tX7E.js";import"./display-D6hZonHv.js";import"./group-DaluXt-5.js";import"./proxiedModel-CYw7-KgF.js";import"./locale-BC9z6YbT.js";import"./FSWrapGroup-BRpaPdC9.js";const E={title:"Foundation/Shared/OptionGroup",component:o,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},r={args:{args:{values1:[...Array(10).keys()].map(e=>({id:e.toString(),icon:`mdi-numeric-${e+1}`})),value1:[],values2:[...Array(6).keys()].map(e=>({id:e.toString(),icon:`mdi-dice-${e+1}-outline`})),value2:null,values3:[...Array(6).keys()].map(e=>({id:e.toString(),icon:`mdi-hexagon-slice-${e+1}`})),value3:[0],value4:"0"}},render:(e,{argTypes:a})=>({components:{FSOptionGroup:o,FSOptionItem:l,FSText:s},props:Object.keys(a),setup(){return{...e}},template:`
+    <div style="display: flex; flex-direction: column; gap: 30px;">
+      <FSText> Props only, wrap, single selection, not required </FSText>
+      <FSOptionGroup
+        :required="false"
+        :values="args.values1"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Props only, slide, single selection, required </FSText>
+      <FSOptionGroup
+        variant="slide"
+        activeColor="success"
+        :values="args.values2"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Props & item template, slide, multiple selection, required </FSText>
+      <FSOptionGroup
+        variant="slide"
+        activeColor="dark"
+        :multiple="true"
+        :values="args.values3"
+        v-model="args.value3"
+      >
+        <template #item>
+          <FSOptionItem
+            appendIcon="mdi-ski-water"
+          />
+        </template>
+      </FSOptionGroup>
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Default template, slide, single selection, not required </FSText>
+      <FSOptionGroup
+        padding="8px"
+        gap="8px"
+        :modelValue="args.value4"
+      >
+        <FSOptionItem
+          icon="mdi-wifi-strength-off-outline"
+          :color="(args.value4 === '0') ? 'success' : 'error'"
+          @click="() => args.value4 === '0' ? args.value4 = null : args.value4 = '0'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-outline"
+          :color="(args.value4 === '1') ? 'success' : 'error'"
+          @click="() => args.value4 === '1' ? args.value4 = null : args.value4 = '1'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-1"
+          :color="(args.value4 === '2') ? 'success' : 'error'"
+          @click="() => args.value4 === '2' ? args.value4 = null : args.value4 = '2'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-2"
+          :color="(args.value4 === '3') ? 'success' : 'error'"
+          @click="() => args.value4 === '3' ? args.value4 = null : args.value4 = '3'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-3"
+          :color="(args.value4 === '4') ? 'success' : 'error'"
+          @click="() => args.value4 === '4' ? args.value4 = null : args.value4 = '4'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-4"
+          :color="(args.value4 === '5') ? 'success' : 'error'"
+          @click="() => args.value4 === '5' ? args.value4 = null : args.value4 = '5'"
+        />
+      </FSOptionGroup>
+    </div>`})};var t,i,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    args: {
+      values1: [...Array(10).keys()].map(i => ({
+        id: i.toString(),
+        icon: \`mdi-numeric-\${i + 1}\`
+      })),
+      value1: [],
+      values2: [...Array(6).keys()].map(i => ({
+        id: i.toString(),
+        icon: \`mdi-dice-\${i + 1}-outline\`
+      })),
+      value2: null,
+      values3: [...Array(6).keys()].map(i => ({
+        id: i.toString(),
+        icon: \`mdi-hexagon-slice-\${i + 1}\`
+      })),
+      value3: [0],
+      value4: "0"
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSOptionGroup,
+      FSOptionItem,
+      FSText
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <div style="display: flex; flex-direction: column; gap: 30px;">
+      <FSText> Props only, wrap, single selection, not required </FSText>
+      <FSOptionGroup
+        :required="false"
+        :values="args.values1"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Props only, slide, single selection, required </FSText>
+      <FSOptionGroup
+        variant="slide"
+        activeColor="success"
+        :values="args.values2"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Props & item template, slide, multiple selection, required </FSText>
+      <FSOptionGroup
+        variant="slide"
+        activeColor="dark"
+        :multiple="true"
+        :values="args.values3"
+        v-model="args.value3"
+      >
+        <template #item>
+          <FSOptionItem
+            appendIcon="mdi-ski-water"
+          />
+        </template>
+      </FSOptionGroup>
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSText> Default template, slide, single selection, not required </FSText>
+      <FSOptionGroup
+        padding="8px"
+        gap="8px"
+        :modelValue="args.value4"
+      >
+        <FSOptionItem
+          icon="mdi-wifi-strength-off-outline"
+          :color="(args.value4 === '0') ? 'success' : 'error'"
+          @click="() => args.value4 === '0' ? args.value4 = null : args.value4 = '0'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-outline"
+          :color="(args.value4 === '1') ? 'success' : 'error'"
+          @click="() => args.value4 === '1' ? args.value4 = null : args.value4 = '1'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-1"
+          :color="(args.value4 === '2') ? 'success' : 'error'"
+          @click="() => args.value4 === '2' ? args.value4 = null : args.value4 = '2'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-2"
+          :color="(args.value4 === '3') ? 'success' : 'error'"
+          @click="() => args.value4 === '3' ? args.value4 = null : args.value4 = '3'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-3"
+          :color="(args.value4 === '4') ? 'success' : 'error'"
+          @click="() => args.value4 === '4' ? args.value4 = null : args.value4 = '4'"
+        />
+        <FSOptionItem
+          icon="mdi-wifi-strength-4"
+          :color="(args.value4 === '5') ? 'success' : 'error'"
+          @click="() => args.value4 === '5' ? args.value4 = null : args.value4 = '5'"
+        />
+      </FSOptionGroup>
+    </div>\`
+  })
+}`,...(n=(i=r.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const z=["Variations"];export{r as Variations,z as __namedExportsOrder,E as default};
