54357db77e4b475bb24b8a91bd694bf1c8570f26
rduteil <rduteil@users.noreply.github.com>
2024-07-05 13:25:21 +0000

    deploy: 0144f9154fe11cd6f311cb8c73e9eea17a90e229

---
+++ assets/EditImage.stories-Duhow6VE.js
@@ -0,0 +1,131 @@
+import{d as M,b as C,c as T,w as x,y as u,z as n,A as i,B as a,J as D,I as l,O as v,E as s,G as p,D as S}from"./vue.esm-bundler-DC82FEWN.js";import{F as O,a as A,u as z}from"./FSButtonFileMini-CHM2SZ0_.js";import{F as G}from"./FSClickable-CvEctr0N.js";import{F as H}from"./FSImage-NMzdlsY0.js";import{F as j}from"./FSCard-TPuCZi-6.js";import{F as J}from"./FSText-jPk40s7Q.js";import{_ as F}from"./FSCol-B7HQy3FB.js";import{_ as r}from"./FSRow-DoCXWKDP.js";import{u as P}from"./css-DYOPUjjE.js";import{u as L,C as w}from"./useColors-WxHDk9aB.js";import{_ as K}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{_ as Q}from"./FSIcon-CqTsM4Lx.js";import{_ as E}from"./FSSpan-CwfDYIua.js";import{V as W}from"./VSpacer-CMZ7Jymr.js";import{F as X}from"./FSButton-cgjePGb6.js";import"./base-BNnJ_ndD.js";import"./VProgressCircular-D8KFt_en.js";import"./color-BhlhQtQT.js";import"./theme-CDGAP9oX.js";import"./VIcon-uFUcYN4x.js";import"./FSLoader-Bx7UBqYr.js";import"./dimensions-fR777bfb.js";import"./elevation-BoGrGvdU.js";import"./locale-BC9z6YbT.js";import"./composableFactory-J8cSLWf9.js";import"./serviceFactory-Bd5jifNi.js";import"./_commonjsHelpers-BosuxZz1.js";import"./lodash-BiW_TGGX.js";import"./VImg-BLQ7D_3g.js";import"./rounded-TFXDyxjJ.js";import"./transition-CJbyFvkq.js";import"./index-yKNzK_fJ.js";import"./useSlots-V2mVelz6.js";import"./index-3zl-tJp_.js";const N=M({name:"FSEditImage",components:{FSButtonRemoveMini:O,FSButtonFileMini:A,FSClickable:G,FSImage:H,FSCard:j,FSText:J,FSCol:F,FSRow:r},props:{height:{type:[Array,String,Number],required:!1,default:null},width:{type:[Array,String,Number],required:!1,default:null},label:{type:String,required:!1,default:null},imageId:{type:String,required:!1,default:null},aspectRatio:{type:String,required:!1,default:null},modelValue:{type:String,required:!1,default:null},variant:{type:String,required:!1,default:"standard"},hideHeader:{type:Boolean,required:!1,default:!1},required:{type:Boolean,required:!1,default:!1}},emits:["update:modelValue","update:imageId"],setup(e,{emit:t}){const{isExtraSmall:V}=P(),{getColors:g}=L(),{readFile:$}=z(),o=C({fileName:"",fileContent:null}),f=C(null),c=g(w.Error),d=g(w.Light),m=g(w.Dark),h=T(()=>({"--fs-edit-image-overline-text-color":d.dark,"--fs-edit-image-color":m.base,"--fs-edit-image-error-color":c.base})),y=async k=>{const b=await $(k);o.value.fileName=k.name,o.value.fileContent=b,t("update:modelValue",b.substring(b.indexOf(",")+1)),e.imageId&&t("update:imageId",null)},U=()=>{o.value.fileName="",o.value.fileContent=null,e.modelValue?t("update:modelValue",null):t("update:imageId",null)};return x(()=>e.modelValue,()=>{e.modelValue||(o.value.fileName="",o.value.fileContent=null)}),{invisibleButtonRef:f,fileSelected:o,isExtraSmall:V,style:h,onUpload:y,onRemove:U}}});function Y(e,t,V,g,$,o){const f=u("FSImage"),c=u("FSCard"),d=u("FSText"),m=u("FSButtonFileMini"),h=u("FSButtonRemoveMini"),y=u("FSClickable");return n(),i(F,{gap:"8px"},{default:a(()=>[e.$props.hideHeader?S("",!0):D(e.$slots,"label",{key:0},()=>[l(r,{wrap:!1},{default:a(()=>[e.$props.label?(n(),i(E,{key:0,class:"fs-edit-image-label",font:"text-overline",style:v(e.style)},{default:a(()=>[s(p(e.$props.label),1)]),_:1},8,["style"])):S("",!0),e.$props.label&&e.$props.required?(n(),i(E,{key:1,class:"fs-edit-image-label",style:v([{"margin-left":"-8px"},e.style]),font:"text-overline",ellipsis:!1},{default:a(()=>[s(" * ")]),_:1},8,["style"])):S("",!0)]),_:1})]),["standard"].includes(e.$props.variant)?(n(),i(c,{key:1,padding:"12px",width:"100%"},{default:a(()=>[l(r,{align:"center-left",wrap:!1},{default:a(()=>[l(r,{gap:"24px",wrap:!1},{default:a(()=>[e.$props.imageId||e.$props.modelValue?(n(),i(f,{key:0,aspectRatio:e.$props.aspectRatio,height:e.$props.height,width:e.$props.width,imageId:e.$props.imageId,imageB64:e.$props.modelValue},null,8,["aspectRatio","height","width","imageId","imageB64"])):(n(),i(c,{key:1,borderStyle:"dashed",aspectRatio:e.$props.aspectRatio,height:e.$props.height,width:e.$props.width},null,8,["aspectRatio","height","width"])),l(F,{align:"center-left",height:"fill",width:"hug",gap:"10px"},{default:a(()=>[e.fileSelected&&e.fileSelected.fileName?(n(),i(d,{key:0,lineClamp:"2",font:"text-body"},{default:a(()=>[s(p(e.fileSelected.fileName),1)]),_:1})):S("",!0),l(F,{gap:"0"},{default:a(()=>[l(d,{class:"fs-edit-image-overline",font:"text-overline",style:v(e.style)},{default:a(()=>[s(p(e.$tr("ui.edit-image.format","PNG or JPG format")),1)]),_:1},8,["style"]),l(d,{class:"fs-edit-image-overline",font:"text-overline",style:v(e.style)},{default:a(()=>[s(p(e.$tr("ui.edit-image.size","10 MB Maximum")),1)]),_:1},8,["style"])]),_:1})]),_:1})]),_:1}),l(W),l(r,{width:"hug"},{default:a(()=>[l(m,{accept:"image/*",readFile:!1,"onUpdate:modelValue":e.onUpload},null,8,["onUpdate:modelValue"]),l(h,{onClick:e.onRemove},null,8,["onClick"])]),_:1})]),_:1})]),_:1})):e.$props.imageId||e.$props.modelValue?(n(),i(r,{key:2,width:e.$props.width,class:"fs-edit-image-full"},{default:a(()=>[l(f,{aspectRatio:e.$props.aspectRatio,height:e.$props.height,width:e.$props.width,imageId:e.$props.imageId,imageB64:e.$props.modelValue},null,8,["aspectRatio","height","width","imageId","imageB64"]),l(r,{class:"fs-edit-image-full-toolbar",padding:"4px"},{default:a(()=>[l(m,{accept:"image/*",readFile:!1,"onUpdate:modelValue":e.onUpload},null,8,["onUpdate:modelValue"]),l(h,{onClick:e.onRemove},null,8,["onClick"])]),_:1})]),_:1},8,["width"])):(n(),i(y,{key:3,borderStyle:"dashed",variant:"background",height:e.$props.height,width:e.$props.width,onClick:t[0]||(t[0]=()=>e.invisibleButtonRef.input.click())},{default:a(()=>[l(r,{align:"center-center",wrap:!1},{default:a(()=>[l(Q,null,{default:a(()=>[s(" mdi-plus-box-outline ")]),_:1}),l(d,{lineClamp:"2",font:"text-body"},{default:a(()=>[s(p(e.$tr("ui.edit-image.add-image","Add an image.")),1)]),_:1}),l(m,{ref:"invisibleButtonRef",class:"fs-edit-image-hidden-button",accept:"image/*",readFile:!1,"onUpdate:modelValue":e.onUpload},null,8,["onUpdate:modelValue"])]),_:1})]),_:1},8,["height","width"]))]),_:3})}const _=K(N,[["render",Y]]);N.__docgenInfo={displayName:"FSEditImage",exportName:"default",description:"",tags:{},props:[{name:"height",type:{name:"string[] | number[] | string | number | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"width",type:{name:"string[] | number[] | string | number | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"label",type:{name:"string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"imageId",type:{name:"string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"aspectRatio",type:{name:"string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"modelValue",type:{name:"string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"variant",values:["standard","full"],type:{name:"string"},required:!1,defaultValue:{func:!1,value:'"standard"'}},{name:"hideHeader",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}},{name:"required",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}}],events:[{name:"update:modelValue"},{name:"update:imageId"}],slots:[{name:"label"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/shared/foundation-shared-components/components/FSEditImage.vue"]};const _e={title:"Foundation/Shared/EditImage",component:_,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},I={args:{args:{imageId1:"1",source1:"",source2:null,source3:null,imageId3:"1",source4:null,source5:null,imageId5:"1",source6:null,imageId6:"1"}},render:(e,{argTypes:t})=>({components:{FSEditImage:_,FSButton:X},props:Object.keys(t),setup(){return{...e}},template:`
+    <div style="display: flex; flex-direction: column; gap: 10px;">
+      Variant Standard : <br>
+      <FSEditImage
+        height="96px"
+        width="96px"
+        v-model="args.source2"
+        label="Edit Image"
+      />
+      <FSButton @click="args.source2 = null">
+        Reset modelValue
+      </FSButton>
+      <FSEditImage
+        height="96px"
+        width="96px"
+        v-model:imageId="args.imageId1"
+        v-model="args.source1"
+        :required="true"
+        label="Edit Image with already set image ID"
+      />
+
+      <br>
+      Variant Full : <br>
+      <FSEditImage
+        variant="full"
+        height="50px"
+        width="100%"
+        v-model="args.source4"
+      />
+      <FSEditImage
+        variant="full"
+        height="150px"
+        width="300px"
+        v-model:imageId="args.imageId3"
+        v-model="args.source3"
+      />
+      <FSEditImage
+        variant="full"
+        :width="500"
+        aspectRatio="16/9"
+        v-model="args.source5"
+        v-model:imageId="args.imageId5"
+      />
+      <FSEditImage
+        variant="full"
+        height="222px"
+        width="100%"
+        v-model="args.source6"
+        v-model:imageId="args.imageId6"
+      />
+    </div>`})};var B,R,q;I.parameters={...I.parameters,docs:{...(B=I.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    args: {
+      imageId1: "1",
+      source1: "",
+      source2: null,
+      source3: null,
+      imageId3: "1",
+      source4: null,
+      source5: null,
+      imageId5: "1",
+      source6: null,
+      imageId6: "1"
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSEditImage,
+      FSButton
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <div style="display: flex; flex-direction: column; gap: 10px;">
+      Variant Standard : <br>
+      <FSEditImage
+        height="96px"
+        width="96px"
+        v-model="args.source2"
+        label="Edit Image"
+      />
+      <FSButton @click="args.source2 = null">
+        Reset modelValue
+      </FSButton>
+      <FSEditImage
+        height="96px"
+        width="96px"
+        v-model:imageId="args.imageId1"
+        v-model="args.source1"
+        :required="true"
+        label="Edit Image with already set image ID"
+      />
+
+      <br>
+      Variant Full : <br>
+      <FSEditImage
+        variant="full"
+        height="50px"
+        width="100%"
+        v-model="args.source4"
+      />
+      <FSEditImage
+        variant="full"
+        height="150px"
+        width="300px"
+        v-model:imageId="args.imageId3"
+        v-model="args.source3"
+      />
+      <FSEditImage
+        variant="full"
+        :width="500"
+        aspectRatio="16/9"
+        v-model="args.source5"
+        v-model:imageId="args.imageId5"
+      />
+      <FSEditImage
+        variant="full"
+        height="222px"
+        width="100%"
+        v-model="args.source6"
+        v-model:imageId="args.imageId6"
+      />
+    </div>\`
+  })
+}`,...(q=(R=I.parameters)==null?void 0:R.docs)==null?void 0:q.source}}};const Ue=["Variations"];export{I as Variations,Ue as __namedExportsOrder,_e as default};
