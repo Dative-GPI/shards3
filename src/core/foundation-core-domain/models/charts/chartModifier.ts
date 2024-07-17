5ae0d28111217a67e9f082797941333226a9be6a
ddonfack <ddonfack@dative-gpi.com>
2024-07-17 11:22:58 +0200

    corect modifier

--- src/core/foundation-core-domain/models/charts/chartModifier.ts
+++ src/core/foundation-core-domain/models/charts/chartModifier.ts
@@ -1,16 +1,22 @@
 export class ChartModifier {
+  id : string;
   chartId : string;
   hiddenCode: string;
   dataDefinitionId: string;
   hideLabel: boolean;
   
   constructor(params: ChartModifierDTO) {
+    this.id = params.id;
+    this.chartId = params.chartId;
+    this.hiddenCode = params.hiddenCode;
     this.dataDefinitionId = params.dataDefinitionId;
     this.hideLabel = params.hideLabel;
   }
 }
   
 export interface ChartModifierDTO {
+  id : string
+  chartId : string;
   hiddenCode: string;
   dataDefinitionId: string;
   hideLabel: boolean;
