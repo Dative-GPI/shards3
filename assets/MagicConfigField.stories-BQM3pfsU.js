c509ea18138086f26e6a2cff37650c52128cb764
rduteil <rduteil@users.noreply.github.com>
2024-07-17 11:05:48 +0000

    deploy: 6be85d1c0c6a71a869a0fd82a3740a5f8f0aca41

---
+++ assets/MagicConfigField.stories-BQM3pfsU.js
@@ -0,0 +1,137 @@
+import{F as o}from"./FSMagicConfigField-CCTNG68z.js";import{_ as n}from"./FSCol-B7HQy3FB.js";import"./vue.esm-bundler-DC82FEWN.js";import"./useMagicFieldProvider-_Wlv2mFM.js";import"./iframe-CfcVzUJv.js";import"../sb-preview/runtime.js";import"./FSButtonAddIcon-Cy64qxYV.js";import"./FSButton-DRPhZ-n9.js";import"./FSClickable-aI6guRt-.js";import"./FSCard-EcIwA-Oh.js";import"./FSRow-DoCXWKDP.js";import"./css-DYOPUjjE.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./useColors-C2RWUFtR.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-_Z9wfsFC.js";import"./theme-B5x8xkbY.js";import"./VProgressCircular-C-U4HXtE.js";import"./color-DvJT0EEa.js";import"./VIcon-Dc-dFojD.js";import"./FSSpan-CwfDYIua.js";import"./useSlots-V2mVelz6.js";import"./FSIcon-ClfMDAJS.js";import"./FSTranslateField-D3ZSICUa.js";import"./FSDialogSubmit-B4KBAXy4.js";import"./FSFadeOut-CvvHY5il.js";import"./uuid-DTaye2KM.js";import"./FSDialog-uwJ6jHsX.js";import"./FSText-CdvV5PI3.js";import"./VDialog-BV_tmW5d.js";import"./VOverlay-QKFvvy4k.js";import"./forwardRefs-DWGaNmQL.js";import"./proxiedModel-slW9zgh3.js";import"./anchor-C6LV5e1c.js";import"./dimensions-Dnxg3zhh.js";import"./display-B8HpKdBf.js";import"./lazy-Cw6rrZWC.js";import"./locale-BC9z6YbT.js";import"./router-DFGAHh1d.js";import"./transition-yl3lVN6J.js";import"./VDefaultsProvider-4deG1Ppv.js";import"./VSpacer-BjECYW1K.js";import"./useTranslations-DV-taHAL.js";import"./FSTextField-PC8aXZax.js";import"./FSBaseField-Df_egr4c.js";import"./useRules-DS9e5-1Q.js";import"./VField-CAe7-ioi.js";import"./index-CvDsTioR.js";import"./VLabel-D_bmQQv3.js";import"./VInput-ClkK-yNd.js";import"./density-CFGlRMM_.js";import"./loader-DENHXnzB.js";import"./rounded-YB6K8mOp.js";import"./index-DN-QUi1L.js";import"./useAppLanguages-BKzJTchM.js";import"./rules-DlyQ2XjG.js";import"./useAppTimeZone-SgthjzoP.js";import"./time-xMV0WbLU.js";const gl={title:"Foundation/Shared/Input fields/MagicConfigField",component:o,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},l={args:{args:{labelDefault1:"NumberField",translations1:[],value1:"",labelDefault2:"Switch",translations2:[],value2:"",labelDefault3:"TextField",translations3:[],value3:"",labelDefault4:"DateTimeField",translations4:[],value4:"",labelDefault5:"IconField",translations5:[],value5:"",labelDefault6:"TimeField",translations6:[],value6:""}},render:(i,{argTypes:r})=>({components:{FSMagicConfigField:o,FSCol:n},props:Object.keys(r),setup(){return{...i}},template:`
+    <FSCol>
+      <FSMagicConfigField
+        label="Number field"
+        :type="1"
+        v-model:translations="args.translations1"
+        v-model:labelDefault="args.labelDefault1"
+        v-model:value="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Switch"
+        :type="2"
+        v-model:translations="args.translations2"
+        v-model:labelDefault="args.labelDefault2"
+        v-model:value="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Text field"
+        :type="3"
+        v-model:translations="args.translations3"
+        v-model:labelDefault="args.labelDefault3"
+        v-model:value="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Date time field"
+        :type="4"
+        v-model:translations="args.translations4"
+        v-model:labelDefault="args.labelDefault4"
+        v-model:value="args.value4"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Icon field"
+        :type="5"
+        v-model:translations="args.translations5"
+        v-model:labelDefault="args.labelDefault5"
+        v-model:value="args.value5"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Time field"
+        :type="6"
+        v-model:translations="args.translations6"
+        v-model:labelDefault="args.labelDefault6"
+        v-model:value="args.value6"
+      />
+    </FSCol>`})};var e,t,a;l.parameters={...l.parameters,docs:{...(e=l.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    args: {
+      labelDefault1: "NumberField",
+      translations1: [],
+      value1: "",
+      labelDefault2: "Switch",
+      translations2: [],
+      value2: "",
+      labelDefault3: "TextField",
+      translations3: [],
+      value3: "",
+      labelDefault4: "DateTimeField",
+      translations4: [],
+      value4: "",
+      labelDefault5: "IconField",
+      translations5: [],
+      value5: "",
+      labelDefault6: "TimeField",
+      translations6: [],
+      value6: ""
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSMagicConfigField,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSMagicConfigField
+        label="Number field"
+        :type="1"
+        v-model:translations="args.translations1"
+        v-model:labelDefault="args.labelDefault1"
+        v-model:value="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Switch"
+        :type="2"
+        v-model:translations="args.translations2"
+        v-model:labelDefault="args.labelDefault2"
+        v-model:value="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Text field"
+        :type="3"
+        v-model:translations="args.translations3"
+        v-model:labelDefault="args.labelDefault3"
+        v-model:value="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Date time field"
+        :type="4"
+        v-model:translations="args.translations4"
+        v-model:labelDefault="args.labelDefault4"
+        v-model:value="args.value4"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Icon field"
+        :type="5"
+        v-model:translations="args.translations5"
+        v-model:labelDefault="args.labelDefault5"
+        v-model:value="args.value5"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMagicConfigField
+        label="Time field"
+        :type="6"
+        v-model:translations="args.translations6"
+        v-model:labelDefault="args.labelDefault6"
+        v-model:value="args.value6"
+      />
+    </FSCol>\`
+  })
+}`,...(a=(t=l.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const ul=["Variations"];export{l as Variations,ul as __namedExportsOrder,gl as default};
