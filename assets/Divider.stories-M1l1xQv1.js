beeeac9cea1e11c0f0f4b98fa881c757d2ae0337
dimitrilexi <dimitrilexi@users.noreply.github.com>
2024-07-26 11:40:33 +0000

    deploy: ff5e5835f4bb2b3a2c1c0f53fc278e70777d014a

--- assets/Divider.stories-M1l1xQv1.js
+++
@@ -1,5 +0,0 @@
-import{F as p}from"./FSDivider-hwMpepI1.js";import"./vue.esm-bundler-DC82FEWN.js";import"./FSText-xgcCPmKK.js";import"./useColors-DQLHFhAZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DfSX31J9.js";import"./theme-B5x8xkbY.js";import"./useSlots-V2mVelz6.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./css-DYOPUjjE.js";import"./FSCol-B7HQy3FB.js";import"./FSRow-DoCXWKDP.js";const W={title:"Foundation/Shared/Divider",component:p,tags:["autodocs"]},r={},t={args:{label:"With Text"}};var e,o,a;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:"{}",...(a=(o=r.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};var s,i,m;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    label: 'With Text'
-  }
-}`,...(m=(i=t.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};const F=["Default","WithText"];export{r as Default,t as WithText,F as __namedExportsOrder,W as default};
