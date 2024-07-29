5af544e6f1b57bfae1eed187d3087aaaff934db4
dimitrilexi <dimitrilexi@users.noreply.github.com>
2024-07-29 16:44:56 +0000

    deploy: ca012ee265fbeeddb5ce423de66b84a324143b8f

---
+++ assets/SimpleIcon.stories-CoNia96y.js
@@ -0,0 +1,53 @@
+import{F as r}from"./FSSimpleTileUI-BeEXwABv.js";import"./vue.esm-bundler-DC82FEWN.js";import"./FSCard-BzleLmPd.js";import"./FSCol-B7HQy3FB.js";import"./css-DYOPUjjE.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FSRow-DoCXWKDP.js";import"./useColors-DEq6MuQy.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-3zl-tJp_.js";import"./theme-B5x8xkbY.js";import"./FSIcon-uD_MRFdb.js";import"./VIcon-Dc-dFojD.js";import"./color-DvJT0EEa.js";import"./FSImage-z4MzVpjg.js";import"./FSLoader-CbNdmyFm.js";import"./dimensions-Dnxg3zhh.js";import"./elevation-BTkUCD39.js";import"./locale-BC9z6YbT.js";import"./base-CmdGny12.js";import"./composableFactory-J8cSLWf9.js";import"./serviceFactory-Bd5jifNi.js";import"./lodash-BiW_TGGX.js";import"./VImg-DgHq50DD.js";import"./rounded-YB6K8mOp.js";import"./transition-yl3lVN6J.js";import"./index-DN-QUi1L.js";import"./FSText-xR-0o23P.js";import"./useSlots-V2mVelz6.js";import"./FSTile-BFrXuspw.js";import"./FSClickable-CNOWN-hR.js";import"./VProgressCircular-C-U4HXtE.js";import"./FSCheckbox-CFeLLHEx.js";import"./FSSpan-CwfDYIua.js";import"./useRules-DS9e5-1Q.js";import"./VSelectionControl-Btb9GcG5.js";import"./density-CFGlRMM_.js";import"./proxiedModel-slW9zgh3.js";import"./index-9jSorrJj.js";import"./VLabel-D_bmQQv3.js";import"./VInput-ClkK-yNd.js";import"./index-CvDsTioR.js";const W={title:"Foundation/Shared/Tiles/SimpleIcon",component:r,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},e={args:{args:{values:[{id:"1",label:"Tile 1",code:"code"},{id:"2",label:"Simple Icon tile 2"},{id:"3",label:"Simple Icon tile with color",colors:["#0000FF","#FFFFFF"]}],selected:[!1,!1,!1]}},render:(n,{argTypes:p})=>({components:{FSSimpleTileUI:r},props:Object.keys(p),setup(){return{...n}},template:`
+    <div style="display: flex; gap: 10px; flex-wrap: wrap; width: 100vw;">
+      <FSSimpleTileUI
+        v-for="(entity, index) in args.values"
+        :key="index"
+        :label="entity.label"
+        :code="entity.code"
+        :color="entity.colors"
+        v-model="args.selected[index]"
+      />
+    </div>`})};var i,o,t;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    args: {
+      values: [{
+        id: "1",
+        label: "Tile 1",
+        code: "code"
+      }, {
+        id: "2",
+        label: "Simple Icon tile 2"
+      }, {
+        id: "3",
+        label: "Simple Icon tile with color",
+        colors: ["#0000FF", "#FFFFFF"]
+      }],
+      selected: [false, false, false]
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSSimpleTileUI
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <div style="display: flex; gap: 10px; flex-wrap: wrap; width: 100vw;">
+      <FSSimpleTileUI
+        v-for="(entity, index) in args.values"
+        :key="index"
+        :label="entity.label"
+        :code="entity.code"
+        :color="entity.colors"
+        v-model="args.selected[index]"
+      />
+    </div>\`
+  })
+}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const X=["Variations"];export{e as Variations,X as __namedExportsOrder,W as default};
