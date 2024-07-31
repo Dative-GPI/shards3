867d2a8d8214563a967ab5a899bd65ffbf164da3
rduteil <rduteil@users.noreply.github.com>
2024-07-31 06:44:55 +0000

    deploy: ef6ea4dcc5907c1aee4b4a74ff16cd956518da28

---
+++ assets/AutocompleteRole.stories-D0C-2dZ9.js
@@ -0,0 +1,53 @@
+import{F as p}from"./FSAutocompleteRole-C0els95p.js";import{_ as l}from"./FSCol-B7HQy3FB.js";import"./vue.esm-bundler-DC82FEWN.js";import"./FSAutocompleteField-BKT9iYho.js";import"./FSSearchField-CrEIMyOU.js";import"./FSTextField-mMS0Kez6.js";import"./FSBaseField-DmIgZozj.js";import"./FSSpan-CEjD71wr.js";import"./useSlots-Dr5tdD4b.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FSRow-DoCXWKDP.js";import"./css-DYOPUjjE.js";import"./useColors-DEq6MuQy.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-3zl-tJp_.js";import"./theme-B5x8xkbY.js";import"./VSpacer-BjECYW1K.js";import"./color-DvJT0EEa.js";import"./FSButton-1EF78Bzf.js";import"./FSClickable-CNOWN-hR.js";import"./FSCard-BzleLmPd.js";import"./VProgressCircular-C-U4HXtE.js";import"./VIcon-Dc-dFojD.js";import"./FSIcon-uD_MRFdb.js";import"./useRules-DS9e5-1Q.js";import"./VField-CAe7-ioi.js";import"./index-CvDsTioR.js";import"./transition-yl3lVN6J.js";import"./VLabel-D_bmQQv3.js";import"./VInput-ClkK-yNd.js";import"./locale-BC9z6YbT.js";import"./density-CFGlRMM_.js";import"./proxiedModel-slW9zgh3.js";import"./loader-DENHXnzB.js";import"./anchor-C6LV5e1c.js";import"./rounded-YB6K8mOp.js";import"./VDefaultsProvider-4deG1Ppv.js";import"./forwardRefs-DWGaNmQL.js";import"./index-DN-QUi1L.js";import"./useTranslations-DV-taHAL.js";import"./FSDialogMenu-cbMj6UFV.js";import"./VDialog-BV_tmW5d.js";import"./VOverlay-QKFvvy4k.js";import"./dimensions-Dnxg3zhh.js";import"./display-B8HpKdBf.js";import"./lazy-Cw6rrZWC.js";import"./router-DFGAHh1d.js";import"./FSRadioGroup-VRhkvpZ9.js";import"./FSRadio-DjVuf49p.js";import"./VSelectionControl-Btb9GcG5.js";import"./index-9jSorrJj.js";import"./FSToggleSet-Br9w1v1b.js";import"./FSSlideGroup-CgIUEdhn.js";import"./uuid-DTaye2KM.js";import"./VSlideGroup-DIpxwRsS.js";import"./group-szJg_hPo.js";import"./FSWrapGroup-OTVy7t6y.js";import"./FSCheckbox-DMyIFYVY.js";import"./FSFadeOut-_RgLKyTO.js";import"./FSLoader-CbNdmyFm.js";import"./elevation-BTkUCD39.js";import"./VSelect-C4zoc8wo.js";import"./VList-BzWIdAYG.js";import"./ssrBoot-D90vJCps.js";import"./border-Be_f35HL.js";import"./VImg-DgHq50DD.js";import"./VMenu-Cn6sY4-s.js";import"./filter-C5_KLKIH.js";import"./FSChip-C9Ie1k_N.js";import"./permissionInfos-BPDgTHQl.js";import"./base-CMiH1YbJ.js";import"./useAppOrganisationId-CkwgOwhD.js";import"./serviceFactory-Bd5jifNi.js";import"./lodash-BiW_TGGX.js";import"./composableFactory-J8cSLWf9.js";import"./useAutocomplete-1Tpu4ol_.js";const jo={title:"Foundation/Core/Autocompletes/AutocompleteRole",component:p,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},o={args:{args:{value1:null,type1:0,value2:"11",type2:2}},render:(m,{argTypes:i})=>({components:{FSAutocompleteRole:p,FSCol:l},props:Object.keys(i),setup(){return{...m}},template:`
+    <FSCol>
+      <FSAutocompleteRole
+        label="Role"
+        v-model="args.value1"
+        v-model:type="args.type1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteRole
+        label="Role with toggleset disabled"
+        :toggleSetDisabled="true"
+        v-model="args.value2"
+        v-model:type="args.type2"
+      />
+    </FSCol>`})};var t,r,e;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      type1: 0,
+      value2: "11",
+      type2: 2
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSAutocompleteRole,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSAutocompleteRole
+        label="Role"
+        v-model="args.value1"
+        v-model:type="args.type1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteRole
+        label="Role with toggleset disabled"
+        :toggleSetDisabled="true"
+        v-model="args.value2"
+        v-model:type="args.type2"
+      />
+    </FSCol>\`
+  })
+}`,...(e=(r=o.parameters)==null?void 0:r.docs)==null?void 0:e.source}}};const Do=["Variations"];export{o as Variations,Do as __namedExportsOrder,jo as default};
--- assets/Divider.stories-CwBG62Da.js
+++
@@ -1,5 +0,0 @@
-import{F as p}from"./FSDivider-BllDBUrY.js";import"./vue.esm-bundler-DC82FEWN.js";import"./FSText-CHX8O1Aj.js";import"./useColors-DEq6MuQy.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-3zl-tJp_.js";import"./theme-B5x8xkbY.js";import"./useSlots-Du4GeqS9.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./css-DYOPUjjE.js";import"./FSCol-B7HQy3FB.js";import"./FSRow-DoCXWKDP.js";const W={title:"Foundation/Shared/Divider",component:p,tags:["autodocs"]},r={},t={args:{label:"With Text"}};var e,o,a;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:"{}",...(a=(o=r.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};var s,i,m;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    label: 'With Text'
-  }
-}`,...(m=(i=t.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};const F=["Default","WithText"];export{r as Default,t as WithText,F as __namedExportsOrder,W as default};
