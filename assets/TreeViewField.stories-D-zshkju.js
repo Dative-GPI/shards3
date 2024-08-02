c15313783481f619389ce9d7c5dcaf96466569b0
rduteil <rduteil@users.noreply.github.com>
2024-08-02 09:27:17 +0000

    deploy: f22b676408f33de3f017b51fc1c535a67f194632

---
+++ assets/TreeViewField.stories-D-zshkju.js
@@ -0,0 +1,229 @@
+import{F as o}from"./FSTreeViewField-CFliYJvK.js";import{F as u}from"./FSForm-COOci7Wc.js";import{_ as g}from"./FSIcon-uD_MRFdb.js";import{_ as p}from"./FSCol-B7HQy3FB.js";import{_ as v}from"./FSRow-DoCXWKDP.js";import{c as F}from"./rules-DlyQ2XjG.js";import"./vue.esm-bundler-DC82FEWN.js";import"./FSDialogMenu-cbMj6UFV.js";import"./FSCard-BzleLmPd.js";import"./useColors-DEq6MuQy.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-3zl-tJp_.js";import"./theme-B5x8xkbY.js";import"./css-DYOPUjjE.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./VDialog-BV_tmW5d.js";import"./VOverlay-QKFvvy4k.js";import"./forwardRefs-DWGaNmQL.js";import"./color-DvJT0EEa.js";import"./proxiedModel-slW9zgh3.js";import"./anchor-C6LV5e1c.js";import"./dimensions-Dnxg3zhh.js";import"./display-B8HpKdBf.js";import"./lazy-Cw6rrZWC.js";import"./locale-BC9z6YbT.js";import"./router-DFGAHh1d.js";import"./transition-yl3lVN6J.js";import"./VDefaultsProvider-4deG1Ppv.js";import"./FSTextField-mMS0Kez6.js";import"./FSBaseField-DmIgZozj.js";import"./FSSpan-CEjD71wr.js";import"./useSlots-Dr5tdD4b.js";import"./VSpacer-BjECYW1K.js";import"./FSButton-1EF78Bzf.js";import"./FSClickable-CNOWN-hR.js";import"./VProgressCircular-C-U4HXtE.js";import"./VIcon-Dc-dFojD.js";import"./useRules-DS9e5-1Q.js";import"./VField-CAe7-ioi.js";import"./index-CvDsTioR.js";import"./VLabel-D_bmQQv3.js";import"./VInput-ClkK-yNd.js";import"./density-CFGlRMM_.js";import"./loader-DENHXnzB.js";import"./rounded-YB6K8mOp.js";import"./index-DN-QUi1L.js";import"./FSCheckbox-DMyIFYVY.js";import"./VSelectionControl-Btb9GcG5.js";import"./index-9jSorrJj.js";import"./FSFadeOut-_RgLKyTO.js";import"./uuid-DTaye2KM.js";import"./FSLoader-CbNdmyFm.js";import"./elevation-BTkUCD39.js";import"./FSRadio-DjVuf49p.js";import"./VList-BzWIdAYG.js";import"./ssrBoot-D90vJCps.js";import"./border-Be_f35HL.js";import"./VImg-DgHq50DD.js";import"./VBtn-x8pr1gDk.js";import"./group-szJg_hPo.js";import"./filter-C5_KLKIH.js";import"./VMenu-Cn6sY4-s.js";import"./useAppTimeZone-SgthjzoP.js";import"./useTranslations-DV-taHAL.js";import"./time-xMV0WbLU.js";const fe={title:"Foundation/Shared/Input fields/TreeViewField",component:o,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},e={args:{args:{items:[{id:0,label:"Group 1",icon:"mdi-account"},{id:1,label:"Group 2"},{id:2,label:"Group 3"},{id:3,label:"Group 4",parentId:0},{id:4,label:"Group 5",parentId:0},{id:5,label:"Group 6",parentId:3}],value1:null,value2:["0","1","2"],value3:"2",value4:"2"}},render:(i,{argTypes:t})=>({components:{FSTreeViewField:o,FSCol:p,FSIcon:g},props:Object.keys(t),setup(){return{...i}},template:`
+    <FSCol>
+      <FSTreeViewField
+        label="Tree view"
+        :items="args.items"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewField
+        label="Multiple tree view"
+        :items="args.items"
+        :multiple="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewField
+        label="Required tree view, with description"
+        description="Description for this field"
+        :required="true"
+        :items="args.items"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewField
+        label="Uneditable tree view, with description"
+        description="Uneditable description"
+        :editable="false"
+        :items="args.items"
+        v-model="args.value4"
+      />
+    </FSCol>`})},r={args:{args:{valid:!1,items:[{id:0,label:"Group 1",icon:"mdi-account"},{id:1,label:"Group 2"},{id:2,label:"Group 3"},{id:3,label:"Group 4",parentId:0},{id:4,label:"Group 5",parentId:0},{id:5,label:"Group 6",parentId:3}],value1:null,value2:null,value3:null,rules:F}},render:(i,{argTypes:t})=>({components:{FSForm:u,FSTreeViewField:o,FSCol:p,FSRow:v},props:Object.keys(t),setup(){return{...i}},template:`
+    <FSForm v-model="args.valid" variant="standard">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTreeViewField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :items="args.items"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTreeViewField
+          label="Rules: min 2 options"
+          :rules="[args.rules.min(2)]"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTreeViewField
+          label="Rules: required & min 2 options & max 3 options"
+          :rules="[args.rules.required(), args.rules.min(2), args.rules.max(3)]"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>`})};var l,n,a;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    args: {
+      items: [{
+        id: 0,
+        label: "Group 1",
+        icon: "mdi-account"
+      }, {
+        id: 1,
+        label: "Group 2"
+      }, {
+        id: 2,
+        label: "Group 3"
+      }, {
+        id: 3,
+        label: "Group 4",
+        parentId: 0
+      }, {
+        id: 4,
+        label: "Group 5",
+        parentId: 0
+      }, {
+        id: 5,
+        label: "Group 6",
+        parentId: 3
+      }],
+      value1: null,
+      value2: ["0", "1", "2"],
+      value3: "2",
+      value4: "2"
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSTreeViewField,
+      FSCol,
+      FSIcon
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSTreeViewField
+        label="Tree view"
+        :items="args.items"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewField
+        label="Multiple tree view"
+        :items="args.items"
+        :multiple="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewField
+        label="Required tree view, with description"
+        description="Description for this field"
+        :required="true"
+        :items="args.items"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewField
+        label="Uneditable tree view, with description"
+        description="Uneditable description"
+        :editable="false"
+        :items="args.items"
+        v-model="args.value4"
+      />
+    </FSCol>\`
+  })
+}`,...(a=(n=e.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};var s,d,m;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    args: {
+      valid: false,
+      items: [{
+        id: 0,
+        label: "Group 1",
+        icon: "mdi-account"
+      }, {
+        id: 1,
+        label: "Group 2"
+      }, {
+        id: 2,
+        label: "Group 3"
+      }, {
+        id: 3,
+        label: "Group 4",
+        parentId: 0
+      }, {
+        id: 4,
+        label: "Group 5",
+        parentId: 0
+      }, {
+        id: 5,
+        label: "Group 6",
+        parentId: 3
+      }],
+      value1: null,
+      value2: null,
+      value3: null,
+      rules: TreeViewRules
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSForm,
+      FSTreeViewField,
+      FSCol,
+      FSRow
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSForm v-model="args.valid" variant="standard">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTreeViewField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :items="args.items"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTreeViewField
+          label="Rules: min 2 options"
+          :rules="[args.rules.min(2)]"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTreeViewField
+          label="Rules: required & min 2 options & max 3 options"
+          :rules="[args.rules.required(), args.rules.min(2), args.rules.max(3)]"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>\`
+  })
+}`,...(m=(d=r.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};const qe=["Variations","Rules"];export{r as Rules,e as Variations,qe as __namedExportsOrder,fe as default};
