444e699b940da21048a4a4c5c4e6b90361dbc006
rduteil <rduteil@users.noreply.github.com>
2024-08-08 08:00:50 +0000

    deploy: 0388d6a2a7da2e22457eaf384f401bab9efc3c44

---
+++ assets/AutocompleteAddress.stories-DajCWqIx.js
@@ -0,0 +1,47 @@
+import{F as m}from"./FSAutoCompleteAddress-Dpl-T9le.js";import{_ as n}from"./FSCol-B7HQy3FB.js";import{A as a}from"./address-CE2z3AEI.js";import"./vue.esm-bundler-DC82FEWN.js";import"./lodash-BiW_TGGX.js";import"./_commonjsHelpers-BosuxZz1.js";import"./FSAutocompleteField-DCuY7KCa.js";import"./FSSearchField-DvIrbDgZ.js";import"./FSTextField-DawumXxY.js";import"./FSBaseField-DioNWcqU.js";import"./FSSpan-Bs4x_ELf.js";import"./useSlots-DN9F4SV8.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FSRow-Dx6kiy_m.js";import"./css-DYOPUjjE.js";import"./useColors-Hqqi3yfr.js";import"./index-3zl-tJp_.js";import"./theme-B5x8xkbY.js";import"./VSpacer-BjECYW1K.js";import"./color-DvJT0EEa.js";import"./FSButton-AO9DHmCC.js";import"./FSClickable-CAOw4IGX.js";import"./FSCard-D5f1qjEi.js";import"./VProgressCircular-C-U4HXtE.js";import"./VIcon-Dc-dFojD.js";import"./FSIcon-Cb1b9cDM.js";import"./useRules-DS9e5-1Q.js";import"./VField-CAe7-ioi.js";import"./index-CvDsTioR.js";import"./transition-yl3lVN6J.js";import"./VLabel-D_bmQQv3.js";import"./VInput-ClkK-yNd.js";import"./locale-BC9z6YbT.js";import"./density-CFGlRMM_.js";import"./proxiedModel-slW9zgh3.js";import"./loader-DENHXnzB.js";import"./anchor-C6LV5e1c.js";import"./rounded-YB6K8mOp.js";import"./VDefaultsProvider-4deG1Ppv.js";import"./forwardRefs-DWGaNmQL.js";import"./index-DN-QUi1L.js";import"./useTranslations-DV-taHAL.js";import"./FSDialogMenu-Ckdw__ZT.js";import"./VDialog-BV_tmW5d.js";import"./VOverlay-QKFvvy4k.js";import"./dimensions-Dnxg3zhh.js";import"./display-B8HpKdBf.js";import"./lazy-Cw6rrZWC.js";import"./router-DFGAHh1d.js";import"./FSRadioGroup-DikQc_4B.js";import"./FSRadio-DMN3zhNx.js";import"./VSelectionControl-Btb9GcG5.js";import"./index-9jSorrJj.js";import"./FSToggleSet-DnteswWg.js";import"./FSSlideGroup-BGzEhbn8.js";import"./uuid-DTaye2KM.js";import"./VSlideGroup-DIpxwRsS.js";import"./group-szJg_hPo.js";import"./FSWrapGroup-Dt2TwvEo.js";import"./FSCheckbox-DMrFH6nU.js";import"./FSFadeOut-DsjIuoaA.js";import"./FSLoader-BCzmDe0l.js";import"./elevation-BTkUCD39.js";import"./VSelect-CTy2MKbl.js";import"./VList-BzWIdAYG.js";import"./ssrBoot-D90vJCps.js";import"./border-Be_f35HL.js";import"./VImg-DgHq50DD.js";import"./VMenu-Cn6sY4-s.js";import"./filter-C5_KLKIH.js";import"./useAutocomplete-cdjbiJw9.js";const Eo={title:"Foundation/Shared/Autocompletes/AutocompleteAddress",component:m,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},o={args:{args:{value1:null,value2:new a({placeId:"ChIJg7vu-43P9EcRRNrbyDxFMDY",placeLabel:"Dative, Rue de Norvège, Colombier-Saugnieu, France",formattedAddress:" 100 Rue de Norvège, 69125 Colombier-Saugnieu",locality:"Colombier-Saugnieu",country:"France",latitude:45.711741,longitude:5.071675})}},render:(p,{argTypes:i})=>({components:{FSAutocompleteAddress:m,FSCol:n},props:Object.keys(i),setup(){return{...p}},template:`
+    <FSCol>
+      <FSAutocompleteAddress
+        v-model="args.value1"
+      />
+      <FSAutocompleteAddress
+        v-model="args.value2"
+      />
+    </FSCol>`})};var r,t,e;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      value2: new Address({
+        placeId: "ChIJg7vu-43P9EcRRNrbyDxFMDY",
+        placeLabel: 'Dative, Rue de Norvège, Colombier-Saugnieu, France',
+        formattedAddress: ' 100 Rue de Norvège, 69125 Colombier-Saugnieu',
+        locality: 'Colombier-Saugnieu',
+        country: 'France',
+        latitude: 45.711741,
+        longitude: 5.071675
+      })
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSAutocompleteAddress,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSAutocompleteAddress
+        v-model="args.value1"
+      />
+      <FSAutocompleteAddress
+        v-model="args.value2"
+      />
+    </FSCol>\`
+  })
+}`,...(e=(t=o.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const _o=["Variations"];export{o as Variations,_o as __namedExportsOrder,Eo as default};
