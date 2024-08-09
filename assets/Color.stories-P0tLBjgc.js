5ee471e6cdd3a83a35214506d5b371395dd3f630
gballigand <gballigand@users.noreply.github.com>
2024-08-09 07:18:01 +0000

    deploy: 55e4efa9609e40f54d7250ecab7e56b7adcfe17a

---
+++ assets/Color.stories-P0tLBjgc.js
@@ -0,0 +1,29 @@
+import{F as i}from"./FSColor-cGSmtxb9.js";import{_ as a}from"./FSSpan-Bs4x_ELf.js";import{_ as t}from"./FSRow-Dx6kiy_m.js";import{V as e}from"./VDivider-Cb-sOTjB.js";import"./vue.esm-bundler-DC82FEWN.js";import"./FSCard-D5f1qjEi.js";import"./FSCol-B7HQy3FB.js";import"./css-DYOPUjjE.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./useColors-Hqqi3yfr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-3zl-tJp_.js";import"./theme-B5x8xkbY.js";import"./useSlots-DN9F4SV8.js";import"./theme-ZEf9-fLI.js";const w={title:"Foundation/Shared/Color",component:i,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},o={render:()=>({components:{VDivider:e,FSColor:i,FSSpan:a,FSRow:t},template:`
+    <div style="display: flex; flex-direction: column; gap: 10px;">
+        <div style="display: flex; gap: 10px;">
+            <FSColor color="primary">
+                <FSRow padding="8px 16px">
+                    <FSSpan> Primary color </FSSpan>
+                </FSRow>
+            </FSColor>
+        </div>
+    </div>`})};var r,p,n;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      VDivider,
+      FSColor,
+      FSSpan,
+      FSRow
+    },
+    template: \`
+    <div style="display: flex; flex-direction: column; gap: 10px;">
+        <div style="display: flex; gap: 10px;">
+            <FSColor color="primary">
+                <FSRow padding="8px 16px">
+                    <FSSpan> Primary color </FSSpan>
+                </FSRow>
+            </FSColor>
+        </div>
+    </div>\`
+  })
+}`,...(n=(p=o.parameters)==null?void 0:p.docs)==null?void 0:n.source}}};const R=["Variations"];export{o as Variations,R as __namedExportsOrder,w as default};
---
+++ assets/Tests.stories-9D5OCqUc.js
@@ -0,0 +1,58 @@
+import e from"./FSIconField-qxOxwV4z.js";import{F as a}from"./FSSlideGroup-BGzEhbn8.js";import{F}from"./FSFadeOut-DsjIuoaA.js";import{F as s}from"./FSButton-AO9DHmCC.js";import{F as d}from"./FSWindow-C9BMKErq.js";import{F as S}from"./FSText-BiNyCChn.js";import{F as p,a as l}from"./FSTab-m4saEiAm.js";import{_ as c}from"./FSCol-B7HQy3FB.js";import"./vue.esm-bundler-DC82FEWN.js";import"./FSToggleSet-DnteswWg.js";import"./FSWrapGroup-Dt2TwvEo.js";import"./uuid-DTaye2KM.js";import"./useSlots-DN9F4SV8.js";import"./useColors-Hqqi3yfr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-3zl-tJp_.js";import"./theme-B5x8xkbY.js";import"./css-DYOPUjjE.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./VSlideGroup-DIpxwRsS.js";import"./index-CvDsTioR.js";import"./color-DvJT0EEa.js";import"./display-B8HpKdBf.js";import"./group-szJg_hPo.js";import"./proxiedModel-slW9zgh3.js";import"./VIcon-Dc-dFojD.js";import"./locale-BC9z6YbT.js";import"./VProgressCircular-C-U4HXtE.js";import"./VInput-ClkK-yNd.js";import"./transition-yl3lVN6J.js";import"./density-CFGlRMM_.js";import"./FSTextField-DawumXxY.js";import"./FSBaseField-DioNWcqU.js";import"./FSSpan-Bs4x_ELf.js";import"./FSRow-Dx6kiy_m.js";import"./VSpacer-BjECYW1K.js";import"./useRules-DS9e5-1Q.js";import"./VField-CAe7-ioi.js";import"./VLabel-D_bmQQv3.js";import"./loader-DENHXnzB.js";import"./anchor-C6LV5e1c.js";import"./rounded-YB6K8mOp.js";import"./VDefaultsProvider-4deG1Ppv.js";import"./forwardRefs-DWGaNmQL.js";import"./index-DN-QUi1L.js";import"./FSIcon-Cb1b9cDM.js";import"./FSClickable-CAOw4IGX.js";import"./FSCard-D5f1qjEi.js";import"./VBtn-x8pr1gDk.js";import"./border-Be_f35HL.js";import"./elevation-BTkUCD39.js";import"./dimensions-Dnxg3zhh.js";import"./router-DFGAHh1d.js";import"./index-9jSorrJj.js";import"./lazy-Cw6rrZWC.js";import"./ssrBoot-D90vJCps.js";const go={title:"Foundation/Shared/Tests",component:p,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},o={args:{args:{tab1:0,tab2:0}},render:(n,{argTypes:m})=>({components:{FSTabs:p,FSTab:l,FSText:S,FSWindow:d,FSCol:c,FSFadeOut:F,FSSlideGroup:a,FSButton:s,FSIconField:e},props:Object.keys(m),setup(){return{...n}},template:`
+      <div style="display: flex; flex-direction: column; gap: 30px;">
+        <FSText> Two FSWindow </FSText>
+        <FSWindow width="500px" :modelValue="args.tab1">
+            <FSIconField />
+        </FSWindow>
+        <FSWindow width="500px" :modelValue="args.tab2">
+            <template v-if="true">
+              <FSCol>
+                  <FSIconField />
+              </FSCol>
+            </template>
+        </FSWindow>
+      </div>
+    `})};var t,r,i;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    args: {
+      tab1: 0,
+      tab2: 0
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSTabs,
+      FSTab,
+      FSText,
+      FSWindow,
+      FSCol,
+      FSFadeOut,
+      FSSlideGroup,
+      FSButton,
+      FSIconField
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+      <div style="display: flex; flex-direction: column; gap: 30px;">
+        <FSText> Two FSWindow </FSText>
+        <FSWindow width="500px" :modelValue="args.tab1">
+            <FSIconField />
+        </FSWindow>
+        <FSWindow width="500px" :modelValue="args.tab2">
+            <template v-if="true">
+              <FSCol>
+                  <FSIconField />
+              </FSCol>
+            </template>
+        </FSWindow>
+      </div>
+    \`
+  })
+}`,...(i=(r=o.parameters)==null?void 0:r.docs)==null?void 0:i.source}}};const To=["Variations"];export{o as Variations,To as __namedExportsOrder,go as default};
