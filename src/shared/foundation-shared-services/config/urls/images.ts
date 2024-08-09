0c58d794ea4230b65dae59236d3d5c38e8a819e1
gballigand <gballigand@dative-gpi.com>
2024-08-09 08:27:07 +0200

    remove extension url

--- src/shared/foundation-shared-services/config/urls/images.ts
+++ src/shared/foundation-shared-services/config/urls/images.ts
@@ -4,7 +4,6 @@ export const IMAGES_URL = () => `${GATEWAY_URL()}/images`;
 
 export const IMAGES_RAW_URL = () => `${IMAGES_URL()}/raw`;
 export const IMAGE_RAW_URL = (imageId: string) => `${IMAGES_RAW_URL()}/${encodeURIComponent(imageId)}`;
-export const IMAGE_RAW_EXTENSION_URL = (imageId: string, jwt: string) => `${IMAGE_RAW_URL(imageId)}?access_token=${jwt}`;
 
 export const IMAGES_THUMBNAIL_URL = () => `${IMAGES_URL()}/thumbnail`;
 export const IMAGE_THUMBNAIL_URL = (imageId: string) => `${IMAGES_THUMBNAIL_URL()}/${encodeURIComponent(imageId)}`;
