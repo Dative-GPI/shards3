fcb231c545327ca82a304ca7e8a128c89faf1de4
rduteil <rduteil@users.noreply.github.com>
2024-08-09 10:44:02 +0000

    deploy: 4e4d9e5617a4b09805ea90d39f38eec99c9263cc

---
+++ assets/MetaField.stories-DGDIP7Xa.js
@@ -0,0 +1,374 @@
+import{F as t}from"./FSMetaField-DX92y76h.js";import{_ as p}from"./FSCol-B7HQy3FB.js";import{P as e}from"./customPropertyEnums-jIhOaYfs.js";import"./vue.esm-bundler-DC82FEWN.js";import"./FSDateTimeField-YeigLe06.js";import"./FSDialogMenu-Ckdw__ZT.js";import"./FSCard-D5f1qjEi.js";import"./FSRow-Dx6kiy_m.js";import"./css-DYOPUjjE.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./useColors-Hqqi3yfr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-3zl-tJp_.js";import"./theme-B5x8xkbY.js";import"./VDialog-BV_tmW5d.js";import"./VOverlay-QKFvvy4k.js";import"./forwardRefs-DWGaNmQL.js";import"./color-DvJT0EEa.js";import"./proxiedModel-slW9zgh3.js";import"./anchor-C6LV5e1c.js";import"./dimensions-Dnxg3zhh.js";import"./display-B8HpKdBf.js";import"./lazy-Cw6rrZWC.js";import"./locale-BC9z6YbT.js";import"./router-DFGAHh1d.js";import"./transition-yl3lVN6J.js";import"./VDefaultsProvider-4deG1Ppv.js";import"./FSTextField-DawumXxY.js";import"./FSBaseField-DioNWcqU.js";import"./FSSpan-Bs4x_ELf.js";import"./useSlots-DN9F4SV8.js";import"./VSpacer-BjECYW1K.js";import"./FSButton-AO9DHmCC.js";import"./FSClickable-CAOw4IGX.js";import"./VProgressCircular-C-U4HXtE.js";import"./VIcon-Dc-dFojD.js";import"./FSIcon-Cb1b9cDM.js";import"./useRules-DS9e5-1Q.js";import"./VField-CAe7-ioi.js";import"./index-CvDsTioR.js";import"./VLabel-D_bmQQv3.js";import"./VInput-ClkK-yNd.js";import"./density-CFGlRMM_.js";import"./loader-DENHXnzB.js";import"./rounded-YB6K8mOp.js";import"./index-DN-QUi1L.js";import"./FSCalendar-ZySsGIwo.js";import"./useAppTimeZone-BkX2SDgh.js";import"./useTranslations-DV-taHAL.js";import"./VLocaleProvider-D38ocs10.js";import"./VBtn-x8pr1gDk.js";import"./border-Be_f35HL.js";import"./elevation-BTkUCD39.js";import"./group-szJg_hPo.js";import"./index-9jSorrJj.js";import"./FSWindow-C9BMKErq.js";import"./ssrBoot-D90vJCps.js";import"./FSClock-BiKxcfo7.js";import"./FSSlider-BKTvn4n5.js";import"./VSlider-bQjOytro.js";import"./VMenu-Cn6sY4-s.js";import"./FSNumberField-Da3YH7o_.js";import"./FSSelectField-B1RRN3Tx.js";import"./FSRadioGroup-DikQc_4B.js";import"./FSRadio-DMN3zhNx.js";import"./VSelectionControl-Btb9GcG5.js";import"./FSCheckbox-DMrFH6nU.js";import"./FSFadeOut-DsjIuoaA.js";import"./uuid-DTaye2KM.js";import"./VList-BzWIdAYG.js";import"./VImg-DgHq50DD.js";import"./VSelect-CTy2MKbl.js";import"./VSlideGroup-DIpxwRsS.js";import"./FSIconField-qxOxwV4z.js";import"./FSToggleSet-DnteswWg.js";import"./FSSlideGroup-BGzEhbn8.js";import"./FSWrapGroup-Dt2TwvEo.js";import"./FSSwitch-BKvVK5Tu.js";import"./FSText-BiNyCChn.js";const _e={title:"Foundation/Core/CustomProperties/MetaField",component:t,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},l={args:{args:{value1:null,string:{id:"1",label:"String",categoryLabel:"Category",index:0,entity:1,dataType:e.String,addInInfos:!0,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!1,allowedValues:{}},value2:null,number:{id:"2",label:"Number",categoryLabel:"Category",index:0,entity:1,dataType:e.Number,addInInfos:!0,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!1,allowedValues:{}},value3:null,bool:{id:"3",label:"Boolean",categoryLabel:"Category",index:0,entity:1,dataType:e.Boolean,addInInfos:!0,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!1,allowedValues:{}},value4:null,date:{id:"4",label:"Date",categoryLabel:"Category",index:0,entity:1,dataType:e.DateTime,addInInfos:!0,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!1,allowedValues:{}},value5:null,icon:{id:"5",label:"Icon",categoryLabel:"Category",index:0,entity:1,dataType:e.Icon,addInInfos:!0,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!1,allowedValues:{}}}},render:(o,{argTypes:n})=>({components:{FSMetaField:t,FSCol:p},props:Object.keys(n),setup(){return{...o}},template:`
+    <FSCol>
+      <FSMetaField
+        label="String"
+        :customProperty="args.string"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMetaField
+        label="Number"
+        :customProperty="args.number"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMetaField
+        label="Bool"
+        :customProperty="args.bool"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMetaField
+        label="Date"
+        :customProperty="args.date"
+        v-model="args.value4"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMetaField
+        label="Icon"
+        :customProperty="args.icon"
+        v-model="args.value5"
+      />
+    </FSCol>`})},a={args:{args:{value1:null,string:{id:"1",label:"String",categoryLabel:"Category",index:0,entity:1,dataType:e.String,addInInfos:!0,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!0,allowedValues:{Alpha:"",Beta:"","third.choice":"Charly (tr)"}},value2:null,number:{id:"2",label:"Number",categoryLabel:"Category",index:0,entity:1,dataType:e.Number,addInInfos:!0,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!0,allowedValues:{0:"Zero (tr)",100:"",200:""}},value3:null,bool:{id:"3",label:"Boolean",categoryLabel:"Category",index:0,entity:1,dataType:e.Boolean,addInInfos:!0,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!0,allowedValues:{true:"",false:"False (tr)"}},value4:null,date:{id:"4",label:"Date",categoryLabel:"Category",index:0,entity:1,dataType:e.DateTime,addInInfos:!0,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!0,allowedValues:{0:"",36e5:"First hour","1704067200000":"1st of January 2024"}},value5:null,icon:{id:"5",label:"Icon",categoryLabel:"Category",index:0,entity:1,dataType:e.Icon,addInInfos:!0,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!0,allowedValues:{"ab-testing":"No icon (tr)","mdi-account-outline":"","mdi-check-circle-outline":""}}}},render:(o,{argTypes:n})=>({components:{FSMetaField:t,FSCol:p},props:Object.keys(n),setup(){return{...o}},template:`
+    <FSCol>
+      <FSMetaField
+        label="String"
+        :customProperty="args.string"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMetaField
+        label="Number"
+        :customProperty="args.number"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMetaField
+        label="Bool"
+        :customProperty="args.bool"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMetaField
+        label="Date"
+        :customProperty="args.date"
+        v-model="args.value4"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMetaField
+        label="Icon"
+        :customProperty="args.icon"
+        v-model="args.value5"
+      />
+    </FSCol>`})};var r,d,s;l.parameters={...l.parameters,docs:{...(r=l.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      string: {
+        id: "1",
+        label: "String",
+        categoryLabel: "Category",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.String,
+        addInInfos: true,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: false,
+        allowedValues: {}
+      },
+      value2: null,
+      number: {
+        id: "2",
+        label: "Number",
+        categoryLabel: "Category",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.Number,
+        addInInfos: true,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: false,
+        allowedValues: {}
+      },
+      value3: null,
+      bool: {
+        id: "3",
+        label: "Boolean",
+        categoryLabel: "Category",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.Boolean,
+        addInInfos: true,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: false,
+        allowedValues: {}
+      },
+      value4: null,
+      date: {
+        id: "4",
+        label: "Date",
+        categoryLabel: "Category",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.DateTime,
+        addInInfos: true,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: false,
+        allowedValues: {}
+      },
+      value5: null,
+      icon: {
+        id: "5",
+        label: "Icon",
+        categoryLabel: "Category",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.Icon,
+        addInInfos: true,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: false,
+        allowedValues: {}
+      }
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSMetaField,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSMetaField
+        label="String"
+        :customProperty="args.string"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMetaField
+        label="Number"
+        :customProperty="args.number"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMetaField
+        label="Bool"
+        :customProperty="args.bool"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMetaField
+        label="Date"
+        :customProperty="args.date"
+        v-model="args.value4"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMetaField
+        label="Icon"
+        :customProperty="args.icon"
+        v-model="args.value5"
+      />
+    </FSCol>\`
+  })
+}`,...(s=(d=l.parameters)==null?void 0:d.docs)==null?void 0:s.source}}};var i,u,y;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      string: {
+        id: "1",
+        label: "String",
+        categoryLabel: "Category",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.String,
+        addInInfos: true,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: true,
+        allowedValues: {
+          "Alpha": "",
+          "Beta": "",
+          "third.choice": "Charly (tr)"
+        }
+      },
+      value2: null,
+      number: {
+        id: "2",
+        label: "Number",
+        categoryLabel: "Category",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.Number,
+        addInInfos: true,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: true,
+        allowedValues: {
+          "0": "Zero (tr)",
+          "100": "",
+          "200": ""
+        }
+      },
+      value3: null,
+      bool: {
+        id: "3",
+        label: "Boolean",
+        categoryLabel: "Category",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.Boolean,
+        addInInfos: true,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: true,
+        allowedValues: {
+          "true": "",
+          "false": "False (tr)"
+        }
+      },
+      value4: null,
+      date: {
+        id: "4",
+        label: "Date",
+        categoryLabel: "Category",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.DateTime,
+        addInInfos: true,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: true,
+        allowedValues: {
+          "0": "",
+          "3600000": "First hour",
+          "1704067200000": "1st of January 2024"
+        }
+      },
+      value5: null,
+      icon: {
+        id: "5",
+        label: "Icon",
+        categoryLabel: "Category",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.Icon,
+        addInInfos: true,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: true,
+        allowedValues: {
+          "ab-testing": "No icon (tr)",
+          "mdi-account-outline": "",
+          "mdi-check-circle-outline": ""
+        }
+      }
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSMetaField,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSMetaField
+        label="String"
+        :customProperty="args.string"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMetaField
+        label="Number"
+        :customProperty="args.number"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMetaField
+        label="Bool"
+        :customProperty="args.bool"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMetaField
+        label="Date"
+        :customProperty="args.date"
+        v-model="args.value4"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSMetaField
+        label="Icon"
+        :customProperty="args.icon"
+        v-model="args.value5"
+      />
+    </FSCol>\`
+  })
+}`,...(y=(u=a.parameters)==null?void 0:u.docs)==null?void 0:y.source}}};const je=["Variations","UseOnlyAllowedValues"];export{a as UseOnlyAllowedValues,l as Variations,je as __namedExportsOrder,_e as default};
