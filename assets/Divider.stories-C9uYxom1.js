c5ceb5a80e257e1380002bc18597353b47e2b6a3
SchroterQuentin <SchroterQuentin@users.noreply.github.com>
2024-08-12 19:07:39 +0000

    deploy: 2918f30601d93f2672b869d257e7dbe1527c693c

---
+++ assets/Divider.stories-C9uYxom1.js
@@ -0,0 +1,5 @@
+import{F as p}from"./FSDivider-Blv6r7fO.js";import"./vue.esm-bundler-gWFTcvUr.js";import"./FSText-D8HncvP7.js";import"./useColors-vfWFlOSD.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-_Z9wfsFC.js";import"./theme-DA9ermhM.js";import"./useSlots-qbJuQIJ1.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./css-Bc8dJONt.js";import"./FSCol-DX423FHf.js";import"./FSRow-Bpa2jjpJ.js";const W={title:"Foundation/Shared/Divider",component:p,tags:["autodocs"]},r={},t={args:{label:"With Text"}};var e,o,a;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:"{}",...(a=(o=r.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};var s,i,m;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    label: 'With Text'
+  }
+}`,...(m=(i=t.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};const F=["Default","WithText"];export{r as Default,t as WithText,F as __namedExportsOrder,W as default};
---
+++ assets/TranslateField.stories-COIXrfKk.js
@@ -0,0 +1,154 @@
+import{F as a}from"./FSTranslateField-HDwTrTU0.js";import{F as u}from"./FSForm-3-6JxcXo.js";import{_ as p}from"./FSCol-DX423FHf.js";import{_ as g}from"./FSRow-Bpa2jjpJ.js";import{T as F}from"./rules-DNRn5gD7.js";import"./vue.esm-bundler-gWFTcvUr.js";import"./FSDialogSubmit-as5O4LuC.js";import"./FSFadeOut-D0BMvcHm.js";import"./uuid-DTaye2KM.js";import"./css-Bc8dJONt.js";import"./useColors-vfWFlOSD.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-_Z9wfsFC.js";import"./theme-DA9ermhM.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FSButton-CDk00Rln.js";import"./FSClickable-BVFP8bZP.js";import"./FSCard-Dkx-Z7Pt.js";import"./VProgressCircular-DqlO5PEF.js";import"./color-CI1OEbFE.js";import"./VIcon-BiBYr0XY.js";import"./FSSpan-BlywDO8Z.js";import"./useSlots-qbJuQIJ1.js";import"./FSIcon-C5ELG8c1.js";import"./FSDialog-oZKAE6lE.js";import"./FSText-D8HncvP7.js";import"./VSpacer-DqpPKsvm.js";import"./VDialog-Dt2j2rPJ.js";import"./VOverlay-BOOfoO8K.js";import"./forwardRefs-DWGaNmQL.js";import"./proxiedModel-ClxH85aG.js";import"./anchor-D8sPv6Az.js";import"./dimensions-CmIA1DmV.js";import"./display-DIiwMx3y.js";import"./lazy-Cvx00My1.js";import"./locale-C1ZNn_CS.js";import"./router-B2tkk-4w.js";import"./transition-14-CCvbq.js";import"./VDefaultsProvider-CYAySfh3.js";import"./useTranslations-DebMVrXy.js";import"./FSTextField-DZxUijBd.js";import"./FSBaseField-DM1DfhdE.js";import"./useRules-CMc02QlE.js";import"./VField-0o7ofOT0.js";import"./index-C2ELxjkU.js";import"./VLabel-B0q5I27e.js";import"./VInput-ClhP4L1h.js";import"./density-cGginCT8.js";import"./loader-DNzkAbrM.js";import"./rounded-DlJ8CYNz.js";import"./index-BHt-17iJ.js";import"./useAppLanguages-BkP4i7xm.js";import"./useAppTimeZone-BTXoL9eb.js";import"./time-B7rb1r3-.js";const vr={title:"Foundation/Shared/Input fields/TranslateField",component:a,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},r={args:{args:{value1:null,translations1:null,value2:"Hello there",translations2:[{languageCode:"fr-FR",label:"Bonjour ici"}],value3:"General Kenobi",translations3:[]}},render:(t,{argTypes:n})=>({components:{FSTranslateField:a,FSCol:p},props:Object.keys(n),setup(){return{...t}},template:`
+    <FSCol>
+      <FSTranslateField
+        label="Translation"
+        v-model="args.value1"
+        v-model:translations="args.translations1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTranslateField
+        label="Required translation, with description"
+        description="Description for this field"
+        :required="true"
+        v-model="args.value2"
+        v-model:translations="args.translations2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTranslateField
+        label="Uneditable translation, with description"
+        description="Uneditable description"
+        :editable="false"
+        v-model="args.value3"
+        v-model:translations="args.translations3"
+      />
+    </FSCol>`})},e={args:{args:{valid:!1,value1:"",translations1:null,value2:"",translations2:null,rules:F}},render:(t,{argTypes:n})=>({components:{FSForm:u,FSTranslateField:a,FSCol:p,FSRow:g},props:Object.keys(n),setup(){return{...t}},template:`
+    <FSForm v-model="args.valid">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTranslateField
+          label="Rules: required & min 10 characters"
+          :rules="[args.rules.required(), args.rules.min(10)]"
+          :required="true"
+          v-model="args.value1"
+          v-model="args.translations1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTranslateField
+          label="Rules: max 10 characters"
+          :rules="[args.rules.max(10)]"
+          :required="true"
+          v-model="args.value2"
+          v-model="args.translations2"
+        />
+      </FSCol>
+    </FSForm>`})};var o,l,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      translations1: null,
+      value2: "Hello there",
+      translations2: [{
+        languageCode: "fr-FR",
+        label: "Bonjour ici"
+      }],
+      value3: "General Kenobi",
+      translations3: []
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSTranslateField,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSTranslateField
+        label="Translation"
+        v-model="args.value1"
+        v-model:translations="args.translations1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTranslateField
+        label="Required translation, with description"
+        description="Description for this field"
+        :required="true"
+        v-model="args.value2"
+        v-model:translations="args.translations2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTranslateField
+        label="Uneditable translation, with description"
+        description="Uneditable description"
+        :editable="false"
+        v-model="args.value3"
+        v-model:translations="args.translations3"
+      />
+    </FSCol>\`
+  })
+}`,...(s=(l=r.parameters)==null?void 0:l.docs)==null?void 0:s.source}}};var i,d,m;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    args: {
+      valid: false,
+      value1: "",
+      translations1: null,
+      value2: "",
+      translations2: null,
+      rules: TextRules
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSForm,
+      FSTranslateField,
+      FSCol,
+      FSRow
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSForm v-model="args.valid">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTranslateField
+          label="Rules: required & min 10 characters"
+          :rules="[args.rules.required(), args.rules.min(10)]"
+          :required="true"
+          v-model="args.value1"
+          v-model="args.translations1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTranslateField
+          label="Rules: max 10 characters"
+          :rules="[args.rules.max(10)]"
+          :required="true"
+          v-model="args.value2"
+          v-model="args.translations2"
+        />
+      </FSCol>
+    </FSForm>\`
+  })
+}`,...(m=(d=e.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};const cr=["Variations","Rules"];export{e as Rules,r as Variations,cr as __namedExportsOrder,vr as default};
