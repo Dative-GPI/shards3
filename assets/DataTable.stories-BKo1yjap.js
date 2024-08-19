66b4a3e949758837a52098b8005571e15d375a65
rduteil <rduteil@users.noreply.github.com>
2024-08-19 07:50:49 +0000

    deploy: b2f55f431e4b7ef3b30753b147f06273cd7d98af

---
+++ assets/DataTable.stories-BKo1yjap.js
@@ -0,0 +1,156 @@
+var N=Object.defineProperty;var q=(t,e,r)=>e in t?N(t,e,{enumerable:!0,configurable:!0,writable:!0,value:r}):t[e]=r;var n=(t,e,r)=>q(t,typeof e!="symbol"?e+"":e,r);import{G as A}from"./userOrganisations.mock-DFErGecc.js";import{b as S,d as I,c as B,y,z as v,A as T,B as c,O as E,L as s,V as z,w as h,M as K,N as j,m as J,I as M,J as H,K as Q}from"./vue.esm-bundler-gWFTcvUr.js";import{F as W,V as X}from"./FSLoader-B2Qh9Z5u.js";import{_ as R}from"./FSCol-DX423FHf.js";import{_ as b}from"./FSRow-Bpa2jjpJ.js";import{u as Y,C as Z}from"./useColors-4hVwr1YC.js";import{_ as P}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{V as ee}from"./VSpacer-DqpPKsvm.js";import{F as te}from"./FSDataTableUI-C75ZDVky.js";import{C as ae}from"./base-DV1z3red.js";import{S as oe}from"./serviceFactory-Bd5jifNi.js";import{C as k}from"./composableFactory-CZGZ62iP.js";import{u as re}from"./FSFadeOut-D80WCGTA.js";import{F as ne}from"./FSGroupTileUI-DzrTTD-u.js";import{F as se}from"./FSSlideGroup-Cl8iPAls.js";import{F as le}from"./FSTagGroup-kPMbqxMJ.js";import{F as ie}from"./FSButton-C0cjY78b.js";import"./css-Bc8dJONt.js";import"./color-CI1OEbFE.js";import"./theme-DA9ermhM.js";import"./dimensions-CmIA1DmV.js";import"./elevation-B3TY2UXi.js";import"./locale-C1ZNn_CS.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-3zl-tJp_.js";import"./vue-router-Dxfr3ZK9.js";import"./uuid-DTaye2KM.js";import"./FSCheckbox-Dh4I1x40.js";import"./FSIcon-C7-l9IT-.js";import"./VIcon-BiBYr0XY.js";import"./FSSpan-BlywDO8Z.js";import"./useSlots-qbJuQIJ1.js";import"./useRules-CMc02QlE.js";import"./VSelectionControl-CyZYkafj.js";import"./density-cGginCT8.js";import"./proxiedModel-ClxH85aG.js";import"./index-BTMLtsqI.js";import"./VLabel-B0q5I27e.js";import"./VInput-ClhP4L1h.js";import"./index-C2ELxjkU.js";import"./transition-14-CCvbq.js";import"./FSCard-B-jXuIJy.js";import"./FSText-CvFl9H1C.js";import"./FSSearchField-C6oupvwj.js";import"./FSTextField-DmPsFJH8.js";import"./FSBaseField-C6NDZpLW.js";import"./VField-0o7ofOT0.js";import"./loader-DNzkAbrM.js";import"./VProgressCircular-DqlO5PEF.js";import"./anchor-D8sPv6Az.js";import"./rounded-DlJ8CYNz.js";import"./VDefaultsProvider-CYAySfh3.js";import"./forwardRefs-DWGaNmQL.js";import"./index-BHt-17iJ.js";import"./useTranslations-DebMVrXy.js";import"./FSSelectField-DK2yvB1s.js";import"./FSDialogMenu-C6wq00J3.js";import"./VDialog-Dt2j2rPJ.js";import"./VOverlay-BOOfoO8K.js";import"./display-DIiwMx3y.js";import"./lazy-Cvx00My1.js";import"./router-B2tkk-4w.js";import"./FSRadioGroup-BAFwCk5I.js";import"./FSRadio-CgLvIMAY.js";import"./VList-BToIo4RO.js";import"./ssrBoot-2G_E7J1h.js";import"./border-CDY4nf3P.js";import"./VImg-CHF0S8ll.js";import"./VSelect-BmHh8iYM.js";import"./VMenu-lXYF3tO5.js";import"./VSlideGroup-DIBYkq4d.js";import"./group-DEdtjQ6D.js";import"./FSDivider-D6wtKdEd.js";import"./FSChip-3bZvXIoL.js";import"./FSOptionGroup-C9yBI0Q8.js";import"./FSClickable-aki8YuqU.js";import"./FSWrapGroup-CRENaYi2.js";import"./FSToggleSet-BcrXmKwm.js";import"./VBtn-BQZMGoLN.js";import"./filter-DSQ_thm0.js";import"./useAppOrganisationId-0pTAMOfQ.js";import"./lodash-BiW_TGGX.js";import"./FSImage-D-21ag_I.js";import"./FSImageUI-D6xCYqus.js";import"./base-CmdGny12.js";import"./FSColor-CjNHQ_xo.js";import"./FSTile-DX3zuiQ2.js";import"./FSTag-D3MGXIrT.js";class me{constructor(){n(this,"called",!1);n(this,"result",null)}call(e){return this.called?this.result:(this.called=!0,this.result=e(),this.result)}}const f=S({}),ue=new me,pe=()=>ue.call(()=>({getTable:r=>f.value[r]!=null?f.value[r]:null,setTable:(r,m)=>{f.value[r]={...m}}}));class de{constructor(e){n(this,"columnId");n(this,"text");n(this,"value");n(this,"sortable");n(this,"filterable");n(this,"index");n(this,"hidden");this.columnId=e.columnId,this.text=e.text,this.value=e.value,this.sortable=e.sortable,this.filterable=e.filterable,this.index=e.index,this.hidden=e.hidden}}class ce{constructor(e){n(this,"id");n(this,"code");n(this,"columns");n(this,"mode");n(this,"rowsPerPage");n(this,"sortByKey");n(this,"sortByOrder");var r;this.id=e.id,this.code=e.code,this.mode=e.mode,this.rowsPerPage=e.rowsPerPage,this.sortByKey=e.sortByKey,this.sortByOrder=e.sortByOrder,this.columns=((r=e.columns)==null?void 0:r.map(m=>new de(m)))??[]}}class ge extends ce{constructor(e){super(e)}}const be=()=>`${ae()}/user-organisation-tables`,w=t=>`${be()}/${encodeURIComponent(t)}`,fe=()=>{const t=S(!1),e=S({headers:[],mode:"table",sortBy:null,rowsPerPage:10,filters:{},page:1});return{initialized:t,table:e,onTableCodeChange:async(u,a,l)=>{if(l){const o=a(l);if(o)e.value=o;else try{const i=await u(l);i.value&&(e.value={headers:i.value.columns,sortBy:{key:i.value.sortByKey,order:i.value.sortByOrder},mode:i.value.mode,rowsPerPage:i.value.rowsPerPage,filters:{},page:1})}catch{}}t.value=!0},computeTable:(u,a)=>({...e.value,headers:e.value.headers.map(l=>({...l,sort:l.value&&u[l.value]||null,sortRaw:l.value&&a[l.value]||null}))}),updateTable:(u,a,l)=>{var o,i;e.value&&(a(l,e.value),u(l,{columns:e.value.headers.map(p=>({columnId:p.columnId,hidden:p.hidden,index:p.index})),rowsPerPage:e.value.rowsPerPage,sortByKey:((o=e.value.sortBy)==null?void 0:o.key)??null,sortByOrder:((i=e.value.sortBy)==null?void 0:i.order)??null,mode:e.value.mode}))}}},x=new oe("userOrgansiationTable",ge).create(t=>t.build(t.addGet(w),t.addUpdate(w),t.addNotify())),Se=k.get(x),ye=k.update(x),D=I({name:"FSLoadDataTable",components:{FSLoader:W,FSCol:R,FSRow:b},setup(){const{getColors:t}=Y(),e=t(Z.Background);return{style:B(()=>({"--fs-load-data-table-background-color":e.base}))}}});function ve(t,e,r,m,d,u){const a=y("FSLoader");return v(),T(R,{class:"fs-load-data-table",gap:"16px",style:E(t.style)},{default:c(()=>[s(b,{align:"bottom-center"},{default:c(()=>[s(a,{variant:"input"}),s(a,{variant:"button"}),s(ee),s(a,{variant:"button"}),s(a,{variant:"button"})]),_:1}),s(b,null,{default:c(()=>[s(a,{variant:"chip"}),s(a,{variant:"chip"}),s(a,{variant:"chip"})]),_:1}),s(X,{type:"table-row-divider@10"}),s(b,{align:"center-right"},{default:c(()=>[s(a,{height:"16px",width:"100px"}),s(a,{width:"120px",height:["40px","36px"]}),s(a,{variant:"button"}),s(a,{variant:"button"}),s(a,{variant:"button"})]),_:1})]),_:1},8,["style"])}const Te=P(D,[["render",ve]]);D.__docgenInfo={displayName:"FSLoadDataTable",exportName:"default",description:"",tags:{},sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/shared/foundation-shared-components/components/lists/FSLoadDataTable.vue"]};const _=I({name:"FSDataTable",components:{FSLoadDataTable:Te,FSDataTableUI:te},props:{tableCode:{type:String,required:!0},debounceTime:{type:Number,required:!1,default:1e3},customSorts:{type:Object,required:!1,default:()=>({})},customSortRaws:{type:Object,required:!1,default:()=>({})}},setup(t){const{get:e,getting:r}=Se(),{initialized:m,table:d,updateTable:u,computeTable:a,onTableCodeChange:l}=fe(),{update:o}=ye(),{getTable:i,setTable:p}=pe(),{debounce:L,cancel:$}=re(),V=B(()=>a(t.customSorts,t.customSortRaws));z(()=>{$(),F()});const F=()=>{u(o,p,t.tableCode)};return h(()=>t.tableCode,async()=>{l(e,i,t.tableCode)},{immediate:!0}),h(()=>d.value,()=>{d.value&&m.value&&L(F,t.debounceTime)},{deep:!0}),{gettingUserOrganisationTable:r,computedTable:V,initialized:m,table:d}}});function Fe(t,e,r,m,d,u){const a=y("FSLoadDataTable"),l=y("FSDataTableUI");return!t.initialized||t.gettingUserOrganisationTable?(v(),T(a,{key:0})):(v(),T(l,J({key:1,"onUpdate:rowsPerPage":e[0]||(e[0]=o=>t.table.rowsPerPage=o),"onUpdate:filters":e[1]||(e[1]=o=>t.table.filters=o),"onUpdate:headers":e[2]||(e[2]=o=>t.table.headers=o),"onUpdate:sortBy":e[3]||(e[3]=o=>t.table.sortBy=o),"onUpdate:mode":e[4]||(e[4]=o=>t.table.mode=o),"onUpdate:page":e[5]||(e[5]=o=>t.table.page=o)},{...t.computedTable,...t.$attrs}),K({_:2},[j(t.$slots,(o,i)=>({name:i,fn:c(p=>[M(t.$slots,i,H(Q(p)))])}))]),1040))}const G=P(_,[["render",Fe]]);_.__docgenInfo={displayName:"FSDataTable",exportName:"default",description:"",tags:{},props:[{name:"tableCode",type:{name:"string"},required:!0},{name:"debounceTime",type:{name:"number"},required:!1,defaultValue:{func:!1,value:"1000"}},{name:"customSorts",type:{name:"{ [key: string]: any }"},required:!1,defaultValue:{func:!0,value:"() => ({})"}},{name:"customSortRaws",type:{name:"{ [key: string]: any }"},required:!1,defaultValue:{func:!0,value:"() => ({})"}}],slots:[{name:"name",scoped:!0,bindings:[{name:"name",title:"binding"}]}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/core/foundation-core-components/components/lists/FSDataTable.vue"]};const oa={title:"Foundation/Core/Lists/DataTable",component:G,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},g={args:{args:{tableCode:"groups1",items:A,value:[],itemTo:t=>({name:"group",params:{groupId:t.id}}),clickRow:()=>{console.log("clicked")},customSorts:{tags:(t,e)=>{if(t==null&&e==null||t.length==e.length)return 0;if(t==null||t.length<e.length)return-1;if(e==null||t.length>e.length)return 1}},customSortRaws:{none:(t,e)=>{if((t==null?void 0:t.id)==null&&(e==null?void 0:e.id)==null)return 0;if((t==null?void 0:t.id)==null||parseFloat(t.id)<parseFloat(e.id))return-1;if((e==null?void 0:e.id)==null||parseFloat(t.id)>parseFloat(e.id))return 1}}}},render:(t,{argTypes:e})=>({components:{FSDataTable:G,FSGroupTileUI:ne,FSSlideGroup:se,FSTagGroup:le,FSButton:ie},props:Object.keys(e),setup(){return{...t,getColor:m=>{if(m)return m.code==="number1"?"primary":"success"}}},template:`
+    <div style="display: flex; flex-direction: column; gap: 10px;">
+      <div style="display: flex; gap: 10px;">
+        <FSDataTable
+          :tableCode="args.tableCode"
+          :items="args.items"
+          :mode="args.mode"
+          :rowColor="getColor"
+          :showSelect="true"
+          :customSorts="args.customSorts"
+          :customSortRaws="args.customSortRaws"
+          rowGap="4px"
+          v-model="args.value"
+        >
+          <template #item.code="{ item }">
+            <FSButton
+              :label="item.code"
+            />
+          </template>
+          <template #item.tags="{ item }">
+            <FSTagGroup
+              variant="slide"
+              :editable="false"
+              :tags="item.tags"
+            />
+          </template>
+          <template #item.none="{ item }">
+            {{ item.id }}
+          </template>
+          <template #item.tile="{ item, toggleSelect }">
+            <FSGroupTileUI
+              variant="standard"
+              :color="getColor(item)"
+              :imageId="item.imageId"
+              :label="item.label"
+              :code="item.code"
+              :modelValue="args.value.includes(item.id)"
+              @update:modelValue="toggleSelect(item)"
+            />
+          </template>
+        </FSDataTable>
+      </div>
+    </div>`})};var C,U,O;g.parameters={...g.parameters,docs:{...(C=g.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    args: {
+      tableCode: "groups1",
+      items: GROUPS,
+      value: [],
+      itemTo: (item: any) => ({
+        name: 'group',
+        params: {
+          groupId: item.id
+        }
+      }),
+      clickRow: () => {
+        console.log("clicked");
+      },
+      customSorts: {
+        'tags': (a: any, b: any) => {
+          if (a == null && b == null || a.length == b.length) {
+            return 0;
+          }
+          if (a == null || a.length < b.length) {
+            return -1;
+          }
+          if (b == null || a.length > b.length) {
+            return 1;
+          }
+        }
+      },
+      customSortRaws: {
+        'none': (a: any, b: any) => {
+          if (a?.id == null && b?.id == null) {
+            return 0;
+          }
+          if (a?.id == null || parseFloat(a.id) < parseFloat(b.id)) {
+            return -1;
+          }
+          if (b?.id == null || parseFloat(a.id) > parseFloat(b.id)) {
+            return 1;
+          }
+        }
+      }
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSDataTable,
+      FSGroupTileUI,
+      FSSlideGroup,
+      FSTagGroup,
+      FSButton
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      const getColor = (row: any) => {
+        if (row) {
+          if (row.code === "number1") {
+            return "primary";
+          } else {
+            return "success";
+          }
+        }
+      };
+      return {
+        ...args,
+        getColor
+      };
+    },
+    template: \`
+    <div style="display: flex; flex-direction: column; gap: 10px;">
+      <div style="display: flex; gap: 10px;">
+        <FSDataTable
+          :tableCode="args.tableCode"
+          :items="args.items"
+          :mode="args.mode"
+          :rowColor="getColor"
+          :showSelect="true"
+          :customSorts="args.customSorts"
+          :customSortRaws="args.customSortRaws"
+          rowGap="4px"
+          v-model="args.value"
+        >
+          <template #item.code="{ item }">
+            <FSButton
+              :label="item.code"
+            />
+          </template>
+          <template #item.tags="{ item }">
+            <FSTagGroup
+              variant="slide"
+              :editable="false"
+              :tags="item.tags"
+            />
+          </template>
+          <template #item.none="{ item }">
+            {{ item.id }}
+          </template>
+          <template #item.tile="{ item, toggleSelect }">
+            <FSGroupTileUI
+              variant="standard"
+              :color="getColor(item)"
+              :imageId="item.imageId"
+              :label="item.label"
+              :code="item.code"
+              :modelValue="args.value.includes(item.id)"
+              @update:modelValue="toggleSelect(item)"
+            />
+          </template>
+        </FSDataTable>
+      </div>
+    </div>\`
+  })
+}`,...(O=(U=g.parameters)==null?void 0:U.docs)==null?void 0:O.source}}};const ra=["Variations"];export{g as Variations,ra as __namedExportsOrder,oa as default};
