016c3454d8aa2ef02c83660999d4fc53aa1fa8ea
SchroterQuentin <SchroterQuentin@users.noreply.github.com>
2024-08-20 09:59:33 +0000

    deploy: 31c7a149c6e4a3d7b7553c485c331e2a34a9b5c1

--- assets/MagicConfigField.stories-WNz5VUp-.js
+++ assets/MagicConfigField.stories-WNz5VUp-.js
@@ -1,11 +1,11 @@
-import{F as o}from"./FSMagicConfigField-CbHLeHFk.js";import{_ as r}from"./FSCol-DX423FHf.js";import"./vue.esm-bundler-gWFTcvUr.js";import"./useMagicFieldProvider-Dob9X8L9.js";import"./iframe-Ddct6BGM.js";import"../sb-preview/runtime.js";import"./FSButtonAddIcon-QVFsNV8a.js";import"./FSButton-C0cjY78b.js";import"./FSClickable-aki8YuqU.js";import"./FSCard-B-jXuIJy.js";import"./FSRow-Bpa2jjpJ.js";import"./css-Bc8dJONt.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./useColors-4hVwr1YC.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-3zl-tJp_.js";import"./theme-DA9ermhM.js";import"./VProgressCircular-DqlO5PEF.js";import"./color-CI1OEbFE.js";import"./VIcon-BiBYr0XY.js";import"./FSSpan-BlywDO8Z.js";import"./useSlots-qbJuQIJ1.js";import"./FSIcon-C7-l9IT-.js";import"./FSTranslateField-Dvu1iBo_.js";import"./FSDialogSubmit-248PSoce.js";import"./FSFadeOut-D80WCGTA.js";import"./uuid-DTaye2KM.js";import"./FSDialog-CsCBuWY3.js";import"./FSText-CvFl9H1C.js";import"./VSpacer-DqpPKsvm.js";import"./VDialog-Dt2j2rPJ.js";import"./VOverlay-BOOfoO8K.js";import"./forwardRefs-DWGaNmQL.js";import"./proxiedModel-ClxH85aG.js";import"./anchor-D8sPv6Az.js";import"./dimensions-CmIA1DmV.js";import"./display-DIiwMx3y.js";import"./lazy-Cvx00My1.js";import"./locale-C1ZNn_CS.js";import"./router-B2tkk-4w.js";import"./transition-14-CCvbq.js";import"./VDefaultsProvider-CYAySfh3.js";import"./useTranslations-DebMVrXy.js";import"./FSTextField-DmPsFJH8.js";import"./FSBaseField-C6NDZpLW.js";import"./useRules-CMc02QlE.js";import"./VField-0o7ofOT0.js";import"./index-C2ELxjkU.js";import"./VLabel-B0q5I27e.js";import"./VInput-ClhP4L1h.js";import"./density-cGginCT8.js";import"./loader-DNzkAbrM.js";import"./rounded-DlJ8CYNz.js";import"./index-BHt-17iJ.js";import"./useAppLanguages-DOMXQ19i.js";import"./rules-BH_IHeol.js";import"./useAppTimeZone-De3FTl5J.js";import"./useAppLanguageCode-CiYDnnfJ.js";import"./time-CXtw-DLM.js";const ul={title:"Foundation/Shared/Input fields/MagicConfigField",component:o,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},l={args:{args:{labelDefault1:"NumberField",translations1:[],value1:"",labelDefault2:"Switch",translations2:[],value2:"",labelDefault3:"TextField",translations3:[],value3:"",labelDefault4:"DateTimeField",translations4:[],value4:"",labelDefault5:"IconField",translations5:[],value5:"",labelDefault6:"TimeField",translations6:[],value6:"",labelDefault7:"TimeStepField",translations7:[],value7:""}},render:(i,{argTypes:n})=>({components:{FSMagicConfigField:o,FSCol:r},props:Object.keys(n),setup(){return{...i}},template:`
+import{F as o}from"./FSMagicConfigField-CbV1VeZu.js";import{_ as r}from"./FSCol-DX423FHf.js";import"./vue.esm-bundler-gWFTcvUr.js";import"./useMagicFieldProvider-CeiIoOmn.js";import"./iframe-lzVdSaM4.js";import"../sb-preview/runtime.js";import"./FSButtonAddIcon-QVFsNV8a.js";import"./FSButton-C0cjY78b.js";import"./FSClickable-aki8YuqU.js";import"./FSCard-B-jXuIJy.js";import"./FSRow-Bpa2jjpJ.js";import"./css-Bc8dJONt.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./useColors-4hVwr1YC.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-3zl-tJp_.js";import"./theme-DA9ermhM.js";import"./VProgressCircular-DqlO5PEF.js";import"./color-CI1OEbFE.js";import"./VIcon-BiBYr0XY.js";import"./FSSpan-BlywDO8Z.js";import"./useSlots-qbJuQIJ1.js";import"./FSIcon-C7-l9IT-.js";import"./FSTranslateField-Dvu1iBo_.js";import"./FSDialogSubmit-248PSoce.js";import"./FSFadeOut-D80WCGTA.js";import"./uuid-DTaye2KM.js";import"./FSDialog-CsCBuWY3.js";import"./FSText-CvFl9H1C.js";import"./VSpacer-DqpPKsvm.js";import"./VDialog-Dt2j2rPJ.js";import"./VOverlay-BOOfoO8K.js";import"./forwardRefs-DWGaNmQL.js";import"./proxiedModel-ClxH85aG.js";import"./anchor-D8sPv6Az.js";import"./dimensions-CmIA1DmV.js";import"./display-DIiwMx3y.js";import"./lazy-Cvx00My1.js";import"./locale-C1ZNn_CS.js";import"./router-B2tkk-4w.js";import"./transition-14-CCvbq.js";import"./VDefaultsProvider-CYAySfh3.js";import"./useTranslations-DebMVrXy.js";import"./FSTextField-DmPsFJH8.js";import"./FSBaseField-C6NDZpLW.js";import"./useRules-CMc02QlE.js";import"./VField-0o7ofOT0.js";import"./index-C2ELxjkU.js";import"./VLabel-B0q5I27e.js";import"./VInput-ClhP4L1h.js";import"./density-cGginCT8.js";import"./loader-DNzkAbrM.js";import"./rounded-DlJ8CYNz.js";import"./index-BHt-17iJ.js";import"./useAppLanguages-DOMXQ19i.js";import"./rules-BH_IHeol.js";import"./useAppTimeZone-De3FTl5J.js";import"./useAppLanguageCode-CiYDnnfJ.js";import"./time-CXtw-DLM.js";const bl={title:"Foundation/Shared/Input fields/MagicConfigField",component:o,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},l={args:{args:{labelDefault1:"NumberField",translations1:[],value1:"",labelDefault2:"Switch",translations2:[],value2:"",labelDefault3:"TextField",translations3:[],value3:"",labelDefault4:"DateTimeField",translations4:[],value4:"",labelDefault5:"IconField",translations5:[],value5:"",labelDefault6:"TimeField",translations6:[],value6:"",labelDefault7:"TimeStepField",translations7:[],value7:""}},render:(i,{argTypes:n})=>({components:{FSMagicConfigField:o,FSCol:r},props:Object.keys(n),setup(){return{...i}},template:`
     <FSCol>
       <FSMagicConfigField
         label="Number field"
         :type="1"
         v-model:translations="args.translations1"
         v-model:labelDefault="args.labelDefault1"
-        v-model:value="args.value1"
+        v-model="args.value1"
       />
       <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
       <FSMagicConfigField
@@ -13,7 +13,7 @@ import{F as o}from"./FSMagicConfigField-CbHLeHFk.js";import{_ as r}from"./FSCol-
         :type="2"
         v-model:translations="args.translations2"
         v-model:labelDefault="args.labelDefault2"
-        v-model:value="args.value2"
+        v-model="args.value2"
       />
       <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
       <FSMagicConfigField
@@ -21,7 +21,7 @@ import{F as o}from"./FSMagicConfigField-CbHLeHFk.js";import{_ as r}from"./FSCol-
         :type="3"
         v-model:translations="args.translations3"
         v-model:labelDefault="args.labelDefault3"
-        v-model:value="args.value3"
+        v-model="args.value3"
       />
       <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
       <FSMagicConfigField
@@ -29,7 +29,7 @@ import{F as o}from"./FSMagicConfigField-CbHLeHFk.js";import{_ as r}from"./FSCol-
         :type="4"
         v-model:translations="args.translations4"
         v-model:labelDefault="args.labelDefault4"
-        v-model:value="args.value4"
+        v-model="args.value4"
       />
       <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
       <FSMagicConfigField
@@ -37,7 +37,7 @@ import{F as o}from"./FSMagicConfigField-CbHLeHFk.js";import{_ as r}from"./FSCol-
         :type="5"
         v-model:translations="args.translations5"
         v-model:labelDefault="args.labelDefault5"
-        v-model:value="args.value5"
+        v-model="args.value5"
       />
       <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
       <FSMagicConfigField
@@ -45,7 +45,7 @@ import{F as o}from"./FSMagicConfigField-CbHLeHFk.js";import{_ as r}from"./FSCol-
         :type="6"
         v-model:translations="args.translations6"
         v-model:labelDefault="args.labelDefault6"
-        v-model:value="args.value6"
+        v-model="args.value6"
       />
       <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
       <FSMagicConfigField
@@ -53,7 +53,7 @@ import{F as o}from"./FSMagicConfigField-CbHLeHFk.js";import{_ as r}from"./FSCol-
         :type="7"
         v-model:translations="args.translations7"
         v-model:labelDefault="args.labelDefault7"
-        v-model:value="args.value7"
+        v-model="args.value7"
       />
     </FSCol>`})};var e,t,a;l.parameters={...l.parameters,docs:{...(e=l.parameters)==null?void 0:e.docs,source:{originalSource:`{
   args: {
@@ -101,7 +101,7 @@ import{F as o}from"./FSMagicConfigField-CbHLeHFk.js";import{_ as r}from"./FSCol-
         :type="1"
         v-model:translations="args.translations1"
         v-model:labelDefault="args.labelDefault1"
-        v-model:value="args.value1"
+        v-model="args.value1"
       />
       <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
       <FSMagicConfigField
@@ -109,7 +109,7 @@ import{F as o}from"./FSMagicConfigField-CbHLeHFk.js";import{_ as r}from"./FSCol-
         :type="2"
         v-model:translations="args.translations2"
         v-model:labelDefault="args.labelDefault2"
-        v-model:value="args.value2"
+        v-model="args.value2"
       />
       <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
       <FSMagicConfigField
@@ -117,7 +117,7 @@ import{F as o}from"./FSMagicConfigField-CbHLeHFk.js";import{_ as r}from"./FSCol-
         :type="3"
         v-model:translations="args.translations3"
         v-model:labelDefault="args.labelDefault3"
-        v-model:value="args.value3"
+        v-model="args.value3"
       />
       <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
       <FSMagicConfigField
@@ -125,7 +125,7 @@ import{F as o}from"./FSMagicConfigField-CbHLeHFk.js";import{_ as r}from"./FSCol-
         :type="4"
         v-model:translations="args.translations4"
         v-model:labelDefault="args.labelDefault4"
-        v-model:value="args.value4"
+        v-model="args.value4"
       />
       <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
       <FSMagicConfigField
@@ -133,7 +133,7 @@ import{F as o}from"./FSMagicConfigField-CbHLeHFk.js";import{_ as r}from"./FSCol-
         :type="5"
         v-model:translations="args.translations5"
         v-model:labelDefault="args.labelDefault5"
-        v-model:value="args.value5"
+        v-model="args.value5"
       />
       <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
       <FSMagicConfigField
@@ -141,7 +141,7 @@ import{F as o}from"./FSMagicConfigField-CbHLeHFk.js";import{_ as r}from"./FSCol-
         :type="6"
         v-model:translations="args.translations6"
         v-model:labelDefault="args.labelDefault6"
-        v-model:value="args.value6"
+        v-model="args.value6"
       />
       <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
       <FSMagicConfigField
@@ -149,8 +149,8 @@ import{F as o}from"./FSMagicConfigField-CbHLeHFk.js";import{_ as r}from"./FSCol-
         :type="7"
         v-model:translations="args.translations7"
         v-model:labelDefault="args.labelDefault7"
-        v-model:value="args.value7"
+        v-model="args.value7"
       />
     </FSCol>\`
   })
-}`,...(a=(t=l.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const vl=["Variations"];export{l as Variations,vl as __namedExportsOrder,ul as default};
+}`,...(a=(t=l.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const ul=["Variations"];export{l as Variations,ul as __namedExportsOrder,bl as default};
