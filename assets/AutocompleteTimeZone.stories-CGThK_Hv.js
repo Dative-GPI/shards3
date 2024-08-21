183fb84c3813722655e786a68d6d6b17d2a9e6e2
dimitrilexi <dimitrilexi@users.noreply.github.com>
2024-08-21 15:28:21 +0000

    deploy: a1e416bf81ace4ced6aec319e52755a348dc375c

---
+++ assets/AutocompleteTimeZone.stories-CGThK_Hv.js
@@ -0,0 +1,75 @@
+var k=Object.defineProperty;var $=(e,o,i)=>o in e?k(e,o,{enumerable:!0,configurable:!0,writable:!0,value:i}):e[o]=i;var d=(e,o,i)=>$(e,typeof o!="symbol"?o+"":o,i);import{d as D,c as q,y as b,z as F,A as v,B as l,m as E,L as a,E as y,G as Z,D as N,M as B,S as M}from"./vue.esm-bundler-gWFTcvUr.js";import{F as U}from"./FSAutocompleteField-CI6xmJnD.js";import{F as C}from"./FSButton-CDk00Rln.js";import{F as x}from"./FSChip-C4GLzprG.js";import{_ as c}from"./FSSpan-BlywDO8Z.js";import{_ as g}from"./FSRow-Bpa2jjpJ.js";import{G as O}from"./base-CmdGny12.js";import{S as G}from"./serviceFactory-Bd5jifNi.js";import{C as R}from"./composableFactory-CZGZ62iP.js";import{u as j}from"./useAutocomplete-DqlTmpvX.js";import{C as z}from"./useColors-vfWFlOSD.js";import{_ as I}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{_ as L}from"./FSCol-DX423FHf.js";import"./FSSearchField-xtHQygQf.js";import"./FSTextField-DZxUijBd.js";import"./FSBaseField-DM1DfhdE.js";import"./useSlots-qbJuQIJ1.js";import"./VSpacer-DqpPKsvm.js";import"./color-CI1OEbFE.js";import"./theme-DA9ermhM.js";import"./useRules-CMc02QlE.js";import"./VField-0o7ofOT0.js";import"./index-C2ELxjkU.js";import"./transition-14-CCvbq.js";import"./VLabel-B0q5I27e.js";import"./VInput-ClhP4L1h.js";import"./locale-C1ZNn_CS.js";import"./VIcon-BiBYr0XY.js";import"./density-cGginCT8.js";import"./proxiedModel-ClxH85aG.js";import"./loader-DNzkAbrM.js";import"./VProgressCircular-DqlO5PEF.js";import"./anchor-D8sPv6Az.js";import"./rounded-DlJ8CYNz.js";import"./VDefaultsProvider-CYAySfh3.js";import"./forwardRefs-DWGaNmQL.js";import"./index-BHt-17iJ.js";import"./useTranslations-DebMVrXy.js";import"./FSDialogMenu-5sq61y-r.js";import"./FSCard-Dkx-Z7Pt.js";import"./css-Bc8dJONt.js";import"./VDialog-Dt2j2rPJ.js";import"./VOverlay-BOOfoO8K.js";import"./dimensions-CmIA1DmV.js";import"./display-DIiwMx3y.js";import"./lazy-Cvx00My1.js";import"./router-B2tkk-4w.js";import"./FSRadioGroup-B2BuItpn.js";import"./FSRadio-0vVABvir.js";import"./FSIcon-C5ELG8c1.js";import"./VSelectionControl-CyZYkafj.js";import"./index-BTMLtsqI.js";import"./FSToggleSet-B78RpUCe.js";import"./FSSlideGroup-Bf0KNeOu.js";import"./uuid-DTaye2KM.js";import"./VSlideGroup-DIBYkq4d.js";import"./group-DEdtjQ6D.js";import"./FSWrapGroup-B3SUGyqE.js";import"./FSCheckbox-DbtWxwzz.js";import"./FSFadeOut-D0BMvcHm.js";import"./FSLoader-Dr6heSeB.js";import"./elevation-B3TY2UXi.js";import"./VSelect-CwYrSzi9.js";import"./VList-BToIo4RO.js";import"./ssrBoot-2G_E7J1h.js";import"./border-CDY4nf3P.js";import"./VImg-CHF0S8ll.js";import"./VMenu-lXYF3tO5.js";import"./filter-DSQ_thm0.js";import"./FSClickable-BVFP8bZP.js";import"./_commonjsHelpers-BosuxZz1.js";import"./lodash-BiW_TGGX.js";import"./index-_Z9wfsFC.js";class w{constructor(o){d(this,"id");d(this,"offset");this.id=o.id,this.offset=o.offset}}class P extends w{constructor(o){super(o)}}const W=()=>`${O()}/time-zones`,Y=new G("timeZone",P).create(e=>e.build(e.addGetMany(W,w),e.addNotify())),H=R.getMany(Y),A=D({name:"FSAutocompleteTimeZone",components:{FSAutocompleteField:U,FSButton:C,FSChip:x,FSSpan:c,FSRow:g},props:{timeZoneFilters:{type:Object,required:!1,default:null},modelValue:{type:[Array,String],required:!1,default:null},multiple:{type:Boolean,required:!1,default:!1},toggleSetDisabled:{type:Boolean,required:!1,default:!1}},emits:["update:modelValue"],setup(e,{emit:o}){const{getMany:i,fetching:f,entities:p}=H(),S=q(()=>t.value&&f.value),r=n=>i({...e.timeZoneFilters,search:n??void 0}),{toggleSet:u,init:t,onUpdate:m}=j(p,[()=>e.timeZoneFilters],o,r,null,n=>n.id,n=>n.id);return{ColorEnum:z,timeZones:p,toggleSet:u,loading:S,onUpdate:m}}});function J(e,o,i,f,p,S){const r=b("FSChip"),u=b("FSAutocompleteField");return F(),v(u,E({itemTitle:"id",toggleSet:!e.$props.toggleSetDisabled&&e.toggleSet,multiple:e.$props.multiple,loading:e.loading,items:e.timeZones,modelValue:e.$props.modelValue,"onUpdate:modelValue":e.onUpdate},e.$attrs),{"autocomplete-selection":l(({item:t})=>[e.$props.modelValue?(F(),v(g,{key:0,align:"center-center",wrap:!1},{default:l(()=>[a(r,{label:t.raw.offset},null,8,["label"]),a(c,null,{default:l(()=>[y(Z(t.raw.id),1)]),_:2},1024)]),_:2},1024)):N("",!0)]),"item-label":l(({item:t,font:m})=>[a(g,{align:"center-left",wrap:!1},{default:l(()=>[a(r,{label:t.raw.offset},null,8,["label"]),a(c,{font:m},{default:l(()=>[y(Z(t.raw.id),1)]),_:2},1032,["font"])]),_:2},1024)]),"toggle-set-item":l(t=>[a(C,{variant:t.getVariant(t.item),color:t.getColor(t.item),class:M(t.getClass(t.item)),label:t.item.id,onClick:m=>t.toggle(t.item)},B({_:2},[t.item.offset?{name:"prepend",fn:l(()=>[a(r,{label:t.item.offset.split(":")[0]},null,8,["label"])]),key:"0"}:void 0]),1032,["variant","color","class","label","onClick"])]),_:1},16,["toggleSet","multiple","loading","items","modelValue","onUpdate:modelValue"])}const V=I(A,[["render",J]]);A.__docgenInfo={displayName:"FSAutocompleteTimeZone",exportName:"default",description:"",tags:{},props:[{name:"timeZoneFilters",type:{name:"TimeZoneFilters"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"modelValue",type:{name:"string[] | string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"multiple",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}},{name:"toggleSetDisabled",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}}],events:[{name:"update:modelValue"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/shared/foundation-shared-components/components/autocompletes/FSAutocompleteTimeZone.vue"]};const bt={title:"Foundation/Shared/Autocompletes/AutocompleteTimeZone",component:V,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},s={args:{args:{value1:null,value2:"Europe/Paris",value3:null,value4:null}},render:(e,{argTypes:o})=>({components:{FSAutocompleteTimeZone:V,FSCol:L},props:Object.keys(o),setup(){return{...e}},template:`
+    <FSCol>
+      <FSAutocompleteTimeZone
+        label="TimeZone"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteTimeZone
+        label="TimeZone with toggleset disabled"
+        :toggleSetDisabled="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteTimeZone
+        label="TimeZone with multiple"
+        :multiple="true"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteTimeZone
+        label="TimeZone with multiple and toggleSet disabled"
+        :multiple="true"
+        :toggleSetDisabled="true"
+        v-model="args.value4"
+      />
+    </FSCol>`})};var T,h,_;s.parameters={...s.parameters,docs:{...(T=s.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      value2: "Europe/Paris",
+      value3: null,
+      value4: null
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSAutocompleteTimeZone,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSAutocompleteTimeZone
+        label="TimeZone"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteTimeZone
+        label="TimeZone with toggleset disabled"
+        :toggleSetDisabled="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteTimeZone
+        label="TimeZone with multiple"
+        :multiple="true"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteTimeZone
+        label="TimeZone with multiple and toggleSet disabled"
+        :multiple="true"
+        :toggleSetDisabled="true"
+        v-model="args.value4"
+      />
+    </FSCol>\`
+  })
+}`,...(_=(h=s.parameters)==null?void 0:h.docs)==null?void 0:_.source}}};const Ft=["Variations"];export{s as Variations,Ft as __namedExportsOrder,bt as default};
