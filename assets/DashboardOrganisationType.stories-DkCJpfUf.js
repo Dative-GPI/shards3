9a0725eafcb7a32098acbf599c696dfed26a2631
rduteil <rduteil@users.noreply.github.com>
2024-08-21 15:59:07 +0000

    deploy: 7586344462a31da3e8e17f45a5f0b46ab2290be9

---
+++ assets/DashboardOrganisationType.stories-DkCJpfUf.js
@@ -0,0 +1,40 @@
+import{d as y,c as I,y as s,z as d,A as p,m as f,o as F,w as V,D as C}from"./vue.esm-bundler-gWFTcvUr.js";import{F as $}from"./FSSimpleTileUI-C46NXiFz.js";import{C as l}from"./useColors-vfWFlOSD.js";import{_ as g}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{F as D}from"./FSLoadTile-DLMMk7vz.js";import{a as k}from"./useDashboardOrganisationTypes-DXpjTB0p.js";import"./FSIconCard-DWg_Dqw2.js";import"./FSCard-Dkx-Z7Pt.js";import"./FSCol-DX423FHf.js";import"./css-Bc8dJONt.js";import"./FSRow-Bpa2jjpJ.js";import"./FSIcon-C5ELG8c1.js";import"./VIcon-BiBYr0XY.js";import"./color-CI1OEbFE.js";import"./theme-DA9ermhM.js";import"./FSImage-SY9y6BiP.js";import"./FSImageUI-BWrRZMsZ.js";import"./base-CmdGny12.js";import"./serviceFactory-Bd5jifNi.js";import"./_commonjsHelpers-BosuxZz1.js";import"./lodash-BiW_TGGX.js";import"./composableFactory-CZGZ62iP.js";import"./FSLoader-Dr6heSeB.js";import"./dimensions-CmIA1DmV.js";import"./elevation-B3TY2UXi.js";import"./locale-C1ZNn_CS.js";import"./VImg-CHF0S8ll.js";import"./rounded-DlJ8CYNz.js";import"./transition-14-CCvbq.js";import"./index-BHt-17iJ.js";import"./FSSpan-BlywDO8Z.js";import"./useSlots-qbJuQIJ1.js";import"./FSTile-GmN7MMvw.js";import"./FSClickable-BVFP8bZP.js";import"./VProgressCircular-DqlO5PEF.js";import"./FSCheckbox-DbtWxwzz.js";import"./useRules-CMc02QlE.js";import"./VSelectionControl-CyZYkafj.js";import"./density-cGginCT8.js";import"./proxiedModel-ClxH85aG.js";import"./index-BTMLtsqI.js";import"./VLabel-B0q5I27e.js";import"./VInput-ClhP4L1h.js";import"./index-C2ELxjkU.js";import"./index-_Z9wfsFC.js";import"./dashboardTranslation-DKajfY1U.js";import"./useAppLanguageCode-CiYDnnfJ.js";import"./base-DV1z3red.js";import"./useAppOrganisationId-0pTAMOfQ.js";const b=y({name:"FSDashboardOrganisationTypeTileUI",components:{FSSimpleTileUI:$},props:{bottomColor:{type:[Array,String],required:!1,default:l.Primary},icon:{type:String,required:!1,default:"mdi-view-dashboard"}},setup(e){return{color:I(()=>Array.isArray(e.bottomColor)&&!e.bottomColor.length?l.Primary:e.bottomColor)}}});function w(e,o,r,t,O,v){const n=s("FSSimpleTileUI");return d(),p(n,f({bottomColor:e.color,icon:e.$props.icon},e.$attrs),null,16,["bottomColor","icon"])}const U=g(b,[["render",w]]);b.__docgenInfo={displayName:"FSDashboardOrganisationTypeTileUI",exportName:"default",description:"",tags:{},props:[{name:"bottomColor",type:{name:"ColorBase | ColorBase[]"},required:!1,defaultValue:{func:!1,value:"ColorEnum.Primary"}},{name:"icon",type:{name:"string"},required:!1,defaultValue:{func:!1,value:'"mdi-view-dashboard"'}}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/shared/foundation-shared-components/components/tiles/FSDashboardOrganisationTypeTileUI.vue"]};const T=y({name:"FSDashboardOrganisationTypeTile",components:{FSDashboardOrganisationTypeTileUI:U,FSLoadTile:D},props:{dashboardOrganisationTypeId:{type:String,required:!0},modelValue:{type:Boolean,required:!1,default:!1},editable:{type:Boolean,required:!1,default:!0}},setup(e){const{get:o,getting:r,entity:t}=k();return F(()=>{o(e.dashboardOrganisationTypeId)}),V(()=>e.dashboardOrganisationTypeId,()=>{o(e.dashboardOrganisationTypeId)}),{getting:r,entity:t}}});function q(e,o,r,t,O,v){const n=s("FSLoadTile"),S=s("FSDashboardOrganisationTypeTileUI");return e.getting?(d(),p(n,{key:0,editable:e.$props.editable,modelValue:e.$props.modelValue,"onUpdate:modelValue":o[0]||(o[0]=i=>e.$emit("update:modelValue",i))},null,8,["editable","modelValue"])):e.entity?(d(),p(S,f({key:1,code:e.entity.code,icon:e.entity.icon,label:e.entity.label,imageId:e.entity.imageId,editable:e.$props.editable,bottomColor:e.entity.colors,modelValue:e.$props.modelValue,"onUpdate:modelValue":o[1]||(o[1]=i=>e.$emit("update:modelValue",i))},e.$attrs),null,16,["code","icon","label","imageId","editable","bottomColor","modelValue"])):C("",!0)}const h=g(T,[["render",q]]);T.__docgenInfo={displayName:"FSDashboardOrganisationTypeTile",exportName:"default",description:"",tags:{},props:[{name:"dashboardOrganisationTypeId",type:{name:"string"},required:!0},{name:"modelValue",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}},{name:"editable",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"true"}}],events:[{name:"update:modelValue"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/core/foundation-core-components/components/tiles/FSDashboardOrganisationTypeTile.vue"]};const De={title:"Foundation/Core/Tiles/DashboardOrganisationType",component:h,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},a={args:{args:{values:["1","2","3"],selected:[!1,!1,!1]}},render:(e,{argTypes:o})=>({components:{FSDashboardOrganisationTypeTile:h},props:Object.keys(o),setup(){return{...e}},template:`
+    <div style="display: flex; gap: 10px; flex-wrap: wrap; width: 100vw;">
+      <FSDashboardOrganisationTypeTile
+        v-for="(dashboardOrganisationTypeId, index) in args.values"
+        :key="index"
+        :dashboardOrganisationTypeId="dashboardOrganisationTypeId"
+        @auxclick="args.values.pop()"
+        v-model="args.selected[index]"
+      />
+    </div>`})};var m,u,c;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    args: {
+      values: ["1", "2", "3"],
+      selected: [false, false, false]
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSDashboardOrganisationTypeTile
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <div style="display: flex; gap: 10px; flex-wrap: wrap; width: 100vw;">
+      <FSDashboardOrganisationTypeTile
+        v-for="(dashboardOrganisationTypeId, index) in args.values"
+        :key="index"
+        :dashboardOrganisationTypeId="dashboardOrganisationTypeId"
+        @auxclick="args.values.pop()"
+        v-model="args.selected[index]"
+      />
+    </div>\`
+  })
+}`,...(c=(u=a.parameters)==null?void 0:u.docs)==null?void 0:c.source}}};const ke=["Variations"];export{a as Variations,ke as __namedExportsOrder,De as default};
---
+++ assets/Tabs.stories-Bc6SBFDG.js
@@ -0,0 +1,342 @@
+import{F as r}from"./FSSlideGroup-Bf0KNeOu.js";import{F as d}from"./FSFadeOut-D0BMvcHm.js";import{F as b}from"./FSButton-CDk00Rln.js";import{F as u}from"./FSWindow-4mngEPBS.js";import{F}from"./FSText-D8HncvP7.js";import{F as o,a as S}from"./FSTab-CvQ96Exf.js";import{_ as c}from"./FSCol-DX423FHf.js";import"./vue.esm-bundler-gWFTcvUr.js";import"./uuid-DTaye2KM.js";import"./useColors-vfWFlOSD.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-_Z9wfsFC.js";import"./theme-DA9ermhM.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./useSlots-qbJuQIJ1.js";import"./css-Bc8dJONt.js";import"./VSlideGroup-DIBYkq4d.js";import"./index-C2ELxjkU.js";import"./color-CI1OEbFE.js";import"./display-DIiwMx3y.js";import"./group-DEdtjQ6D.js";import"./proxiedModel-ClxH85aG.js";import"./VIcon-BiBYr0XY.js";import"./locale-C1ZNn_CS.js";import"./VProgressCircular-DqlO5PEF.js";import"./FSClickable-BVFP8bZP.js";import"./FSCard-Dkx-Z7Pt.js";import"./FSRow-Bpa2jjpJ.js";import"./FSSpan-BlywDO8Z.js";import"./FSIcon-C5ELG8c1.js";import"./VBtn-BQZMGoLN.js";import"./border-CDY4nf3P.js";import"./density-cGginCT8.js";import"./elevation-B3TY2UXi.js";import"./rounded-DlJ8CYNz.js";import"./dimensions-CmIA1DmV.js";import"./loader-DNzkAbrM.js";import"./anchor-D8sPv6Az.js";import"./router-B2tkk-4w.js";import"./index-BTMLtsqI.js";import"./VDefaultsProvider-CYAySfh3.js";import"./lazy-Cvx00My1.js";import"./ssrBoot-2G_E7J1h.js";import"./transition-14-CCvbq.js";import"./forwardRefs-DWGaNmQL.js";import"./VSpacer-DqpPKsvm.js";const ie={title:"Foundation/Shared/Tabs",component:o,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},e={args:{args:{tab1:0,tab2:1,tab3:2,tab4:3,tab5:0}},render:(l,{argTypes:i})=>({components:{FSTabs:o,FSTab:S,FSText:F,FSWindow:u,FSCol:c,FSFadeOut:d,FSSlideGroup:r,FSButton:b},props:Object.keys(i),setup(){return{...l}},template:`
+      <div style="display: flex; flex-direction: column; gap: 30px;">
+        <FSText> Tabs in FadeOut </FSText>
+        <FSFadeOut height="200px">
+          <FSCol>
+            <FSTabs v-model:tab="args.tab5" color="success">
+              <FSTab :value="0" prependIcon="mdi-numeric-1-circle-outline" appendIcon="mdi-dice-1-outline" tag="1" />
+              <FSTab :value="1" label="Tab 2" appendIcon="mdi-dice-2-outline" tag="2" />
+            </FSTabs>
+            <FSWindow :modelValue="args.tab5" width="100%">
+              <FSCol :value="0">
+                <FSText v-for="(arg, index) in 50">
+                  Line {{ index }}
+                </FSText>
+              </FSCol>
+              <FSCol :value="1">
+                <FSText v-for="(arg, index) in 5">
+                  Line {{ index }}
+                </FSText>
+              </FSCol>
+            </FSWindow>
+          </FSCol>
+        </FSFadeOut>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSText> Fade outs in tabs </FSText>
+        <FSTabs v-model:tab="args.tab5" color="success">
+          <FSTab :value="0" prependIcon="mdi-numeric-1-circle-outline" appendIcon="mdi-dice-1-outline" tag="1" />
+          <FSTab :value="1" label="Tab 2" appendIcon="mdi-dice-2-outline" tag="2" />
+        </FSTabs>
+        <FSWindow v-model="args.tab5" width="100%">
+          <FSFadeOut :value="0" height="200px">
+            <FSCol>
+              <FSText v-for="(arg, index) in 50">
+                Line {{ index }}
+              </FSText>
+            </FSCol>
+          </FSFadeOut>
+          <FSFadeOut :value="1" height="200px">
+            <FSCol>
+              <FSText v-for="(arg, index) in 5">
+                Line {{ index }}
+              </FSText>
+            </FSCol>
+          </FSFadeOut>
+        </FSWindow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSText> Slide groups in tabs </FSText>
+        <FSTabs v-model:tab="args.tab5" color="success">
+          <FSTab :value="0" prependIcon="mdi-numeric-1-circle-outline" appendIcon="mdi-dice-1-outline" tag="1" />
+          <FSTab :value="1" label="Tab 2" appendIcon="mdi-dice-2-outline" tag="2" />
+        </FSTabs>
+        <FSWindow v-model="args.tab5" width="100%">
+          <FSCol :value="0">
+            <FSSlideGroup :value="0">
+              <FSButton label="Button 1" />
+              <FSButton label="Button 2" />
+              <FSButton label="Button 3" />
+              <FSButton label="Button 4" />
+              <FSButton label="Button 5" />
+              <FSButton label="Button 7" />
+              <FSButton label="Button 8" />
+              <FSButton label="Button 9" />
+              <FSButton label="Button 10" />
+              <FSButton label="Button 11" />
+              <FSButton label="Button 12" />
+              <FSButton label="Button 13" />
+              <FSButton label="Button 14" />
+            </FSSlideGroup>
+          </FSCol>
+          <FSSlideGroup :value="1">
+            <FSButton label="Button 1" />
+            <FSButton label="Button 2" />
+            <FSButton label="Button 3" />
+            <FSButton label="Button 4" />
+            <FSButton label="Button 5" />
+            <FSButton label="Button 7" />
+            <FSButton label="Button 8" />
+            <FSButton label="Button 9" />
+            <FSButton label="Button 10" />
+            <FSButton label="Button 11" />
+            <FSButton label="Button 12" />
+            <FSButton label="Button 13" />
+            <FSButton label="Button 14" />
+            <FSButton label="Button 15" />
+            <FSButton label="Button 16" />
+            <FSButton label="Button 17" />
+            <FSButton label="Button 18" />
+            <FSButton label="Button 19" />
+          </FSSlideGroup>
+        </FSWindow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+
+        <FSText> Primary color, labels only </FSText>
+        <FSTabs v-model:tab="args.tab1">
+          <FSTab label="Tab 1" :value="0" />
+          <FSTab label="Tab 2" :value="1" />
+          <FSTab label="Tab 3" :value="2" />
+          <FSTab label="Tab 4" :value="3" />
+          <FSTab label="Tab 5" :value="4" />
+        </FSTabs>
+        <FSWindow v-model="args.tab1">
+          <FSCol v-for="arg in 5" :key="arg">
+            <FSCol>
+              <FSText font="text-h1">
+                Tab {{ arg }}
+              </FSText>
+              <FSText font="text-body">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+              </FSText>
+            </FSCol>
+          </FSCol>
+        </FSWindow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSText> Warning color, labels & tags </FSText>
+        <FSTabs v-model:tab="args.tab2" color="warning">
+          <FSTab label="Tab 1" :value="1" tag="1" />
+          <FSTab label="Tab 2" :value="2" tag="2" />
+          <FSTab label="Tab 3" :value="3" tag="3" />
+          <FSTab label="Tab 4" :value="4" tag="4" />
+          <FSTab label="Tab 5" :value="5" tag="5" />
+        </FSTabs>
+        <FSWindow v-model="args.tab2">
+          <FSText v-for="arg in 5" :value="arg">
+            Tab {{ arg }}
+          </FSText>
+        </FSWindow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSText> Error color, prepend icons & large labels </FSText>
+        <FSTabs v-model:tab="args.tab3" color="error">
+          <FSTab label="Tab with a really really long name (and an icon) 1" :value="0" prependIcon="mdi-numeric-1-circle-outline" />
+          <FSTab label="Tab with a really really long name (and an icon) 2" :value="1" prependIcon="mdi-numeric-2-circle-outline" />
+          <FSTab label="Tab with a really really long name (and an icon) 3" :value="2" prependIcon="mdi-numeric-3-circle-outline" />
+          <FSTab label="Tab with a really really long name (and an icon) 4" :value="3" prependIcon="mdi-numeric-4-circle-outline" />
+          <FSTab label="Tab with a really really long name (and an icon) 5" :value="4" prependIcon="mdi-numeric-5-circle-outline" />
+        </FSTabs>
+        <FSWindow v-model="args.tab3">
+          <FSText v-for="(arg, index) in 5">
+            Tab {{ index }}
+          </FSText>
+        </FSWindow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSText> Success color, prepend icons, labels, tags, append icons </FSText>
+        <FSTabs v-model:tab="args.tab4" color="success">
+          <FSTab :value="0" prependIcon="mdi-numeric-1-circle-outline" appendIcon="mdi-dice-1-outline" tag="1" />
+          <FSTab :value="1" label="Tab 2" appendIcon="mdi-dice-2-outline" tag="2" />
+          <FSTab :value="2" label="Tab 3" prependIcon="mdi-numeric-3-circle-outline" tag="3" />
+          <FSTab :value="3" label="Tab 4" prependIcon="mdi-numeric-4-circle-outline" appendIcon="mdi-dice-4-outline" />
+          <FSTab :value="4" label="Tab 5" prependIcon="mdi-numeric-5-circle-outline" appendIcon="mdi-dice-5-outline" tag="5" />
+        </FSTabs>
+        <FSWindow v-model="args.tab4">
+          <FSText v-for="(arg, index) in 5">
+            Tab {{ index }}
+          </FSText>
+        </FSWindow>
+      </div>
+    `})};var t,n,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    args: {
+      tab1: 0,
+      tab2: 1,
+      tab3: 2,
+      tab4: 3,
+      tab5: 0
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSTabs,
+      FSTab,
+      FSText,
+      FSWindow,
+      FSCol,
+      FSFadeOut,
+      FSSlideGroup,
+      FSButton
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+      <div style="display: flex; flex-direction: column; gap: 30px;">
+        <FSText> Tabs in FadeOut </FSText>
+        <FSFadeOut height="200px">
+          <FSCol>
+            <FSTabs v-model:tab="args.tab5" color="success">
+              <FSTab :value="0" prependIcon="mdi-numeric-1-circle-outline" appendIcon="mdi-dice-1-outline" tag="1" />
+              <FSTab :value="1" label="Tab 2" appendIcon="mdi-dice-2-outline" tag="2" />
+            </FSTabs>
+            <FSWindow :modelValue="args.tab5" width="100%">
+              <FSCol :value="0">
+                <FSText v-for="(arg, index) in 50">
+                  Line {{ index }}
+                </FSText>
+              </FSCol>
+              <FSCol :value="1">
+                <FSText v-for="(arg, index) in 5">
+                  Line {{ index }}
+                </FSText>
+              </FSCol>
+            </FSWindow>
+          </FSCol>
+        </FSFadeOut>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSText> Fade outs in tabs </FSText>
+        <FSTabs v-model:tab="args.tab5" color="success">
+          <FSTab :value="0" prependIcon="mdi-numeric-1-circle-outline" appendIcon="mdi-dice-1-outline" tag="1" />
+          <FSTab :value="1" label="Tab 2" appendIcon="mdi-dice-2-outline" tag="2" />
+        </FSTabs>
+        <FSWindow v-model="args.tab5" width="100%">
+          <FSFadeOut :value="0" height="200px">
+            <FSCol>
+              <FSText v-for="(arg, index) in 50">
+                Line {{ index }}
+              </FSText>
+            </FSCol>
+          </FSFadeOut>
+          <FSFadeOut :value="1" height="200px">
+            <FSCol>
+              <FSText v-for="(arg, index) in 5">
+                Line {{ index }}
+              </FSText>
+            </FSCol>
+          </FSFadeOut>
+        </FSWindow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSText> Slide groups in tabs </FSText>
+        <FSTabs v-model:tab="args.tab5" color="success">
+          <FSTab :value="0" prependIcon="mdi-numeric-1-circle-outline" appendIcon="mdi-dice-1-outline" tag="1" />
+          <FSTab :value="1" label="Tab 2" appendIcon="mdi-dice-2-outline" tag="2" />
+        </FSTabs>
+        <FSWindow v-model="args.tab5" width="100%">
+          <FSCol :value="0">
+            <FSSlideGroup :value="0">
+              <FSButton label="Button 1" />
+              <FSButton label="Button 2" />
+              <FSButton label="Button 3" />
+              <FSButton label="Button 4" />
+              <FSButton label="Button 5" />
+              <FSButton label="Button 7" />
+              <FSButton label="Button 8" />
+              <FSButton label="Button 9" />
+              <FSButton label="Button 10" />
+              <FSButton label="Button 11" />
+              <FSButton label="Button 12" />
+              <FSButton label="Button 13" />
+              <FSButton label="Button 14" />
+            </FSSlideGroup>
+          </FSCol>
+          <FSSlideGroup :value="1">
+            <FSButton label="Button 1" />
+            <FSButton label="Button 2" />
+            <FSButton label="Button 3" />
+            <FSButton label="Button 4" />
+            <FSButton label="Button 5" />
+            <FSButton label="Button 7" />
+            <FSButton label="Button 8" />
+            <FSButton label="Button 9" />
+            <FSButton label="Button 10" />
+            <FSButton label="Button 11" />
+            <FSButton label="Button 12" />
+            <FSButton label="Button 13" />
+            <FSButton label="Button 14" />
+            <FSButton label="Button 15" />
+            <FSButton label="Button 16" />
+            <FSButton label="Button 17" />
+            <FSButton label="Button 18" />
+            <FSButton label="Button 19" />
+          </FSSlideGroup>
+        </FSWindow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+
+        <FSText> Primary color, labels only </FSText>
+        <FSTabs v-model:tab="args.tab1">
+          <FSTab label="Tab 1" :value="0" />
+          <FSTab label="Tab 2" :value="1" />
+          <FSTab label="Tab 3" :value="2" />
+          <FSTab label="Tab 4" :value="3" />
+          <FSTab label="Tab 5" :value="4" />
+        </FSTabs>
+        <FSWindow v-model="args.tab1">
+          <FSCol v-for="arg in 5" :key="arg">
+            <FSCol>
+              <FSText font="text-h1">
+                Tab {{ arg }}
+              </FSText>
+              <FSText font="text-body">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+              </FSText>
+            </FSCol>
+          </FSCol>
+        </FSWindow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSText> Warning color, labels & tags </FSText>
+        <FSTabs v-model:tab="args.tab2" color="warning">
+          <FSTab label="Tab 1" :value="1" tag="1" />
+          <FSTab label="Tab 2" :value="2" tag="2" />
+          <FSTab label="Tab 3" :value="3" tag="3" />
+          <FSTab label="Tab 4" :value="4" tag="4" />
+          <FSTab label="Tab 5" :value="5" tag="5" />
+        </FSTabs>
+        <FSWindow v-model="args.tab2">
+          <FSText v-for="arg in 5" :value="arg">
+            Tab {{ arg }}
+          </FSText>
+        </FSWindow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSText> Error color, prepend icons & large labels </FSText>
+        <FSTabs v-model:tab="args.tab3" color="error">
+          <FSTab label="Tab with a really really long name (and an icon) 1" :value="0" prependIcon="mdi-numeric-1-circle-outline" />
+          <FSTab label="Tab with a really really long name (and an icon) 2" :value="1" prependIcon="mdi-numeric-2-circle-outline" />
+          <FSTab label="Tab with a really really long name (and an icon) 3" :value="2" prependIcon="mdi-numeric-3-circle-outline" />
+          <FSTab label="Tab with a really really long name (and an icon) 4" :value="3" prependIcon="mdi-numeric-4-circle-outline" />
+          <FSTab label="Tab with a really really long name (and an icon) 5" :value="4" prependIcon="mdi-numeric-5-circle-outline" />
+        </FSTabs>
+        <FSWindow v-model="args.tab3">
+          <FSText v-for="(arg, index) in 5">
+            Tab {{ index }}
+          </FSText>
+        </FSWindow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSText> Success color, prepend icons, labels, tags, append icons </FSText>
+        <FSTabs v-model:tab="args.tab4" color="success">
+          <FSTab :value="0" prependIcon="mdi-numeric-1-circle-outline" appendIcon="mdi-dice-1-outline" tag="1" />
+          <FSTab :value="1" label="Tab 2" appendIcon="mdi-dice-2-outline" tag="2" />
+          <FSTab :value="2" label="Tab 3" prependIcon="mdi-numeric-3-circle-outline" tag="3" />
+          <FSTab :value="3" label="Tab 4" prependIcon="mdi-numeric-4-circle-outline" appendIcon="mdi-dice-4-outline" />
+          <FSTab :value="4" label="Tab 5" prependIcon="mdi-numeric-5-circle-outline" appendIcon="mdi-dice-5-outline" tag="5" />
+        </FSTabs>
+        <FSWindow v-model="args.tab4">
+          <FSText v-for="(arg, index) in 5">
+            Tab {{ index }}
+          </FSText>
+        </FSWindow>
+      </div>
+    \`
+  })
+}`,...(a=(n=e.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const re=["Variations"];export{e as Variations,re as __namedExportsOrder,ie as default};
