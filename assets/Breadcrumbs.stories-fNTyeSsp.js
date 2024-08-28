af14cfe41fa65ddb021bba71dd419c07fe703b8b
rduteil <rduteil@users.noreply.github.com>
2024-08-28 12:30:25 +0000

    deploy: 4c4fce3654cf69e71bcc276c196219cac6c77945

---
+++ assets/Breadcrumbs.stories-fNTyeSsp.js
@@ -0,0 +1,41 @@
+import{_ as s}from"./FSBreadcrumbs-BNTCTayZ.js";import"./vue.esm-bundler--l9E1zCM.js";import"./FSSpan-Drxl5cqQ.js";import"./useSlots-sbMHjG0F.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FSIcon-BHbXv1sW.js";import"./css-CqyMbT8T.js";import"./useColors-CcOmb8TW.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-3zl-tJp_.js";import"./theme-uJWLcFzd.js";import"./VIcon-CRzxwZ_P.js";import"./color-DkN3uAGq.js";import"./router-B0OmHOdA.js";import"./density-BES7uE0q.js";import"./rounded-B2xBfeOT.js";import"./VDefaultsProvider-Dnoh-HtE.js";const F={title:"Foundation/Shared/Breadcrumbs",component:s,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},e={args:{args:{items:[{title:"First (clickable)",disabled:!1,active:!1},{title:"Second (disabled)",disabled:!0,active:!1},{title:"Third (clickable)",disabled:!1,active:!1},{title:"Last (active)",disabled:!0,active:!0}]}},render:(n,{argTypes:i})=>({components:{Breadcrumbs:s},props:Object.keys(i),setup(){return{...n}},template:`
+        <Breadcrumbs v-bind="args" />
+    `})};var t,r,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    args: {
+      items: [{
+        title: "First (clickable)",
+        disabled: false,
+        active: false
+      }, {
+        title: "Second (disabled)",
+        disabled: true,
+        active: false
+      }, {
+        title: "Third (clickable)",
+        disabled: false,
+        active: false
+      }, {
+        title: "Last (active)",
+        disabled: true,
+        active: true
+      }]
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      Breadcrumbs
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+        <Breadcrumbs v-bind="args" />
+    \`
+  })
+}`,...(a=(r=e.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const h=["Standard"];export{e as Standard,h as __namedExportsOrder,F as default};
