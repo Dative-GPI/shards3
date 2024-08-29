cffaafe41a7b50669509f9558a5376f6419962b0
peyronth <peyronth@users.noreply.github.com>
2024-08-29 10:07:22 +0000

    deploy: 1c6714c90b207ba6577a40c4f79868254d7d8b0f

---
+++ assets/TextField.stories-DyT5YTTE.js
@@ -0,0 +1,162 @@
+import{F as o}from"./FSTextField-Dr1SvTCK.js";import{F as m}from"./FSForm-RyJxgkZj.js";import{_ as p}from"./FSCol-BA_LQqMv.js";import{_ as g}from"./FSRow-DW_Nd3uI.js";import{T as F}from"./rules-CHpPMWvs.js";import"./vue.esm-bundler--l9E1zCM.js";import"./FSBaseField-vXRxqDM_.js";import"./FSSpan-Drxl5cqQ.js";import"./useSlots-sbMHjG0F.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./useColors-CcOmb8TW.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-3zl-tJp_.js";import"./theme-uJWLcFzd.js";import"./VSpacer-CY3ZXRlU.js";import"./color-DkN3uAGq.js";import"./FSButton-BklW4eaS.js";import"./FSClickable-DwRlwPhM.js";import"./FSCard-DXUcd4gZ.js";import"./css-CqyMbT8T.js";import"./VProgressCircular-DiflYTWZ.js";import"./VIcon-CRzxwZ_P.js";import"./FSIcon-BHbXv1sW.js";import"./useRules-eaGGgTH2.js";import"./VField-4heSQh6E.js";import"./index-U_F843Id.js";import"./transition-CYrBIr8o.js";import"./VLabel-CtWliwLG.js";import"./VInput-exXSDqX0.js";import"./locale-DRqPrS0N.js";import"./density-BES7uE0q.js";import"./proxiedModel-DBJRU-Dx.js";import"./form-26vhu4DQ.js";import"./loader-BeDPJNvc.js";import"./anchor-CJDpEDtq.js";import"./rounded-B2xBfeOT.js";import"./VDefaultsProvider-Dnoh-HtE.js";import"./forwardRefs-DWGaNmQL.js";import"./index-CDY7nOpr.js";import"./useDateFormat-CzGJd4V5.js";import"./useTranslations-ByKtt_-5.js";import"./useAppLanguageCode-0juUkaeD.js";import"./useAppTimeZone-D5nI5Dd_.js";import"./time-BdfI25e1.js";const ae={title:"Foundation/Shared/Input fields/TextField",component:o,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},e={args:{args:{value1:null,value2:null,value3:"Hello there",value4:"General Kenobi"}},render:(t,{argTypes:l})=>({components:{FSTextField:o,FSCol:p},props:Object.keys(l),setup(){return{...t}},template:`
+    <FSCol>
+      <FSTextField
+        label="Text"
+        v-model="args.value1"
+      />
+      <FSTextField
+        label="Text with suffix"
+        suffix="Hello there"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTextField
+        label="Required text, with description"
+        description="Description for this field"
+        :required="true"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTextField
+        label="Uneditable text, with description"
+        description="Uneditable description"
+        :editable="false"
+        v-model="args.value4"
+      />
+    </FSCol>`})},r={args:{args:{valid:!1,value1:"",value2:"",value3:"",rules:F}},render:(t,{argTypes:l})=>({components:{FSForm:m,FSTextField:o,FSCol:p,FSRow:g},props:Object.keys(l),setup(){return{...t}},template:`
+    <FSForm v-model="args.valid" variant="standard">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTextField
+          label="Rules: required & min 10 characters"
+          :rules="[args.rules.required(), args.rules.min(10)]"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTextField
+          label="Rules: max 10 characters"
+          :rules="[args.rules.max(10)]"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTextField
+          label="Rules: required & min 10 characters & at least one special & at least one uppercase & at least one lowercase & at least one digit"
+          :rules="[args.rules.required(), args.rules.min(10), args.rules.special(), args.rules.uppercase(), args.rules.lowercase(), args.rules.digit()]"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>`})};var a,i,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      value2: null,
+      value3: "Hello there",
+      value4: "General Kenobi"
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSTextField,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSTextField
+        label="Text"
+        v-model="args.value1"
+      />
+      <FSTextField
+        label="Text with suffix"
+        suffix="Hello there"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTextField
+        label="Required text, with description"
+        description="Description for this field"
+        :required="true"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTextField
+        label="Uneditable text, with description"
+        description="Uneditable description"
+        :editable="false"
+        v-model="args.value4"
+      />
+    </FSCol>\`
+  })
+}`,...(s=(i=e.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};var n,d,u;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    args: {
+      valid: false,
+      value1: "",
+      value2: "",
+      value3: "",
+      rules: TextRules
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSForm,
+      FSTextField,
+      FSCol,
+      FSRow
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSForm v-model="args.valid" variant="standard">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTextField
+          label="Rules: required & min 10 characters"
+          :rules="[args.rules.required(), args.rules.min(10)]"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTextField
+          label="Rules: max 10 characters"
+          :rules="[args.rules.max(10)]"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTextField
+          label="Rules: required & min 10 characters & at least one special & at least one uppercase & at least one lowercase & at least one digit"
+          :rules="[args.rules.required(), args.rules.min(10), args.rules.special(), args.rules.uppercase(), args.rules.lowercase(), args.rules.digit()]"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>\`
+  })
+}`,...(u=(d=r.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};const ie=["Variations","Rules"];export{r as Rules,e as Variations,ie as __namedExportsOrder,ae as default};
---
+++ assets/TreeViewField.stories-Bc0mOFff.js
@@ -0,0 +1,229 @@
+import{F as o}from"./FSTreeViewField-DFodi_ET.js";import{F as u}from"./FSForm-RyJxgkZj.js";import{_ as g}from"./FSIcon-BHbXv1sW.js";import{_ as p}from"./FSCol-BA_LQqMv.js";import{_ as v}from"./FSRow-DW_Nd3uI.js";import{c as F}from"./rules-CHpPMWvs.js";import"./vue.esm-bundler--l9E1zCM.js";import"./FSDialogMenu-B4qN8dQk.js";import"./FSCard-DXUcd4gZ.js";import"./useColors-CcOmb8TW.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-3zl-tJp_.js";import"./theme-uJWLcFzd.js";import"./css-CqyMbT8T.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./VDialog-CEZ-m3nI.js";import"./VOverlay-CiTOWjN9.js";import"./forwardRefs-DWGaNmQL.js";import"./color-DkN3uAGq.js";import"./proxiedModel-DBJRU-Dx.js";import"./anchor-CJDpEDtq.js";import"./dimensions-BKNjgbGk.js";import"./display-8UJ6HxQ1.js";import"./lazy-Bq-CkOF6.js";import"./locale-DRqPrS0N.js";import"./router-B0OmHOdA.js";import"./transition-CYrBIr8o.js";import"./VDefaultsProvider-Dnoh-HtE.js";import"./FSTextField-Dr1SvTCK.js";import"./FSBaseField-vXRxqDM_.js";import"./FSSpan-Drxl5cqQ.js";import"./useSlots-sbMHjG0F.js";import"./VSpacer-CY3ZXRlU.js";import"./FSButton-BklW4eaS.js";import"./FSClickable-DwRlwPhM.js";import"./VProgressCircular-DiflYTWZ.js";import"./VIcon-CRzxwZ_P.js";import"./useRules-eaGGgTH2.js";import"./VField-4heSQh6E.js";import"./index-U_F843Id.js";import"./VLabel-CtWliwLG.js";import"./VInput-exXSDqX0.js";import"./density-BES7uE0q.js";import"./form-26vhu4DQ.js";import"./loader-BeDPJNvc.js";import"./rounded-B2xBfeOT.js";import"./index-CDY7nOpr.js";import"./FSCheckbox-O0BYl1sh.js";import"./VSelectionControl-CjbL6NQr.js";import"./index-mEzhRiE5.js";import"./FSFadeOut-D6eFi9gL.js";import"./uuid-DTaye2KM.js";import"./VImg-r61Tw9eL.js";import"./elevation-Cf1uc8UZ.js";import"./FSRadio-CPRKwXqX.js";import"./VList-CMkQDUHu.js";import"./ssrBoot-De1byr4N.js";import"./border-CWknNQjA.js";import"./VBtn-Drw7FOOP.js";import"./group-DYO-GdsM.js";import"./filter-BQY2ohDP.js";import"./VMenu-DGJWui2w.js";import"./useDateFormat-CzGJd4V5.js";import"./useTranslations-ByKtt_-5.js";import"./useAppLanguageCode-0juUkaeD.js";import"./useAppTimeZone-D5nI5Dd_.js";import"./time-BdfI25e1.js";const Ie={title:"Foundation/Shared/Input fields/TreeViewField",component:o,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},e={args:{args:{items:[{id:0,label:"Group 1",icon:"mdi-account"},{id:1,label:"Group 2"},{id:2,label:"Group 3"},{id:3,label:"Group 4",parentId:0},{id:4,label:"Group 5",parentId:0},{id:5,label:"Group 6",parentId:3}],value1:null,value2:["0","1","2"],value3:"2",value4:"2"}},render:(i,{argTypes:t})=>({components:{FSTreeViewField:o,FSCol:p,FSIcon:g},props:Object.keys(t),setup(){return{...i}},template:`
+    <FSCol>
+      <FSTreeViewField
+        label="Tree view"
+        :items="args.items"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewField
+        label="Multiple tree view"
+        :items="args.items"
+        :multiple="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewField
+        label="Required tree view, with description"
+        description="Description for this field"
+        :required="true"
+        :items="args.items"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewField
+        label="Uneditable tree view, with description"
+        description="Uneditable description"
+        :editable="false"
+        :items="args.items"
+        v-model="args.value4"
+      />
+    </FSCol>`})},r={args:{args:{valid:!1,items:[{id:0,label:"Group 1",icon:"mdi-account"},{id:1,label:"Group 2"},{id:2,label:"Group 3"},{id:3,label:"Group 4",parentId:0},{id:4,label:"Group 5",parentId:0},{id:5,label:"Group 6",parentId:3}],value1:null,value2:null,value3:null,rules:F}},render:(i,{argTypes:t})=>({components:{FSForm:u,FSTreeViewField:o,FSCol:p,FSRow:v},props:Object.keys(t),setup(){return{...i}},template:`
+    <FSForm v-model="args.valid" variant="standard">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTreeViewField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :items="args.items"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTreeViewField
+          label="Rules: min 2 options"
+          :rules="[args.rules.min(2)]"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTreeViewField
+          label="Rules: required & min 2 options & max 3 options"
+          :rules="[args.rules.required(), args.rules.min(2), args.rules.max(3)]"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>`})};var l,n,a;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    args: {
+      items: [{
+        id: 0,
+        label: "Group 1",
+        icon: "mdi-account"
+      }, {
+        id: 1,
+        label: "Group 2"
+      }, {
+        id: 2,
+        label: "Group 3"
+      }, {
+        id: 3,
+        label: "Group 4",
+        parentId: 0
+      }, {
+        id: 4,
+        label: "Group 5",
+        parentId: 0
+      }, {
+        id: 5,
+        label: "Group 6",
+        parentId: 3
+      }],
+      value1: null,
+      value2: ["0", "1", "2"],
+      value3: "2",
+      value4: "2"
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSTreeViewField,
+      FSCol,
+      FSIcon
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSTreeViewField
+        label="Tree view"
+        :items="args.items"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewField
+        label="Multiple tree view"
+        :items="args.items"
+        :multiple="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewField
+        label="Required tree view, with description"
+        description="Description for this field"
+        :required="true"
+        :items="args.items"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewField
+        label="Uneditable tree view, with description"
+        description="Uneditable description"
+        :editable="false"
+        :items="args.items"
+        v-model="args.value4"
+      />
+    </FSCol>\`
+  })
+}`,...(a=(n=e.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};var s,d,m;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    args: {
+      valid: false,
+      items: [{
+        id: 0,
+        label: "Group 1",
+        icon: "mdi-account"
+      }, {
+        id: 1,
+        label: "Group 2"
+      }, {
+        id: 2,
+        label: "Group 3"
+      }, {
+        id: 3,
+        label: "Group 4",
+        parentId: 0
+      }, {
+        id: 4,
+        label: "Group 5",
+        parentId: 0
+      }, {
+        id: 5,
+        label: "Group 6",
+        parentId: 3
+      }],
+      value1: null,
+      value2: null,
+      value3: null,
+      rules: TreeViewRules
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSForm,
+      FSTreeViewField,
+      FSCol,
+      FSRow
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSForm v-model="args.valid" variant="standard">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTreeViewField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :items="args.items"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTreeViewField
+          label="Rules: min 2 options"
+          :rules="[args.rules.min(2)]"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTreeViewField
+          label="Rules: required & min 2 options & max 3 options"
+          :rules="[args.rules.required(), args.rules.min(2), args.rules.max(3)]"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>\`
+  })
+}`,...(m=(d=r.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};const Ce=["Variations","Rules"];export{r as Rules,e as Variations,Ce as __namedExportsOrder,Ie as default};
