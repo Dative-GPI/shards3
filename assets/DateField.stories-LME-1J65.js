5ebebc276c607c90d9d7a1e62b3a9869aad920b8
SchroterQuentin <SchroterQuentin@users.noreply.github.com>
2024-09-05 16:47:03 +0000

    deploy: 6a6b6c8bbe0727f403dacdb5813c199a4e0cc104

---
+++ assets/DateField.stories-LME-1J65.js
@@ -0,0 +1,194 @@
+import{F as a}from"./FSDateField-RWl4wCF7.js";import{F}from"./FSButton-BeXPz3qD.js";import{F as c}from"./FSForm-C6-7llH_.js";import{_ as p}from"./FSCol-Bk577joi.js";import{_ as u}from"./FSRow-BuYwVvwM.js";import{u as g}from"./useAppTimeZone-ghwi0HtC.js";import{D as v}from"./rules-BlSYZiTY.js";import"./vue.esm-bundler-Vj2X7okO.js";import"./lodash-BiW_TGGX.js";import"./_commonjsHelpers-BosuxZz1.js";import"./FSDialogMenu-DJMBxSqq.js";import"./FSCard-IkM7vPXg.js";import"./useColors-ClGgFu9Q.js";import"./index-DfSX31J9.js";import"./theme-DqOMA9k1.js";import"./css-CbCR1nbl.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./VDialog-skjiTtId.js";import"./VOverlay-Dml741yE.js";import"./forwardRefs-DWGaNmQL.js";import"./color-Dk23S21P.js";import"./proxiedModel-CZPY81Tp.js";import"./anchor-J8DF-zwD.js";import"./dimensions-CBrtQk_X.js";import"./display-B4Z--0w1.js";import"./lazy-Ceu0wY4v.js";import"./locale-CMOn_BOp.js";import"./router-DPbzaFf1.js";import"./transition-Cs7QtK7U.js";import"./VDefaultsProvider-3kZaGa5j.js";import"./FSTextField-CMP-67X5.js";import"./FSBaseField-Uq_chVlu.js";import"./FSSpan-etgUZXmX.js";import"./useSlots-DXmhvOIb.js";import"./VSpacer-BJ2TWwuy.js";import"./useRules-DpnTqc7T.js";import"./VField-C1LOclAH.js";import"./index-7QBgy7JM.js";import"./VLabel-CtKTiIK0.js";import"./VInput-CifUC8kS.js";import"./VIcon-Dc5M3VhK.js";import"./tag-BDy8Psij.js";import"./density-BWgXlOb-.js";import"./form-BN2rR47-.js";import"./loader-DsDTT_wf.js";import"./VProgressCircular-BALYDMME.js";import"./rounded-B-50KYhN.js";import"./index-DQeMThUY.js";import"./FSCalendar-CxDKPbIQ.js";import"./useDateFormat-DZcIDWp6.js";import"./datesTools-RiUw1NMp.js";import"./useTranslations-DyPMaPr_.js";import"./useAppLanguageCode-Dd4JQvnz.js";import"./VLocaleProvider-BF_zvtsJ.js";import"./VBtn-8V3HVfv8.js";import"./border-BD8WTLKh.js";import"./elevation-DH7kyzD7.js";import"./group-CT1H1YXM.js";import"./index-BN7Lbnw3.js";import"./VMenu-BOpQjLNS.js";import"./FSClickable-B6i4bUTy.js";import"./FSIcon-CNrZYw3j.js";import"./time-BOZ1S5aJ.js";const Te={title:"Foundation/Shared/Input fields/DateField",component:a,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},e={args:{args:{setTimeZone:g().setAppTimeZone,value1:null,value2:17012088e5,value3:17012088e5}},render:(t,{argTypes:o})=>({components:{FSDateField:a,FSButton:F,FSCol:p,FSRow:u},props:Object.keys(o),setup(){return{...t}},template:`
+    <FSCol>
+      <FSRow>
+        <FSButton
+          label="Africa/Abidjan"
+          @click="args.setTimeZone('Africa/Abidjan')"
+        />
+        <FSButton
+          label="Europe/Paris"
+          @click="args.setTimeZone('Europe/Paris')"
+        />
+        <FSButton
+          label="America/Guatemala"
+          @click="args.setTimeZone('America/Guatemala')"
+        />
+        <FSButton
+          label="Asia/Kamchatka"
+          @click="args.setTimeZone('Asia/Kamchatka')"
+        />
+      </FSRow>
+      <FSDateField
+        label="Date, primary color"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSDateField
+        label="Required date, warning color, with description"
+        description="Description for this field"
+        color="warning"
+        :required="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSDateField
+        label="Uneditable date, error color, with description"
+        description="Uneditable description"
+        color="error"
+        :editable="false"
+        v-model="args.value3"
+      />
+    </FSCol>`})},r={args:{args:{valid:!1,value1:null,value2:null,value3:null,rules:v}},render:(t,{argTypes:o})=>({components:{FSForm:c,FSDateField:a,FSCol:p,FSRow:u},props:Object.keys(o),setup(){return{...t}},template:`
+    <FSForm v-model="args.valid" variant="standard">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: before 2024-01-31 00:00"
+          :rules="[args.rules.max(1706738400000)]"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: required & before 2024-01-31 00:00 & after 2024-01-02 00:00"
+          :rules="[args.rules.required(), args.rules.max(1706738400000), args.rules.min(1704146400000)]"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>`})};var i,l,n;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    args: {
+      setTimeZone: useAppTimeZone().setAppTimeZone,
+      value1: null,
+      value2: 1701208800000,
+      value3: 1701208800000
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSDateField,
+      FSButton,
+      FSCol,
+      FSRow
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSRow>
+        <FSButton
+          label="Africa/Abidjan"
+          @click="args.setTimeZone('Africa/Abidjan')"
+        />
+        <FSButton
+          label="Europe/Paris"
+          @click="args.setTimeZone('Europe/Paris')"
+        />
+        <FSButton
+          label="America/Guatemala"
+          @click="args.setTimeZone('America/Guatemala')"
+        />
+        <FSButton
+          label="Asia/Kamchatka"
+          @click="args.setTimeZone('Asia/Kamchatka')"
+        />
+      </FSRow>
+      <FSDateField
+        label="Date, primary color"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSDateField
+        label="Required date, warning color, with description"
+        description="Description for this field"
+        color="warning"
+        :required="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSDateField
+        label="Uneditable date, error color, with description"
+        description="Uneditable description"
+        color="error"
+        :editable="false"
+        v-model="args.value3"
+      />
+    </FSCol>\`
+  })
+}`,...(n=(l=e.parameters)==null?void 0:l.docs)==null?void 0:n.source}}};var s,d,m;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    args: {
+      valid: false,
+      value1: null,
+      value2: null,
+      value3: null,
+      rules: DateRules
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSForm,
+      FSDateField,
+      FSCol,
+      FSRow
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSForm v-model="args.valid" variant="standard">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: before 2024-01-31 00:00"
+          :rules="[args.rules.max(1706738400000)]"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: required & before 2024-01-31 00:00 & after 2024-01-02 00:00"
+          :rules="[args.rules.required(), args.rules.max(1706738400000), args.rules.min(1704146400000)]"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>\`
+  })
+}`,...(m=(d=r.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};const ke=["Variations","Rules"];export{r as Rules,e as Variations,ke as __namedExportsOrder,Te as default};
---
+++ assets/Slider.stories-DiYsAgVo.js
@@ -0,0 +1,93 @@
+import{F as i}from"./FSSlider-T-m_fY9d.js";import{_ as a}from"./FSCol-Bk577joi.js";import"./vue.esm-bundler-Vj2X7okO.js";import"./FSSpan-etgUZXmX.js";import"./useSlots-DXmhvOIb.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FSRow-BuYwVvwM.js";import"./css-CbCR1nbl.js";import"./useColors-ClGgFu9Q.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DfSX31J9.js";import"./theme-DqOMA9k1.js";import"./VSlider-DiloVQg1.js";import"./elevation-DH7kyzD7.js";import"./locale-CMOn_BOp.js";import"./rounded-B-50KYhN.js";import"./index-7QBgy7JM.js";import"./color-Dk23S21P.js";import"./index-BN7Lbnw3.js";import"./VInput-CifUC8kS.js";import"./VIcon-Dc5M3VhK.js";import"./tag-BDy8Psij.js";import"./transition-Cs7QtK7U.js";import"./density-BWgXlOb-.js";import"./proxiedModel-CZPY81Tp.js";import"./form-BN2rR47-.js";import"./VLabel-CtKTiIK0.js";const E={title:"Foundation/Shared/Slider",component:i,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},e={args:{args:{value1:0,value2:5,value3:15,value4:50}},render:(n,{argTypes:l})=>({components:{FSSlider:i,FSCol:a},props:Object.keys(l),setup(){return{...n}},template:`
+    <FSCol>
+      <FSSlider
+        label="Slider - dark color"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSlider
+        color="primary"
+        label="Slider - min: 0 - max: 10 - step: 1 - primary color"
+        :min="0"
+        :max="10"
+        :step="1"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSlider
+        color="warning"
+        label="Required slider - min: 0 - max: 10 - step: 1 - ticks - warning color"
+        description="Description for this field"
+        :required="true"
+        :showTicks="'always'"
+        :min="0"
+        :max="25"
+        :step="1"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSlider
+        label="Uneditable"
+        description="Uneditable description"
+        :editable="false"
+        v-model="args.value4"
+      />
+    </FSCol>`})};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: 0,
+      value2: 5,
+      value3: 15,
+      value4: 50
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSSlider,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSSlider
+        label="Slider - dark color"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSlider
+        color="primary"
+        label="Slider - min: 0 - max: 10 - step: 1 - primary color"
+        :min="0"
+        :max="10"
+        :step="1"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSlider
+        color="warning"
+        label="Required slider - min: 0 - max: 10 - step: 1 - ticks - warning color"
+        description="Description for this field"
+        :required="true"
+        :showTicks="'always'"
+        :min="0"
+        :max="25"
+        :step="1"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSlider
+        label="Uneditable"
+        description="Uneditable description"
+        :editable="false"
+        v-model="args.value4"
+      />
+    </FSCol>\`
+  })
+}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const z=["Variations"];export{e as Variations,z as __namedExportsOrder,E as default};
