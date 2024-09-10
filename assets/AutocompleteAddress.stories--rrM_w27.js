5704a5b1adb14d10fb35b13b40e08484f843d9bb
SchroterQuentin <SchroterQuentin@users.noreply.github.com>
2024-09-10 09:32:58 +0000

    deploy: 4ad32eea6080bfb18b6d1eed7483ff37c2965a6e

---
+++ assets/AutocompleteAddress.stories--rrM_w27.js
@@ -0,0 +1,47 @@
+import{d as P,b as _,o as k,w as I,y as E,z as N,A as O,m as R}from"./vue.esm-bundler-Vj2X7okO.js";import{_ as v}from"./lodash-BiW_TGGX.js";import{A as F}from"./address-CE2z3AEI.js";import{F as T}from"./FSAutocompleteField-CCs3-7Gg.js";import{u as D}from"./useAutocomplete-S-mYVlG5.js";import{_ as L}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{_ as U}from"./FSCol-Bk577joi.js";import"./_commonjsHelpers-BosuxZz1.js";import"./FSSearchField-Br299av8.js";import"./FSTextField-BQbZuAPn.js";import"./FSBaseField-DEWTBdWD.js";import"./FSSpan-etgUZXmX.js";import"./useSlots-DXmhvOIb.js";import"./FSRow-BuYwVvwM.js";import"./css-CbCR1nbl.js";import"./useColors-C8ZtkfWa.js";import"./index-DfSX31J9.js";import"./theme-D8Xq3tpQ.js";import"./VSpacer-YT0o3rZT.js";import"./color-CxiNukW_.js";import"./FSButton-D9OonqCF.js";import"./FSClickable-CfNRAgb8.js";import"./FSCard-D5wiJKub.js";import"./VProgressCircular-DnrgPZB3.js";import"./tag-DxanhlL3.js";import"./FSIcon-DYxpTLuT.js";import"./VIcon-DptP6m9n.js";import"./useRules-DpnTqc7T.js";import"./VField-DaMwaaXY.js";import"./index-joExpvzU.js";import"./transition-CQDsmRPd.js";import"./VLabel-BPqk0hPA.js";import"./VInput-Dln06fOF.js";import"./locale-CMOn_BOp.js";import"./density-BM1QLeh4.js";import"./proxiedModel-BE9FlVMJ.js";import"./form-CgybbRT3.js";import"./loader-CbF0edCX.js";import"./anchor-DHDhudvL.js";import"./rounded-BWAMdTmW.js";import"./VDefaultsProvider-CztrVRTm.js";import"./forwardRefs-DWGaNmQL.js";import"./index-jvVque46.js";import"./useTranslations-DyPMaPr_.js";import"./FSDialogMenu-BPvC2ERZ.js";import"./VDialog-1j0g9zHx.js";import"./VOverlay-BT06S-GU.js";import"./dimensions-CzmId2tp.js";import"./display-Cx0CWWL8.js";import"./lazy-CqgcpKMK.js";import"./router-CbAEScZe.js";import"./FSSlideGroup-COYtI-Nu.js";import"./uuid-DTaye2KM.js";import"./group-DgCbl5w-.js";import"./FSToggleSet-BMnFEOfS.js";import"./FSWrapGroup-CSqxXTzY.js";import"./FSCheckbox-B0fh_Rt7.js";import"./VSelectionControl-DcHbqw-H.js";import"./index-DjMs4W1j.js";import"./FSFadeOut-CB131b1X.js";import"./VImg-Da3f9R37.js";import"./elevation-BH8AnXAq.js";import"./FSRadio-B3ak8RSo.js";import"./VList-DgcfM2IV.js";import"./ssrBoot-D42UTWSS.js";import"./border-CgF0busY.js";import"./VSelect-iR212XTF.js";import"./VMenu-DOKVMVxb.js";import"./filter-CkY1Z-lm.js";const $=()=>{let r=!1,i,c,p;const m=async()=>{await window.initMap,i=new google.maps.places.AutocompleteService,c=new google.maps.places.PlacesService(document.getElementById("places")),p=new google.maps.places.AutocompleteSessionToken,r=!0},d=async s=>(r||await m(),S(s).then(o=>v.map(o,t=>({id:t.place_id,label:t.description}))).catch(()=>[])),u=async s=>{var t,a;r||await m();const o=await f(s.id);if(p=new google.maps.places.AutocompleteSessionToken,o.address_components&&o.formatted_address&&o.geometry)return new F({formattedAddress:o.formatted_address,locality:e(o.address_components,"locality"),country:e(o.address_components,"country"),latitude:((t=o.geometry.location)==null?void 0:t.lat())??0,longitude:((a=o.geometry.location)==null?void 0:a.lng())??0,placeId:s.id,placeLabel:s.label});throw new Error("missing informations")},g=async(s,o)=>(r||await m(),y(s,o).then(t=>{var a,n;if(t.length>0){const l=t[0];if(l.address_components&&l.formatted_address&&l.geometry)return new F({formattedAddress:l.formatted_address,locality:e(l.address_components,"locality"),country:e(l.address_components,"country"),latitude:((a=l.geometry.location)==null?void 0:a.lat())??0,longitude:((n=l.geometry.location)==null?void 0:n.lng())??0,placeId:l.place_id,placeLabel:l.formatted_address})}throw new Error("missing informations")})),S=s=>new Promise((o,t)=>{i.getPlacePredictions({input:s,sessionToken:p},function(a,n){n!=google.maps.places.PlacesServiceStatus.OK||!a?t(n):o(a)})}),y=(s,o)=>new Promise((t,a)=>{new google.maps.Geocoder().geocode({location:{lat:s,lng:o}},(n,l)=>{l!=google.maps.GeocoderStatus.OK||!n?a(l):t(n)})}),f=s=>new Promise((o,t)=>{c.getDetails({placeId:s,sessionToken:p,fields:["formatted_address","geometry","address_components","name"]},(a,n)=>{n!=google.maps.places.PlacesServiceStatus.OK||!a?t(n):o(a)})}),e=(s,o)=>{const t=v.find(s,a=>v.some(a.types,n=>n===o));return(t==null?void 0:t.long_name)??""};return{search:d,get:u,reverseSearch:g}},C=P({name:"FSAutocompleteAddress",components:{FSAutocompleteField:T},props:{modelValue:{type:Object,required:!1,default:null}},emits:["update:modelValue"],setup(r,{emit:i}){const{search:c,get:p}=$(),m=_([]),d=_(null),u=async e=>e===null?Promise.resolve([]):(m.value=await c(e),Promise.resolve([])),g=async e=>{if(e===null){i("update:modelValue",null);return}Array.isArray(e)&&(e=e[0]);const s=await p(e);i("update:modelValue",s)},{search:S,onUpdate:y}=D(m,[],i,u,g,e=>e.id,e=>e.label,!0,!1),f=e=>({id:e.placeId,label:e.placeLabel});return k(()=>{r.modelValue&&(d.value=f(r.modelValue))}),I(()=>r.modelValue,e=>{e&&(d.value=f(e))}),{modelValuePlace:d,places:m,search:S,onUpdate:y}}});function j(r,i,c,p,m,d){const u=E("FSAutocompleteField");return N(),O(u,R({clearable:!1,toggleSet:!1,multiple:!1,items:r.places,modelValue:r.modelValuePlace,"onUpdate:modelValue":r.onUpdate,search:r.search,"onUpdate:search":i[0]||(i[0]=g=>r.search=g)},r.$attrs),null,16,["items","modelValue","onUpdate:modelValue","search"])}const V=L(C,[["render",j]]);C.__docgenInfo={displayName:"FSAutocompleteAddress",exportName:"default",description:"",tags:{},props:[{name:"modelValue",type:{name:"Object as () => Address | null"},required:!1,defaultValue:{func:!1,value:"null"}}],events:[{name:"update:modelValue"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/shared/foundation-shared-components/components/autocompletes/FSAutoCompleteAddress.vue"]};const oo={title:"Foundation/Shared/Autocompletes/AutocompleteAddress",component:V,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},A={args:{args:{value1:null,value2:new F({placeId:"ChIJg7vu-43P9EcRRNrbyDxFMDY",placeLabel:"Dative, Rue de Norvège, Colombier-Saugnieu, France",formattedAddress:" 100 Rue de Norvège, 69125 Colombier-Saugnieu",locality:"Colombier-Saugnieu",country:"France",latitude:45.711741,longitude:5.071675})}},render:(r,{argTypes:i})=>({components:{FSAutocompleteAddress:V,FSCol:U},props:Object.keys(i),setup(){return{...r}},template:`
+    <FSCol>
+      <FSAutocompleteAddress
+        v-model="args.value1"
+      />
+      <FSAutocompleteAddress
+        v-model="args.value2"
+      />
+    </FSCol>`})};var h,w,b;A.parameters={...A.parameters,docs:{...(h=A.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      value2: new Address({
+        placeId: "ChIJg7vu-43P9EcRRNrbyDxFMDY",
+        placeLabel: 'Dative, Rue de Norvège, Colombier-Saugnieu, France',
+        formattedAddress: ' 100 Rue de Norvège, 69125 Colombier-Saugnieu',
+        locality: 'Colombier-Saugnieu',
+        country: 'France',
+        latitude: 45.711741,
+        longitude: 5.071675
+      })
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSAutocompleteAddress,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSAutocompleteAddress
+        v-model="args.value1"
+      />
+      <FSAutocompleteAddress
+        v-model="args.value2"
+      />
+    </FSCol>\`
+  })
+}`,...(b=(w=A.parameters)==null?void 0:w.docs)==null?void 0:b.source}}};const to=["Variations"];export{A as Variations,to as __namedExportsOrder,oo as default};
---
+++ assets/UserOrganisation.stories-D3PTYC3z.js
@@ -0,0 +1,121 @@
+import{F as i}from"./FSUserOrganisationTileUI-W2fQRVBq.js";import"./vue.esm-bundler-Vj2X7okO.js";import"./FSImage-GlEhSKTp.js";import"./FSImageUI-IzO1fAdz.js";import"./base-CmdGny12.js";import"./serviceFactory-Bd5jifNi.js";import"./_commonjsHelpers-BosuxZz1.js";import"./lodash-BiW_TGGX.js";import"./composableFactory-B0937_6N.js";import"./VImg-Da3f9R37.js";import"./css-CbCR1nbl.js";import"./useColors-C8ZtkfWa.js";import"./index-DfSX31J9.js";import"./theme-D8Xq3tpQ.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./color-CxiNukW_.js";import"./dimensions-CzmId2tp.js";import"./elevation-BH8AnXAq.js";import"./locale-CMOn_BOp.js";import"./rounded-BWAMdTmW.js";import"./transition-CQDsmRPd.js";import"./index-jvVque46.js";import"./FSSpan-etgUZXmX.js";import"./useSlots-DXmhvOIb.js";import"./FSTile-d1l9KX5c.js";import"./FSClickable-CfNRAgb8.js";import"./FSCard-D5wiJKub.js";import"./FSCol-Bk577joi.js";import"./FSRow-BuYwVvwM.js";import"./VProgressCircular-DnrgPZB3.js";import"./tag-DxanhlL3.js";import"./FSCheckbox-B0fh_Rt7.js";import"./FSIcon-DYxpTLuT.js";import"./VIcon-DptP6m9n.js";import"./useRules-DpnTqc7T.js";import"./VSelectionControl-DcHbqw-H.js";import"./density-BM1QLeh4.js";import"./proxiedModel-BE9FlVMJ.js";import"./index-DjMs4W1j.js";import"./VLabel-BPqk0hPA.js";import"./VInput-Dln06fOF.js";import"./index-joExpvzU.js";import"./form-CgybbRT3.js";import"./useTranslations-DyPMaPr_.js";const ne={title:"Foundation/Shared/Tiles/UserOrganisation",component:i,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},e={args:{args:{values:[{id:"1",name:"Jean-Michel Quelqu'un",admin:!0},{id:"2",name:"Jean-Michel Personne",roleLabel:"Red shirt",roleIcon:"mdi-cog",admin:!1},{id:"3",name:"Juano Alphonso Santos de la Cruz de la Balina",roleLabel:"User",roleIcon:"mdi-account-outline",admin:!1}],selected:[!1,!1,!1]}},render:(a,{argTypes:r})=>({components:{FSUserOrganisationTileUI:i},props:Object.keys(r),setup(){return{...a}},template:`
+    <div style="display: flex; gap: 10px; flex-wrap: wrap; width: 100vw;">
+      <FSUserOrganisationTileUI
+        v-for="(userOrganisation, index) in args.values"
+        :key="index"
+        :imageId="userOrganisation.imageId"
+        :name="userOrganisation.name"
+        :roleIcon="userOrganisation.roleIcon"
+        :roleLabel="userOrganisation.roleLabel"
+        :admin="userOrganisation.admin"
+        v-model="args.selected[index]"
+      />
+    </div>`})},n={args:{args:{values:[{id:"1",name:"Jean-Michel Quelqu'un",admin:!0},{id:"2",name:"Jean-Michel Personne",roleLabel:"Red shirt",roleIcon:"mdi-cog",admin:!1},{id:"3",name:"Juano Alphonso Santos de la Cruz de la Balina",roleLabel:"User",roleIcon:"mdi-account-outline",admin:!1}],selected:[!1,!1]}},render:(a,{argTypes:r})=>({components:{FSUserOrganisationTileUI:i},props:Object.keys(r),setup(){return{...a}},template:`
+    <div style="display: flex; gap: 10px; flex-wrap: wrap; width: 100vw;">
+      <FSUserOrganisationTileUI
+        v-for="(userOrganisation, index) in args.values"
+        :key="index"
+        :name="userOrganisation.name"
+        :roleIcon="userOrganisation.roleIcon"
+        :roleLabel="userOrganisation.roleLabel"
+        :admin="userOrganisation.admin"
+        v-model="args.selected[index]"
+      />
+    </div>`})};var o,s,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    args: {
+      values: [{
+        id: "1",
+        name: "Jean-Michel Quelqu'un",
+        admin: true
+      }, {
+        id: "2",
+        name: "Jean-Michel Personne",
+        roleLabel: "Red shirt",
+        roleIcon: "mdi-cog",
+        admin: false
+      }, {
+        id: "3",
+        name: "Juano Alphonso Santos de la Cruz de la Balina",
+        roleLabel: "User",
+        roleIcon: "mdi-account-outline",
+        admin: false
+      }],
+      selected: [false, false, false]
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSUserOrganisationTileUI
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <div style="display: flex; gap: 10px; flex-wrap: wrap; width: 100vw;">
+      <FSUserOrganisationTileUI
+        v-for="(userOrganisation, index) in args.values"
+        :key="index"
+        :imageId="userOrganisation.imageId"
+        :name="userOrganisation.name"
+        :roleIcon="userOrganisation.roleIcon"
+        :roleLabel="userOrganisation.roleLabel"
+        :admin="userOrganisation.admin"
+        v-model="args.selected[index]"
+      />
+    </div>\`
+  })
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var l,m,d;n.parameters={...n.parameters,docs:{...(l=n.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    args: {
+      values: [{
+        id: "1",
+        name: "Jean-Michel Quelqu'un",
+        admin: true
+      }, {
+        id: "2",
+        name: "Jean-Michel Personne",
+        roleLabel: "Red shirt",
+        roleIcon: "mdi-cog",
+        admin: false
+      }, {
+        id: "3",
+        name: "Juano Alphonso Santos de la Cruz de la Balina",
+        roleLabel: "User",
+        roleIcon: "mdi-account-outline",
+        admin: false
+      }],
+      selected: [false, false]
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSUserOrganisationTileUI
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <div style="display: flex; gap: 10px; flex-wrap: wrap; width: 100vw;">
+      <FSUserOrganisationTileUI
+        v-for="(userOrganisation, index) in args.values"
+        :key="index"
+        :name="userOrganisation.name"
+        :roleIcon="userOrganisation.roleIcon"
+        :roleLabel="userOrganisation.roleLabel"
+        :admin="userOrganisation.admin"
+        v-model="args.selected[index]"
+      />
+    </div>\`
+  })
+}`,...(d=(m=n.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};const ae=["Variations","Imageless"];export{n as Imageless,e as Variations,ae as __namedExportsOrder,ne as default};
