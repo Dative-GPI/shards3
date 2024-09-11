f3aba04a4143cc62fb50d45ac07fe5e27c461085
peyronth <peyron.th@gmail.com>
2024-09-11 12:41:47 +0200

    Add search, label and fadeout to FSSimpleList

--- src/shared/foundation-shared-components/components/lists/FSSimpleList.vue
+++ src/shared/foundation-shared-components/components/lists/FSSimpleList.vue
@@ -1,91 +1,147 @@
 <template>
-  <component
-    :is="$props.direction == 'row' ? FSRow : FSCol"
-    v-bind="$attrs"
+  <FSCol
+    v-bind="$props"
+    gap="12px"
   >
-    <FSTile
-      v-for="item in items"
-      :key="item.id"
-      v-bind="tileProps(item)"
-      :width="$props.direction == 'row' ? 'fit-content' : '100%'"
-      height="fit-content"
+    <FSText
+      v-if="$props.label"
+      font="text-button"
     >
-      <slot
-        name="item"
-        :item="item"
+      {{ $props.label }}
+    </FSText>
+    <FSSearchField
+      v-if="$props.searchable"
+      padding="0 12px 0 0"
+      :hideHeader="true"
+      :modelValue="actualSearch"
+      placeholder="Search"
+      @update:modelValue="onSearch"
+    />
+    <FSFadeOut
+      padding="0 4px 0 0"
+      :maxHeight="$props.maxHeight"
+      :maskHeight="0"
+    >
+      <component
+        :is="$props.direction == 'row' ? FSRow : FSCol"
       >
-        <FSRow
-          align="center-left"
+        <template
+          v-if="$props.loading"
         >
-          <FSButtonDragIcon
-            v-if="showDraggable"
+          <FSLoader
+            
+            v-for="i in 4"
+            :key="i"
+            :width="$props.direction == 'row' ? '220px' : '100%'"
+            height="50px"
           />
-          <slot
-            name="itemContent"
-            :item="item"
+        </template>
+        <template
+          v-else
+        >
+          <FSTile
+            v-for="item in filteredItems"
+            :key="item.id"
+            v-bind="tileProps(item)"
+            :width="$props.direction == 'row' ? 'fit-content' : '100%'"
+            height="fit-content"
+            :editable="false"
+            :singleSelect="$props.clickable"
+            @update:modelValue="$emit('click:item', item.id)"
           >
-            <!-- TODO : add draggable option -->
-            <FSImage
-              v-if="item.imageId"
-              :imageId="item.imageId"
-              width="24px"
-              height="24px"
-            />
-            <FSIcon
-              size="24px"
-              v-else-if="item.icon"
-              :icon="item.icon"
-            />
-            <FSSpan>{{ item[itemLabel] }}</FSSpan>
-          </slot>
-          <FSRow
-            align="center-right"
-          >
-            <FSButtonEditIcon
-              v-if="showEdit"
-              @click="$emit('click:edit', item.id)"
-            />
-            <FSButtonRemoveIcon
-              v-if="showRemove"
-              @click="$emit('click:remove', item.id)"
-            />
-          </FSRow>
-        </FSRow>
-      </slot>
-    </FSTile>
-  </component>
+            <slot
+              name="item"
+              :item="item"
+            >
+              <FSRow
+                align="center-left"
+                :wrap="false"
+              >
+                <FSButtonDragIcon
+                  v-if="showDraggable"
+                />
+                <slot
+                  name="itemContent"
+                  :item="item"
+                >
+                  <!-- TODO : add draggable option -->
+                  <FSImage
+                    v-if="item.imageId"
+                    :imageId="item.imageId"
+                    width="24px"
+                    height="24px"
+                  />
+                  <FSIcon
+                    size="24px"
+                    v-else-if="item.icon"
+                    :icon="item.icon"
+                  />
+                  <FSSpan>{{ item[itemLabel] }}</FSSpan>
+                </slot>
+                <FSRow
+                  align="center-right"
+                >
+                  <FSButtonEditIcon
+                    v-if="showEdit"
+                    @click="$emit('click:edit', item.id)"
+                  />
+                  <FSButtonRemoveIcon
+                    v-if="showRemove"
+                    @click="$emit('click:remove', item.id)"
+                  />
+                </FSRow>
+              </FSRow>
+            </slot>
+          </FSTile>
+        </template>
+      </component>
+    </FSFadeOut>
+  </FSCol>
 </template>
 
 
 <script lang="ts">
-import { defineComponent, type PropType } from "vue";
+import { defineComponent, type PropType, ref, watch } from "vue";
 
 import { ColorEnum } from "../../models"
 
 import FSRow from "../FSRow.vue";
 import FSCol from "../FSCol.vue";
-import FSImage from "../FSImage.vue";
+import FSText from "../FSText.vue";
 import FSIcon from "../FSIcon.vue";
 import FSSpan from "../FSSpan.vue";
+import FSImage from "../FSImage.vue";
+import FSLoader from "../FSLoader.vue";
 import FSTile from "../tiles/FSTile.vue";
-import FSButtonRemoveIcon from "../buttons/FSButtonRemoveIcon.vue";
+import FSFadeOut from "../FSFadeOut.vue";
+import FSSearchField from "../fields/FSSearchField.vue";
 import FSButtonEditIcon from "../buttons/FSButtonEditIcon.vue";
 import FSButtonDragIcon from "../buttons/FSButtonDragIcon.vue";
+import FSButtonRemoveIcon from "../buttons/FSButtonRemoveIcon.vue";
+import { computed } from "vue";
 
 export default defineComponent({
   name: "FSSimpleList",
   components: {
     FSRow,
     FSCol,
+    FSText,
     FSTile,
-    FSImage,
     FSIcon,
     FSSpan,
-    FSButtonRemoveIcon,
+    FSImage,
+    FSLoader,
+    FSFadeOut,
+    FSSearchField,
     FSButtonEditIcon,
     FSButtonDragIcon,
+    FSButtonRemoveIcon,
   },
   props: {
+    label: {
+      type: String,
+      required: false
+    },
     items: {
       type: Array as PropType<{id: string, label?: string, icon?: string, imageId?: string, [index: string]: any}[]>,
       required: true
@@ -110,6 +166,31 @@ export default defineComponent({
       required: false,
       default: false
     },
+    clickable: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    searchable: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    search: {
+      type: String,
+      required: false,
+      default: ""
+    },
+    noFilter: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    maxHeight: {
+      type: [Array, String, Number] as PropType<string[] | number[] | string | number | null | undefined>,
+      required: false,
+      default: undefined
+    },
     direction: {
       type: String as PropType<"row" | "column">,
       required: false,
@@ -119,14 +200,47 @@ export default defineComponent({
       type: String,
       required: false,
       default: "label"
+    },
+    loading: {
+      type: Boolean,
+      required: false,
+      default: false
     }
   },
-  emits: ["click:edit", "click:remove"],
-  setup(){
+  emits: ["click:edit", "click:remove", "click:item", "update:search"],
+  setup(props, { emit }) {
+    const actualSearch = ref<string | null>(props.search);
+    const filteredItems = computed(() => {
+      if(!props.noFilter && actualSearch.value) {
+        const formattedSearch = actualSearch.value.toLowerCase();
+        return props.items.filter((item) => {
+          if (!JSON.stringify(item).toLowerCase().includes(formattedSearch)) {
+            return false;
+          }
+        });
+      }
+      return props.items;
+    });
+
+    const onSearch = (value: string) => {
+      if(props.noFilter) {
+        emit("update:search", value);
+      } else {
+        actualSearch.value = value;
+      }
+    }
+
+    watch(() => props.search, (value) => {
+      actualSearch.value = value;
+    });
+
     return {
+      actualSearch,
+      filteredItems,
       ColorEnum,
+      onSearch,
       FSRow,
-      FSCol
+      FSCol,
     }
   }
 });
