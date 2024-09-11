fa727bc5a43b227951bd528e390d2e7623187aae
ddonfack <ddonfack@dative-gpi.com>
2024-09-11 17:44:30 +0200

    alert missing properties

--- src/core/foundation-core-domain/models/alerts/alertDetails.ts
+++ src/core/foundation-core-domain/models/alerts/alertDetails.ts
@@ -2,14 +2,22 @@ import { AlertInfos, type AlertInfosDTO } from "./alertInfos";
 
 export class AlertDetails extends AlertInfos {
   description: string;
+  comments: string;
+  scenarioOrganisationsIds: string[];
+  scenarioOrganisationTypesIds: string[];
 
   constructor(params: AlertDetailsDTO) {
     super(params);
-
+    this.comments = params.comments;
     this.description = params.description;
+    this.scenarioOrganisationsIds = params.scenarioOrganisationsIds;
+    this.scenarioOrganisationTypesIds = params.scenarioOrganisationTypesIds;
   }
 }
 
 export interface AlertDetailsDTO extends AlertInfosDTO {
   description: string;
+  comments: string;
+  scenarioOrganisationsIds: string[];
+  scenarioOrganisationTypesIds: string[];
 }
