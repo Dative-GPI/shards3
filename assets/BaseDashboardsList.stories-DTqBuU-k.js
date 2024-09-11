954dd752dd1995c34fa38d158be9bc6bf65c5b56
peyronth <peyronth@users.noreply.github.com>
2024-09-11 10:45:08 +0000

    deploy: 152a30973e41803aa1a118fa602ef25983cf8ffb

---
+++ assets/BaseDashboardsList.stories-DTqBuU-k.js
@@ -0,0 +1,24 @@
+import m from"./FSBaseDashboardsList-S_Odo5Q4.js";import{F as s}from"./FSDataTable-BqTo56-8.js";import"./lodash-BiW_TGGX.js";import"./_commonjsHelpers-BosuxZz1.js";import"./vue.esm-bundler-BSyvBTC9.js";import"./FSIcon-D23Cpf0F.js";import"./css-DLfrm0pR.js";import"./useColors-DlnF7i6T.js";import"./index-DfSX31J9.js";import"./theme-D4rXHj3O.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./VIcon-BIpY_noq.js";import"./color-BygXFKwQ.js";import"./tag-DB5D_XjQ.js";import"./FSDashboardShallowTileUI-DGXZGvXp.js";import"./FSSimpleTileUI-jIlLLIOg.js";import"./FSIconCard-12gEYM-S.js";import"./FSCard-D-fFYNec.js";import"./FSCol-YmDykHL-.js";import"./FSRow-CVY2l52N.js";import"./FSImage-Bal15wTA.js";import"./FSImageUI-xIDH797J.js";import"./base-CmdGny12.js";import"./serviceFactory-Bd5jifNi.js";import"./composableFactory-wHNy9kQa.js";import"./FSLoader-CtbqJzfZ.js";import"./dimensions-DyYT4L19.js";import"./elevation-Ben4zuE4.js";import"./locale-C-s_GJlL.js";import"./VImg-DW7Ik4XH.js";import"./rounded-Dg8mz8II.js";import"./transition-CNA6wh-m.js";import"./index-D_lBla-s.js";import"./FSSpan-CCGpb4KF.js";import"./useSlots-P12pG1X5.js";import"./FSTile-ChlvKokJ.js";import"./FSClickable-DdK5daMz.js";import"./VProgressCircular-Dh9H7lZx.js";import"./FSCheckbox-VrXx14Lw.js";import"./useRules-B3HKjmia.js";import"./VSelectionControl-BRDFiqFl.js";import"./density-CDPzHPmZ.js";import"./proxiedModel-Bq4wip6p.js";import"./index-DPxysH4G.js";import"./VLabel-owjBjdIA.js";import"./VInput-DO9fMQeR.js";import"./index-CHbvZ5Sj.js";import"./form-B61Sg6X-.js";import"./FSDashboardOrganisationTileUI-DV98tT4G.js";import"./FSDashboardOrganisationTypeTileUI-BeXkVPE1.js";import"./useDashboardOrganisationTypes-BwvTnTaA.js";import"./dashboardTranslation-C8Wdb1gl.js";import"./useAppLanguageCode-CwkNj4O3.js";import"./base-1FyQZYnT.js";import"./useAppOrganisationId-BfPsGMlN.js";import"./useDashboardOrganisations-QK1n8jHf.js";import"./pathCrumb-Db-cq5HI.js";import"./useDashboardShallows-DeHwXP_Z.js";import"./useUserOrganisations-CrfAsfeZ.js";import"./permissionInfos-BPDgTHQl.js";import"./useOrganisations-BZ8t9e4-.js";import"./dashboards-JSNI7pIu.js";import"./VSpacer-C4plVoTE.js";import"./FSDataTableUI-DzinQmej.js";import"./vue-router-BhvpcqFa.js";import"./uuid-DTaye2KM.js";import"./FSText-DyldgdXP.js";import"./FSSearchField-BIrdfUzJ.js";import"./FSTextField-BKDOMXYp.js";import"./FSBaseField-_Isn_Ha_.js";import"./FSButton-BNq-0lOE.js";import"./VField-DndYz9t7.js";import"./loader-pPiux2KM.js";import"./anchor-D9N-895C.js";import"./VDefaultsProvider-YWdCRIjF.js";import"./forwardRefs-DWGaNmQL.js";import"./useTranslations-CdSqQFV3.js";import"./FSSelectField-CTZAdwe6.js";import"./FSDialogMenu-BULt7rkH.js";import"./VDialog-BkP6MFQ7.js";import"./VOverlay-CLFcrAVK.js";import"./display-BMkR1-zV.js";import"./lazy-CAOrKziU.js";import"./router-D2IrSaAD.js";import"./FSSlideGroup-CcWB8C_f.js";import"./group-CBCTR8u6.js";import"./FSToggleSet-CI3bzWWf.js";import"./FSWrapGroup-Choo5L6h.js";import"./FSFadeOut-D96pvLZm.js";import"./FSRadio-BDiw7DXd.js";import"./VList-BSxk2mkg.js";import"./ssrBoot-SGvAq9J5.js";import"./border-njGtMOdT.js";import"./VSelect-B_I0944e.js";import"./VMenu-BVgNprPc.js";import"./FSDivider-SLQwEwFD.js";import"./FSChip-BrwvSycq.js";import"./FSOptionGroup-CE_5FDQP.js";import"./VBtn-CC1p3R0h.js";import"./filter-DuGNEgHB.js";const $t={title:"Foundation/Core/Lists/Base Lists/BaseDashboardsList",component:m,subcomponents:{FSDataTable:s},tags:["autodocs"],argTypes:{}},t={render:i=>({components:{FSBaseDashboardsList:m},setup(){return{args:i}},template:`
+      <FSBaseDashboardsList
+        :tableCode="args.tableCode"
+      />
+    `}),args:{tableCode:"dashboards1"}};var r,o,p;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      FSBaseDashboardsList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <FSBaseDashboardsList
+        :tableCode="args.tableCode"
+      />
+    \`
+  }),
+  args: {
+    tableCode: 'dashboards1'
+  }
+}`,...(p=(o=t.parameters)==null?void 0:o.docs)==null?void 0:p.source}}};const tr=["Default"];export{t as Default,tr as __namedExportsOrder,$t as default};
