48db8b8dd0e84e9a169c674fa4c1108fe9de4f0a
ddonfack <ddonfack@dative-gpi.com>
2024-09-12 14:01:09 +0200

    add scenario scope

--- src/core/foundation-core-domain/models/alerts/alertDetails.ts
+++ src/core/foundation-core-domain/models/alerts/alertDetails.ts
@@ -1,23 +1,24 @@
+import type { ApplicationScope } from "@/shared/foundation-shared-domain/enums";
 import { AlertInfos, type AlertInfosDTO } from "./alertInfos";
 
 export class AlertDetails extends AlertInfos {
   description: string;
   comments: string;
-  scenarioOrganisationsIds: string[];
-  scenarioOrganisationTypesIds: string[];
+  deprecatedScenarioId: string;
+  scope: ApplicationScope;
 
   constructor(params: AlertDetailsDTO) {
     super(params);
     this.comments = params.comments;
     this.description = params.description;
-    this.scenarioOrganisationsIds = params.scenarioOrganisationsIds;
-    this.scenarioOrganisationTypesIds = params.scenarioOrganisationTypesIds;
+    this.deprecatedScenarioId = params.deprecatedScenarioId;
+    this.scope = params.scope;
   }
 }
 
 export interface AlertDetailsDTO extends AlertInfosDTO {
   description: string;
   comments: string;
-  scenarioOrganisationsIds: string[];
-  scenarioOrganisationTypesIds: string[];
+  deprecatedScenarioId: string;
+  scope: ApplicationScope;
 }
