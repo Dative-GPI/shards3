914bd6b12ddcc3aa866dfe8450c40dd35e32e90b
rduteil <rduteil@dative-gpi.com>
2024-09-13 15:21:31 +0200

    More stories

--- dev/storybook/src/stories/shared/Image.stories.ts
+++ dev/storybook/src/stories/shared/Image.stories.ts
@@ -16,7 +16,85 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Variations: Story = {
+export const Source: Story = {
+  args: {
+  },
+  render: (args, { argTypes }) => ({
+    components: { FSImage, FSCol, FSRow },
+    props: Object.keys(argTypes),
+    setup() {
+      return { ...args };
+    },
+    template: `
+    <div style="display: flex; flex-direction: column; gap: 10px;">
+      <FSCol>
+        <FSImage
+          source="https://products.dative-gpi.io/api/foundation/shared/v1/images/8da71ca6-3e31-47b2-93e6-a7a026ea062a/raw"
+          :cover="false"
+          :aspectRatio="1"
+          :height="80"
+        />
+        <FSImage
+          source="https://products.dative-gpi.io/api/foundation/shared/v1/images/8da71ca6-3e31-47b2-93e6-a7a026ea062a/raw"
+          height="80px"
+          width="80px"
+        />
+        <FSImage
+          source="https://products.dative-gpi.io/api/foundation/shared/v1/images/8da71ca6-3e31-47b2-93e6-a7a026ea062a/raw"
+          height="80px"
+          width="100%"
+        />
+        <FSImage
+          source="https://products.dative-gpi.io/api/foundation/shared/v1/images/8da71ca6-3e31-47b2-93e6-a7a026ea062a/raw"
+          :cover="false"
+          height="80px"
+          width="100%"
+        />
+      </FSCol>
+    </div>`
+  })
+}
+
+export const ImageId: Story = {
+  args: {
+  },
+  render: (args, { argTypes }) => ({
+    components: { FSImage, FSCol, FSRow },
+    props: Object.keys(argTypes),
+    setup() {
+      return { ...args };
+    },
+    template: `
+    <div style="display: flex; flex-direction: column; gap: 10px;">
+      <FSCol>
+        <FSImage
+          imageId="1"
+          :cover="false"
+          :aspectRatio="1"
+          :height="80"
+        />
+        <FSImage
+          imageId="1"
+          height="80px"
+          width="80px"
+        />
+        <FSImage
+          imageId="1"
+          height="80px"
+          width="100%"
+        />
+        <FSImage
+          imageId="1"
+          :cover="false"
+          height="80px"
+          width="100%"
+        />
+      </FSCol>
+    </div>`
+  })
+}
+
+export const ImageB64: Story = {
   args: {
     args: {
       imgBase64: "iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
@@ -32,31 +110,26 @@ export const Variations: Story = {
     <div style="display: flex; flex-direction: column; gap: 10px;">
       <FSCol>
         <FSImage
-          imageId="1"
-          height="80px"
-          width="80px"
-        />
-        <FSImage
           :imageB64="args.imgBase64"
-          height="80px"
-          width="80px"
-        />
-        <FSImage
-          source="https://products.dative-gpi.io/api/foundation/shared/v1/images/8da71ca6-3e31-47b2-93e6-a7a026ea062a/raw"
-          :aspectRatio="1"
           :cover="false"
+          :aspectRatio="1"
           :height="80"
         />
         <FSImage
-          imageId="1"
+          :imageB64="args.imgBase64"
           height="80px"
-          width="100%"
+          width="80px"
         />
         <FSImage
           :imageB64="args.imgBase64"
           height="80px"
           width="100%"
+        />
+        <FSImage
+          :imageB64="args.imgBase64"
           :cover="false"
+          height="80px"
+          width="100%"
         />
       </FSCol>
     </div>`
