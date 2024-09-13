5d99d6ffa04d42fa1a1d95b0f8f55ee6fbd09c43
rduteil <rduteil@dative-gpi.com>
2024-09-13 10:55:40 +0200

    Pagination on FSDialogMultiForm

--- src/shared/foundation-shared-services/config/urls/images.ts
+++ src/shared/foundation-shared-services/config/urls/images.ts
@@ -3,6 +3,12 @@ import { GATEWAY_URL } from "./base";
 export const IMAGES_URL = () => `${GATEWAY_URL()}/images`;
 
 export const IMAGE_URL = (imageId: string) => `${IMAGES_URL()}/${encodeURIComponent(imageId)}`;
-export const IMAGE_RAW_URL = (imageId: string, authToken?: string) => `${IMAGE_URL(imageId)}/raw${authToken ? `?authToken=${authToken}` : ''}`;
+export const IMAGE_RAW_URL = (imageId: string, authToken?: string) => {
+  let url = IMAGE_URL(imageId);
+  if (authToken) {
+    url += `?authToken=${encodeURIComponent(authToken)}`;
+  }
+  return url;
+};
 
 export const IMAGE_THUMBNAIL_URL = (imageId: string) => `${IMAGE_URL(imageId)}/thumbnail`;
