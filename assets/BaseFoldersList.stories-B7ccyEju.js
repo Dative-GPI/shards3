b6485ee99bd71ab445169b414e138e4d866d134e
SchroterQuentin <SchroterQuentin@users.noreply.github.com>
2024-09-16 16:09:06 +0000

    deploy: 84d9843979ee67a6b562977d56accb4c760c8e34

---
+++ assets/BaseFoldersList.stories-B7ccyEju.js
@@ -0,0 +1,24 @@
+import m from"./FSBaseFoldersList-CEU6oEM7.js";import{F as e}from"./FSDataTable-xX9JgO_Q.js";import"./lodash-BiW_TGGX.js";import"./_commonjsHelpers-BosuxZz1.js";import"./vue.esm-bundler-BSyvBTC9.js";import"./FSIcon-DwJNOKvG.js";import"./css-DLfrm0pR.js";import"./useColors-DlnF7i6T.js";import"./index-DfSX31J9.js";import"./theme-D4rXHj3O.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./VIcon-BIpY_noq.js";import"./color-BygXFKwQ.js";import"./tag-DB5D_XjQ.js";import"./FSFolderTileUI-DOQyjG0y.js";import"./FSSimpleTileUI-B8zfVAtV.js";import"./FSIconCard-G1MZXmQi.js";import"./FSCard-Dakm4bPu.js";import"./FSCol-DZA6pxA1.js";import"./FSRow-CA1vgNXx.js";import"./FSImage-BxDhaxzU.js";import"./FSImageUI-C0XOuHIu.js";import"./FSLoader-CtbqJzfZ.js";import"./dimensions-DyYT4L19.js";import"./elevation-Ben4zuE4.js";import"./locale-C-s_GJlL.js";import"./VImg-DW7Ik4XH.js";import"./rounded-Dg8mz8II.js";import"./transition-CNA6wh-m.js";import"./index-D_lBla-s.js";import"./useImages-BaeRdzQ-.js";import"./base-CmdGny12.js";import"./serviceFactory-Bd5jifNi.js";import"./composableFactory-wHNy9kQa.js";import"./FSSpan-CCGpb4KF.js";import"./useSlots-P12pG1X5.js";import"./FSTile-ClEhpyRn.js";import"./FSClickable-CS9kdIxp.js";import"./VProgressCircular-Dh9H7lZx.js";import"./FSCheckbox-DPKcfP0u.js";import"./useRules-B3HKjmia.js";import"./VSelectionControl-BRDFiqFl.js";import"./density-CDPzHPmZ.js";import"./proxiedModel-Bq4wip6p.js";import"./index-DPxysH4G.js";import"./VLabel-owjBjdIA.js";import"./VInput-DO9fMQeR.js";import"./index-CHbvZ5Sj.js";import"./form-B61Sg6X-.js";import"./useFolders-C2Y4eB4b.js";import"./pathCrumb-Db-cq5HI.js";import"./base-1FyQZYnT.js";import"./useAppOrganisationId-BfPsGMlN.js";import"./dashboards-DDo4KjxB.js";import"./dashboards-JSNI7pIu.js";import"./useTranslations-CdSqQFV3.js";import"./VSpacer-C4plVoTE.js";import"./FSDataTableUI-DmL672AK.js";import"./vue-router-BhvpcqFa.js";import"./uuid-DTaye2KM.js";import"./FSText-DyldgdXP.js";import"./FSSearchField-Cjbg81V-.js";import"./FSTextField-DIyIIzs_.js";import"./FSBaseField-BkzmckaL.js";import"./FSButton-CyN0itWL.js";import"./VField-DndYz9t7.js";import"./loader-pPiux2KM.js";import"./anchor-D9N-895C.js";import"./VDefaultsProvider-YWdCRIjF.js";import"./forwardRefs-DWGaNmQL.js";import"./FSSelectField-CBLEXrby.js";import"./FSDialogMenu-D5T4tlYN.js";import"./VDialog-BkP6MFQ7.js";import"./VOverlay-CLFcrAVK.js";import"./display-BMkR1-zV.js";import"./lazy-CAOrKziU.js";import"./router-D2IrSaAD.js";import"./FSSlideGroup-BogZOmN_.js";import"./group-CBCTR8u6.js";import"./FSToggleSet-Cx7T0QZL.js";import"./FSWrapGroup-yFK4C9rw.js";import"./FSFadeOut-D96pvLZm.js";import"./FSRadio-CTi9smel.js";import"./VList-BSxk2mkg.js";import"./ssrBoot-SGvAq9J5.js";import"./border-njGtMOdT.js";import"./VSelect-vIgl-vXK.js";import"./VMenu-BVgNprPc.js";import"./FSDivider-DQ-5tMcN.js";import"./FSChip-Cu5YGhb-.js";import"./FSOptionGroup-BXRAcD1O.js";import"./filter-CTnmeGG4.js";import"./VBtn-CC1p3R0h.js";import"./filter-DuGNEgHB.js";const Ut={title:"Foundation/Core/Lists/Base Lists/BaseFoldersList",component:m,subcomponents:{FSDataTable:e},tags:["autodocs"],argTypes:{}},t={render:i=>({components:{FSBaseFoldersList:m},setup(){return{args:i}},template:`
+      <FSBaseFoldersList
+        :tableCode="args.tableCode"
+      />
+    `}),args:{tableCode:"folders1"}};var r,o,p;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      FSBaseFoldersList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <FSBaseFoldersList
+        :tableCode="args.tableCode"
+      />
+    \`
+  }),
+  args: {
+    tableCode: 'folders1'
+  }
+}`,...(p=(o=t.parameters)==null?void 0:o.docs)==null?void 0:p.source}}};const Vt=["Default"];export{t as Default,Vt as __namedExportsOrder,Ut as default};
