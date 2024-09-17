aad9d122ae71a128244d4419a494398f2a786816
SchroterQuentin <SchroterQuentin@users.noreply.github.com>
2024-09-17 15:29:25 +0000

    deploy: 0c877c4a139a88e213c3048617ec981b408bdd6a

---
+++ assets/Location.stories-lMGYqx67.js
@@ -0,0 +1,30 @@
+import{F as V}from"./FSTile-D-5PKG1-.js";import{F as u}from"./FSLocationTileUI-CbL1Fgt2.js";import{d as F,w as y,y as i,z as l,A as m,m as S,D as T}from"./vue.esm-bundler-BSyvBTC9.js";import{F as I}from"./FSLoadTile-CcrbcnT5.js";import{a as L}from"./useLocations-Bj7Qu8qZ.js";import{_ as v}from"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FSClickable-BfYZKNeL.js";import"./FSCard-CiUY3jF9.js";import"./FSCol-DZA6pxA1.js";import"./css-DLfrm0pR.js";import"./FSRow-CA1vgNXx.js";import"./useColors-Ccf8xVyg.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-3zl-tJp_.js";import"./theme-D4rXHj3O.js";import"./VProgressCircular-Dh9H7lZx.js";import"./color-BygXFKwQ.js";import"./tag-DB5D_XjQ.js";import"./FSCheckbox-4wBZQYNd.js";import"./FSIcon-rAhoF0bo.js";import"./VIcon-BIpY_noq.js";import"./FSSpan-CCGpb4KF.js";import"./useSlots-P12pG1X5.js";import"./useRules-B3HKjmia.js";import"./VSelectionControl-BRDFiqFl.js";import"./density-CDPzHPmZ.js";import"./proxiedModel-Bq4wip6p.js";import"./index-DPxysH4G.js";import"./VLabel-owjBjdIA.js";import"./VInput-DO9fMQeR.js";import"./locale-C-s_GJlL.js";import"./index-CHbvZ5Sj.js";import"./transition-CNA6wh-m.js";import"./form-B61Sg6X-.js";import"./FSIconCard-DPROATcM.js";import"./FSColor-C7pBGL-O.js";import"./FSLoader-DuEJL8Ft.js";import"./dimensions-DyYT4L19.js";import"./elevation-Ben4zuE4.js";import"./address-CE2z3AEI.js";import"./base-1FyQZYnT.js";import"./useAppOrganisationId-BfPsGMlN.js";import"./serviceFactory-Bd5jifNi.js";import"./lodash-BiW_TGGX.js";import"./composableFactory-wHNy9kQa.js";const c=F({name:"FSLocationTile",components:{FSLocationTileUI:u,FSLoadTile:I},props:{locationId:{type:String,required:!0},modelValue:{type:Boolean,required:!1,default:!1},editable:{type:Boolean,required:!1,default:!0}},setup(e){const{get:o,getting:n,entity:r}=L();return y(()=>e.locationId,()=>{o(e.locationId)},{immediate:!0}),{getting:n,entity:r}}});function $(e,o,n,r,C,q){const g=i("FSLoadTile"),b=i("FSLocationTileUI");return e.getting?(l(),m(g,{key:0,editable:e.$props.editable,modelValue:e.modelValue,"onUpdate:modelValue":o[0]||(o[0]=a=>e.$emit("update:modelValue",a))},null,8,["editable","modelValue"])):e.entity?(l(),m(b,S({key:1,icon:e.entity.icon,label:e.entity.label,code:e.entity.code,color:e.entity.color,deviceCount:e.entity.deviceOrganisationsCount,editable:e.$props.editable,modelValue:e.modelValue,"onUpdate:modelValue":o[1]||(o[1]=a=>e.$emit("update:modelValue",a))},e.$attrs),null,16,["icon","label","code","color","deviceCount","editable","modelValue"])):T("",!0)}const f=v(c,[["render",$]]);c.__docgenInfo={displayName:"FSLocationTile",exportName:"default",description:"",tags:{},props:[{name:"locationId",type:{name:"string"},required:!0},{name:"modelValue",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}},{name:"editable",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"true"}}],events:[{name:"update:modelValue"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/core/foundation-core-components/components/tiles/FSLocationTile.vue"]};const ye={title:"Foundation/Core/Tiles/Location",component:f,subcomponents:{FSLocationTileUI:u,FSTile:V},tags:["autodocs"],argTypes:{locationId:{control:"select",options:["1","2","3"]}}},t={render:e=>({components:{FSLocationTile:f},setup(){return{args:e}},template:`
+      <FSLocationTile
+        :locationId="args.locationId"
+        :editable="args.editable"
+        v-model="args.modelValue"
+      />
+    `}),args:{locationId:"1",modelValue:!0,editable:!0}};var p,d,s;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      FSLocationTile
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <FSLocationTile
+        :locationId="args.locationId"
+        :editable="args.editable"
+        v-model="args.modelValue"
+      />
+    \`
+  }),
+  args: {
+    locationId: '1',
+    modelValue: true,
+    editable: true
+  }
+}`,...(s=(d=t.parameters)==null?void 0:d.docs)==null?void 0:s.source}}};const Se=["Default"];export{t as Default,Se as __namedExportsOrder,ye as default};
---
+++ assets/TreeViewGroup.stories-CDNBUu2g.js
@@ -0,0 +1,60 @@
+import{d as y,c as S,y as h,z as m,A as s,B as d,m as G,E as T,G as b,D as x}from"./vue.esm-bundler-BSyvBTC9.js";import{F as C}from"./FSTreeViewField-C-w9nrSY.js";import{F}from"./FSIcon-rAhoF0bo.js";import{u as _}from"./useGroups-BEpPMhMx.js";import{u as k}from"./useTreeView-Bjndlt42.js";import{_ as q}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{F as $}from"./FSCol-DZA6pxA1.js";import"./FSDialogMenu-EkZski4P.js";import"./FSCard-CiUY3jF9.js";import"./FSRow-CA1vgNXx.js";import"./css-DLfrm0pR.js";import"./useColors-Ccf8xVyg.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-3zl-tJp_.js";import"./theme-D4rXHj3O.js";import"./VDialog-BkP6MFQ7.js";import"./VOverlay-CLFcrAVK.js";import"./forwardRefs-DWGaNmQL.js";import"./color-BygXFKwQ.js";import"./proxiedModel-Bq4wip6p.js";import"./anchor-D9N-895C.js";import"./dimensions-DyYT4L19.js";import"./display-BMkR1-zV.js";import"./lazy-CAOrKziU.js";import"./locale-C-s_GJlL.js";import"./router-D2IrSaAD.js";import"./transition-CNA6wh-m.js";import"./VDefaultsProvider-YWdCRIjF.js";import"./FSTextField-BEw-xMN9.js";import"./FSBaseField-gW2RxCAK.js";import"./FSSpan-CCGpb4KF.js";import"./useSlots-P12pG1X5.js";import"./VSpacer-C4plVoTE.js";import"./FSButton-DrXuY6Y3.js";import"./FSClickable-BfYZKNeL.js";import"./VProgressCircular-Dh9H7lZx.js";import"./tag-DB5D_XjQ.js";import"./useRules-B3HKjmia.js";import"./VField-DndYz9t7.js";import"./index-CHbvZ5Sj.js";import"./VLabel-owjBjdIA.js";import"./VInput-DO9fMQeR.js";import"./VIcon-BIpY_noq.js";import"./density-CDPzHPmZ.js";import"./form-B61Sg6X-.js";import"./loader-pPiux2KM.js";import"./rounded-Dg8mz8II.js";import"./index-D_lBla-s.js";import"./FSCheckbox-4wBZQYNd.js";import"./VSelectionControl-BRDFiqFl.js";import"./index-DPxysH4G.js";import"./FSFadeOut-Chaq66Nt.js";import"./uuid-DTaye2KM.js";import"./FSLoader-DuEJL8Ft.js";import"./elevation-Ben4zuE4.js";import"./FSRadio-9XeVwuGU.js";import"./VList-BSxk2mkg.js";import"./ssrBoot-SGvAq9J5.js";import"./border-njGtMOdT.js";import"./VImg-DW7Ik4XH.js";import"./VBtn-CC1p3R0h.js";import"./group-CBCTR8u6.js";import"./filter-DuGNEgHB.js";import"./VMenu-BVgNprPc.js";import"./pathCrumb-Db-cq5HI.js";import"./base-1FyQZYnT.js";import"./useAppOrganisationId-BfPsGMlN.js";import"./serviceFactory-Bd5jifNi.js";import"./lodash-BiW_TGGX.js";import"./composableFactory-wHNy9kQa.js";const V=y({name:"FSTreeViewGroup",components:{FSTreeViewField:C,FSIcon:F},props:{groupFilters:{type:Object,required:!1,default:null},exclude:{type:String,required:!1,default:null},modelValue:{type:[Array,String],required:!1,default:null},multiple:{type:Boolean,required:!1,default:!1}},emits:["update:modelValue"],setup(e,{emit:r}){const{getMany:n,fetching:a,entities:i}=_(),l=S(()=>e.exclude?i.value.filter(u=>u.id!==e.exclude&&!u.path.some(w=>w.id===e.exclude)):i.value),p=()=>n({...e.groupFilters}),{onUpdate:o}=k(l,[()=>e.groupFilters],r,p);return{fetchingGroups:a,groups:l,onUpdate:o}}});function B(e,r,n,a,i,l){const p=h("FSTreeViewField");return m(),s(p,G({multiple:e.$props.multiple,loading:e.fetchingGroups,items:e.groups,modelValue:e.$props.modelValue,"onUpdate:modelValue":e.onUpdate},e.$attrs),{"menu-prepend":d(({item:o})=>[o.icon?(m(),s(F,{key:0},{default:d(()=>[T(b(o.icon),1)]),_:2},1024)):x("",!0)]),_:1},16,["multiple","loading","items","modelValue","onUpdate:modelValue"])}const v=q(V,[["render",B]]);V.__docgenInfo={displayName:"FSTreeViewGroup",exportName:"default",description:"",tags:{},props:[{name:"groupFilters",type:{name:"GroupFilters"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"exclude",type:{name:"string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"modelValue",type:{name:"string[] | string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"multiple",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}}],events:[{name:"update:modelValue"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/core/foundation-core-components/components/treeviews/FSTreeViewGroup.vue"]};const Re={title:"Foundation/Core/TreeViews/TreeViewGroup",component:v,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},t={args:{args:{value1:null,value2:null,value3:null}},render:(e,{argTypes:r})=>({components:{FSTreeViewGroup:v,FSCol:$},props:Object.keys(r),setup(){return{...e}},template:`
+    <FSCol>
+      <FSTreeViewGroup
+        label="Group"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewGroup
+        label="Group with multiple selection"
+        :multiple="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewGroup
+        label="Group with sawn branch"
+        exclude="12"
+        v-model="args.value3"
+      />
+    </FSCol>`})};var c,f,g;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      value2: null,
+      value3: null
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSTreeViewGroup,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSTreeViewGroup
+        label="Group"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewGroup
+        label="Group with multiple selection"
+        :multiple="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTreeViewGroup
+        label="Group with sawn branch"
+        exclude="12"
+        v-model="args.value3"
+      />
+    </FSCol>\`
+  })
+}`,...(g=(f=t.parameters)==null?void 0:f.docs)==null?void 0:g.source}}};const We=["Variations"];export{t as Variations,We as __namedExportsOrder,Re as default};
