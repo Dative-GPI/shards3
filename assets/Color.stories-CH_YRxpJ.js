987a7898a6e2a5c2929ecfc2ec29ecf12fc3f5f5
SchroterQuentin <SchroterQuentin@users.noreply.github.com>
2024-09-17 16:35:50 +0000

    deploy: 2774a6e01e85f2b27b543f3fdd91a9370b95fbee

---
+++ assets/Color.stories-CH_YRxpJ.js
@@ -0,0 +1,34 @@
+import{F as a}from"./FSColor-CSobaj7A.js";import{_ as t}from"./FSSpan-CCGpb4KF.js";import{F as e}from"./FSRow-CA1vgNXx.js";import{V as s}from"./VDivider-DUY0boKc.js";import"./vue.esm-bundler-BSyvBTC9.js";import"./FSCard-Dakm4bPu.js";import"./FSCol-DZA6pxA1.js";import"./css-DLfrm0pR.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./useColors-DlnF7i6T.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DfSX31J9.js";import"./theme-D4rXHj3O.js";import"./useSlots-P12pG1X5.js";import"./theme-DpKY80SA.js";const V={title:"Foundation/Shared/Color",component:a,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},o={render:i=>({components:{VDivider:s,FSColor:a,FSSpan:t,FSRow:e},setup(){return{args:i}},template:`
+    <div style="display: flex; flex-direction: column; gap: 10px;">
+        <div style="display: flex; gap: 10px;">
+            <FSColor v-bind="args">
+                <FSRow padding="8px 16px">
+                    <FSSpan> Primary color </FSSpan>
+                </FSRow>
+            </FSColor>
+        </div>
+    </div>`})};var r,n,p;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      VDivider,
+      FSColor,
+      FSSpan,
+      FSRow
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+    <div style="display: flex; flex-direction: column; gap: 10px;">
+        <div style="display: flex; gap: 10px;">
+            <FSColor v-bind="args">
+                <FSRow padding="8px 16px">
+                    <FSSpan> Primary color </FSSpan>
+                </FSRow>
+            </FSColor>
+        </div>
+    </div>\`
+  })
+}`,...(p=(n=o.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};const _=["Variations"];export{o as Variations,_ as __namedExportsOrder,V as default};
---
+++ assets/ErrorToast.stories-Dltxkeeq.js
@@ -0,0 +1,69 @@
+import{F as n}from"./FSErrorToast-CJJYQZTy.js";import{F as i}from"./FSRow-CA1vgNXx.js";import{F as p}from"./FSButton-CyN0itWL.js";import"./vue.esm-bundler-BSyvBTC9.js";import"./FSIcon-DwJNOKvG.js";import"./css-DLfrm0pR.js";import"./useColors-DlnF7i6T.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DfSX31J9.js";import"./theme-D4rXHj3O.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./VIcon-BIpY_noq.js";import"./color-BygXFKwQ.js";import"./tag-DB5D_XjQ.js";import"./FSSpan-CCGpb4KF.js";import"./useSlots-P12pG1X5.js";import"./FSCol-DZA6pxA1.js";import"./FSClickable-CS9kdIxp.js";import"./FSCard-Dakm4bPu.js";import"./VProgressCircular-Dh9H7lZx.js";const j={title:"Foundation/Shared/ErrorToast",component:n,tags:["autodocs"]},r={args:{args:{code1:"errors.missingorganisation",code2:"errors.noadminprivilege",code3:"errors.alreadyregistereduser"}},render:(a,{argTypes:s})=>({components:{FSErrorToast:n,FSRow:i,FSButton:p},props:Object.keys(s),setup(){return{...a}},template:`
+      <FSRow gap="10px" align="bottom-center">
+        <FSErrorToast
+          :errorCode="args.code1"
+          variant="standard"
+        >
+        <template #footer>
+          <FSRow align="center-center">
+            <FSButton
+              color="error"
+              label="Delete"
+              variant="full"
+            />
+          </FSRow>
+        </template>
+        </FSErrorToast>
+        <FSErrorToast
+          :errorCode="args.code2"
+        />
+        <FSErrorToast
+          :errorCode="args.code3"
+        />
+      </FSRow>`})};var o,e,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    args: {
+      code1: "errors.missingorganisation",
+      code2: "errors.noadminprivilege",
+      code3: "errors.alreadyregistereduser"
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSErrorToast,
+      FSRow,
+      FSButton
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+      <FSRow gap="10px" align="bottom-center">
+        <FSErrorToast
+          :errorCode="args.code1"
+          variant="standard"
+        >
+        <template #footer>
+          <FSRow align="center-center">
+            <FSButton
+              color="error"
+              label="Delete"
+              variant="full"
+            />
+          </FSRow>
+        </template>
+        </FSErrorToast>
+        <FSErrorToast
+          :errorCode="args.code2"
+        />
+        <FSErrorToast
+          :errorCode="args.code3"
+        />
+      </FSRow>\`
+  })
+}`,...(t=(e=r.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const k=["Variations"];export{r as Variations,k as __namedExportsOrder,j as default};
