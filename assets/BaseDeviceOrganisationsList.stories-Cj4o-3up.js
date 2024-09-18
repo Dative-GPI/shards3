dfdad436cc0e3a805061ca0367a0e85d88912860
rduteil <rduteil@users.noreply.github.com>
2024-09-18 10:23:35 +0000

    deploy: 06932410d8d19a5a36cf3b8a39523b7332a138b9

---
+++ assets/BaseDeviceOrganisationsList.stories-Cj4o-3up.js
@@ -0,0 +1,59 @@
+import r from"./FSBaseDeviceOrganisationsList-BKF7YQ15.js";import{F as l}from"./FSDataTable-BY4HduKa.js";import"./vue.esm-bundler-BSyvBTC9.js";import"./lodash-BiW_TGGX.js";import"./_commonjsHelpers-BosuxZz1.js";import"./FSMetaValue-Bxi5-2Kt.js";import"./useAppTimeZone-D_npl1cn.js";import"./customProperties-DqkON8hk.js";import"./FSIconCheck-CNlH9Jza.js";import"./FSIcon-D0oLoXA8.js";import"./useColors-Ccf8xVyg.js";import"./index-3zl-tJp_.js";import"./theme-D4rXHj3O.js";import"./css-0IiqzUPc.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./VIcon-BIpY_noq.js";import"./color-BygXFKwQ.js";import"./tag-DB5D_XjQ.js";import"./FSSpan-CCGpb4KF.js";import"./useSlots-P12pG1X5.js";import"./FSText-BN3VvfV9.js";import"./useDateFormat-CNSspUCn.js";import"./datesTools-BRWlXl6j.js";import"./useTranslations-CdSqQFV3.js";import"./useAppLanguageCode-CwkNj4O3.js";import"./FSDeviceOrganisationTileUI-Db73nced.js";import"./FSStatusesCarousel-CVAdeZ-l.js";import"./FSButton-77cfmuX3.js";import"./FSClickable-DytVXb9R.js";import"./FSCard-BinAVVx_.js";import"./FSCol-BU2q2sTe.js";import"./FSRow-DzlUM3r6.js";import"./VProgressCircular-Dh9H7lZx.js";import"./FSWindow-tDtE4elS.js";import"./group-CBCTR8u6.js";import"./proxiedModel-Bq4wip6p.js";import"./locale-C-s_GJlL.js";import"./VBtn-CC1p3R0h.js";import"./border-njGtMOdT.js";import"./density-CDPzHPmZ.js";import"./elevation-Ben4zuE4.js";import"./rounded-Dg8mz8II.js";import"./dimensions-DyYT4L19.js";import"./loader-pPiux2KM.js";import"./anchor-D9N-895C.js";import"./router-D2IrSaAD.js";import"./index-DPxysH4G.js";import"./VDefaultsProvider-YWdCRIjF.js";import"./lazy-CAOrKziU.js";import"./ssrBoot-SGvAq9J5.js";import"./transition-CNA6wh-m.js";import"./FSStatus-CnVIioJY.js";import"./FSChip-fUG8p2n0.js";import"./FSColor-DzXFGj3V.js";import"./VMenu-BVgNprPc.js";import"./VOverlay-CLFcrAVK.js";import"./forwardRefs-DWGaNmQL.js";import"./display-BMkR1-zV.js";import"./alerts-pw2xvgtT.js";import"./FSDivider-CuFEGgLT.js";import"./FSImage-BSHLJI6z.js";import"./FSImageUI-RgjYdf_W.js";import"./FSLoader-hEATKEG-.js";import"./VImg-DW7Ik4XH.js";import"./index-D_lBla-s.js";import"./useImages-BaeRdzQ-.js";import"./base-CmdGny12.js";import"./serviceFactory-Bd5jifNi.js";import"./composableFactory-wHNy9kQa.js";import"./FSTile-C4wpebwx.js";import"./FSCheckbox-ILpIKg_O.js";import"./useRules-B3HKjmia.js";import"./VSelectionControl-BRDFiqFl.js";import"./VLabel-owjBjdIA.js";import"./VInput-DO9fMQeR.js";import"./index-CHbvZ5Sj.js";import"./form-B61Sg6X-.js";import"./FSTagGroup-xrP0TRup.js";import"./FSWrapGroup-DUGqUei2.js";import"./uuid-DTaye2KM.js";import"./FSSlideGroup-HxrzZDKP.js";import"./FSTag-CIkbjBOL.js";import"./base-1FyQZYnT.js";import"./useAppOrganisationId-BfPsGMlN.js";import"./useDeviceOrganisations-CvIgpWuO.js";import"./hubFactory-CTsGCpS5.js";import"./pathCrumb-Db-cq5HI.js";import"./FSDataTableUI-DBp9zqfN.js";import"./vue-router-BhvpcqFa.js";import"./FSSearchField-jIW8dpQv.js";import"./FSTextField-BRxSBTNG.js";import"./FSBaseField-D7aNDTnY.js";import"./VSpacer-C4plVoTE.js";import"./VField-DndYz9t7.js";import"./FSSelectField-R8OzCLdx.js";import"./FSDialogMenu-DSPGm4fK.js";import"./VDialog-BkP6MFQ7.js";import"./FSToggleSet-BDkTog43.js";import"./FSFadeOut-BJ1ngUIg.js";import"./FSRadio-ByoRox_c.js";import"./VList-BSxk2mkg.js";import"./VSelect-C2EXjQHz.js";import"./FSOptionGroup-BxhrpUUr.js";import"./filter-DC84psoM.js";import"./filter-DuGNEgHB.js";const at={title:"Foundation/Core/Lists/Base Lists/BaseDeviceOrganisationsList",component:r,subcomponents:{FSDataTable:l},tags:["autodocs"],argTypes:{tableCode:{control:"select",options:["devices1","devices2"]}}},o={args:{modelValue:[],tableCode:"devices1",connectedOnly:!1},render:e=>({components:{FSBaseDeviceOrganisationsList:r},setup(){return{args:e}},template:`
+      <FSBaseDeviceOrganisationsList
+        :connectedOnly="args.connectedOnly"
+        :tableCode="args.tableCode"
+        v-model:modelValue="args.modelValue"
+      />
+    `})},t={args:{modelValue:[],tableCode:"devices2",connectedOnly:!1},render:e=>({components:{FSBaseDeviceOrganisationsList:r},setup(){return{args:e}},template:`
+      <FSBaseDeviceOrganisationsList
+        :connectedOnly="args.connectedOnly"
+        :tableCode="args.tableCode"
+        v-model:modelValue="args.modelValue"
+      />
+    `})};var i,m,p;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    modelValue: [],
+    tableCode: 'devices1',
+    connectedOnly: false
+  },
+  render: args => ({
+    components: {
+      FSBaseDeviceOrganisationsList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <FSBaseDeviceOrganisationsList
+        :connectedOnly="args.connectedOnly"
+        :tableCode="args.tableCode"
+        v-model:modelValue="args.modelValue"
+      />
+    \`
+  })
+}`,...(p=(m=o.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var n,s,a;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    modelValue: [],
+    tableCode: 'devices2',
+    connectedOnly: false
+  },
+  render: args => ({
+    components: {
+      FSBaseDeviceOrganisationsList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <FSBaseDeviceOrganisationsList
+        :connectedOnly="args.connectedOnly"
+        :tableCode="args.tableCode"
+        v-model:modelValue="args.modelValue"
+      />
+    \`
+  })
+}`,...(a=(s=t.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const lt=["Default","CustomProperties"];export{t as CustomProperties,o as Default,lt as __namedExportsOrder,at as default};
