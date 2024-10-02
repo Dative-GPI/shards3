d024d6c4eedcb0969eba01603bc0c220852c26a8
rduteil <rduteil@dative-gpi.com>
2024-10-02 12:39:50 +0200

    Add app auth token in files URLs

--- src/shared/foundation-shared-services/config/urls/files.ts
+++ src/shared/foundation-shared-services/config/urls/files.ts
@@ -1,4 +1,10 @@
 import { GATEWAY_URL } from "./base";
 
 export const FILES_URL = () => `${GATEWAY_URL()}/files`;
-export const FILE_URL = (fileId: string) => `${FILES_URL()}/${encodeURIComponent(fileId)}`;
+export const FILE_URL = (fileId: string, accessToken?: string) => {
+  let url = `${FILES_URL()}/${encodeURIComponent(fileId)}`;
+  if (accessToken) {
+    url += `?access_token=${encodeURIComponent(accessToken)}`;
+  }
+  return url;
+};
