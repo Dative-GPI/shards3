a6885e77c60820233b95aee264b181aa1125c283
peyronth <peyronth@users.noreply.github.com>
2024-10-09 13:12:17 +0000

    deploy: fa92b7cd2a678d34499e5b5485b456b7cf7b1384

---
+++ assets/RichTextField.stories-C105Cuo3.js
@@ -0,0 +1,239 @@
+import{F as o}from"./FSRichTextField-CwObgcqh.js";import{F as s}from"./FSCol-CvoATp5Q.js";import{F as c}from"./FSText-BIOb5fBT.js";import{F as v}from"./FSTextField-BLVYx7ij.js";import{_ as y}from"./FSRow-BnsweOMX.js";import"./vue.esm-bundler-CWdIlc2r.js";import"./FSAutocompleteField-DbBWI7VU.js";import"./FSSearchField-_SEFeUDc.js";import"./FSButton-Bc8wnoJs.js";import"./FSClickable-JTcIAbNo.js";import"./FSCard-BxmDRzjE.js";import"./useColors-CrLBSafx.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DfSX31J9.js";import"./theme-DkyKeiX7.js";import"./css-BWDYXGFo.js";import"./useBreakpoints-INFVV7da.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./VProgressCircular-CbhEC8Of.js";import"./color-B0htL_ZM.js";import"./resizeObserver-CTZzan_-.js";import"./VIcon-Bo7R9vIQ.js";import"./FSSpan-Bnbdrku6.js";import"./useSlots-zwOl0abH.js";import"./FSIcon-CyS8zLbP.js";import"./useTranslations-CEnu7PZQ.js";import"./FSDialogMenu-D9HgWnX3.js";import"./VDialog-B02D0PNk.js";import"./VOverlay-m3LxlMVX.js";import"./forwardRefs-DWGaNmQL.js";import"./proxiedModel-BN8GSVE7.js";import"./anchor-CiFPvmMB.js";import"./dimensions-Cku-Ba4r.js";import"./display-4460MzAg.js";import"./lazy-Bq8C33UW.js";import"./locale-BGMMnFfJ.js";import"./router-lEm-a6ws.js";import"./transition-BvKGRC-_.js";import"./VDefaultsProvider-DPT0EuQR.js";import"./FSSlideGroup-qOOOxz_-.js";import"./uuid-DTaye2KM.js";import"./FSButtonNextIcon-DhMVExMM.js";import"./VSlideGroupItem-CGRm5ZGO.js";import"./group-D4GO8Lly.js";import"./index-D2mfwXxT.js";import"./FSToggleSet-7WnTN5nE.js";import"./FSWrapGroup-BTCmZHSW.js";import"./VInput-Clj2quuL.js";import"./density-Cf5q3L_6.js";import"./form-D-VMHmUh.js";import"./FSBaseField-BlQcLyVQ.js";import"./VSpacer-DLFo12A6.js";import"./FSCheckbox-bKRtBqVc.js";import"./useRules-CY0rKEBQ.js";import"./VSelectionControl-Bl6rLftH.js";import"./index-E_I8r0kf.js";import"./VLabel-hsX9xD9s.js";import"./FSFadeOut-BhgieYVE.js";import"./FSLoader-h4slsrT9.js";import"./elevation-DH_sKWqU.js";import"./FSRadio-n9JXvfPW.js";import"./VList-Cl6AOEM5.js";import"./ssrBoot-QID7OAaG.js";import"./border-ByF30Ccd.js";import"./rounded-C73YOAN4.js";import"./VImg-Bj7MyRJQ.js";import"./index-BPQCAcxH.js";import"./VSelect-D5P4hFmo.js";import"./VMenu-OPxqXTkz.js";import"./filter-pNTMaczj.js";import"./VField-Bf3tV3N7.js";import"./loader-DuCiGAVb.js";const _e={title:"Foundation/Shared/Input fields/RichTextField",component:o,tags:["autodocs"],argTypes:{}},e={args:{args:{value1:'{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Hello I am ","type":"text","version":1},{"type":"variable","version":1,"code":"name","defaultValue":"defaultName"}],"direction":"ltr","format":"","indent":0,"type":"heading","version":1,"tag":"h1"},{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"I come from ","type":"text","version":1},{"type":"variable","version":1,"code":"country","defaultValue":"World"},{"detail":0,"format":0,"mode":"normal","style":"","text":" and I am ","type":"text","version":1},{"type":"variable","version":1,"code":"age","defaultValue":"defaultAge"},{"detail":0,"format":0,"mode":"normal","style":"","text":".","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1},{"children":[],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}',variablePreset1:[{code:"name",defaultValue:"DefaultName",label:"Name"},{code:"age",defaultValue:"18",label:"Age"},{code:"country",defaultValue:"World",label:"Country"},{code:"city",defaultValue:"Capital",label:"City"}],variableValues1:{name:"John",age:25},value2:'{"root":{"children":[{"children":[],"direction":null,"format":"","indent":0,"type":"paragraph","version":1}],"direction":null,"format":"","indent":0,"type":"root","version":1}}',value3:'{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Click ","type":"text","version":1},{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"here","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"link","version":1,"rel":"noreferrer","target":"_blank","title":"","url":"https://www.dative-gpi.com/"},{"detail":0,"format":0,"mode":"normal","style":"","text":" to visit a marvelous website","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}',value4:`{"root": {"type": "root", "children": [{"type": "paragraph", "children": [{"text": "Recette pour environ 50 baguettes tradition avec le process Paneotrad", "type": "text"}]}, {"type": "paragraph", "children": []}, {"tag": "h5", "type": "heading", "format": "", "indent": 0, "version": 1, "children": [{"mode": "normal", "text": "Ingrédients", "type": "text", "style": "", "detail": 0, "format": 0, "version": 1}], "direction": "ltr"}, {"type": "paragraph", "format": "", "indent": 0, "version": 1, "children": [], "direction": null}, {"type": "paragraph", "format": "", "indent": 0, "version": 1, "children": [{"mode": "normal", "text": "10 kg de farine de Tradition T65", "type": "text", "style": "", "detail": 0, "format": 0, "version": 1}], "direction": "ltr"}, {"type": "paragraph", "format": "", "indent": 0, "version": 1, "children": [{"mode": "normal", "text": "6,8 L + 0,4 L d'eau", "type": "text", "style": "", "detail": 0, "format": 0, "version": 1}], "direction": "ltr"}, {"type": "paragraph", "format": "", "indent": 0, "version": 1, "children": [{"mode": "normal", "text": "70 g de levure", "type": "text", "style": "", "detail": 0, "format": 0, "version": 1}], "direction": "ltr"}, {"type": "paragraph", "format": "", "indent": 0, "version": 1, "children": [{"mode": "normal", "text": "180 g de sel", "type": "text", "style": "", "detail": 0, "format": 0, "version": 1}], "direction": "ltr"}]}}`}},render:(r,{argTypes:a})=>({components:{FSRichTextField:o,FSCol:s},props:Object.keys(a),setup(){return{...r}},template:`
+    <FSCol>
+      <FSRichTextField
+        label="Rich text"
+        :variableReferences="args.variablePreset1"
+        v-model="args.value4"
+      />
+      <FSRichTextField
+        label="Rich text"
+        variant="readonly"
+        :variableValues="args.variableValues1"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSRichTextField
+        label="Rich text, 2 rows"
+        :rows="2"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSRichTextField
+        label="Uneditable rich text, with description"
+        description="Uneditable description"
+        :editable="false"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSRichTextField
+        label="Readonly rich text"
+        description="Readonly description"
+        variant="readonly"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSRichTextField
+        label="Readonly rich text"
+        emptyLabel="Empty label (modelValue linked with the Rich text, 2 rows modelValue)"
+        variant="readonly"
+        v-model="args.value2"
+      />
+    </FSCol>`})},t={args:{modelValue:'{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Hello I am ","type":"text","version":1},{"type":"variable","version":1,"code":"name","defaultValue":"defaultName"}],"direction":"ltr","format":"","indent":0,"type":"heading","version":1,"tag":"h1"},{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"I come from ","type":"text","version":1},{"type":"variable","version":1,"code":"country","defaultValue":"World"},{"detail":0,"format":0,"mode":"normal","style":"","text":" and I am ","type":"text","version":1},{"type":"variable","version":1,"code":"age","defaultValue":"defaultAge"},{"detail":0,"format":0,"mode":"normal","style":"","text":".","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1},{"children":[],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}',variableReferences:[{code:"name",defaultValue:"DefaultName",label:"Name"},{code:"age",defaultValue:"18",label:"Age"},{code:"country",defaultValue:"World",label:"Country"},{code:"city",defaultValue:"Capital",label:"City"}],variableValues:{name:"John",age:"25"}},render:(r,{argTypes:a})=>({components:{FSRichTextField:o,FSCol:s,FSText:c,FSTextField:v,FSRow:y},props:Object.keys(a),setup(){return{args:r}},template:`
+    <FSCol
+      gap="24px"
+    >
+      <FSCol>
+        <FSRichTextField
+          :variableReferences="args.variableReferences"
+          v-model="args.modelValue"
+        />
+      </FSCol>
+      <FSCol>
+        <FSText
+          font="text-button">
+          Variables
+        </FSText>
+        <FSRow>
+          <FSTextField
+            v-for="variable in args.variableReferences"
+            :key="variable.code"
+            :label="variable.label"
+            v-model="args.variableValues[variable.code]"
+          />
+        </FSRow>
+      </FSCol>
+      <FSCol>
+        <FSText
+          font="text-button">
+          Result
+        </FSText>
+        <FSRichTextField
+          variant="readonly"
+          :variableValues="args.variableValues"
+          :modelValue="args.modelValue"
+        />
+      </FSCol>
+    </FSCol>`})};var n,l,i;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: "{\\"root\\":{\\"children\\":[{\\"children\\":[{\\"detail\\":0,\\"format\\":0,\\"mode\\":\\"normal\\",\\"style\\":\\"\\",\\"text\\":\\"Hello I am \\",\\"type\\":\\"text\\",\\"version\\":1},{\\"type\\":\\"variable\\",\\"version\\":1,\\"code\\":\\"name\\",\\"defaultValue\\":\\"defaultName\\"}],\\"direction\\":\\"ltr\\",\\"format\\":\\"\\",\\"indent\\":0,\\"type\\":\\"heading\\",\\"version\\":1,\\"tag\\":\\"h1\\"},{\\"children\\":[{\\"detail\\":0,\\"format\\":0,\\"mode\\":\\"normal\\",\\"style\\":\\"\\",\\"text\\":\\"I come from \\",\\"type\\":\\"text\\",\\"version\\":1},{\\"type\\":\\"variable\\",\\"version\\":1,\\"code\\":\\"country\\",\\"defaultValue\\":\\"World\\"},{\\"detail\\":0,\\"format\\":0,\\"mode\\":\\"normal\\",\\"style\\":\\"\\",\\"text\\":\\" and I am \\",\\"type\\":\\"text\\",\\"version\\":1},{\\"type\\":\\"variable\\",\\"version\\":1,\\"code\\":\\"age\\",\\"defaultValue\\":\\"defaultAge\\"},{\\"detail\\":0,\\"format\\":0,\\"mode\\":\\"normal\\",\\"style\\":\\"\\",\\"text\\":\\".\\",\\"type\\":\\"text\\",\\"version\\":1}],\\"direction\\":\\"ltr\\",\\"format\\":\\"\\",\\"indent\\":0,\\"type\\":\\"paragraph\\",\\"version\\":1},{\\"children\\":[],\\"direction\\":\\"ltr\\",\\"format\\":\\"\\",\\"indent\\":0,\\"type\\":\\"paragraph\\",\\"version\\":1}],\\"direction\\":\\"ltr\\",\\"format\\":\\"\\",\\"indent\\":0,\\"type\\":\\"root\\",\\"version\\":1}}",
+      variablePreset1: [{
+        code: "name",
+        defaultValue: "DefaultName",
+        label: "Name"
+      }, {
+        code: "age",
+        defaultValue: "18",
+        label: "Age"
+      }, {
+        code: "country",
+        defaultValue: "World",
+        label: "Country"
+      }, {
+        code: "city",
+        defaultValue: "Capital",
+        label: "City"
+      }],
+      variableValues1: {
+        name: "John",
+        age: 25
+      },
+      value2: "{\\"root\\":{\\"children\\":[{\\"children\\":[],\\"direction\\":null,\\"format\\":\\"\\",\\"indent\\":0,\\"type\\":\\"paragraph\\",\\"version\\":1}],\\"direction\\":null,\\"format\\":\\"\\",\\"indent\\":0,\\"type\\":\\"root\\",\\"version\\":1}}",
+      value3: "{\\"root\\":{\\"children\\":[{\\"children\\":[{\\"detail\\":0,\\"format\\":0,\\"mode\\":\\"normal\\",\\"style\\":\\"\\",\\"text\\":\\"Click \\",\\"type\\":\\"text\\",\\"version\\":1},{\\"children\\":[{\\"detail\\":0,\\"format\\":0,\\"mode\\":\\"normal\\",\\"style\\":\\"\\",\\"text\\":\\"here\\",\\"type\\":\\"text\\",\\"version\\":1}],\\"direction\\":\\"ltr\\",\\"format\\":\\"\\",\\"indent\\":0,\\"type\\":\\"link\\",\\"version\\":1,\\"rel\\":\\"noreferrer\\",\\"target\\":\\"_blank\\",\\"title\\":\\"\\",\\"url\\":\\"https://www.dative-gpi.com/\\"},{\\"detail\\":0,\\"format\\":0,\\"mode\\":\\"normal\\",\\"style\\":\\"\\",\\"text\\":\\" to visit a marvelous website\\",\\"type\\":\\"text\\",\\"version\\":1}],\\"direction\\":\\"ltr\\",\\"format\\":\\"\\",\\"indent\\":0,\\"type\\":\\"paragraph\\",\\"version\\":1}],\\"direction\\":\\"ltr\\",\\"format\\":\\"\\",\\"indent\\":0,\\"type\\":\\"root\\",\\"version\\":1}}",
+      value4: "{\\"root\\": {\\"type\\": \\"root\\", \\"children\\": [{\\"type\\": \\"paragraph\\", \\"children\\": [{\\"text\\": \\"Recette pour environ 50 baguettes tradition avec le process Paneotrad\\", \\"type\\": \\"text\\"}]}, {\\"type\\": \\"paragraph\\", \\"children\\": []}, {\\"tag\\": \\"h5\\", \\"type\\": \\"heading\\", \\"format\\": \\"\\", \\"indent\\": 0, \\"version\\": 1, \\"children\\": [{\\"mode\\": \\"normal\\", \\"text\\": \\"Ingrédients\\", \\"type\\": \\"text\\", \\"style\\": \\"\\", \\"detail\\": 0, \\"format\\": 0, \\"version\\": 1}], \\"direction\\": \\"ltr\\"}, {\\"type\\": \\"paragraph\\", \\"format\\": \\"\\", \\"indent\\": 0, \\"version\\": 1, \\"children\\": [], \\"direction\\": null}, {\\"type\\": \\"paragraph\\", \\"format\\": \\"\\", \\"indent\\": 0, \\"version\\": 1, \\"children\\": [{\\"mode\\": \\"normal\\", \\"text\\": \\"10 kg de farine de Tradition T65\\", \\"type\\": \\"text\\", \\"style\\": \\"\\", \\"detail\\": 0, \\"format\\": 0, \\"version\\": 1}], \\"direction\\": \\"ltr\\"}, {\\"type\\": \\"paragraph\\", \\"format\\": \\"\\", \\"indent\\": 0, \\"version\\": 1, \\"children\\": [{\\"mode\\": \\"normal\\", \\"text\\": \\"6,8 L + 0,4 L d'eau\\", \\"type\\": \\"text\\", \\"style\\": \\"\\", \\"detail\\": 0, \\"format\\": 0, \\"version\\": 1}], \\"direction\\": \\"ltr\\"}, {\\"type\\": \\"paragraph\\", \\"format\\": \\"\\", \\"indent\\": 0, \\"version\\": 1, \\"children\\": [{\\"mode\\": \\"normal\\", \\"text\\": \\"70 g de levure\\", \\"type\\": \\"text\\", \\"style\\": \\"\\", \\"detail\\": 0, \\"format\\": 0, \\"version\\": 1}], \\"direction\\": \\"ltr\\"}, {\\"type\\": \\"paragraph\\", \\"format\\": \\"\\", \\"indent\\": 0, \\"version\\": 1, \\"children\\": [{\\"mode\\": \\"normal\\", \\"text\\": \\"180 g de sel\\", \\"type\\": \\"text\\", \\"style\\": \\"\\", \\"detail\\": 0, \\"format\\": 0, \\"version\\": 1}], \\"direction\\": \\"ltr\\"}]}}"
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSRichTextField,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSRichTextField
+        label="Rich text"
+        :variableReferences="args.variablePreset1"
+        v-model="args.value4"
+      />
+      <FSRichTextField
+        label="Rich text"
+        variant="readonly"
+        :variableValues="args.variableValues1"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSRichTextField
+        label="Rich text, 2 rows"
+        :rows="2"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSRichTextField
+        label="Uneditable rich text, with description"
+        description="Uneditable description"
+        :editable="false"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSRichTextField
+        label="Readonly rich text"
+        description="Readonly description"
+        variant="readonly"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSRichTextField
+        label="Readonly rich text"
+        emptyLabel="Empty label (modelValue linked with the Rich text, 2 rows modelValue)"
+        variant="readonly"
+        v-model="args.value2"
+      />
+    </FSCol>\`
+  })
+}`,...(i=(l=e.parameters)==null?void 0:l.docs)==null?void 0:i.source}}};var d,m,p;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    modelValue: "{\\"root\\":{\\"children\\":[{\\"children\\":[{\\"detail\\":0,\\"format\\":0,\\"mode\\":\\"normal\\",\\"style\\":\\"\\",\\"text\\":\\"Hello I am \\",\\"type\\":\\"text\\",\\"version\\":1},{\\"type\\":\\"variable\\",\\"version\\":1,\\"code\\":\\"name\\",\\"defaultValue\\":\\"defaultName\\"}],\\"direction\\":\\"ltr\\",\\"format\\":\\"\\",\\"indent\\":0,\\"type\\":\\"heading\\",\\"version\\":1,\\"tag\\":\\"h1\\"},{\\"children\\":[{\\"detail\\":0,\\"format\\":0,\\"mode\\":\\"normal\\",\\"style\\":\\"\\",\\"text\\":\\"I come from \\",\\"type\\":\\"text\\",\\"version\\":1},{\\"type\\":\\"variable\\",\\"version\\":1,\\"code\\":\\"country\\",\\"defaultValue\\":\\"World\\"},{\\"detail\\":0,\\"format\\":0,\\"mode\\":\\"normal\\",\\"style\\":\\"\\",\\"text\\":\\" and I am \\",\\"type\\":\\"text\\",\\"version\\":1},{\\"type\\":\\"variable\\",\\"version\\":1,\\"code\\":\\"age\\",\\"defaultValue\\":\\"defaultAge\\"},{\\"detail\\":0,\\"format\\":0,\\"mode\\":\\"normal\\",\\"style\\":\\"\\",\\"text\\":\\".\\",\\"type\\":\\"text\\",\\"version\\":1}],\\"direction\\":\\"ltr\\",\\"format\\":\\"\\",\\"indent\\":0,\\"type\\":\\"paragraph\\",\\"version\\":1},{\\"children\\":[],\\"direction\\":\\"ltr\\",\\"format\\":\\"\\",\\"indent\\":0,\\"type\\":\\"paragraph\\",\\"version\\":1}],\\"direction\\":\\"ltr\\",\\"format\\":\\"\\",\\"indent\\":0,\\"type\\":\\"root\\",\\"version\\":1}}",
+    variableReferences: [{
+      code: "name",
+      defaultValue: "DefaultName",
+      label: "Name"
+    }, {
+      code: "age",
+      defaultValue: "18",
+      label: "Age"
+    }, {
+      code: "country",
+      defaultValue: "World",
+      label: "Country"
+    }, {
+      code: "city",
+      defaultValue: "Capital",
+      label: "City"
+    }],
+    variableValues: {
+      name: "John",
+      age: "25"
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSRichTextField,
+      FSCol,
+      FSText,
+      FSTextField,
+      FSRow
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+    <FSCol
+      gap="24px"
+    >
+      <FSCol>
+        <FSRichTextField
+          :variableReferences="args.variableReferences"
+          v-model="args.modelValue"
+        />
+      </FSCol>
+      <FSCol>
+        <FSText
+          font="text-button">
+          Variables
+        </FSText>
+        <FSRow>
+          <FSTextField
+            v-for="variable in args.variableReferences"
+            :key="variable.code"
+            :label="variable.label"
+            v-model="args.variableValues[variable.code]"
+          />
+        </FSRow>
+      </FSCol>
+      <FSCol>
+        <FSText
+          font="text-button">
+          Result
+        </FSText>
+        <FSRichTextField
+          variant="readonly"
+          :variableValues="args.variableValues"
+          :modelValue="args.modelValue"
+        />
+      </FSCol>
+    </FSCol>\`
+  })
+}`,...(p=(m=t.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const De=["Default","Variables"];export{e as Default,t as Variables,De as __namedExportsOrder,_e as default};
