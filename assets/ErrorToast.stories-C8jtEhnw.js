254dab9b8752c534cfea7d14a6756013f0b1f3da
peyronth <peyronth@users.noreply.github.com>
2024-10-09 08:11:35 +0000

    deploy: b2ea52d54b254301eca6e27153911f959e2bfb43

---
+++ assets/ErrorToast.stories-C8jtEhnw.js
@@ -0,0 +1,69 @@
+import{F as n}from"./FSErrorToast-DYFxFRHi.js";import{_ as i}from"./FSRow-CFFgZ74C.js";import{F as p}from"./FSButton-C0EbsNmZ.js";import"./vue.esm-bundler-DTB_q9xr.js";import"./FSIcon-CaaGuK6D.js";import"./useBreakpoints-ERBVGIQe.js";import"./useColors-BWKJNLz8.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DfSX31J9.js";import"./theme-CX_N7n5F.js";import"./css-B7hQ34V-.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./VIcon-DzZvThha.js";import"./color-Du5JXqac.js";import"./FSSpan-DGSHCe_l.js";import"./useSlots-BnylMYIF.js";import"./FSCol-CYWCfIBP.js";import"./FSClickable-BkJRwj5f.js";import"./FSCard-C6D9gNrD.js";import"./VProgressCircular-DjSdELIn.js";import"./resizeObserver-BhfhgZOk.js";const j={title:"Foundation/Shared/ErrorToast",component:n,tags:["autodocs"]},r={args:{args:{code1:"errors.missingorganisation",code2:"errors.noadminprivilege",code3:"errors.alreadyregistereduser"}},render:(a,{argTypes:s})=>({components:{FSErrorToast:n,FSRow:i,FSButton:p},props:Object.keys(s),setup(){return{...a}},template:`
+      <FSRow gap="10px" align="bottom-center">
+        <FSErrorToast
+          :errorCode="args.code1"
+          variant="standard"
+        >
+        <template #footer>
+          <FSRow align="center-center">
+            <FSButton
+              color="error"
+              label="Delete"
+              variant="full"
+            />
+          </FSRow>
+        </template>
+        </FSErrorToast>
+        <FSErrorToast
+          :errorCode="args.code2"
+        />
+        <FSErrorToast
+          :errorCode="args.code3"
+        />
+      </FSRow>`})};var o,e,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    args: {
+      code1: "errors.missingorganisation",
+      code2: "errors.noadminprivilege",
+      code3: "errors.alreadyregistereduser"
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSErrorToast,
+      FSRow,
+      FSButton
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+      <FSRow gap="10px" align="bottom-center">
+        <FSErrorToast
+          :errorCode="args.code1"
+          variant="standard"
+        >
+        <template #footer>
+          <FSRow align="center-center">
+            <FSButton
+              color="error"
+              label="Delete"
+              variant="full"
+            />
+          </FSRow>
+        </template>
+        </FSErrorToast>
+        <FSErrorToast
+          :errorCode="args.code2"
+        />
+        <FSErrorToast
+          :errorCode="args.code3"
+        />
+      </FSRow>\`
+  })
+}`,...(t=(e=r.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const k=["Variations"];export{r as Variations,k as __namedExportsOrder,j as default};
