bd0484d17b7c2005332d1b11b2bec5c7dab28bd9
dimitrilexi <dimitrilexi@users.noreply.github.com>
2024-10-10 12:14:13 +0000

    deploy: 594b0216bbafcca1bc92ffcfe415bc84053394fa

---
+++ assets/TimeRangeField.stories-CITI2SP-.js
@@ -0,0 +1,92 @@
+import{d as $,c as d,y as H,z as S,A as T,C as p,N as o,F as E}from"./vue.esm-bundler-DTB_q9xr.js";import{F as k}from"./FSSelectField-CUwTiuac.js";import{F as B}from"./FSBaseField-D2wwYX9r.js";import{F as O}from"./FSClock-BsPvJo5Z.js";import{_ as y}from"./FSRow-CFFgZ74C.js";import{a as i,d as A}from"./timeRangeTools-NJNoYYCS.js";import{u as I}from"./useRules-D0gcbU57.js";import{a as h,D as s}from"./dates-DFP4a3ym.js";import{C as L}from"./useColors-BWKJNLz8.js";import{u as N}from"./useTranslations-BehwFxDb.js";import{g as j}from"./enumTools-BEsapygt.js";import{_ as z}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{F as G}from"./FSCol-CYWCfIBP.js";import"./FSDialogMenu-BA4IKkHH.js";import"./FSCard-C6D9gNrD.js";import"./css-B7hQ34V-.js";import"./useBreakpoints-ERBVGIQe.js";import"./VDialog-5T0GPOZ7.js";import"./VOverlay-Coy41zxg.js";import"./forwardRefs-DWGaNmQL.js";import"./color-Du5JXqac.js";import"./theme-CX_N7n5F.js";import"./proxiedModel-Atn7Z0Ko.js";import"./anchor-0mwNUa2-.js";import"./dimensions-CqoZOW6j.js";import"./display-BhWGQDVt.js";import"./lazy-CgkI0iJG.js";import"./locale-BJQwrXJ7.js";import"./router-DxFny788.js";import"./transition-DhTIerNz.js";import"./VDefaultsProvider-B8_oQgnX.js";import"./FSSlideGroup--K704DO9.js";import"./uuid-DTaye2KM.js";import"./FSButtonNextIcon-DF3eQVXp.js";import"./FSButton-C0EbsNmZ.js";import"./FSClickable-BkJRwj5f.js";import"./VProgressCircular-DjSdELIn.js";import"./resizeObserver-BhfhgZOk.js";import"./VIcon-DzZvThha.js";import"./FSSpan-DGSHCe_l.js";import"./useSlots-BnylMYIF.js";import"./FSIcon-CaaGuK6D.js";import"./VSlideGroupItem-D8F5yfrm.js";import"./group-CBQYGNTq.js";import"./index-UH0ZuYvc.js";import"./FSToggleSet-CvGKl15-.js";import"./FSWrapGroup-Dplk4rjv.js";import"./VInput-DGUvNq3c.js";import"./density-CmGe4Vfq.js";import"./form-_oo3cshG.js";import"./FSTextField-B-binuYf.js";import"./VField-Do2gtyNA.js";import"./VLabel-33JaQml4.js";import"./loader-BYp4SDfR.js";import"./rounded-ErtzAGHK.js";import"./index-DS9MhfY5.js";import"./FSCheckbox-Bdwx0Alv.js";import"./VSelectionControl-CFrRrZff.js";import"./index-DMxVfgfA.js";import"./FSFadeOut-d8eb0S_F.js";import"./FSLoader-IaO7i80m.js";import"./elevation-JfXKLVfD.js";import"./FSRadio-DA5JqUMw.js";import"./VList-CgmTLoPW.js";import"./ssrBoot-BTOmRKcz.js";import"./VSpacer-Di7g3RCw.js";import"./border-Va--AsO6.js";import"./VImg-C1aVTsth.js";import"./VSelect-oAa5CmWC.js";import"./VMenu-CuKNZEBs.js";import"./FSSlider-COcqcW8-.js";import"./VSlider-CGSYcLks.js";import"./useDateFormat-0HRqVufQ.js";import"./datesTools-CGhnvN9k.js";import"./startOfWeek-uXTpkxA4.js";import"./startOfDay-C4pDH4rb.js";import"./useAppLanguageCode-CJD7t5rC.js";import"./useAppTimeZone-ByDiY2xy.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DfSX31J9.js";const w=$({name:"FSTimeRangeField",components:{FSSelectField:k,FSBaseField:B,FSClock:O,FSRow:y},props:{label:{type:String,required:!1,default:null},description:{type:String,required:!1,default:null},modelValue:{type:Object,required:!0,default:null},hideHeader:{type:Boolean,required:!1,default:!1},required:{type:Boolean,required:!1,default:!1},rules:{type:Array,required:!1,default:()=>[]},messages:{type:Array,required:!1,default:null},editable:{type:Boolean,required:!1,default:!0},showVariant:{type:Boolean,required:!1,default:!1}},emits:["update:modelValue"],setup(e,{emit:r}){const{$tr:c}=N(),{validateOn:D,getMessages:F}=I(),M=[{id:h.Local,label:c("ui.common.local","Local")},{id:h.UTC,label:c("ui.common.utc","UTC")}],u=d(()=>j(s).map(t=>({id:t.value,label:A(t.value)}))),a=d(()=>i(e.modelValue,!1)),v=d(()=>a.value?a.value.startHour*60*60*1e3+a.value.startMinute*60*1e3:0),m=d(()=>a.value?a.value.endHour*60*60*1e3+a.value.endMinute*60*1e3:0),l=d(()=>e.messages??F(e.modelValue,e.rules));return{dateTypeItems:M,validateOn:D,daysItems:u,ColorEnum:L,startTime:v,realTime:a,messages:l,DateType:h,endTime:m,onChangeHourStart:t=>{const n=t?Math.floor(t/36e5):0,g=t?Math.floor(t%(60*60*1e3)/(60*1e3)):0,V={startDay:a.value.startDay,startHour:n,startMinute:g,endDay:a.value.endDay,endHour:a.value.endHour,endMinute:a.value.endMinute,variant:a.value.variant},b=i(V,!0);r("update:modelValue",b)},onChangeDayStart:t=>{if(t===s.AllDays)r("update:modelValue",{startDay:t,startHour:e.modelValue.startHour,startMinute:e.modelValue.startMinute,endDay:t,endHour:e.modelValue.endHour,endMinute:e.modelValue.endMinute,variant:e.modelValue.variant});else{const n=i({startDay:t,startHour:a.value.startHour,startMinute:a.value.startMinute,endDay:e.modelValue.endDay==s.AllDays?t:a.value.endDay,endHour:a.value.endHour,endMinute:a.value.endMinute,variant:a.value.variant},!0);r("update:modelValue",n)}},onChangeHourEnd:t=>{const n=t?Math.floor(t/36e5):0,g=t?Math.floor(t%(60*60*1e3)/(60*1e3)):0;if(a.value){const V={startDay:a.value.startDay,startHour:a.value.startHour,startMinute:a.value.startMinute,endDay:a.value.endDay,endHour:n,endMinute:g,variant:a.value.variant},b=i(V,!0);r("update:modelValue",b)}},onUpdateVariant:t=>{const n=i({startDay:a.value.startDay,startHour:a.value.startHour,startMinute:a.value.startMinute,endDay:a.value.endDay,endHour:a.value.endHour,endMinute:a.value.endMinute,variant:t},!0);r("update:modelValue",n)},onChangeDayEnd:t=>{if(t===s.AllDays){r("update:modelValue",{startDay:t,startHour:e.modelValue.startHour,startMinute:e.modelValue.startMinute,endDay:t,endHour:e.modelValue.endHour,endMinute:e.modelValue.endMinute,variant:e.modelValue.variant});return}else{const n=i({startDay:e.modelValue.startDay==s.AllDays?t:a.value.startDay,startHour:a.value.startHour,startMinute:a.value.startMinute,endDay:t,endHour:a.value.endHour,endMinute:a.value.endMinute,variant:a.value.variant},!0);r("update:modelValue",n)}}}}});function J(e,r,c,D,F,M){const u=H("FSSelectField"),a=H("FSClock"),v=H("FSBaseField");return S(),T(v,{description:e.$props.description,hideHeader:e.$props.hideHeader,required:e.$props.required,editable:e.$props.editable,label:e.$props.label,messages:e.messages},{default:p(()=>[o(y,null,{default:p(()=>{var m;return[o(y,{wrap:!1},{default:p(()=>{var l;return[o(u,{validationValue:e.$props.modelValue,editable:e.$props.editable,validateOn:e.validateOn,rules:e.$props.rules,items:e.daysItems,hideHeader:!0,clearable:!1,modelValue:((l=e.realTime)==null?void 0:l.startDay)??0,"onUpdate:modelValue":e.onChangeDayStart},null,8,["validationValue","editable","validateOn","rules","items","modelValue","onUpdate:modelValue"]),o(a,{editable:e.$props.editable,color:e.ColorEnum.Light,slider:!1,modelValue:e.startTime,"onUpdate:modelValue":e.onChangeHourStart},null,8,["editable","color","modelValue","onUpdate:modelValue"])]}),_:1}),o(y,{wrap:!1},{default:p(()=>{var l;return[o(u,{editable:e.$props.editable,items:e.daysItems,hideHeader:!0,clearable:!1,modelValue:((l=e.realTime)==null?void 0:l.endDay)??0,"onUpdate:modelValue":e.onChangeDayEnd},null,8,["editable","items","modelValue","onUpdate:modelValue"]),o(a,{editable:e.$props.editable,color:e.ColorEnum.Light,slider:!1,modelValue:e.endTime,"onUpdate:modelValue":e.onChangeHourEnd},null,8,["editable","color","modelValue","onUpdate:modelValue"])]}),_:1}),e.$props.showVariant?(S(),T(u,{key:0,width:"hug",label:e.$tr("ui.common.type","Type"),items:e.dateTypeItems,hideHeader:!0,clearable:!1,modelValue:((m=e.modelValue)==null?void 0:m.variant)??e.DateType.Local,"onUpdate:modelValue":e.onUpdateVariant},null,8,["label","items","modelValue","onUpdate:modelValue"])):E("",!0)]}),_:1})]),_:1},8,["description","hideHeader","required","editable","label","messages"])}const U=z(w,[["render",J]]);w.__docgenInfo={displayName:"FSTimeRangeField",exportName:"default",description:"",tags:{},props:[{name:"label",type:{name:"string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"description",type:{name:"string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"modelValue",type:{name:"DateTimeRange"},required:!0,defaultValue:{func:!1,value:"null"}},{name:"hideHeader",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}},{name:"required",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}},{name:"rules",type:{name:"any[]"},required:!1,defaultValue:{func:!0,value:"() => []"}},{name:"messages",type:{name:"string[]"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"editable",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"true"}},{name:"showVariant",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}}],events:[{name:"update:modelValue"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/shared/foundation-shared-components/components/fields/FSTimeRangeField.vue"]};const Da={title:"Foundation/Shared/Input fields/TimeRangeField",component:U,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},f={args:{args:{value0:{startDay:0,startHour:22,startMinute:0,endDay:0,endHour:0,endMinute:0,variant:2},value1:{startDay:0,startHour:0,startMinute:0,endDay:0,endHour:0,endMinute:0,variant:1},value2:{startDay:1,startHour:1,startMinute:0,endDay:2,endHour:2,endMinute:0,variant:1}}},render:(e,{argTypes:r})=>({components:{FSTimeRangeField:U,FSCol:G},props:Object.keys(r),setup(){return{...e}},template:`
+    <FSCol>
+      <FSTimeRangeField
+        label="Time Range"
+        :modelValue="args.value0"
+        :showVariant="true"
+        v-model="args.value0"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTimeRangeField
+        label="Required time Range, with description"
+        description="Description for this field"
+        :required="true"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTimeRangeField
+        label="Uneditable time Range, with description"
+        description="Uneditable description"
+        :editable="false"
+        v-model="args.value2"
+      />
+    </FSCol>`})};var C,q,R;f.parameters={...f.parameters,docs:{...(C=f.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value0: {
+        startDay: 0,
+        startHour: 22,
+        startMinute: 0,
+        endDay: 0,
+        endHour: 0,
+        endMinute: 0,
+        variant: 2
+      },
+      value1: {
+        startDay: 0,
+        startHour: 0,
+        startMinute: 0,
+        endDay: 0,
+        endHour: 0,
+        endMinute: 0,
+        variant: 1
+      },
+      value2: {
+        startDay: 1,
+        startHour: 1,
+        startMinute: 0,
+        endDay: 2,
+        endHour: 2,
+        endMinute: 0,
+        variant: 1
+      }
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSTimeRangeField,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSTimeRangeField
+        label="Time Range"
+        :modelValue="args.value0"
+        :showVariant="true"
+        v-model="args.value0"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTimeRangeField
+        label="Required time Range, with description"
+        description="Description for this field"
+        :required="true"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTimeRangeField
+        label="Uneditable time Range, with description"
+        description="Uneditable description"
+        :editable="false"
+        v-model="args.value2"
+      />
+    </FSCol>\`
+  })
+}`,...(R=(q=f.parameters)==null?void 0:q.docs)==null?void 0:R.source}}};const Fa=["Variations"];export{f as Variations,Fa as __namedExportsOrder,Da as default};
---
+++ assets/enumTools-BEsapygt.js
@@ -0,0 +1,1 @@
+const r=t=>Object.keys(t).filter(e=>isNaN(Number(e))).map(e=>({key:e,value:t[e]}));export{r as g};
