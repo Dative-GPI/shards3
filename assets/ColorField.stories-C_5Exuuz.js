54f832c55ff2d9c3450e5150412a0a493ee1782c
peyronth <peyronth@users.noreply.github.com>
2024-10-10 09:55:43 +0000

    deploy: 32a77d04b33ebdd1f4a343d479d7142baa2d1c04

---
+++ assets/ColorField.stories-C_5Exuuz.js
@@ -0,0 +1,93 @@
+import{F as i}from"./FSColorField-DnRBpidT.js";import{F as p}from"./FSCol-CYWCfIBP.js";import"./vue.esm-bundler-DTB_q9xr.js";import"./FSBaseField-D2wwYX9r.js";import"./FSSpan-DGSHCe_l.js";import"./useBreakpoints-ERBVGIQe.js";import"./useSlots-BnylMYIF.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FSRow-CFFgZ74C.js";import"./css-B7hQ34V-.js";import"./useColors-BWKJNLz8.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DfSX31J9.js";import"./theme-CX_N7n5F.js";import"./VSpacer-Di7g3RCw.js";import"./color-Du5JXqac.js";import"./FSCard-C6D9gNrD.js";import"./FSIcon-CaaGuK6D.js";import"./VIcon-DzZvThha.js";import"./FSText-7jyalPrS.js";import"./VMenu-CuKNZEBs.js";import"./VOverlay-Coy41zxg.js";import"./forwardRefs-DWGaNmQL.js";import"./proxiedModel-Atn7Z0Ko.js";import"./anchor-0mwNUa2-.js";import"./dimensions-CqoZOW6j.js";import"./display-BhWGQDVt.js";import"./lazy-CgkI0iJG.js";import"./locale-BJQwrXJ7.js";import"./router-DxFny788.js";import"./transition-DhTIerNz.js";import"./VDefaultsProvider-B8_oQgnX.js";import"./resizeObserver-BhfhgZOk.js";import"./VBtn-BUwP05fv.js";import"./border-Va--AsO6.js";import"./density-CmGe4Vfq.js";import"./elevation-JfXKLVfD.js";import"./rounded-ErtzAGHK.js";import"./group-CBQYGNTq.js";import"./loader-BYp4SDfR.js";import"./VProgressCircular-DjSdELIn.js";import"./index-DMxVfgfA.js";import"./VSlider-CGSYcLks.js";import"./index-UH0ZuYvc.js";import"./VInput-DGUvNq3c.js";import"./form-_oo3cshG.js";import"./VLabel-33JaQml4.js";const ro={title:"Foundation/Shared/Input fields/ColorField",component:i,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},o={args:{args:{value1:"something",opacity1:1,value2:"#00FF12",opacity2:.2,value3:"primary",opacity3:1,value4:"primary",opacity4:.5}},render:(l,{argTypes:a})=>({components:{FSColorField:i,FSCol:p},props:Object.keys(a),setup(){return{...l}},template:`
+    <FSCol>
+      <FSColorField
+        label="Color"
+        :allowOpacity="false"
+        v-model="args.value1"
+        v-model:opacityValue="args.opacity1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSColorField
+        label="Required color, with description"
+        description="Description for this field"
+        :required="true"
+        v-model="args.value2"
+        v-model:opacityValue="args.opacity2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSColorField
+        :hideHeader="true"
+        :onlyBaseColors="false"      
+        v-model="args.value3"
+        v-model:opacityValue="args.opacity3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSColorField
+        label="Base color picker"
+        :editable="false"
+        :onlyBaseColors="false"     
+        description="Description for this field"
+        v-model="args.value4"
+        v-model:opacityValue="args.opacity4"
+      />
+    </FSCol>`})};var r,e,t;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: "something",
+      opacity1: 1,
+      value2: "#00FF12",
+      opacity2: 0.2,
+      value3: "primary",
+      opacity3: 1,
+      value4: "primary",
+      opacity4: 0.5
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSColorField,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSColorField
+        label="Color"
+        :allowOpacity="false"
+        v-model="args.value1"
+        v-model:opacityValue="args.opacity1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSColorField
+        label="Required color, with description"
+        description="Description for this field"
+        :required="true"
+        v-model="args.value2"
+        v-model:opacityValue="args.opacity2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSColorField
+        :hideHeader="true"
+        :onlyBaseColors="false"      
+        v-model="args.value3"
+        v-model:opacityValue="args.opacity3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSColorField
+        label="Base color picker"
+        :editable="false"
+        :onlyBaseColors="false"     
+        description="Description for this field"
+        v-model="args.value4"
+        v-model:opacityValue="args.opacity4"
+      />
+    </FSCol>\`
+  })
+}`,...(t=(e=o.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const eo=["Variations"];export{o as Variations,eo as __namedExportsOrder,ro as default};
---
+++ assets/SimpleUserOrganisationList.stories-Db4ED3xO.js
@@ -0,0 +1,104 @@
+import{a as t}from"./chunk-MZXVCX43-CM0pFb8Z.js";import o from"./FSSimpleUserOrganisationsList-CU_9KT67.js";import{F as l}from"./FSSimpleList-B1PTPpvG.js";import{F as g}from"./FSTile-NKozYWaP.js";import"./v4-CQkTLCs1.js";import"./vue.esm-bundler-DTB_q9xr.js";import"./useUserOrganisations-B4HIo215.js";import"./permissionInfos-BPDgTHQl.js";import"./base-XmH0JLOD.js";import"./useAppOrganisationId-C2nceDRs.js";import"./serviceFactory-DAEdCiSt.js";import"./_commonjsHelpers-BosuxZz1.js";import"./lodash-BiW_TGGX.js";import"./composableFactory-CG8osPlO.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FSRow-CFFgZ74C.js";import"./css-B7hQ34V-.js";import"./useBreakpoints-ERBVGIQe.js";import"./FSCol-CYWCfIBP.js";import"./FSFadeOut-d8eb0S_F.js";import"./uuid-DTaye2KM.js";import"./useColors-BWKJNLz8.js";import"./index-DfSX31J9.js";import"./theme-CX_N7n5F.js";import"./FSSlideGroup--K704DO9.js";import"./FSButtonNextIcon-DF3eQVXp.js";import"./FSButton-C0EbsNmZ.js";import"./FSClickable-BkJRwj5f.js";import"./FSCard-C6D9gNrD.js";import"./VProgressCircular-DjSdELIn.js";import"./color-Du5JXqac.js";import"./resizeObserver-BhfhgZOk.js";import"./VIcon-DzZvThha.js";import"./FSSpan-DGSHCe_l.js";import"./useSlots-BnylMYIF.js";import"./FSIcon-CaaGuK6D.js";import"./VSlideGroupItem-D8F5yfrm.js";import"./group-CBQYGNTq.js";import"./proxiedModel-Atn7Z0Ko.js";import"./index-UH0ZuYvc.js";import"./display-BhWGQDVt.js";import"./locale-BJQwrXJ7.js";import"./FSSearchField-CAI8fEsN.js";import"./FSTextField-B-binuYf.js";import"./FSBaseField-D2wwYX9r.js";import"./VSpacer-Di7g3RCw.js";import"./useRules-D0gcbU57.js";import"./VField-Do2gtyNA.js";import"./transition-DhTIerNz.js";import"./VLabel-33JaQml4.js";import"./VInput-DGUvNq3c.js";import"./density-CmGe4Vfq.js";import"./form-_oo3cshG.js";import"./loader-BYp4SDfR.js";import"./anchor-0mwNUa2-.js";import"./rounded-ErtzAGHK.js";import"./VDefaultsProvider-B8_oQgnX.js";import"./forwardRefs-DWGaNmQL.js";import"./index-DS9MhfY5.js";import"./useTranslations-BehwFxDb.js";import"./FSImage-BI5HPXDV.js";import"./FSImageUI-DJb3mSkN.js";import"./FSLoader-IaO7i80m.js";import"./dimensions-CqoZOW6j.js";import"./elevation-JfXKLVfD.js";import"./VImg-C1aVTsth.js";import"./useImages-DHwuL45g.js";import"./base-CmdGny12.js";import"./useAppAuthToken-B5E2e0uj.js";import"./FSButtonEditIcon-CXvW9leR.js";import"./FSButtonRemoveIcon-BfvzWFF8.js";import"./filter-C1K_d8Vd.js";import"./FSCheckbox-Bdwx0Alv.js";import"./VSelectionControl-CFrRrZff.js";import"./index-DMxVfgfA.js";const We={title:"Foundation/Core/Lists/Simple Lists/SimpleUserOrganisationsList",component:o,subcomponents:{FSSimpleList:l,FSTile:g},tags:["autodocs"],argTypes:{direction:{control:"select",options:["column","row"]},"click:edit":{action:"click:edit"},"click:remove":{action:"click:remove"}}},i={render:e=>({components:{FSSimpleUserOrganisationsList:o},setup(){return{args:e}},template:`
+      <FSSimpleUserOrganisationsList
+        :maxHeight="args.maxHeight"
+        :showEdit="args.showEdit"
+        :showRemove="args.showRemove"
+        :showDraggable="args.showDraggable"
+        :direction="args.direction"
+        :itemLabel="args.itemLabel"
+        :searchable="args.searchable"
+        :tileHeight="args.tileHeight"
+        direction="row"
+        @click:edit="args['click:edit']"
+        @click:remove="args['click:remove']"
+      />
+    `}),args:{maxHeight:100,showEdit:!1,showRemove:!1,showDraggable:!1,direction:"row",itemLabel:"name",searchable:!0,tileHeight:"60px",tileProps:e=>({onClick:()=>t("onClick:item")(e)}),"click:edit":t("click:edit"),"click:remove":t("click:remove")}},r={args:{maxHeight:0,showEdit:!0,showRemove:!0,showDraggable:!1,direction:"row",itemLabel:"name",searchable:!1,tileHeight:"60px"},render:e=>({components:{FSSimpleUserOrganisationsList:o},setup(){return{args:e}},template:`
+      <FSSimpleUserOrganisationsList
+        :maxHeight="args.maxHeight"
+        :showEdit="args.showEdit"
+        :showRemove="args.showRemove"
+        :showDraggable="args.showDraggable"
+        :direction="args.direction"
+        :itemLabel="args.itemLabel"
+        :searchable="args.searchable"
+        :tileHeight="args.tileHeight"
+        @click:edit="args['click:edit']"
+        @click:remove="args['click:remove']"
+      />
+    `})};var a,s,m;i.parameters={...i.parameters,docs:{...(a=i.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      FSSimpleUserOrganisationsList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <FSSimpleUserOrganisationsList
+        :maxHeight="args.maxHeight"
+        :showEdit="args.showEdit"
+        :showRemove="args.showRemove"
+        :showDraggable="args.showDraggable"
+        :direction="args.direction"
+        :itemLabel="args.itemLabel"
+        :searchable="args.searchable"
+        :tileHeight="args.tileHeight"
+        direction="row"
+        @click:edit="args['click:edit']"
+        @click:remove="args['click:remove']"
+      />
+    \`
+  }),
+  args: {
+    maxHeight: 100,
+    showEdit: false,
+    showRemove: false,
+    showDraggable: false,
+    direction: "row",
+    itemLabel: "name",
+    searchable: true,
+    tileHeight: '60px',
+    tileProps: item => ({
+      onClick: () => action("onClick:item")(item)
+    }),
+    "click:edit": action("click:edit"),
+    "click:remove": action("click:remove")
+  }
+}`,...(m=(s=i.parameters)==null?void 0:s.docs)==null?void 0:m.source}}};var n,c,p;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    maxHeight: 0,
+    showEdit: true,
+    showRemove: true,
+    showDraggable: false,
+    direction: "row",
+    itemLabel: "name",
+    searchable: false,
+    tileHeight: '60px'
+  },
+  render: args => ({
+    components: {
+      FSSimpleUserOrganisationsList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <FSSimpleUserOrganisationsList
+        :maxHeight="args.maxHeight"
+        :showEdit="args.showEdit"
+        :showRemove="args.showRemove"
+        :showDraggable="args.showDraggable"
+        :direction="args.direction"
+        :itemLabel="args.itemLabel"
+        :searchable="args.searchable"
+        :tileHeight="args.tileHeight"
+        @click:edit="args['click:edit']"
+        @click:remove="args['click:remove']"
+      />
+    \`
+  })
+}`,...(p=(c=r.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};const _e=["Default","ListWithoutSearch"];export{i as Default,r as ListWithoutSearch,_e as __namedExportsOrder,We as default};
