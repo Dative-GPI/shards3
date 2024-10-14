912b508796daeaa8ad5596f688a63558e3558d5f
rduteil <rduteil@users.noreply.github.com>
2024-10-14 15:04:21 +0000

    deploy: f972f6dd10941b75c419929f591bf22a71a28997

---
+++ assets/BaseLocationsList.stories-CA03snf4.js
@@ -0,0 +1,27 @@
+import i from"./FSBaseLocationsList-BkBkqxUA.js";import{F as e}from"./FSDataTable-D3E_ve_q.js";import"./lodash-BiW_TGGX.js";import"./_commonjsHelpers-BosuxZz1.js";import"./vue.esm-bundler-DTB_q9xr.js";import"./FSIcon-CaaGuK6D.js";import"./useBreakpoints-ERBVGIQe.js";import"./useColors-BWKJNLz8.js";import"./index-DfSX31J9.js";import"./theme-CX_N7n5F.js";import"./css-B7hQ34V-.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./VIcon-DzZvThha.js";import"./color-Du5JXqac.js";import"./FSLocationTileUI-4WzkX01P.js";import"./FSIconCard-DaZBidMJ.js";import"./FSCard-C6D9gNrD.js";import"./FSCol-CYWCfIBP.js";import"./FSRow-CFFgZ74C.js";import"./FSColor-BQxGv4SQ.js";import"./FSSpan-DGSHCe_l.js";import"./useSlots-BnylMYIF.js";import"./FSTile-NKozYWaP.js";import"./FSClickable-BkJRwj5f.js";import"./VProgressCircular-DjSdELIn.js";import"./resizeObserver-BhfhgZOk.js";import"./FSCheckbox-Bdwx0Alv.js";import"./useRules-D0gcbU57.js";import"./VSelectionControl-CFrRrZff.js";import"./density-CmGe4Vfq.js";import"./proxiedModel-Atn7Z0Ko.js";import"./index-DMxVfgfA.js";import"./VLabel-33JaQml4.js";import"./VInput-DGUvNq3c.js";import"./locale-BJQwrXJ7.js";import"./index-UH0ZuYvc.js";import"./transition-DhTIerNz.js";import"./form-_oo3cshG.js";import"./useLocations-CjGzyaax.js";import"./address-CE2z3AEI.js";import"./base-XmH0JLOD.js";import"./useAppOrganisationId-C2nceDRs.js";import"./serviceFactory-DAEdCiSt.js";import"./composableFactory-CG8osPlO.js";import"./FSLoader-BFHjXl9p.js";import"./dimensions-CqoZOW6j.js";import"./elevation-JfXKLVfD.js";import"./VSpacer-Di7g3RCw.js";import"./FSDataTableUI-B590w3FH.js";import"./vue-router-Bp8FxDoy.js";import"./uuid-DTaye2KM.js";import"./FSText-7jyalPrS.js";import"./FSSearchField-CAI8fEsN.js";import"./FSTextField-B-binuYf.js";import"./FSBaseField-D2wwYX9r.js";import"./FSButton-C0EbsNmZ.js";import"./VField-Do2gtyNA.js";import"./loader-BYp4SDfR.js";import"./anchor-0mwNUa2-.js";import"./rounded-ErtzAGHK.js";import"./VDefaultsProvider-B8_oQgnX.js";import"./forwardRefs-DWGaNmQL.js";import"./index-DS9MhfY5.js";import"./useTranslations-BehwFxDb.js";import"./FSSelectField-D3PrfN9X.js";import"./FSDialogMenu-BA4IKkHH.js";import"./VDialog-5T0GPOZ7.js";import"./VOverlay-Coy41zxg.js";import"./display-BhWGQDVt.js";import"./lazy-CgkI0iJG.js";import"./router-DxFny788.js";import"./FSSlideGroup--K704DO9.js";import"./FSButtonNextIcon-DF3eQVXp.js";import"./VSlideGroupItem-D8F5yfrm.js";import"./group-CBQYGNTq.js";import"./FSToggleSet-CvGKl15-.js";import"./FSWrapGroup-Dplk4rjv.js";import"./FSFadeOut-DevRILST.js";import"./FSRadio-DA5JqUMw.js";import"./VList-CgmTLoPW.js";import"./ssrBoot-BTOmRKcz.js";import"./border-Va--AsO6.js";import"./VImg-C1aVTsth.js";import"./VSelect-oAa5CmWC.js";import"./VMenu-CuKNZEBs.js";import"./FSDivider-BNA1ohhL.js";import"./FSChip-D8HPW226.js";import"./FSOptionGroup-CYF5vXq9.js";import"./filter-C1K_d8Vd.js";import"./VBtn-BUwP05fv.js";import"./filter-COIlslko.js";const Po={title:"Foundation/Core/Lists/Base Lists/BaseLocationsList",component:i,subcomponents:{FSDataTable:e},tags:["autodocs"],argTypes:{}},o={render:p=>({components:{FSBaseLocationsList:i},setup(){return{args:p}},template:`
+      <FSBaseLocationsList
+        v-model:modelValue="args.modelValue"
+        :tableCode="args.tableCode"
+      />
+    `}),args:{modelValue:[],tableCode:"locations1"}};var t,r,m;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      FSBaseLocationsList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <FSBaseLocationsList
+        v-model:modelValue="args.modelValue"
+        :tableCode="args.tableCode"
+      />
+    \`
+  }),
+  args: {
+    modelValue: [],
+    tableCode: 'locations1'
+  }
+}`,...(m=(r=o.parameters)==null?void 0:r.docs)==null?void 0:m.source}}};const Qo=["Default"];export{o as Default,Qo as __namedExportsOrder,Po as default};
