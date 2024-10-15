84d791b5c456906f0ee032f1bd2792f7fd91f576
SchroterQuentin <SchroterQuentin@users.noreply.github.com>
2024-10-15 07:24:37 +0000

    deploy: 3c98f8cdac87c00cd60e36655ecc6f3c834ece0b

---
+++ assets/BaseFoldersList.stories-iaqBMdfF.js
@@ -0,0 +1,24 @@
+import m from"./FSBaseFoldersList-7VnBdVHJ.js";import{F as e}from"./FSDataTable-TiBFRwrO.js";import"./lodash-BiW_TGGX.js";import"./_commonjsHelpers-BosuxZz1.js";import"./vue.esm-bundler-DTB_q9xr.js";import"./FSIcon-8KdV556C.js";import"./useBreakpoints-ERBVGIQe.js";import"./useColors-CeHYzu6a.js";import"./index-3zl-tJp_.js";import"./theme-CX_N7n5F.js";import"./css-B7hQ34V-.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./VIcon-DzZvThha.js";import"./color-Du5JXqac.js";import"./FSFolderTileUI-CSv6rLfL.js";import"./FSSimpleTileUI-BR8k9-C2.js";import"./FSIconCard-0iP8e_wx.js";import"./FSCard-B1dz5tzT.js";import"./FSCol-CYWCfIBP.js";import"./FSRow-CFFgZ74C.js";import"./FSImage-CQBut9Qm.js";import"./FSImageUI-BSBOp46R.js";import"./FSLoader-DZRtMKjr.js";import"./dimensions-CqoZOW6j.js";import"./elevation-JfXKLVfD.js";import"./locale-BJQwrXJ7.js";import"./VImg-C1aVTsth.js";import"./rounded-ErtzAGHK.js";import"./transition-DhTIerNz.js";import"./index-DS9MhfY5.js";import"./useImages-DHwuL45g.js";import"./base-CmdGny12.js";import"./serviceFactory-DAEdCiSt.js";import"./composableFactory-CG8osPlO.js";import"./useAppAuthToken-B5E2e0uj.js";import"./FSSpan-DGSHCe_l.js";import"./useSlots-BnylMYIF.js";import"./FSTile-CeYrXfsZ.js";import"./FSClickable-BhVwKM7P.js";import"./VProgressCircular-DjSdELIn.js";import"./resizeObserver-BhfhgZOk.js";import"./FSCheckbox-D8gZetLs.js";import"./useRules-D0gcbU57.js";import"./VSelectionControl-CFrRrZff.js";import"./density-CmGe4Vfq.js";import"./proxiedModel-Atn7Z0Ko.js";import"./index-DMxVfgfA.js";import"./VLabel-33JaQml4.js";import"./VInput-DGUvNq3c.js";import"./index-UH0ZuYvc.js";import"./form-_oo3cshG.js";import"./useFolders-PK1DE3Vk.js";import"./pathCrumb-Db-cq5HI.js";import"./base-XmH0JLOD.js";import"./useAppOrganisationId-C2nceDRs.js";import"./dashboards-Dy3_kHT6.js";import"./dashboards-JSNI7pIu.js";import"./useTranslations-BehwFxDb.js";import"./VSpacer-Di7g3RCw.js";import"./FSDataTableUI-DVROxwfa.js";import"./vue-router-Bp8FxDoy.js";import"./uuid-DTaye2KM.js";import"./FSText-GEqj0Yf_.js";import"./FSSearchField-qOslkjkq.js";import"./FSTextField-CB_VZ4XI.js";import"./FSBaseField-CMNvz_3q.js";import"./FSButton-D5EwxixH.js";import"./VField-Do2gtyNA.js";import"./loader-BYp4SDfR.js";import"./anchor-0mwNUa2-.js";import"./VDefaultsProvider-B8_oQgnX.js";import"./forwardRefs-DWGaNmQL.js";import"./FSSelectField-llvlalV-.js";import"./FSDialogMenu-YRnMwSLB.js";import"./VDialog-5T0GPOZ7.js";import"./VOverlay-Coy41zxg.js";import"./display-BhWGQDVt.js";import"./lazy-CgkI0iJG.js";import"./router-DxFny788.js";import"./FSSlideGroup-CkmcxhiK.js";import"./FSButtonNextIcon-DTv-WVMA.js";import"./VSlideGroupItem-D8F5yfrm.js";import"./group-CBQYGNTq.js";import"./FSToggleSet-BfQXxOjP.js";import"./FSWrapGroup-DVMuqcCh.js";import"./FSFadeOut-BcBUJmEE.js";import"./FSRadio-Cnla-8h4.js";import"./VList-CgmTLoPW.js";import"./ssrBoot-BTOmRKcz.js";import"./border-Va--AsO6.js";import"./VSelect-615V5VDz.js";import"./VMenu-CuKNZEBs.js";import"./FSDivider-D0i7_MA_.js";import"./FSChip-B9LiwYIc.js";import"./FSOptionGroup-eNZlGB0Q.js";import"./filter-C1K_d8Vd.js";import"./VBtn-BUwP05fv.js";import"./filter-COIlslko.js";const Yt={title:"Foundation/Core/Lists/Base Lists/BaseFoldersList",component:m,subcomponents:{FSDataTable:e},tags:["autodocs"],argTypes:{}},t={render:i=>({components:{FSBaseFoldersList:m},setup(){return{args:i}},template:`
+      <FSBaseFoldersList
+        :tableCode="args.tableCode"
+      />
+    `}),args:{tableCode:"folders1"}};var r,o,p;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      FSBaseFoldersList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <FSBaseFoldersList
+        :tableCode="args.tableCode"
+      />
+    \`
+  }),
+  args: {
+    tableCode: 'folders1'
+  }
+}`,...(p=(o=t.parameters)==null?void 0:o.docs)==null?void 0:p.source}}};const Zt=["Default"];export{t as Default,Zt as __namedExportsOrder,Yt as default};
---
+++ assets/BaseGroupsList.stories-DgmcWOTv.js
@@ -0,0 +1,27 @@
+import p from"./FSBaseGroupsList-CT6W127C.js";import{F as e}from"./FSDataTable-TiBFRwrO.js";import"./vue.esm-bundler-DTB_q9xr.js";import"./lodash-BiW_TGGX.js";import"./_commonjsHelpers-BosuxZz1.js";import"./FSIcon-8KdV556C.js";import"./useBreakpoints-ERBVGIQe.js";import"./useColors-CeHYzu6a.js";import"./index-3zl-tJp_.js";import"./theme-CX_N7n5F.js";import"./css-B7hQ34V-.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./VIcon-DzZvThha.js";import"./color-Du5JXqac.js";import"./FSImage-CQBut9Qm.js";import"./FSImageUI-BSBOp46R.js";import"./FSLoader-DZRtMKjr.js";import"./dimensions-CqoZOW6j.js";import"./elevation-JfXKLVfD.js";import"./locale-BJQwrXJ7.js";import"./VImg-C1aVTsth.js";import"./rounded-ErtzAGHK.js";import"./transition-DhTIerNz.js";import"./index-DS9MhfY5.js";import"./useImages-DHwuL45g.js";import"./base-CmdGny12.js";import"./serviceFactory-DAEdCiSt.js";import"./composableFactory-CG8osPlO.js";import"./useAppAuthToken-B5E2e0uj.js";import"./FSTagGroup-BBQdwUMV.js";import"./FSWrapGroup-DVMuqcCh.js";import"./uuid-DTaye2KM.js";import"./useSlots-BnylMYIF.js";import"./VSlideGroupItem-D8F5yfrm.js";import"./group-CBQYGNTq.js";import"./proxiedModel-Atn7Z0Ko.js";import"./index-UH0ZuYvc.js";import"./display-BhWGQDVt.js";import"./resizeObserver-BhfhgZOk.js";import"./FSTag-B-5O6Uat.js";import"./FSSpan-DGSHCe_l.js";import"./FSRow-CFFgZ74C.js";import"./VBtn-BUwP05fv.js";import"./border-Va--AsO6.js";import"./density-CmGe4Vfq.js";import"./loader-BYp4SDfR.js";import"./VProgressCircular-DjSdELIn.js";import"./anchor-0mwNUa2-.js";import"./router-DxFny788.js";import"./index-DMxVfgfA.js";import"./VDefaultsProvider-B8_oQgnX.js";import"./FSSlideGroup-CkmcxhiK.js";import"./FSButtonNextIcon-DTv-WVMA.js";import"./FSButton-D5EwxixH.js";import"./FSClickable-BhVwKM7P.js";import"./FSCard-B1dz5tzT.js";import"./FSCol-CYWCfIBP.js";import"./FSGroupTileUI-CAFT99uO.js";import"./FSColor-CgZZAfyB.js";import"./FSTile-CeYrXfsZ.js";import"./FSCheckbox-D8gZetLs.js";import"./useRules-D0gcbU57.js";import"./VSelectionControl-CFrRrZff.js";import"./VLabel-33JaQml4.js";import"./VInput-DGUvNq3c.js";import"./form-_oo3cshG.js";import"./useGroups-CUtD8zj6.js";import"./pathCrumb-Db-cq5HI.js";import"./base-XmH0JLOD.js";import"./useAppOrganisationId-C2nceDRs.js";import"./VSpacer-Di7g3RCw.js";import"./FSDataTableUI-DVROxwfa.js";import"./vue-router-Bp8FxDoy.js";import"./FSText-GEqj0Yf_.js";import"./FSSearchField-qOslkjkq.js";import"./FSTextField-CB_VZ4XI.js";import"./FSBaseField-CMNvz_3q.js";import"./VField-Do2gtyNA.js";import"./forwardRefs-DWGaNmQL.js";import"./useTranslations-BehwFxDb.js";import"./FSSelectField-llvlalV-.js";import"./FSDialogMenu-YRnMwSLB.js";import"./VDialog-5T0GPOZ7.js";import"./VOverlay-Coy41zxg.js";import"./lazy-CgkI0iJG.js";import"./FSToggleSet-BfQXxOjP.js";import"./FSFadeOut-BcBUJmEE.js";import"./FSRadio-Cnla-8h4.js";import"./VList-CgmTLoPW.js";import"./ssrBoot-BTOmRKcz.js";import"./VSelect-615V5VDz.js";import"./VMenu-CuKNZEBs.js";import"./FSDivider-D0i7_MA_.js";import"./FSChip-B9LiwYIc.js";import"./FSOptionGroup-eNZlGB0Q.js";import"./filter-C1K_d8Vd.js";import"./filter-COIlslko.js";const Yo={title:"Foundation/Core/Lists/Base Lists/BaseGroupsList",component:p,subcomponents:{FSDataTable:e},tags:["autodocs"],argTypes:{}},o={render:i=>({components:{FSBaseGroupsList:p},setup(){return{args:i}},template:`
+      <FSBaseGroupsList
+        v-model:modelValue="args.modelValue"
+        :tableCode="args.tableCode"
+      />
+    `}),args:{modelValue:[],tableCode:"groups1"}};var t,r,m;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      FSBaseGroupsList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <FSBaseGroupsList
+        v-model:modelValue="args.modelValue"
+        :tableCode="args.tableCode"
+      />
+    \`
+  }),
+  args: {
+    modelValue: [],
+    tableCode: 'groups1'
+  }
+}`,...(m=(r=o.parameters)==null?void 0:r.docs)==null?void 0:m.source}}};const Zo=["Default"];export{o as Default,Zo as __namedExportsOrder,Yo as default};
