66e629b5ecda40a9ff877c2bc5fb0d638c0984b4
gballigand <balligandgm@gmail.com>
2024-10-16 11:47:47 +0200

    review before pr

--- src/core/foundation-core-components/components/autocompletes/FSAutocompleteRole.vue
+++ src/core/foundation-core-components/components/autocompletes/FSAutocompleteRole.vue
@@ -154,12 +154,12 @@ export default defineComponent({
       if (Array.isArray(value)) {
         emit("update:modelValue", value.map(v => v.id));
         emit("update:type", value.map(v => v.type));
-        emit("update", { value: value.map(v => v.id), type: value.map(v => v.type) });
+        emit("update", { modelValue: value.map(v => v.id), type: value.map(v => v.type) });
       }
       else {
         emit("update:modelValue", value?.id);
         emit("update:type", value?.type);
-        emit("update", { value: value?.id, type: value?.type });
+        emit("update", { modelValue: value?.id, type: value?.type });
       }
     };
 
