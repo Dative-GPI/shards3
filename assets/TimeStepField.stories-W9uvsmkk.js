48170d5b1924bc38914cc842c2cca1b90cbaf8c2
SchroterQuentin <SchroterQuentin@users.noreply.github.com>
2024-10-18 16:02:32 +0000

    deploy: 7994094f1fef299ca5e9da89baa82fd8dc1727f5

---
+++ assets/TimeStepField.stories-W9uvsmkk.js
@@ -0,0 +1,73 @@
+import o from"./FSTimeStepField-C-Cpu8Xw.js";import{F as n}from"./FSCol-DD5MeAoT.js";import"./vue.esm-bundler-l-siv0w9.js";import"./FSNumberField-hZ8lT6yP.js";import"./FSTextField-CIXfVN3n.js";import"./FSBaseField-CfZ7AiUY.js";import"./FSSpan-DwE651wI.js";import"./useBreakpoints-mbnqo5a1.js";import"./useSlots-C_N_WAus.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FSRow-BjeBkMPV.js";import"./css-C_oz6kYw.js";import"./useColors-Bs2u1_6J.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DfSX31J9.js";import"./theme-C2MnUTNI.js";import"./FSButton-cXGosxtu.js";import"./FSClickable-B8_y_vmt.js";import"./FSCard-Cocsqd0c.js";import"./VProgressCircular-RI9KLr8_.js";import"./color-De-77ojK.js";import"./resizeObserver-CjMDZ25n.js";import"./VIcon-D6ql3n-m.js";import"./FSIcon-DfqW6nqQ.js";import"./useRules-QHk8iD4m.js";import"./VField-DaI8sxeG.js";import"./index-B089V-om.js";import"./transition-AZsQzdUZ.js";import"./VLabel-Dswg7Ov5.js";import"./VInput-BkciVi2d.js";import"./locale-CTwwbPAi.js";import"./density-Dt_pQxP_.js";import"./proxiedModel-KpugNjnZ.js";import"./form-CLJq5y5w.js";import"./loader-avZiZGL_.js";import"./anchor-C2Y5FAYh.js";import"./rounded-uHrmshEX.js";import"./VDefaultsProvider-CfqLszOw.js";import"./forwardRefs-DWGaNmQL.js";import"./index-C6OXlV61.js";import"./FSSelectField-DqO1gPa4.js";import"./FSDialogMenu-vSYpuZ_d.js";import"./VDialog-BXBtUIC2.js";import"./VOverlay-Dwc5HfFO.js";import"./dimensions-9QoL2_-9.js";import"./display-BWzbFTnS.js";import"./lazy-DuBjUwFb.js";import"./router-CIHGWUSM.js";import"./FSSlideGroup-CntZC7f2.js";import"./uuid-DTaye2KM.js";import"./FSButtonNextIcon-F5m9he5w.js";import"./VSlideGroupItem-BXc1MbHI.js";import"./group-Dk9dfj8w.js";import"./FSToggleSet-92mUnG-8.js";import"./FSWrapGroup-CgUIplkM.js";import"./FSCheckbox-BKUwhDef.js";import"./VSelectionControl-Br2dHavx.js";import"./index-Cr304eD2.js";import"./FSFadeOut-CszUv8uk.js";import"./FSLoader-DoOybHH0.js";import"./elevation-BZDXH0CP.js";import"./FSRadio-CfSjScM8.js";import"./VList-Bzb582fb.js";import"./ssrBoot-DD96sY1N.js";import"./border-DXAdoVnk.js";import"./VImg-CZhZqiQJ.js";import"./VSelect-R5W67bQu.js";import"./VMenu-DQq2E1HW.js";import"./time-CxERy6W9.js";import"./useTranslations-CJK8D24x.js";const xt={title:"Foundation/Shared/Input fields/TimeStepField",component:o,tags:["autodocs"]},t={args:{args:{value1:{unit:2,value:0},value2:{unit:4,value:1},value3:{unit:5,value:1}}},render:(p,{argTypes:m})=>({components:{FSTimeStepField:o,FSCol:n},props:Object.keys(m),setup(){return{...p}},template:`
+    <FSCol>
+      <FSTimeStepField
+        label="Time"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTimeStepField
+        label="Required time, with description"
+        description="Description for this field"
+        :required="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTimeStepField
+        label="Uneditable time, with description"
+        description="Uneditable description"
+        :editable="false"
+        v-model="args.value3"
+      />
+    </FSCol>`})};var i,e,r;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: {
+        unit: 2,
+        value: 0
+      },
+      value2: {
+        unit: 4,
+        value: 1
+      },
+      value3: {
+        unit: 5,
+        value: 1
+      }
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSTimeStepField,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSTimeStepField
+        label="Time"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTimeStepField
+        label="Required time, with description"
+        description="Description for this field"
+        :required="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTimeStepField
+        label="Uneditable time, with description"
+        description="Uneditable description"
+        :editable="false"
+        v-model="args.value3"
+      />
+    </FSCol>\`
+  })
+}`,...(r=(e=t.parameters)==null?void 0:e.docs)==null?void 0:r.source}}};const Ct=["Variations"];export{t as Variations,Ct as __namedExportsOrder,xt as default};
