e5350cd01dd4734770e04174fe886f0fe45c55d1
SchroterQuentin <SchroterQuentin@users.noreply.github.com>
2024-10-24 15:13:14 +0000

    deploy: d8b023e3e626a31543fad3527e1c4dc1fcb7fe08

---
+++ assets/SelectField.stories-BZG_p_lT.js
@@ -0,0 +1,203 @@
+import{F as l}from"./FSSelectField-BkUcFimZ.js";import{F as u}from"./FSForm-4A05UEAw.js";import{F as p}from"./FSCol-BvqytbKT.js";import{_ as g}from"./FSRow-CbEgVN08.js";import{S as c}from"./rules-BafDybJE.js";import"./vue.esm-bundler-l-siv0w9.js";import"./FSDialogMenu-2hgW8eHf.js";import"./FSCard-DL4SLpmS.js";import"./useColors-Bs2u1_6J.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DfSX31J9.js";import"./theme-C2MnUTNI.js";import"./css-CP2C2-Fe.js";import"./useBreakpoints-C5IzU8MC.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./VDialog-BXBtUIC2.js";import"./VOverlay-Dwc5HfFO.js";import"./forwardRefs-DWGaNmQL.js";import"./color-De-77ojK.js";import"./proxiedModel-KpugNjnZ.js";import"./anchor-C2Y5FAYh.js";import"./dimensions-9QoL2_-9.js";import"./display-BWzbFTnS.js";import"./lazy-DuBjUwFb.js";import"./locale-CTwwbPAi.js";import"./router-CIHGWUSM.js";import"./transition-AZsQzdUZ.js";import"./VDefaultsProvider-CfqLszOw.js";import"./FSSlideGroup-CBieAKh7.js";import"./uuid-DTaye2KM.js";import"./FSButtonNextIcon-B9vgItlM.js";import"./FSButton-DcUuAZqz.js";import"./FSClickable-Up0p3Vk8.js";import"./VProgressCircular-RI9KLr8_.js";import"./resizeObserver-CjMDZ25n.js";import"./VIcon-D6ql3n-m.js";import"./FSSpan-Di50moR2.js";import"./useSlots-C_N_WAus.js";import"./FSIcon-uIr9VdFq.js";import"./VSlideGroupItem-BXc1MbHI.js";import"./group-Dk9dfj8w.js";import"./index-B089V-om.js";import"./FSToggleSet-B0bG_RDz.js";import"./FSWrapGroup-cmIYWMlH.js";import"./VInput-BkciVi2d.js";import"./density-Dt_pQxP_.js";import"./form-CLJq5y5w.js";import"./FSBaseField-CpNJbDpF.js";import"./FSTextField-CaG6gJzS.js";import"./useRules-QHk8iD4m.js";import"./VField-DaI8sxeG.js";import"./VLabel-Dswg7Ov5.js";import"./loader-avZiZGL_.js";import"./rounded-uHrmshEX.js";import"./index-C6OXlV61.js";import"./FSCheckbox-BlsFOPfN.js";import"./VSelectionControl-Br2dHavx.js";import"./index-Cr304eD2.js";import"./FSFadeOut-BUOQ-fRr.js";import"./FSLoader-kY4Od3NB.js";import"./elevation-BZDXH0CP.js";import"./FSRadio-CqjI9hQz.js";import"./VList-Bzb582fb.js";import"./ssrBoot-DD96sY1N.js";import"./border-DXAdoVnk.js";import"./VImg-CZhZqiQJ.js";import"./VSelect-CELx6cWV.js";import"./VMenu-DQq2E1HW.js";import"./useDateFormat-BNPZ7oj9.js";import"./datesTools-CGhnvN9k.js";import"./startOfWeek-uXTpkxA4.js";import"./startOfDay-C4pDH4rb.js";import"./useAppLanguageCode-BzH56rSm.js";import"./useAppTimeZone-gpO-zv_9.js";import"./useTranslations-CJK8D24x.js";import"./time-CxERy6W9.js";const Me={title:"Foundation/Shared/Input fields/SelectField",component:l,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},t={args:{args:{items:Array.from(Array(5).keys()).map(e=>({id:e.toString(),label:`Option ${e+1}`})),value1:null,value2:null,value3:["0","1","2"],value4:"2",value5:"2"}},render:(e,{argTypes:i})=>({components:{FSSelectField:l,FSCol:p},props:Object.keys(i),setup(){return{...e}},template:`
+    <FSCol>
+      <FSSelectField
+        label="Select"
+        :items="args.items"
+        v-model="args.value1"
+      />
+      <FSSelectField
+        label="Select with suffix"
+        suffix="Hello there"
+        :items="args.items"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSelectField
+        label="Multiple select"
+        :items="args.items"
+        :multiple="true"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSelectField
+        label="Required select, with description"
+        description="Description for this field"
+        :required="true"
+        :items="args.items"
+        v-model="args.value4"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSelectField
+        label="Uneditable select, with description"
+        description="Uneditable description"
+        :editable="false"
+        :items="args.items"
+        v-model="args.value5"
+      />
+    </FSCol>`})},r={args:{args:{valid:!1,items:Array.from(Array(5).keys()).map(e=>({id:e.toString(),label:`Option ${e+1}`})),value1:null,value2:null,value3:null,rules:c}},render:(e,{argTypes:i})=>({components:{FSForm:u,FSSelectField:l,FSCol:p,FSRow:g},props:Object.keys(i),setup(){return{...e}},template:`
+    <FSForm v-model="args.valid" variant="standard">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSSelectField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :items="args.items"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSSelectField
+          label="Rules: min 2 options"
+          :rules="[args.rules.min(2)]"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSSelectField
+          label="Rules: required & min 2 options & max 3 options"
+          :rules="[args.rules.required(), args.rules.min(2), args.rules.max(3)]"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>`})};var o,s,n;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    args: {
+      items: Array.from(Array(5).keys()).map(i => ({
+        id: i.toString(),
+        label: \`Option \${i + 1}\`
+      })),
+      value1: null,
+      value2: null,
+      value3: ["0", "1", "2"],
+      value4: "2",
+      value5: "2"
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSSelectField,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSSelectField
+        label="Select"
+        :items="args.items"
+        v-model="args.value1"
+      />
+      <FSSelectField
+        label="Select with suffix"
+        suffix="Hello there"
+        :items="args.items"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSelectField
+        label="Multiple select"
+        :items="args.items"
+        :multiple="true"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSelectField
+        label="Required select, with description"
+        description="Description for this field"
+        :required="true"
+        :items="args.items"
+        v-model="args.value4"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSelectField
+        label="Uneditable select, with description"
+        description="Uneditable description"
+        :editable="false"
+        :items="args.items"
+        v-model="args.value5"
+      />
+    </FSCol>\`
+  })
+}`,...(n=(s=t.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var a,m,d;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    args: {
+      valid: false,
+      items: Array.from(Array(5).keys()).map(i => ({
+        id: i.toString(),
+        label: \`Option \${i + 1}\`
+      })),
+      value1: null,
+      value2: null,
+      value3: null,
+      rules: SelectRules
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSForm,
+      FSSelectField,
+      FSCol,
+      FSRow
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSForm v-model="args.valid" variant="standard">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSSelectField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :items="args.items"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSSelectField
+          label="Rules: min 2 options"
+          :rules="[args.rules.min(2)]"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSSelectField
+          label="Rules: required & min 2 options & max 3 options"
+          :rules="[args.rules.required(), args.rules.min(2), args.rules.max(3)]"
+          :items="args.items"
+          :multiple="true"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>\`
+  })
+}`,...(d=(m=r.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};const Ve=["Variations","Rules"];export{r as Rules,t as Variations,Ve as __namedExportsOrder,Me as default};
