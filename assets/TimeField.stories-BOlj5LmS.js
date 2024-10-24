02d55665520554c95328696d4e0d41ea86ed1d20
rduteil <rduteil@users.noreply.github.com>
2024-10-24 07:23:24 +0000

    deploy: 6ad08bd95fe1b33818e594048f52e7e50a740dd6

---
+++ assets/TimeField.stories-BOlj5LmS.js
@@ -0,0 +1,151 @@
+import o from"./FSTimeField-C5VEADli.js";import{F as u}from"./FSForm-4A05UEAw.js";import{F as p}from"./FSCol-BvqytbKT.js";import{_ as F}from"./FSRow-CbEgVN08.js";import{b as g}from"./rules-BafDybJE.js";import"./vue.esm-bundler-l-siv0w9.js";import"./FSNumberField-lpIHkas9.js";import"./FSTextField-CaG6gJzS.js";import"./FSBaseField-CpNJbDpF.js";import"./FSSpan-Di50moR2.js";import"./useBreakpoints-C5IzU8MC.js";import"./useSlots-C_N_WAus.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./useColors-Bs2u1_6J.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DfSX31J9.js";import"./theme-C2MnUTNI.js";import"./FSButton-DcUuAZqz.js";import"./FSClickable-Up0p3Vk8.js";import"./FSCard-DL4SLpmS.js";import"./css-CP2C2-Fe.js";import"./VProgressCircular-RI9KLr8_.js";import"./color-De-77ojK.js";import"./resizeObserver-CjMDZ25n.js";import"./VIcon-D6ql3n-m.js";import"./FSIcon-uIr9VdFq.js";import"./useRules-QHk8iD4m.js";import"./VField-DaI8sxeG.js";import"./index-B089V-om.js";import"./transition-AZsQzdUZ.js";import"./VLabel-Dswg7Ov5.js";import"./VInput-BkciVi2d.js";import"./locale-CTwwbPAi.js";import"./density-Dt_pQxP_.js";import"./proxiedModel-KpugNjnZ.js";import"./form-CLJq5y5w.js";import"./loader-avZiZGL_.js";import"./anchor-C2Y5FAYh.js";import"./rounded-uHrmshEX.js";import"./VDefaultsProvider-CfqLszOw.js";import"./forwardRefs-DWGaNmQL.js";import"./index-C6OXlV61.js";import"./FSSelectField-BkUcFimZ.js";import"./FSDialogMenu-2hgW8eHf.js";import"./VDialog-BXBtUIC2.js";import"./VOverlay-Dwc5HfFO.js";import"./dimensions-9QoL2_-9.js";import"./display-BWzbFTnS.js";import"./lazy-DuBjUwFb.js";import"./router-CIHGWUSM.js";import"./FSSlideGroup-CBieAKh7.js";import"./uuid-DTaye2KM.js";import"./FSButtonNextIcon-B9vgItlM.js";import"./VSlideGroupItem-BXc1MbHI.js";import"./group-Dk9dfj8w.js";import"./FSToggleSet-B0bG_RDz.js";import"./FSWrapGroup-cmIYWMlH.js";import"./FSCheckbox-BlsFOPfN.js";import"./VSelectionControl-Br2dHavx.js";import"./index-Cr304eD2.js";import"./FSFadeOut-BUOQ-fRr.js";import"./FSLoader-kY4Od3NB.js";import"./elevation-BZDXH0CP.js";import"./FSRadio-CqjI9hQz.js";import"./VList-Bzb582fb.js";import"./ssrBoot-DD96sY1N.js";import"./border-DXAdoVnk.js";import"./VImg-CZhZqiQJ.js";import"./VSelect-CELx6cWV.js";import"./VMenu-DQq2E1HW.js";import"./time-CxERy6W9.js";import"./useTranslations-CJK8D24x.js";import"./useDateFormat-BNPZ7oj9.js";import"./datesTools-CGhnvN9k.js";import"./startOfWeek-uXTpkxA4.js";import"./startOfDay-C4pDH4rb.js";import"./useAppLanguageCode-BzH56rSm.js";import"./useAppTimeZone-gpO-zv_9.js";const ze={title:"Foundation/Shared/Input fields/TimeField",component:o,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},e={args:{args:{value1:null,value2:3600,value3:691200}},render:(t,{argTypes:i})=>({components:{FSTimeField:o,FSCol:p},props:Object.keys(i),setup(){return{...t}},template:`
+    <FSCol>
+      <div style="color: red" class="text-h2">DEPRECATED : use FSTimeStepField instead</div>
+      <FSTimeField
+        label="Time"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTimeField
+        label="Required time, with description"
+        description="Description for this field"
+        :required="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTimeField
+        label="Uneditable time, with description"
+        description="Uneditable description"
+        :editable="false"
+        v-model="args.value3"
+      />
+    </FSCol>`})},r={args:{args:{valid:!1,value1:null,value2:3660,value3:null,rules:g}},render:(t,{argTypes:i})=>({components:{FSForm:u,FSTimeField:o,FSCol:p,FSRow:F},props:Object.keys(i),setup(){return{...t}},template:`
+    <FSForm v-model="args.valid">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTimeField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTimeField
+          label="Rules: max 120 seconds"
+          :rules="[args.rules.max(120)]"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTimeField
+          label="Rules: required & max 7200 seconds & min 3600 seconds"
+          :rules="[args.rules.required(), args.rules.max(7200), args.rules.min(3600)]"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>`})};var l,s,m;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      value2: 3600,
+      value3: 691200
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSTimeField,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <div style="color: red" class="text-h2">DEPRECATED : use FSTimeStepField instead</div>
+      <FSTimeField
+        label="Time"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTimeField
+        label="Required time, with description"
+        description="Description for this field"
+        :required="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSTimeField
+        label="Uneditable time, with description"
+        description="Uneditable description"
+        :editable="false"
+        v-model="args.value3"
+      />
+    </FSCol>\`
+  })
+}`,...(m=(s=e.parameters)==null?void 0:s.docs)==null?void 0:m.source}}};var n,d,a;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    args: {
+      valid: false,
+      value1: null,
+      value2: 3660,
+      value3: null,
+      rules: TimeRules
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSForm,
+      FSTimeField,
+      FSCol,
+      FSRow
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSForm v-model="args.valid">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTimeField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTimeField
+          label="Rules: max 120 seconds"
+          :rules="[args.rules.max(120)]"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSTimeField
+          label="Rules: required & max 7200 seconds & min 3600 seconds"
+          :rules="[args.rules.required(), args.rules.max(7200), args.rules.min(3600)]"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>\`
+  })
+}`,...(a=(d=r.parameters)==null?void 0:d.docs)==null?void 0:a.source}}};const Be=["Variations","Rules"];export{r as Rules,e as Variations,Be as __namedExportsOrder,ze as default};
