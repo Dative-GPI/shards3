ca1b95b008144c6f1d3ad0eefa03ce78654e53e7
SchroterQuentin <SchroterQuentin@users.noreply.github.com>
2024-10-25 15:48:56 +0000

    deploy: 7dd0f5ca5812fafc3d3fa8ebb6da70b7d4c42c67

---
+++ assets/AutocompleteServiceAccountRoleOrganisation.stories-BxggtJKk.js
@@ -0,0 +1,75 @@
+var w=Object.defineProperty;var T=(e,o,t)=>o in e?w(e,o,{enumerable:!0,configurable:!0,writable:!0,value:t}):e[o]=t;var i=(e,o,t)=>T(e,typeof o!="symbol"?o+"":o,t);import{d as $,c as p,y as N,z as d,A as g,C as v,m as U,H as D,I as k,F as q}from"./vue.esm-bundler-l-siv0w9.js";import{F as E}from"./FSAutocompleteField-Dc7MOQbe.js";import{F as O}from"./FSIcon-uIr9VdFq.js";import{P as x}from"./permissionInfos-BPDgTHQl.js";import{C as L}from"./base-BWToAT4M.js";import{S as B}from"./serviceFactory-DAEdCiSt.js";import{C as r}from"./composableFactory-C5FI4e55.js";import{u as j}from"./useAutocomplete-ConHaqE0.js";import{u as M}from"./useTranslations-CJK8D24x.js";import{_ as P}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{F as G}from"./FSCol-BvqytbKT.js";import"./FSSearchField-C1NE5kA-.js";import"./FSTextField-CaG6gJzS.js";import"./FSBaseField-CpNJbDpF.js";import"./FSSpan-Di50moR2.js";import"./useBreakpoints-C5IzU8MC.js";import"./useSlots-C_N_WAus.js";import"./FSRow-CbEgVN08.js";import"./css-CP2C2-Fe.js";import"./useColors-Bs2u1_6J.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DfSX31J9.js";import"./theme-C2MnUTNI.js";import"./FSButton-DcUuAZqz.js";import"./FSClickable-Up0p3Vk8.js";import"./FSCard-DL4SLpmS.js";import"./VProgressCircular-RI9KLr8_.js";import"./color-De-77ojK.js";import"./resizeObserver-CjMDZ25n.js";import"./VIcon-D6ql3n-m.js";import"./useRules-QHk8iD4m.js";import"./VField-DaI8sxeG.js";import"./index-B089V-om.js";import"./transition-AZsQzdUZ.js";import"./VLabel-Dswg7Ov5.js";import"./VInput-BkciVi2d.js";import"./locale-CTwwbPAi.js";import"./density-Dt_pQxP_.js";import"./proxiedModel-KpugNjnZ.js";import"./form-CLJq5y5w.js";import"./loader-avZiZGL_.js";import"./anchor-C2Y5FAYh.js";import"./rounded-uHrmshEX.js";import"./VDefaultsProvider-CfqLszOw.js";import"./forwardRefs-DWGaNmQL.js";import"./index-C6OXlV61.js";import"./FSDialogMenu-2hgW8eHf.js";import"./VDialog-BXBtUIC2.js";import"./VOverlay-Dwc5HfFO.js";import"./dimensions-9QoL2_-9.js";import"./display-BWzbFTnS.js";import"./lazy-DuBjUwFb.js";import"./router-CIHGWUSM.js";import"./FSSlideGroup-CBieAKh7.js";import"./uuid-DTaye2KM.js";import"./FSButtonNextIcon-B9vgItlM.js";import"./VSlideGroupItem-BXc1MbHI.js";import"./group-Dk9dfj8w.js";import"./FSToggleSet-B0bG_RDz.js";import"./FSWrapGroup-cmIYWMlH.js";import"./FSCheckbox-BlsFOPfN.js";import"./VSelectionControl-Br2dHavx.js";import"./index-Cr304eD2.js";import"./FSFadeOut-K3Ty9hFX.js";import"./FSLoader-kY4Od3NB.js";import"./elevation-BZDXH0CP.js";import"./FSRadio-CqjI9hQz.js";import"./VList-Bzb582fb.js";import"./ssrBoot-DD96sY1N.js";import"./border-DXAdoVnk.js";import"./VImg-CZhZqiQJ.js";import"./VSelect-CELx6cWV.js";import"./VMenu-DQq2E1HW.js";import"./filter-BjRVyZGj.js";import"./useAppOrganisationId-Cv6r7Qq7.js";import"./lodash-BiW_TGGX.js";class R{constructor(o){i(this,"id");i(this,"roleId");i(this,"organisationId");i(this,"userType");i(this,"icon");i(this,"code");i(this,"label");i(this,"tags");i(this,"permissions");this.id=o.id,this.roleId=o.roleId,this.organisationId=o.organisationId,this.userType=o.userType,this.icon=o.icon,this.code=o.code,this.label=o.label,this.tags=o.tags.slice(),this.permissions=o.permissions.map(t=>new x(t))}}class z extends R{constructor(t){super(t);i(this,"description");this.description=t.description}}const h=()=>`${L()}/service-account-role-organisations`,H=e=>`${h()}/${encodeURIComponent(e)}`,n=new B("serviceAccountOrganisation",z).createComplete(h,H,R);r.get(n);const J=r.getMany(n);r.create(n);r.update(n);r.remove(n);const b=$({name:"FSAutocompleteServiceAccountRoleOrganisation",components:{FSAutocompleteField:E,FSIcon:O},props:{serviceAccountRoleOrganisationsFilters:{type:Object,required:!1,default:null},modelValue:{type:[Array,String],required:!1,default:null},multiple:{type:Boolean,required:!1,default:!1},toggleSetDisabled:{type:Boolean,required:!1,default:!1}},emits:["update:modelValue"],setup(e,{emit:o}){const{getMany:t,fetching:u,entities:s}=J(),{$tr:m}=M(),c=p(()=>V.value&&u.value),l=p(()=>e.multiple&&e.modelValue?m("ui.autocomplete-service-account-role-organisation.placeholder","{0} role(s) selected",e.modelValue.length):null),y=_=>t({...e.serviceAccountRoleOrganisationsFilters,search:_??void 0}),{toggleSet:C,init:V,onUpdate:I}=j(s,[()=>e.serviceAccountRoleOrganisationsFilters],o,y);return{serviceAccountRoleOrganisations:s,placeholder:l,toggleSet:C,loading:c,onUpdate:I}}});function K(e,o,t,u,s,m){const c=N("FSAutocompleteField");return d(),g(c,U({toggleSet:!e.$props.toggleSetDisabled&&e.toggleSet,items:e.serviceAccountRoleOrganisations,multiple:e.$props.multiple,placeholder:e.placeholder,loading:e.loading,modelValue:e.$props.modelValue,"onUpdate:modelValue":e.onUpdate},e.$attrs),{"item-prepend":v(({item:l})=>[l.icon?(d(),g(O,{key:0},{default:v(()=>[D(k(l.icon),1)]),_:2},1024)):q("",!0)]),_:1},16,["toggleSet","items","multiple","placeholder","loading","modelValue","onUpdate:modelValue"])}const F=P(b,[["render",K]]);b.__docgenInfo={displayName:"FSAutocompleteServiceAccountRoleOrganisation",exportName:"default",description:"",tags:{},props:[{name:"serviceAccountRoleOrganisationsFilters",type:{name:"ServiceAccountRoleOrganisationFilters"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"modelValue",type:{name:"string[] | string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"multiple",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}},{name:"toggleSetDisabled",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}}],events:[{name:"update:modelValue"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/core/foundation-core-components/components/autocompletes/FSAutocompleteServiceAccountRoleOrganisation.vue"]};const Oo={title:"Foundation/Core/Autocompletes/AutocompleteServiceAccountRoleOrganisation",component:F,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},a={args:{args:{value1:null,value2:null,value3:null,value4:null}},render:(e,{argTypes:o})=>({components:{FSAutocompleteServiceAccountRoleOrganisation:F,FSCol:G},props:Object.keys(o),setup(){return{...e}},template:`
+    <FSCol>
+      <FSAutocompleteServiceAccountRoleOrganisation
+        label="ServiceAccountRoleOrganisation"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteServiceAccountRoleOrganisation
+        label="ServiceAccountRoleOrganisation with toggleset disabled"
+        :toggleSetDisabled="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteServiceAccountRoleOrganisation
+        label="ServiceAccountRoleOrganisation with multiple selection"
+        :multiple="true"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteServiceAccountRoleOrganisation
+        label="ServiceAccountRoleOrganisation with toggleset disabled and multiple selection"
+        :multiple="true"
+        :toggleSetDisabled="true"
+        v-model="args.value4"
+      />
+    </FSCol>`})};var S,A,f;a.parameters={...a.parameters,docs:{...(S=a.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      value2: null,
+      value3: null,
+      value4: null
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSAutocompleteServiceAccountRoleOrganisation,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSAutocompleteServiceAccountRoleOrganisation
+        label="ServiceAccountRoleOrganisation"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteServiceAccountRoleOrganisation
+        label="ServiceAccountRoleOrganisation with toggleset disabled"
+        :toggleSetDisabled="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteServiceAccountRoleOrganisation
+        label="ServiceAccountRoleOrganisation with multiple selection"
+        :multiple="true"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteServiceAccountRoleOrganisation
+        label="ServiceAccountRoleOrganisation with toggleset disabled and multiple selection"
+        :multiple="true"
+        :toggleSetDisabled="true"
+        v-model="args.value4"
+      />
+    </FSCol>\`
+  })
+}`,...(f=(A=a.parameters)==null?void 0:A.docs)==null?void 0:f.source}}};const Ro=["Variations"];export{a as Variations,Ro as __namedExportsOrder,Oo as default};
