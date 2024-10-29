d3bc80047df5c5c32c0199043625d91099a66db8
ddonfack <ddonfack@dative-gpi.com>
2024-10-29 16:09:51 +0100

    correct filter for enum

--- src/core/foundation-core-components/components/lists/scenarioOrganisationTypes/FSBaseScenarioOrganisationTypesList.vue
+++ src/core/foundation-core-components/components/lists/scenarioOrganisationTypes/FSBaseScenarioOrganisationTypesList.vue
@@ -127,7 +127,7 @@ export default defineComponent({
 
     const headersOptions = computed(() => ({
       criticity: {
-        fixedFilters: getEnumEntries(Criticity).filter(f => f.value != Criticity.None).map(e => ({
+        fixedFilters: getEnumEntries(Criticity).map(e => ({
           value: e.value,
           text: AlertTools.criticityLabel(e.value)
         })),
