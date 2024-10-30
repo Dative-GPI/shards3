bc7d775f44ed6e604d5cb16b5948881da0541dc3
peyronth <peyronth@users.noreply.github.com>
2024-10-30 16:25:57 +0000

    deploy: 3b77f55a018f73f270d68fc2342cd076bc6c50d1

---
+++ assets/EditImage.stories-CTz_6LWw.js
@@ -0,0 +1,50 @@
+import{a as c}from"./chunk-MZXVCX43-CM0pFb8Z.js";import{F as I}from"./FSButton-Doewyidj.js";import{d as h,c as f,y as E,z as F,A as S,m as V}from"./vue.esm-bundler-l-siv0w9.js";import{F as d}from"./FSEditImageUI-y30cipcp.js";import{u as v,a as b}from"./useImages-CdDysFg4.js";import{_ as y}from"./_plugin-vue_export-helper-DlAUqK2U.js";import"./v4-CQkTLCs1.js";import"./FSClickable-BUTMQJc7.js";import"./FSCard-CZcF3hjs.js";import"./FSCol-BvqytbKT.js";import"./css-CP2C2-Fe.js";import"./useBreakpoints-C5IzU8MC.js";import"./FSRow-CbEgVN08.js";import"./useColors-_VkINegi.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-_Z9wfsFC.js";import"./theme-C2MnUTNI.js";import"./VProgressCircular-RI9KLr8_.js";import"./color-De-77ojK.js";import"./resizeObserver-CjMDZ25n.js";import"./VIcon-D6ql3n-m.js";import"./FSSpan-Di50moR2.js";import"./useSlots-C_N_WAus.js";import"./FSIcon-CxSeYbPm.js";import"./FSButtonFileMini-Dky3dTwd.js";import"./useAppAuthToken-CwPnmj0J.js";import"./base-CmdGny12.js";import"./FSImageUI-BlKs_vZE.js";import"./FSLoader-ylzW_XnC.js";import"./dimensions-9QoL2_-9.js";import"./elevation-BZDXH0CP.js";import"./locale-CTwwbPAi.js";import"./VImg-CZhZqiQJ.js";import"./rounded-uHrmshEX.js";import"./transition-AZsQzdUZ.js";import"./index-C6OXlV61.js";import"./FSText-DZ8qsh3h.js";import"./serviceFactory-DAEdCiSt.js";import"./lodash-BiW_TGGX.js";import"./composableFactory-C5FI4e55.js";const s=h({name:"FSEditImage",components:{FSEditImageUI:d},props:{imageId:{type:String,required:!1,default:null}},emits:["update:imageId"],setup(e){const{get:a,entity:t}=v(),o=f(()=>e.imageId?b(e.imageId):null);return{image:t,source:o,onError:()=>{e.imageId&&(console.error("Error loading image",e.imageId),a(e.imageId))}}}});function U(e,a,t,o,l,_){const u=E("FSEditImageUI");return F(),S(u,V({source:e.source,blurHash:e.image,onError:e.onError,"onUpdate:source":a[0]||(a[0]=g=>e.$emit("update:imageId",g))},e.$attrs),null,16,["source","blurHash","onError"])}const p=y(s,[["render",U]]);s.__docgenInfo={displayName:"FSEditImage",exportName:"default",description:"",tags:{},props:[{name:"imageId",type:{name:"string | null"},required:!1,defaultValue:{func:!1,value:"null"}}],events:[{name:"update:imageId"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/shared/foundation-shared-components/components/FSEditImage.vue"]};const ge={title:"Foundation/Shared/EditImage/EditImage",component:p,parameters:{docs:{description:{component:"EditImage will fetch the image from Foundation API by asking the gateway."}}},subcomponents:{FSEditImageUI:d},tags:["autodocs"],argTypes:{variant:{control:"select",options:["standard","full"]},"onUpdate:modelValue":{action:"update:modelValue"}}},r={args:{imageId:"1",modelValue:null,height:96,width:96,label:"Edit Image",variant:"standard",hideHeader:!1,required:!1,"onUpdate:modelValue":c("onUpdate:modelValue")},render:(e,{argTypes:a})=>({components:{FSEditImage:p,FSButton:I},props:Object.keys(a),setup(){return{args:e}},template:`
+      <FSEditImage 
+        :imageId="args.imageId"
+        :height="args.height"
+        :width="args.width"
+        :label="args.label"
+        :variant="args.variant"
+        :hideHeader="args.hideHeader"
+        :required="args.required"
+        v-model="args.modelValue"
+        @update:modelValue="args['onUpdate:modelValue']"
+      />`})};var n,i,m;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    imageId: "1",
+    modelValue: null,
+    height: 96,
+    width: 96,
+    label: 'Edit Image',
+    variant: 'standard',
+    hideHeader: false,
+    required: false,
+    "onUpdate:modelValue": action("onUpdate:modelValue")
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSEditImage,
+      FSButton
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <FSEditImage 
+        :imageId="args.imageId"
+        :height="args.height"
+        :width="args.width"
+        :label="args.label"
+        :variant="args.variant"
+        :hideHeader="args.hideHeader"
+        :required="args.required"
+        v-model="args.modelValue"
+        @update:modelValue="args['onUpdate:modelValue']"
+      />\`
+  })
+}`,...(m=(i=r.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};const ce=["Default"];export{r as Default,ce as __namedExportsOrder,ge as default};
