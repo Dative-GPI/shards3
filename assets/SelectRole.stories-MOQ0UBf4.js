9ceca3651585f93bd58e322a005ac6cd8b31539e
rduteil <rduteil@users.noreply.github.com>
2024-10-30 07:18:38 +0000

    deploy: c4602652b60dd35cbce5658b45ff487af700a5c1

---
+++ assets/SelectRole.stories-MOQ0UBf4.js
@@ -0,0 +1,459 @@
+import{F as l}from"./FSSelectField-DVM2ddcd.js";import{F as r}from"./FSButton-DcUuAZqz.js";import{F as m}from"./FSChip-CH8gQQD0.js";import{F as a}from"./FSIcon-uIr9VdFq.js";import{F as s}from"./FSCol-BvqytbKT.js";import"./vue.esm-bundler-l-siv0w9.js";import"./FSDialogMenu-2hgW8eHf.js";import"./FSCard-DL4SLpmS.js";import"./FSRow-CbEgVN08.js";import"./css-CP2C2-Fe.js";import"./useBreakpoints-C5IzU8MC.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./useColors-Bs2u1_6J.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DfSX31J9.js";import"./theme-C2MnUTNI.js";import"./VDialog-BXBtUIC2.js";import"./VOverlay-Dwc5HfFO.js";import"./forwardRefs-DWGaNmQL.js";import"./color-De-77ojK.js";import"./proxiedModel-KpugNjnZ.js";import"./anchor-C2Y5FAYh.js";import"./dimensions-9QoL2_-9.js";import"./display-BWzbFTnS.js";import"./lazy-DuBjUwFb.js";import"./locale-CTwwbPAi.js";import"./router-CIHGWUSM.js";import"./transition-AZsQzdUZ.js";import"./VDefaultsProvider-CfqLszOw.js";import"./FSSlideGroup-CBieAKh7.js";import"./uuid-DTaye2KM.js";import"./FSButtonNextIcon-B9vgItlM.js";import"./useSlots-C_N_WAus.js";import"./VSlideGroupItem-BXc1MbHI.js";import"./group-Dk9dfj8w.js";import"./index-B089V-om.js";import"./VIcon-D6ql3n-m.js";import"./resizeObserver-CjMDZ25n.js";import"./FSToggleSet-B0bG_RDz.js";import"./FSWrapGroup-cmIYWMlH.js";import"./VInput-BkciVi2d.js";import"./density-Dt_pQxP_.js";import"./form-CLJq5y5w.js";import"./FSBaseField-CpNJbDpF.js";import"./FSSpan-Di50moR2.js";import"./FSTextField-CaG6gJzS.js";import"./useRules-QHk8iD4m.js";import"./VField-DaI8sxeG.js";import"./VLabel-Dswg7Ov5.js";import"./loader-avZiZGL_.js";import"./VProgressCircular-RI9KLr8_.js";import"./rounded-uHrmshEX.js";import"./index-C6OXlV61.js";import"./FSCheckbox-BlsFOPfN.js";import"./VSelectionControl-Br2dHavx.js";import"./index-Cr304eD2.js";import"./FSFadeOut-K3Ty9hFX.js";import"./FSLoader-kY4Od3NB.js";import"./elevation-BZDXH0CP.js";import"./FSRadio-CqjI9hQz.js";import"./VList-Cicg58Bq.js";import"./ssrBoot-DD96sY1N.js";import"./border-DXAdoVnk.js";import"./VImg-CZhZqiQJ.js";import"./VSelect-d_NLzp9y.js";import"./VMenu-DQq2E1HW.js";import"./FSClickable-Up0p3Vk8.js";const Le={title:"Foundation/Shared/Selects/SelectRole",component:l,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},e={args:{args:{items:[{id:"01",icon:"mdi-account",label:"Role 1",chipColor:"warning",chipLabel:"Custom",type:1},{id:"02",icon:"mdi-account-outline",label:"Role 2",chipColor:"warning",chipLabel:"Custom",type:1},{id:"11",icon:"mdi-account-circle",label:"Role 11",chipColor:"primary",chipLabel:"Shared",type:2},{id:"12",icon:"mdi-account-circle-outline",label:"Role 12",chipColor:"primary",chipLabel:"Shared",type:2}],value1:null,value2:null,value3:null,value4:null}},render:(i,{argTypes:n})=>({components:{FSSelectField:l,FSButton:r,FSChip:m,FSIcon:a,FSCol:s},props:Object.keys(n),setup(){return{...i}},template:`
+    <FSCol>
+      <FSSelectField
+        label="Role"
+        placeholder="One or more roles selected"
+        :toggleSet="true"
+        :items="args.items"
+        v-model="args.value1"
+      >
+        <template
+          #item-prepend="{ item }"
+        >
+          <FSIcon
+            v-if="item.icon"
+          >
+            {{ item.icon }}
+          </FSIcon>
+        </template>
+        <template
+          #item-append="{ item }"
+        >
+          <FSChip
+            :color="item.chipColor"
+            :label="item.chipLabel"
+            :editable="false"
+          />
+        </template>
+        <template
+          #toggle-set-item="props"
+        >
+          <FSButton
+            :prependIcon="props.item.icon"
+            :variant="props.getVariant(props.item)"
+            :color="props.getColor(props.item)"
+            :class="props.getClass(props.item)"
+            :label="props.item.label"
+            @click="props.toggle(props.item)"
+          >
+            <template
+              v-if="props.item.type"
+              #append
+            >
+              <FSChip
+                :color="props.item.chipColor"
+                :label="props.item.chipLabel"
+                :editable="false"
+              />
+            </template>
+          </FSButton>
+        </template>
+      </FSSelectField>
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSelectField
+        label="Role with toggleset disabled"
+        placeholder="One or more roles selected"
+        :items="args.items"
+        v-model="args.value2"
+      >
+        <template
+          #item-prepend="{ item }"
+        >
+          <FSIcon
+            v-if="item.icon"
+          >
+            {{ item.icon }}
+          </FSIcon>
+        </template>
+        <template
+          #item-append="{ item }"
+        >
+          <FSChip
+            :color="item.chipColor"
+            :label="item.chipLabel"
+            :editable="false"
+          />
+        </template>
+        <template
+          #toggle-set-item="props"
+        >
+          <FSButton
+            :prependIcon="props.item.icon"
+            :variant="props.getVariant(props.item)"
+            :color="props.getColor(props.item)"
+            :class="props.getClass(props.item)"
+            :label="props.item.label"
+            @click="props.toggle(props.item)"
+          >
+            <template
+              v-if="props.item.type"
+              #append
+            >
+              <FSChip
+                :color="props.item.chipColor"
+                :label="props.item.chipLabel"
+                :editable="false"
+              />
+            </template>
+          </FSButton>
+        </template>
+      </FSSelectField>
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSelectField
+        label="Role with multiple"
+        placeholder="One or more roles selected"
+        :toggleSet="true"
+        :multiple="true"
+        :items="args.items"
+        v-model="args.value3"
+      >
+        <template
+          #item-prepend="{ item }"
+        >
+          <FSIcon
+            v-if="item.icon"
+          >
+            {{ item.icon }}
+          </FSIcon>
+        </template>
+        <template
+          #item-append="{ item }"
+        >
+          <FSChip
+            :color="item.chipColor"
+            :label="item.chipLabel"
+            :editable="false"
+          />
+        </template>
+        <template
+          #toggle-set-item="props"
+        >
+          <FSButton
+            :prependIcon="props.item.icon"
+            :variant="props.getVariant(props.item)"
+            :color="props.getColor(props.item)"
+            :class="props.getClass(props.item)"
+            :label="props.item.label"
+            @click="props.toggle(props.item)"
+          >
+            <template
+              v-if="props.item.type"
+              #append
+            >
+              <FSChip
+                :color="props.item.chipColor"
+                :label="props.item.chipLabel"
+                :editable="false"
+              />
+            </template>
+          </FSButton>
+        </template>
+      </FSSelectField>
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSelectField
+        label="Role with multiple and toggleSet disabled"
+        placeholder="One or more roles selected"
+        :multiple="true"
+        :items="args.items"
+        v-model="args.value4"
+      >
+        <template
+          #item-prepend="{ item }"
+        >
+          <FSIcon
+            v-if="item.icon"
+          >
+            {{ item.icon }}
+          </FSIcon>
+        </template>
+        <template
+          #item-append="{ item }"
+        >
+          <FSChip
+            :color="item.chipColor"
+            :label="item.chipLabel"
+            :editable="false"
+          />
+        </template>
+        <template
+          #toggle-set-item="props"
+        >
+          <FSButton
+            :prependIcon="props.item.icon"
+            :variant="props.getVariant(props.item)"
+            :color="props.getColor(props.item)"
+            :class="props.getClass(props.item)"
+            :label="props.item.label"
+            @click="props.toggle(props.item)"
+          >
+            <template
+              v-if="props.item.type"
+              #append
+            >
+              <FSChip
+                :color="props.item.chipColor"
+                :label="props.item.chipLabel"
+                :editable="false"
+              />
+            </template>
+          </FSButton>
+        </template>
+      </FSSelectField>
+    </FSCol>`})};var t,p,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    args: {
+      items: [{
+        id: "01",
+        icon: "mdi-account",
+        label: "Role 1",
+        chipColor: "warning",
+        chipLabel: "Custom",
+        type: 1
+      }, {
+        id: "02",
+        icon: "mdi-account-outline",
+        label: "Role 2",
+        chipColor: "warning",
+        chipLabel: "Custom",
+        type: 1
+      }, {
+        id: "11",
+        icon: "mdi-account-circle",
+        label: "Role 11",
+        chipColor: "primary",
+        chipLabel: "Shared",
+        type: 2
+      }, {
+        id: "12",
+        icon: "mdi-account-circle-outline",
+        label: "Role 12",
+        chipColor: "primary",
+        chipLabel: "Shared",
+        type: 2
+      }],
+      value1: null,
+      value2: null,
+      value3: null,
+      value4: null
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSSelectField,
+      FSButton,
+      FSChip,
+      FSIcon,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSSelectField
+        label="Role"
+        placeholder="One or more roles selected"
+        :toggleSet="true"
+        :items="args.items"
+        v-model="args.value1"
+      >
+        <template
+          #item-prepend="{ item }"
+        >
+          <FSIcon
+            v-if="item.icon"
+          >
+            {{ item.icon }}
+          </FSIcon>
+        </template>
+        <template
+          #item-append="{ item }"
+        >
+          <FSChip
+            :color="item.chipColor"
+            :label="item.chipLabel"
+            :editable="false"
+          />
+        </template>
+        <template
+          #toggle-set-item="props"
+        >
+          <FSButton
+            :prependIcon="props.item.icon"
+            :variant="props.getVariant(props.item)"
+            :color="props.getColor(props.item)"
+            :class="props.getClass(props.item)"
+            :label="props.item.label"
+            @click="props.toggle(props.item)"
+          >
+            <template
+              v-if="props.item.type"
+              #append
+            >
+              <FSChip
+                :color="props.item.chipColor"
+                :label="props.item.chipLabel"
+                :editable="false"
+              />
+            </template>
+          </FSButton>
+        </template>
+      </FSSelectField>
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSelectField
+        label="Role with toggleset disabled"
+        placeholder="One or more roles selected"
+        :items="args.items"
+        v-model="args.value2"
+      >
+        <template
+          #item-prepend="{ item }"
+        >
+          <FSIcon
+            v-if="item.icon"
+          >
+            {{ item.icon }}
+          </FSIcon>
+        </template>
+        <template
+          #item-append="{ item }"
+        >
+          <FSChip
+            :color="item.chipColor"
+            :label="item.chipLabel"
+            :editable="false"
+          />
+        </template>
+        <template
+          #toggle-set-item="props"
+        >
+          <FSButton
+            :prependIcon="props.item.icon"
+            :variant="props.getVariant(props.item)"
+            :color="props.getColor(props.item)"
+            :class="props.getClass(props.item)"
+            :label="props.item.label"
+            @click="props.toggle(props.item)"
+          >
+            <template
+              v-if="props.item.type"
+              #append
+            >
+              <FSChip
+                :color="props.item.chipColor"
+                :label="props.item.chipLabel"
+                :editable="false"
+              />
+            </template>
+          </FSButton>
+        </template>
+      </FSSelectField>
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSelectField
+        label="Role with multiple"
+        placeholder="One or more roles selected"
+        :toggleSet="true"
+        :multiple="true"
+        :items="args.items"
+        v-model="args.value3"
+      >
+        <template
+          #item-prepend="{ item }"
+        >
+          <FSIcon
+            v-if="item.icon"
+          >
+            {{ item.icon }}
+          </FSIcon>
+        </template>
+        <template
+          #item-append="{ item }"
+        >
+          <FSChip
+            :color="item.chipColor"
+            :label="item.chipLabel"
+            :editable="false"
+          />
+        </template>
+        <template
+          #toggle-set-item="props"
+        >
+          <FSButton
+            :prependIcon="props.item.icon"
+            :variant="props.getVariant(props.item)"
+            :color="props.getColor(props.item)"
+            :class="props.getClass(props.item)"
+            :label="props.item.label"
+            @click="props.toggle(props.item)"
+          >
+            <template
+              v-if="props.item.type"
+              #append
+            >
+              <FSChip
+                :color="props.item.chipColor"
+                :label="props.item.chipLabel"
+                :editable="false"
+              />
+            </template>
+          </FSButton>
+        </template>
+      </FSSelectField>
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSSelectField
+        label="Role with multiple and toggleSet disabled"
+        placeholder="One or more roles selected"
+        :multiple="true"
+        :items="args.items"
+        v-model="args.value4"
+      >
+        <template
+          #item-prepend="{ item }"
+        >
+          <FSIcon
+            v-if="item.icon"
+          >
+            {{ item.icon }}
+          </FSIcon>
+        </template>
+        <template
+          #item-append="{ item }"
+        >
+          <FSChip
+            :color="item.chipColor"
+            :label="item.chipLabel"
+            :editable="false"
+          />
+        </template>
+        <template
+          #toggle-set-item="props"
+        >
+          <FSButton
+            :prependIcon="props.item.icon"
+            :variant="props.getVariant(props.item)"
+            :color="props.getColor(props.item)"
+            :class="props.getClass(props.item)"
+            :label="props.item.label"
+            @click="props.toggle(props.item)"
+          >
+            <template
+              v-if="props.item.type"
+              #append
+            >
+              <FSChip
+                :color="props.item.chipColor"
+                :label="props.item.chipLabel"
+                :editable="false"
+              />
+            </template>
+          </FSButton>
+        </template>
+      </FSSelectField>
+    </FSCol>\`
+  })
+}`,...(o=(p=e.parameters)==null?void 0:p.docs)==null?void 0:o.source}}};const Be=["Variations"];export{e as Variations,Be as __namedExportsOrder,Le as default};
