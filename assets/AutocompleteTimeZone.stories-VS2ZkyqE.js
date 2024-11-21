7f8a3f2e0e62aa7d2fe5e1aa61fb69030c52fad5
gballigand <gballigand@users.noreply.github.com>
2024-11-21 10:59:17 +0000

    deploy: 174c6aae9201b87fa499b668a80ef618e2d37054

---
+++ assets/AutocompleteTimeZone.stories-VS2ZkyqE.js
@@ -0,0 +1,75 @@
+var _=Object.defineProperty;var $=(e,t,l)=>t in e?_(e,t,{enumerable:!0,configurable:!0,writable:!0,value:l}):e[t]=l;var s=(e,t,l)=>$(e,typeof t!="symbol"?t+"":t,l);import{d as k,c as b,A as S,B as u,C as p,E as d,j as q,H as v,P as D,F as z}from"./vue.esm-bundler-Zcuj-zyO.js";import{F as B}from"./FSAutocompleteField-D_wwul2P.js";import{F as T}from"./FSButton-BzTwanlk.js";import{F as E}from"./FSChip-DMpdcIDo.js";import{G as N}from"./base-CmdGny12.js";import{S as U}from"./serviceFactory-DTTRhbEw.js";import{C as M}from"./composableFactory-DJbHulE4.js";import{u as O}from"./useAutocomplete-DDmNThuF.js";import{C as j}from"./useColors-Cz5tOQmp.js";import{u as x}from"./useTranslations-DN7QCs30.js";import{_ as G}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{F as I}from"./FSCol-DLyNVOXQ.js";import"./FSSearchField-CB7t8Nmn.js";import"./FSTextField-BcsUOOXo.js";import"./FSBaseField-DHv7mzhZ.js";import"./FSSpan-Bb6VK1Ws.js";import"./useBreakpoints-IB1lZo1P.js";import"./useSlots-DzOFTavN.js";import"./FSRow-4Qz6Bbpg.js";import"./css-YWErTUWV.js";import"./useRules-CvuqAlPS.js";import"./VField-BtCsV974.js";import"./index-D14OnwAh.js";import"./color-mm3JSYKM.js";import"./theme-DppimMvq.js";import"./transition-shOmLEJh.js";import"./VLabel-C9T0eyno.js";import"./VInput-DP_atbEr.js";import"./locale-Beavd5eP.js";import"./VIcon-DsLd3bsf.js";import"./density-DMrbDfgW.js";import"./dimensions-BWgpWc0X.js";import"./proxiedModel-Xz4IZg0a.js";import"./form-ChxmsGl9.js";import"./loader-EtqkGPwj.js";import"./VProgressCircular-B73DQeX0.js";import"./resizeObserver-BOpsWgva.js";import"./anchor-BS5WaiJh.js";import"./rounded-3VvIKpNW.js";import"./VDefaultsProvider-BGrIgR0V.js";import"./forwardRefs-DWGaNmQL.js";import"./index-DnJxNQYu.js";import"./VList-nN2lrumh.js";import"./FSCard-4bilSlvW.js";import"./VDialog-TaqcJ_9B.js";import"./VOverlay-CfTyIhml.js";import"./display-Db__yhk9.js";import"./scopeId-CPcZlDEY.js";import"./router-DnAkll3L.js";import"./ssrBoot-B8tyBOTn.js";import"./border-C0mvfulf.js";import"./elevation-BrHPj82J.js";import"./index-CMMTOWvF.js";import"./VImg-DaKoZ_qJ.js";import"./FSSlideGroup-CtBhRuPR.js";import"./uuid-DTaye2KM.js";import"./FSButtonNextIcon-BV5U7bCA.js";import"./VSlideGroupItem-B0P2Hyp-.js";import"./group-ATTNrdHt.js";import"./FSToggleSet-DhGGvbF6.js";import"./FSWrapGroup-DSD82cKU.js";import"./FSCheckbox-CunxM6rm.js";import"./FSIcon-CKEWDmaC.js";import"./VSelectionControl-u1dDpc9z.js";import"./FSFadeOut-CeMJmSZZ.js";import"./FSLoader-CRmsosiO.js";import"./FSRadio-DnH66ibu.js";import"./VSelect-pAG77NtN.js";import"./VMenu-C3kJxa_y.js";import"./filter-DiySb8XN.js";import"./FSRouterLink-BdruYWDU.js";import"./vue-router-D-G4AqQz.js";import"./FSClickable-CUaqPDhz.js";import"./_commonjsHelpers-BosuxZz1.js";import"./lodash-BiW_TGGX.js";import"./index-DfSX31J9.js";const L=()=>`${N()}/time-zones`,P=new U("timeZone",H).create(e=>e.build(e.addGetMany(L,Z),e.addNotify())),R=M.getMany(P);class Z{constructor(t){s(this,"id");s(this,"offset");this.id=t.id,this.offset=t.offset}}class H extends Z{constructor(t){super(t)}}const C=k({name:"FSAutocompleteTimeZone",components:{FSAutocompleteField:B,FSButton:T,FSChip:E},props:{timeZoneFilters:{type:Object,required:!1,default:null},modelValue:{type:[Array,String],required:!1,default:null},multiple:{type:Boolean,required:!1,default:!1},toggleSetDisabled:{type:Boolean,required:!1,default:!1},label:{type:String,required:!1,default:null}},emits:["update:modelValue"],setup(e,{emit:t}){const{getMany:l,fetching:c,entities:a}=R(),{$tr:g}=x(),n=b(()=>V.value&&c.value),m=b(()=>e.multiple&&e.modelValue?g("autocomplete.time-zone.placeholder","{0} time zone(s) selected",e.modelValue.length):null),o=i=>l({...e.timeZoneFilters,search:i??void 0}),{toggleSet:f,init:V,onUpdate:w}=O(a,[()=>e.timeZoneFilters],t,o,null,i=>i.id,i=>i.id);return{placeholder:m,ColorEnum:j,timeZones:a,toggleSet:f,loading:n,onUpdate:w}}});function W(e,t,l,c,a,g){const n=S("FSChip"),m=S("FSAutocompleteField");return u(),p(m,q({itemTitle:"id",label:e.$tr("autocomplete.time-zone.label","Time zone"),toggleSet:!e.$props.toggleSetDisabled&&e.toggleSet,multiple:e.$props.multiple,placeholder:e.placeholder,loading:e.loading,items:e.timeZones,modelValue:e.$props.modelValue,"onUpdate:modelValue":e.onUpdate},e.$attrs),{"item-append":d(({item:o})=>[o.offset?(u(),p(n,{key:0,label:o.offset.split(":")[0]},null,8,["label"])):v("",!0)]),"toggle-set-item":d(o=>[D(T,{variant:o.getVariant(o.item),color:o.getColor(o.item),class:z(o.getClass(o.item)),label:o.item.id,onClick:f=>o.toggle(o.item)},{append:d(()=>[o.item.offset?(u(),p(n,{key:0,label:o.item.offset.split(":")[0]},null,8,["label"])):v("",!0)]),_:2},1032,["variant","color","class","label","onClick"])]),_:1},16,["label","toggleSet","multiple","placeholder","loading","items","modelValue","onUpdate:modelValue"])}const A=G(C,[["render",W]]);C.__docgenInfo={displayName:"FSAutocompleteTimeZone",exportName:"default",description:"",tags:{},props:[{name:"timeZoneFilters",type:{name:"TimeZoneFilters"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"modelValue",type:{name:"string[] | string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"multiple",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}},{name:"toggleSetDisabled",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}},{name:"label",type:{name:"string | null"},required:!1,defaultValue:{func:!1,value:"null"}}],events:[{name:"update:modelValue"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/shared/foundation-shared-components/components/autocompletes/FSAutocompleteTimeZone.vue"]};const vt={title:"Foundation/Shared/Autocompletes/AutocompleteTimeZone",component:A,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},r={args:{args:{value1:null,value2:null,value3:null,value4:null}},render:(e,{argTypes:t})=>({components:{FSAutocompleteTimeZone:A,FSCol:I},props:Object.keys(t),setup(){return{...e}},template:`
+    <FSCol>
+      <FSAutocompleteTimeZone
+        label="TimeZone"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteTimeZone
+        label="TimeZone with toggleset disabled"
+        :toggleSetDisabled="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteTimeZone
+        label="TimeZone with multiple"
+        :multiple="true"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteTimeZone
+        label="TimeZone with multiple and toggleSet disabled"
+        :multiple="true"
+        :toggleSetDisabled="true"
+        v-model="args.value4"
+      />
+    </FSCol>`})};var F,h,y;r.parameters={...r.parameters,docs:{...(F=r.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    args: {
+      value1: null,
+      value2: null,
+      value3: null,
+      value4: null
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSAutocompleteTimeZone,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSAutocompleteTimeZone
+        label="TimeZone"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteTimeZone
+        label="TimeZone with toggleset disabled"
+        :toggleSetDisabled="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteTimeZone
+        label="TimeZone with multiple"
+        :multiple="true"
+        v-model="args.value3"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSAutocompleteTimeZone
+        label="TimeZone with multiple and toggleSet disabled"
+        :multiple="true"
+        :toggleSetDisabled="true"
+        v-model="args.value4"
+      />
+    </FSCol>\`
+  })
+}`,...(y=(h=r.parameters)==null?void 0:h.docs)==null?void 0:y.source}}};const Ft=["Variations"];export{r as Variations,Ft as __namedExportsOrder,vt as default};
---
+++ assets/MetaHistory.stories-CATuEXxQ.js
@@ -0,0 +1,61 @@
+var O=Object.defineProperty;var I=(e,t,o)=>t in e?O(e,t,{enumerable:!0,configurable:!0,writable:!0,value:o}):e[t]=o;var m=(e,t,o)=>I(e,typeof t!="symbol"?t+"":t,o);import{d as E,b as w,w as $,A as l,B as u,G as f,P as r,E as n,I as F,Q as q,C as k,J as v,K as H}from"./vue.esm-bundler-Zcuj-zyO.js";import{_ as N}from"./lodash-BiW_TGGX.js";import{F as P}from"./FSButton-BzTwanlk.js";import{F as R}from"./FSDialog-Xj-hinzf.js";import{F as L,a as g}from"./FSMetaValue-C5eAvfOi.js";import{C as U}from"./base-DsQovONZ.js";import{i as D}from"./datesTools-CGhnvN9k.js";import{S as A}from"./serviceFactory-DTTRhbEw.js";import{C as B}from"./composableFactory-DJbHulE4.js";import{u as x}from"./useDateFormat-CYsDMoti.js";import{_ as j}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{F as y}from"./FSCol-DLyNVOXQ.js";import{_ as z}from"./FSRow-4Qz6Bbpg.js";import{P as G}from"./customProperties-DqkON8hk.js";import"./_commonjsHelpers-BosuxZz1.js";import"./FSRouterLink-BdruYWDU.js";import"./vue-router-D-G4AqQz.js";import"./FSClickable-CUaqPDhz.js";import"./FSCard-4bilSlvW.js";import"./useColors-Cz5tOQmp.js";import"./index-DfSX31J9.js";import"./theme-DppimMvq.js";import"./css-YWErTUWV.js";import"./useBreakpoints-IB1lZo1P.js";import"./VProgressCircular-B73DQeX0.js";import"./color-mm3JSYKM.js";import"./resizeObserver-BOpsWgva.js";import"./VIcon-DsLd3bsf.js";import"./FSSpan-Bb6VK1Ws.js";import"./useSlots-DzOFTavN.js";import"./FSIcon-CKEWDmaC.js";import"./FSText-DqK2dTtm.js";import"./VDialog-TaqcJ_9B.js";import"./VOverlay-CfTyIhml.js";import"./forwardRefs-DWGaNmQL.js";import"./proxiedModel-Xz4IZg0a.js";import"./anchor-BS5WaiJh.js";import"./dimensions-BWgpWc0X.js";import"./display-Db__yhk9.js";import"./scopeId-CPcZlDEY.js";import"./locale-Beavd5eP.js";import"./router-DnAkll3L.js";import"./transition-shOmLEJh.js";import"./VDefaultsProvider-BGrIgR0V.js";import"./useAppTimeZone-F2KFQp0H.js";import"./FSIconCheck-DUTkCF55.js";import"./useAppOrganisationId-HHayVd6L.js";import"./startOfWeek-uXTpkxA4.js";import"./uuid-DTaye2KM.js";import"./useAppLanguageCode-DjpqeQYI.js";import"./startOfDay-C4pDH4rb.js";import"./useTranslations-DN7QCs30.js";class J{constructor(t){m(this,"value");m(this,"timestamp");this.value=t.value,this.timestamp=t.timestamp?D(t.timestamp):null}}const K=(e,t,o)=>`${U()}/custom-property-values/${encodeURIComponent(e)}/${encodeURIComponent(t)}/${encodeURIComponent(o)}`,Q={...A.addCustom("getMany",(e,t,o,i)=>e.get(K(t,o,i)),e=>e.map(t=>new J(t)))},Y=B.custom(Q.getMany),V=E({name:"FSMetaHistory",components:{FSMetaValue:L,FSButton:P,FSDialog:R},props:{customProperty:{type:Object,required:!0},entityType:{type:Number,required:!0},entityId:{type:String,required:!0}},setup(e){const{epochToLongTimeFormat:t}=x(),{fetch:o,entity:i}=Y(),c=w(!1);return $(()=>e.entityId,async(d,p)=>{N.isEqual(d,p)||await o(e.entityType,e.entityId,e.customProperty.code)},{immediate:!0}),{customPropertyValues:i,dialog:c,epochToLongTimeFormat:t}}});function W(e,t,o,i,c,d){const p=l("FSText"),b=l("FSMetaValue"),_=l("FSDialog");return u(),f(F,null,[r(P,{prependIcon:"mdi-history",label:e.$tr("ui.custom-property.history","History"),onClick:t[0]||(t[0]=a=>e.dialog=!e.dialog)},null,8,["label"]),r(_,{width:"500px",label:e.$tr("ui.custom-property.history","History"),modelValue:e.dialog,"onUpdate:modelValue":t[1]||(t[1]=a=>e.dialog=a)},{body:n(()=>[r(y,null,{default:n(()=>[(u(!0),f(F,null,q(e.customPropertyValues,(a,h)=>(u(),k(z,{key:h},{default:n(()=>[r(p,null,{default:n(()=>[v(H(e.epochToLongTimeFormat(a.timestamp)),1)]),_:2},1024),r(y,{padding:"0 8px 0 0",align:"center-right"},{default:n(()=>[r(b,{meta:{code:a.value},customProperty:e.$props.customProperty},null,8,["meta","customProperty"])]),_:2},1024)]),_:2},1024))),128))]),_:1})]),_:1},8,["label","modelValue"])],64)}const M=j(V,[["render",W]]);V.__docgenInfo={displayName:"FSMetaHistory",exportName:"default",description:"",tags:{},props:[{name:"customProperty",type:{name:"CustomPropertyInfos"},required:!0},{name:"entityType",type:{name:"PropertyEntity"},required:!0},{name:"entityId",type:{name:"string"},required:!0}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/core/foundation-core-components/components/customProperties/FSMetaHistory.vue"]};const We={title:"Foundation/Core/CustomProperties/MetaHistory",component:M,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},s={args:{args:{customProperty:{id:"1",code:"code",label:"Number",categoryLabel:"Category",index:0,entity:1,dataType:G.Number,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!0,colorMap:[{priority:1,filterType:g.MoreOrEqual,filterValues:[5],color:"#0000FF"},{priority:2,filterType:g.MoreOrEqual,filterValues:[50],color:"#FF0000"}],useOnlyAllowedValues:!1,allowedValues:{}}}},render:(e,{argTypes:t})=>({components:{FSMetaHistory:M,FSCol:y},props:Object.keys(t),setup(){return{...e}},template:`
+    <FSCol>
+      <FSMetaHistory
+        :customProperty="args.customProperty"
+        :entityType="1"
+        :entityId="'1'" />
+    </FSCol>`})};var S,C,T;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    args: {
+      customProperty: {
+        id: "1",
+        code: "code",
+        label: "Number",
+        categoryLabel: "Category",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.Number,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: true,
+        colorMap: [{
+          priority: 1,
+          filterType: FilterType.MoreOrEqual,
+          filterValues: [5],
+          color: "#0000FF"
+        }, {
+          priority: 2,
+          filterType: FilterType.MoreOrEqual,
+          filterValues: [50],
+          color: "#FF0000"
+        }],
+        useOnlyAllowedValues: false,
+        allowedValues: {}
+      }
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSMetaHistory,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSMetaHistory
+        :customProperty="args.customProperty"
+        :entityType="1"
+        :entityId="'1'" />
+    </FSCol>\`
+  })
+}`,...(T=(C=s.parameters)==null?void 0:C.docs)==null?void 0:T.source}}};const Xe=["Variations"];export{s as Variations,Xe as __namedExportsOrder,We as default};
