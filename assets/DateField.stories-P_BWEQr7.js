97e224fd3c9a1d68a594d478715fcb7c98d019d5
gballigand <gballigand@users.noreply.github.com>
2024-11-25 16:32:53 +0000

    deploy: ee5b5dd11812a3b9afe00f3345c40d61aa843b30

---
+++ assets/DateField.stories-P_BWEQr7.js
@@ -0,0 +1,194 @@
+import{d as M,b as F,c as H,w as G,A as b,B as v,C as D,E as o,G as N,P as l,D as w,Q as $,M as p,N as k,O as R,I as K,j as L}from"./vue.esm-bundler-Zcuj-zyO.js";import{_ as I}from"./lodash-BiW_TGGX.js";import{F as z}from"./VList-nN2lrumh.js";import{F as S}from"./FSTextField-BcsUOOXo.js";import{F as Q}from"./FSCalendar-DXApRH2_.js";import{F as s}from"./FSButton-BzTwanlk.js";import{F as J}from"./FSCard-4bilSlvW.js";import{F as d}from"./FSCol-DLyNVOXQ.js";import{C as T}from"./useColors-Cz5tOQmp.js";import{u as W}from"./useDateFormat-CYsDMoti.js";import{u as X}from"./useRules-CvuqAlPS.js";import{u as Y}from"./useBreakpoints-IB1lZo1P.js";import{_ as x}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{V as _}from"./VMenu-C3kJxa_y.js";import{F as ee}from"./FSForm-BEWm1ROj.js";import{_ as P}from"./FSRow-4Qz6Bbpg.js";import{u as re}from"./useAppTimeZone-F2KFQp0H.js";import{D as ae}from"./rules-LGZSNZpJ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./VDialog-TaqcJ_9B.js";import"./VOverlay-CfTyIhml.js";import"./forwardRefs-DWGaNmQL.js";import"./color-mm3JSYKM.js";import"./theme-DppimMvq.js";import"./proxiedModel-Xz4IZg0a.js";import"./anchor-BS5WaiJh.js";import"./dimensions-BWgpWc0X.js";import"./display-Db__yhk9.js";import"./scopeId-CPcZlDEY.js";import"./locale-Beavd5eP.js";import"./router-DnAkll3L.js";import"./transition-shOmLEJh.js";import"./VDefaultsProvider-BGrIgR0V.js";import"./index-D14OnwAh.js";import"./VIcon-DsLd3bsf.js";import"./ssrBoot-B8tyBOTn.js";import"./border-C0mvfulf.js";import"./density-DMrbDfgW.js";import"./elevation-BrHPj82J.js";import"./rounded-3VvIKpNW.js";import"./index-CMMTOWvF.js";import"./VImg-DaKoZ_qJ.js";import"./index-DnJxNQYu.js";import"./FSBaseField-DHv7mzhZ.js";import"./FSSpan-Bb6VK1Ws.js";import"./useSlots-DzOFTavN.js";import"./VField-BtCsV974.js";import"./VLabel-C9T0eyno.js";import"./VInput-DP_atbEr.js";import"./form-ChxmsGl9.js";import"./loader-EtqkGPwj.js";import"./VProgressCircular-B73DQeX0.js";import"./resizeObserver-BOpsWgva.js";import"./FSSelectField-b29JGwjP.js";import"./FSSlideGroup-CtBhRuPR.js";import"./uuid-DTaye2KM.js";import"./FSButtonNextIcon-BV5U7bCA.js";import"./css-YWErTUWV.js";import"./VSlideGroupItem-B0P2Hyp-.js";import"./group-ATTNrdHt.js";import"./FSToggleSet-DhGGvbF6.js";import"./FSWrapGroup-DSD82cKU.js";import"./FSCheckbox-CunxM6rm.js";import"./FSIcon-CKEWDmaC.js";import"./VSelectionControl-u1dDpc9z.js";import"./FSFadeOut-CeMJmSZZ.js";import"./FSLoader-CRmsosiO.js";import"./FSRadio-DnH66ibu.js";import"./VSelect-pAG77NtN.js";import"./useAppLanguageCode-DjpqeQYI.js";import"./VLocaleProvider-BOBWKwQe.js";import"./VBtn-CKg6JYm3.js";import"./FSRouterLink-BdruYWDU.js";import"./vue-router-D-G4AqQz.js";import"./FSClickable-CUaqPDhz.js";import"./index-DfSX31J9.js";import"./datesTools-CGhnvN9k.js";import"./startOfWeek-uXTpkxA4.js";import"./startOfDay-C4pDH4rb.js";import"./useTranslations-DN7QCs30.js";import"./time-D2iTm1EC.js";const j=M({name:"FSDateField",components:{FSDialogMenu:z,FSTextField:S,FSCalendar:Q,FSButton:s,FSCard:J,FSCol:d},props:{label:{type:String,required:!1,default:null},description:{type:String,required:!1,default:null},modelValue:{type:Number,required:!1,default:null},color:{type:String,required:!1,default:T.Primary},hideHeader:{type:Boolean,required:!1,default:!1},required:{type:Boolean,required:!1,default:!1},rules:{type:Array,required:!1,default:()=>[]},editable:{type:Boolean,required:!1,default:!0}},emits:["update:modelValue"],setup(e,{emit:r}){const{epochToLongDateFormat:V}=W(),{validateOn:h,getMessages:q}=X(),{isExtraSmall:C}=Y(),n=F(!1),m=F(!1),t=F(e.modelValue),a=H(()=>q(e.modelValue,e.rules)),u=()=>{e.editable&&(n.value=!0)},i=()=>{r("update:modelValue",null),t.value=null},g=()=>{r("update:modelValue",t.value),n.value=!1,m.value=!1};return G(()=>e.modelValue,()=>{I.isEqual(t.value,e.modelValue)||(t.value=e.modelValue)}),{isExtraSmall:C,validateOn:h,ColorEnum:T,innerDate:t,messages:a,dialog:n,menu:m,epochToLongDateFormat:V,openMobileOverlay:u,onSubmit:g,onClear:i}}});function le(e,r,V,h,q,C){const n=b("FSCalendar"),m=b("FSDialogMenu"),t=b("FSCard");return v(),D(d,null,{default:o(()=>[e.isExtraSmall?(v(),N(K,{key:0},[l(S,{class:"fs-date-field",label:e.$props.label,description:e.$props.description,color:e.$props.color,hideHeader:e.$props.hideHeader,required:e.$props.required,editable:e.$props.editable,readonly:!0,rules:e.$props.rules,messages:e.messages,validateOn:e.validateOn,validationValue:e.$props.modelValue,modelValue:e.epochToLongDateFormat(e.$props.modelValue),"onUpdate:modelValue":e.onClear,onClick:e.openMobileOverlay},w({"prepend-inner":o(()=>[p(e.$slots,"prepend-inner",{},()=>[l(s,{variant:"icon",icon:"mdi-calendar",editable:e.$props.editable,color:e.ColorEnum.Dark,onClick:e.openMobileOverlay},null,8,["editable","color","onClick"])])]),_:2},[$(e.$slots,(a,u)=>({name:u,fn:o(i=>[p(e.$slots,u,k(R(i)))])}))]),1032,["label","description","color","hideHeader","required","editable","rules","messages","validateOn","validationValue","modelValue","onUpdate:modelValue","onClick"]),l(m,{modelValue:e.dialog,"onUpdate:modelValue":r[1]||(r[1]=a=>e.dialog=a)},{body:o(()=>[l(d,{width:"hug"},{default:o(()=>[l(n,{color:e.$props.color,modelValue:e.innerDate,"onUpdate:modelValue":r[0]||(r[0]=a=>e.innerDate=a)},null,8,["color","modelValue"]),l(s,{width:"100%",color:e.$props.color,label:e.$tr("ui.common.validate","Validate"),onClick:e.onSubmit},null,8,["color","label","onClick"])]),_:1})]),_:1},8,["modelValue"])],64)):(v(),D(_,{key:1,closeOnContentClick:!1,modelValue:e.menu&&e.$props.editable,"onUpdate:modelValue":r[3]||(r[3]=a=>e.menu=a)},{activator:o(({props:a})=>[l(S,L({class:"fs-date-field",label:e.$props.label,description:e.$props.description,color:e.$props.color,hideHeader:e.$props.hideHeader,required:e.$props.required,editable:e.$props.editable,readonly:!0,rules:e.$props.rules,messages:e.messages,validateOn:e.validateOn,validationValue:e.$props.modelValue,modelValue:e.epochToLongDateFormat(e.$props.modelValue),"onUpdate:modelValue":e.onClear},a),w({"prepend-inner":o(()=>[p(e.$slots,"prepend-inner",{},()=>[l(s,{variant:"icon",icon:"mdi-calendar",editable:e.$props.editable,color:e.ColorEnum.Dark},null,8,["editable","color"])])]),_:2},[$(e.$slots,(u,i)=>({name:i,fn:o(g=>[p(e.$slots,i,k(R(g)))])}))]),1040,["label","description","color","hideHeader","required","editable","rules","messages","validateOn","validationValue","modelValue","onUpdate:modelValue"])]),default:o(()=>[l(t,{padding:"8px",elevation:!0,border:!1},{default:o(()=>[l(d,{width:"hug"},{default:o(()=>[l(n,{color:e.$props.color,modelValue:e.innerDate,"onUpdate:modelValue":r[2]||(r[2]=a=>e.innerDate=a)},null,8,["color","modelValue"]),l(s,{width:"100%",color:e.$props.color,label:e.$tr("ui.common.validate","Validate"),onClick:e.onSubmit},null,8,["color","label","onClick"])]),_:1})]),_:1})]),_:3},8,["modelValue"]))]),_:3})}const y=x(j,[["render",le]]);j.__docgenInfo={displayName:"FSDateField",exportName:"default",description:"",tags:{},props:[{name:"label",type:{name:"string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"description",type:{name:"string | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"modelValue",type:{name:"number | null"},required:!1,defaultValue:{func:!1,value:"null"}},{name:"color",type:{name:"ColorBase"},required:!1,defaultValue:{func:!1,value:"ColorEnum.Primary"}},{name:"hideHeader",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}},{name:"required",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}},{name:"rules",type:{name:"any[]"},required:!1,defaultValue:{func:!0,value:"() => []"}},{name:"editable",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"true"}}],events:[{name:"update:modelValue"}],slots:[{name:"prepend-inner"},{name:"name",scoped:!0,bindings:[{name:"name",title:"binding"}]}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/shared/foundation-shared-components/components/fields/FSDateField.vue"]};const Ar={title:"Foundation/Shared/Input fields/DateField",component:y,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},f={args:{args:{setTimeZone:re().setAppTimeZone,value1:null,value2:17012088e5,value3:17012088e5}},render:(e,{argTypes:r})=>({components:{FSDateField:y,FSButton:s,FSCol:d,FSRow:P},props:Object.keys(r),setup(){return{...e}},template:`
+    <FSCol>
+      <FSRow>
+        <FSButton
+          label="Africa/Abidjan"
+          @click="args.setTimeZone('Africa/Abidjan')"
+        />
+        <FSButton
+          label="Europe/Paris"
+          @click="args.setTimeZone('Europe/Paris')"
+        />
+        <FSButton
+          label="America/Guatemala"
+          @click="args.setTimeZone('America/Guatemala')"
+        />
+        <FSButton
+          label="Asia/Kamchatka"
+          @click="args.setTimeZone('Asia/Kamchatka')"
+        />
+      </FSRow>
+      <FSDateField
+        label="Date, primary color"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSDateField
+        label="Required date, warning color, with description"
+        description="Description for this field"
+        color="warning"
+        :required="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSDateField
+        label="Uneditable date, error color, with description"
+        description="Uneditable description"
+        color="error"
+        :editable="false"
+        v-model="args.value3"
+      />
+    </FSCol>`})},c={args:{args:{valid:!1,value1:null,value2:null,value3:null,rules:ae}},render:(e,{argTypes:r})=>({components:{FSForm:ee,FSDateField:y,FSCol:d,FSRow:P},props:Object.keys(r),setup(){return{...e}},template:`
+    <FSForm v-model="args.valid" variant="standard">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: before 2024-01-31 00:00"
+          :rules="[args.rules.max(1706738400000)]"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: required & before 2024-01-31 00:00 & after 2024-01-02 00:00"
+          :rules="[args.rules.required(), args.rules.max(1706738400000), args.rules.min(1704146400000)]"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>`})};var A,B,O;f.parameters={...f.parameters,docs:{...(A=f.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    args: {
+      setTimeZone: useAppTimeZone().setAppTimeZone,
+      value1: null,
+      value2: 1701208800000,
+      value3: 1701208800000
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSDateField,
+      FSButton,
+      FSCol,
+      FSRow
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSRow>
+        <FSButton
+          label="Africa/Abidjan"
+          @click="args.setTimeZone('Africa/Abidjan')"
+        />
+        <FSButton
+          label="Europe/Paris"
+          @click="args.setTimeZone('Europe/Paris')"
+        />
+        <FSButton
+          label="America/Guatemala"
+          @click="args.setTimeZone('America/Guatemala')"
+        />
+        <FSButton
+          label="Asia/Kamchatka"
+          @click="args.setTimeZone('Asia/Kamchatka')"
+        />
+      </FSRow>
+      <FSDateField
+        label="Date, primary color"
+        v-model="args.value1"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSDateField
+        label="Required date, warning color, with description"
+        description="Description for this field"
+        color="warning"
+        :required="true"
+        v-model="args.value2"
+      />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+      <FSDateField
+        label="Uneditable date, error color, with description"
+        description="Uneditable description"
+        color="error"
+        :editable="false"
+        v-model="args.value3"
+      />
+    </FSCol>\`
+  })
+}`,...(O=(B=f.parameters)==null?void 0:B.docs)==null?void 0:O.source}}};var E,Z,U;c.parameters={...c.parameters,docs:{...(E=c.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    args: {
+      valid: false,
+      value1: null,
+      value2: null,
+      value3: null,
+      rules: DateRules
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSForm,
+      FSDateField,
+      FSCol,
+      FSRow
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSForm v-model="args.valid" variant="standard">
+      <FSCol>
+        <FSRow>
+          <div class="text-body">
+            Form validity: {{ args.valid ?? "false" }}
+          </div>
+        </FSRow>
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: required"
+          :rules="[args.rules.required()]"
+          :required="true"
+          v-model="args.value1"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: before 2024-01-31 00:00"
+          :rules="[args.rules.max(1706738400000)]"
+          :required="true"
+          v-model="args.value2"
+        />
+        <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+        <FSDateField
+          label="Rules: required & before 2024-01-31 00:00 & after 2024-01-02 00:00"
+          :rules="[args.rules.required(), args.rules.max(1706738400000), args.rules.min(1704146400000)]"
+          :required="true"
+          v-model="args.value3"
+        />
+      </FSCol>
+    </FSForm>\`
+  })
+}`,...(U=(Z=c.parameters)==null?void 0:Z.docs)==null?void 0:U.source}}};const Br=["Variations","Rules"];export{c as Rules,f as Variations,Br as __namedExportsOrder,Ar as default};
---
+++ assets/MetaFormContent.stories-CKpxf4Kj.js
@@ -0,0 +1,144 @@
+import{d as M,A as u,B as r,C as d,E as l,G as y,Q as c,P as f,J as P,K as x,I as g}from"./vue.esm-bundler-Zcuj-zyO.js";import{F as _}from"./FSText-DqK2dTtm.js";import{F as v}from"./FSMetaField-0tNsO_bD.js";import{_ as L}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{F as p}from"./FSCol-DLyNVOXQ.js";import{_ as k}from"./FSRow-4Qz6Bbpg.js";import{P as o}from"./customProperties-DqkON8hk.js";import"./useColors-Cz5tOQmp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DfSX31J9.js";import"./theme-DppimMvq.js";import"./useBreakpoints-IB1lZo1P.js";import"./useSlots-DzOFTavN.js";import"./FSDateTimeField-Dhgk2EVR.js";import"./VList-nN2lrumh.js";import"./FSCard-4bilSlvW.js";import"./css-YWErTUWV.js";import"./VDialog-TaqcJ_9B.js";import"./VOverlay-CfTyIhml.js";import"./forwardRefs-DWGaNmQL.js";import"./color-mm3JSYKM.js";import"./proxiedModel-Xz4IZg0a.js";import"./anchor-BS5WaiJh.js";import"./dimensions-BWgpWc0X.js";import"./display-Db__yhk9.js";import"./scopeId-CPcZlDEY.js";import"./locale-Beavd5eP.js";import"./router-DnAkll3L.js";import"./transition-shOmLEJh.js";import"./VDefaultsProvider-BGrIgR0V.js";import"./index-D14OnwAh.js";import"./VIcon-DsLd3bsf.js";import"./ssrBoot-B8tyBOTn.js";import"./border-C0mvfulf.js";import"./density-DMrbDfgW.js";import"./elevation-BrHPj82J.js";import"./rounded-3VvIKpNW.js";import"./index-CMMTOWvF.js";import"./VImg-DaKoZ_qJ.js";import"./index-DnJxNQYu.js";import"./FSTextField-BcsUOOXo.js";import"./FSBaseField-DHv7mzhZ.js";import"./FSSpan-Bb6VK1Ws.js";import"./FSButton-BzTwanlk.js";import"./FSRouterLink-BdruYWDU.js";import"./vue-router-D-G4AqQz.js";import"./FSClickable-CUaqPDhz.js";import"./VProgressCircular-B73DQeX0.js";import"./resizeObserver-BOpsWgva.js";import"./FSIcon-CKEWDmaC.js";import"./useRules-CvuqAlPS.js";import"./VField-BtCsV974.js";import"./VLabel-C9T0eyno.js";import"./VInput-DP_atbEr.js";import"./form-ChxmsGl9.js";import"./loader-EtqkGPwj.js";import"./FSCalendar-DXApRH2_.js";import"./FSSelectField-b29JGwjP.js";import"./FSSlideGroup-CtBhRuPR.js";import"./uuid-DTaye2KM.js";import"./FSButtonNextIcon-BV5U7bCA.js";import"./VSlideGroupItem-B0P2Hyp-.js";import"./group-ATTNrdHt.js";import"./FSToggleSet-DhGGvbF6.js";import"./FSWrapGroup-DSD82cKU.js";import"./FSCheckbox-CunxM6rm.js";import"./VSelectionControl-u1dDpc9z.js";import"./FSFadeOut-CeMJmSZZ.js";import"./FSLoader-CRmsosiO.js";import"./FSRadio-DnH66ibu.js";import"./VSelect-pAG77NtN.js";import"./VMenu-C3kJxa_y.js";import"./useDateFormat-CYsDMoti.js";import"./useAppLanguageCode-DjpqeQYI.js";import"./useAppTimeZone-F2KFQp0H.js";import"./datesTools-CGhnvN9k.js";import"./startOfWeek-uXTpkxA4.js";import"./startOfDay-C4pDH4rb.js";import"./useTranslations-DN7QCs30.js";import"./VLocaleProvider-BOBWKwQe.js";import"./VBtn-CKg6JYm3.js";import"./FSWindow-Cnn6JjJi.js";import"./FSClock-kkHi0Cxl.js";import"./FSSlider-BqRP0F7G.js";import"./VSlider-B_AKVe6L.js";import"./FSNumberField-DwEYYhZY.js";import"./FSIconField-D2JdXBAF.js";import"./FSSwitch-CyXxQ8EU.js";const S=M({name:"FSMetaFormContent",components:{FSMetaField:v,FSText:_},props:{customProperties:{type:[Object],required:!0},modelValue:{type:Object,required:!0},editable:{type:Boolean,required:!1,default:!0}},setup(e){return{categories:e.customProperties.reduce((t,a)=>(t[a.categoryLabel]===void 0&&(t[a.categoryLabel]=[]),t[a.categoryLabel].push(a),t),{})}}});function z(e,i,t,a,D,B){const h=u("FSText"),F=u("FSMetaField");return r(),d(p,{gap:24},{default:l(()=>[(r(!0),y(g,null,c(e.categories,(w,m)=>(r(),d(p,{key:m,gap:16},{default:l(()=>[f(k,null,{default:l(()=>[f(h,{font:"text-h3"},{default:l(()=>[P(x(m),1)]),_:2},1024)]),_:2},1024),(r(!0),y(g,null,c(w,(s,T)=>(r(),d(F,{customProperty:s,editable:e.editable,key:T,modelValue:e.modelValue[s.code],"onUpdate:modelValue":A=>e.$emit("update:modelValue",{...e.modelValue,[s.code]:A})},null,8,["customProperty","editable","modelValue","onUpdate:modelValue"]))),128))]),_:2},1024))),128))]),_:1})}const O=L(S,[["render",z]]);S.__docgenInfo={displayName:"FSMetaFormContent",exportName:"default",description:"",tags:{},props:[{name:"customProperties",type:{name:"CustomPropertyInfos[]"},required:!0},{name:"modelValue",type:{name:"{ [key: string]: string }"},required:!0},{name:"editable",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"true"}}],events:[{name:"update:modelValue"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/core/foundation-core-components/components/customProperties/FSMetaFormContent.vue"]};const bo={title:"Foundation/Core/CustomProperties/MetaFormContent",component:O,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},n={args:{args:{customProperties:[{id:"1",label:"Selector",code:"selector",categoryLabel:"Category1",index:0,entity:1,dataType:o.String,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!0,allowedValues:{1:"value1",2:"value 2",3:"value 3","key without value":null}},{id:"2",label:"Number",code:"number",categoryLabel:"Category1",index:0,entity:1,dataType:o.Number,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!1,allowedValues:{}},{id:"3",label:"Boolean",code:"bool",categoryLabel:"Category1",index:0,entity:1,dataType:o.Boolean,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!1,allowedValues:{}},{id:"4",label:"String",code:"string",categoryLabel:"Category2",index:0,entity:1,dataType:o.String,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!1,allowedValues:{}},{id:"5",label:"Date",code:"date",categoryLabel:"Category2",index:0,entity:1,dataType:o.DateTime,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!1,allowedValues:{}},{id:"6",label:"Icon",code:"icon",categoryLabel:"Category3",index:0,entity:1,dataType:o.Icon,readOnlyCore:!1,readOnlyAdmin:!1,defaultValue:"",history:!1,historySize:0,colorful:!1,colorMap:[],useOnlyAllowedValues:!1,allowedValues:{}}],meta:{existing:"test"}}},render:(e,{argTypes:i})=>({components:{FSMetaFormContent:O,FSCol:p},props:Object.keys(i),setup(){return{...e}},template:`
+    <FSCol>
+      <FSMetaFormContent
+        :customProperties="args.customProperties"
+        v-model="args.meta" />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+    </FSCol>`})};var V,b,C;n.parameters={...n.parameters,docs:{...(V=n.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    args: {
+      customProperties: [{
+        id: "1",
+        label: "Selector",
+        code: "selector",
+        categoryLabel: "Category1",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.String,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: true,
+        allowedValues: {
+          ["1"]: "value1",
+          ["2"]: "value 2",
+          ["3"]: "value 3",
+          ["key without value"]: null
+        }
+      }, {
+        id: "2",
+        label: "Number",
+        code: "number",
+        categoryLabel: "Category1",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.Number,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: false,
+        allowedValues: {}
+      }, {
+        id: "3",
+        label: "Boolean",
+        code: "bool",
+        categoryLabel: "Category1",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.Boolean,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: false,
+        allowedValues: {}
+      }, {
+        id: "4",
+        label: "String",
+        code: "string",
+        categoryLabel: "Category2",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.String,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: false,
+        allowedValues: {}
+      }, {
+        id: "5",
+        label: "Date",
+        code: "date",
+        categoryLabel: "Category2",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.DateTime,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: false,
+        allowedValues: {}
+      }, {
+        id: "6",
+        label: "Icon",
+        code: "icon",
+        categoryLabel: "Category3",
+        index: 0,
+        entity: 1,
+        dataType: PropertyDataType.Icon,
+        readOnlyCore: false,
+        readOnlyAdmin: false,
+        defaultValue: "",
+        history: false,
+        historySize: 0,
+        colorful: false,
+        colorMap: [],
+        useOnlyAllowedValues: false,
+        allowedValues: {}
+      }],
+      meta: {
+        existing: "test"
+      }
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSMetaFormContent,
+      FSCol
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <FSCol>
+      <FSMetaFormContent
+        :customProperties="args.customProperties"
+        v-model="args.meta" />
+      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
+    </FSCol>\`
+  })
+}`,...(C=(b=n.parameters)==null?void 0:b.docs)==null?void 0:C.source}}};const Co=["Variations"];export{n as Variations,Co as __namedExportsOrder,bo as default};
