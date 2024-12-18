2b01d3aa252c5b1fba33cdf6fa06787ec21c2563
rduteil <rduteil@dative-gpi.com>
2024-12-18 14:58:22 +0100

    Fix mobile pages

--- src/shared/foundation-shared-components/components/views/mobile/FSBaseDefaultMobileView.vue
+++ src/shared/foundation-shared-components/components/views/mobile/FSBaseDefaultMobileView.vue
@@ -14,7 +14,7 @@
       >
 
         <FSRow
-          padding="24px 16px 16px 24px"
+          padding="24px 24px 16px 24px"
           style="position: sticky; top: 0px; z-index: 3;"
           :style="{ backgroundColor, marginTop: $props.stickyTitleTopOffset }"
         >
@@ -45,7 +45,7 @@
         </FSCol>
         <FSRow
           v-if="$slots.toolbar"
-          padding="0px 16px 8px 24px"
+          padding="0px 24px 8px 24px"
           :style="stickyToolbar ? `position: sticky; top: ${$props.toolbarTopOffset}; z-index: 3; background-color: ${backgroundColor}` : undefined"
         >
           <FSSlideGroup>
@@ -58,7 +58,7 @@
 
       <FSCol
         height="fill"
-        :padding="$slots.toolbar ? '8px 16px 24px 24px' : '16px 16px 24px 24px'"
+        :padding="$slots.toolbar ? '8px 24px 24px 24px' : '16px 24px 24px 24px'"
         gap="0px"
       >
         <slot />
