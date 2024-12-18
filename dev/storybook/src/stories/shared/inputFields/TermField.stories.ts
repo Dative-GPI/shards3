12b6dc33126b5a9f6b555fee6634c3a5c04aae5b
peyronth <peyron.th@gmail.com>
2024-12-18 10:31:53 +0100

    Add termfield date evaluation

--- dev/storybook/src/stories/shared/inputFields/TermField.stories.ts
+++ dev/storybook/src/stories/shared/inputFields/TermField.stories.ts
@@ -1,85 +1,114 @@
 import type { Meta, StoryObj } from '@storybook/vue3';
+import { reactive, computed, ref } from 'vue';
 
-import FSTermField from "@dative-gpi/foundation-shared-components/components/fields/FSTermField.vue";
+import { useDateFormat, useTermFieldDate } from '@dative-gpi/foundation-shared-services/composables';
+
 import FSCol from "@dative-gpi/foundation-shared-components/components/FSCol.vue";
+import FSText from '@dative-gpi/foundation-shared-components/components/FSText.vue';
+import FSTermField from "@dative-gpi/foundation-shared-components/components/fields/FSTermField.vue";
+import FSTextField from '@dative-gpi/foundation-shared-components/components/fields/FSTextField.vue';
 
 const meta = {
   title: 'Foundation/Shared/Input fields/TermField',
   component: FSTermField,
   tags: ['autodocs'],
   argTypes: {
-    onClick: { action: 'clicked' }
+    variant: {
+      control: { type: 'select' },
+      options: ['default', 'before-after']
+    },
+    'onUpdate': { action: 'update' },
+    'onUpdate:startDate': { action: 'update:startDate' },
+    'onUpdate:endDate': { action: 'update:endDate' },
   },
 } satisfies Meta<typeof FSTermField>;
 
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
 export const Variations: Story = {
   args: {
-    args: {
-      start1: "now/h",
-      end1: "now",
-      start2: "now - 3d",
-      end2: "now",
-      start3: "alertstart - 1h",
-      end3: "alertend + 1h",
-      start4: "2023-01-01T00:00:00",
-      end4: "2024-01-01T00:00:00",
-      meta: {}
-    }
+    label: 'Term',
+    description: null,
+    startDate: 'now - 1d',
+    endDate: 'now',
+    variant: 'default',
+    hideHeader: false,
+    lastPeriod: false,
+    required: false,
+    editable: true,
   },
   render: (args, { argTypes }) => ({
-    components: { FSCol, FSTermField },
-    props: Object.keys(argTypes),
+    components: { FSCol, FSTermField, FSText, FSTextField },
     setup() {
-      return { ...args };
+      // Crée une copie locale réactive des args
+      const localArgs = reactive({
+        ...args
+      });
+
+      const { convert } = useTermFieldDate();
+      const { epochToLongTimeFormat } = useDateFormat();
+
+      const variables = ref({
+        variable: 'now - 2d',
+      });
+
+      const convertedStartDate = computed(() => epochToLongTimeFormat(convert(localArgs.startDate, variables.value)));
+      const convertedEndDate = computed(() => epochToLongTimeFormat(convert(localArgs.endDate, variables.value)));
+
+      // Émet les événements d'update vers Storybook
+      const updateStartDate = (value: string) => {
+        localArgs.startDate = value;
+        args['onUpdate:startDate']?.(value);
+      };
+
+      const updateEndDate = (value: string) => {
+        localArgs.endDate = value;
+        args['onUpdate:endDate']?.(value);
+      };
+
+      return { localArgs, convertedStartDate, convertedEndDate, updateStartDate, updateEndDate, variables };
     },
     template: `
-    <FSCol>
-      <FSTermField
-        label="Term"
-        v-model:startDate="args.start1"
-        v-model:endDate="args.end1"
-      />
-      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
-      <FSTermField
-        label="Required default term, with description"
-        description="Description for this field"
-        :lastPeriod="true"
-        :required="true"
-        v-model:startDate="args.start2"
-        v-model:endDate="args.end2"
-      />
-      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
-      <FSTermField
-        label="Required before-after term, with description"
-        description="Description for this field"
-        variant="before-after"
-        :lastPeriod="true"
-        :required="true"
-        v-model:startDate="args.start3"
-        v-model:endDate="args.end3"
-      />
-      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
-      <FSTermField
-        label="Uneditable term, with description"
-        description="Description for this field"
-        :lastPeriod="true"
-        :editable="false"
-        :required="true"
-        v-model:startDate="args.start4"
-        v-model:endDate="args.end4"
-      />
-      <div style="width: 100%; border-bottom: 2px dotted lightgrey" />
-      <FSTermField
-        label="Using global update"
-        description="Description for this field"
-        :startDate="args.meta?.startDate"
-        :endDate="args.meta?.endDate"
-        :required="true"
-        @update="args.meta = { ...$event }"
-      />
-    </FSCol>`
-  })
-}
+      <FSCol
+        gap="20px"
+      >
+        <FSTermField
+          v-bind="localArgs"
+          v-model:startDate="localArgs.startDate"
+          v-model:endDate="localArgs.endDate"
+          @update="localArgs['onUpdate']"
+          @update:startDate="updateStartDate"
+          @update:endDate="updateEndDate"
+        />
+        <FSCol gap="4px">
+          <FSText
+            font="text-h3"
+          >
+            Evaluation de la date de début et de fin avec useTermFieldDate:
+          </FSText>
+          <FSRow>
+            <FSText
+              font="text-button"
+            >
+              Date de début:
+            </FSText>
+            <FSText>{{ convertedStartDate }}</FSText>
+          </FSRow>
+          <FSRow>
+            <FSText
+              font="text-button"
+            >
+              Date de fin:
+            </FSText>
+            <FSText>{{ convertedEndDate }}</FSText>
+          </FSRow>
+          <FSText>
+            Variables: {{ variables }}
+          </FSText>
+        </FSCol>
+      </FSCol>
+    `
+  }),
+};
---
+++ src/shared/foundation-shared-services/composables/useTermFieldDate.ts
@@ -0,0 +1,137 @@
+import { useAppTimeZone } from './app/useAppTimeZone';
+import { useDateFormat } from './useDateFormat';
+
+export const useTermFieldDate = () => {
+  const { todayToEpoch } = useDateFormat();
+  const { getUserOffset } = useAppTimeZone();
+
+  const EXPRESSION = /(?:(?:([-\+])(\d*))?(\w+))?(?:\/(\w))?/g;
+  const NOW = 'now';
+
+  const convert = (value: string, variables: { [key: string]: number | string } = {}): number => {
+    //Try to convert the value to a date
+    const date = new Date(value);
+    if (!isNaN(date.getTime())) {
+      return date.getTime() + getUserOffset(date.getTime());
+    }
+
+    const currentVariables = {
+      ...variables,
+      [NOW]: todayToEpoch()
+    };
+
+    return applyFormula(value, currentVariables);
+  }
+
+  const applyFormula = (expression: string, variables: { [key: string]: number | string }): number => {
+    expression = expression.replace(/\s/g, '');
+    const matches = expression.matchAll(EXPRESSION);    
+
+    if (!matches) {
+      console.error('Invalid expression');
+      return NaN;
+    }
+
+    let timestamp = 0;
+
+    // Retrieving group in order are: operator (+ or -), number, unit, and special (variable)
+    matches.forEach(match => {
+      const operator = match[1];
+      const number = match[2];
+      const unit = match[3];
+      const special = match[4];
+
+      if (unit) {
+        timestamp = applyUnit(timestamp, operator, number, unit, variables);
+      }
+
+      if (special) {
+        timestamp = applySpecial(timestamp, special);
+      }
+    });
+
+    return timestamp;
+  }
+
+  const applyUnit = (timestamp: number, operator: string, number: string, unit: string, variables: { [key: string]: number | string }): number => {
+    let factor = operator === '-' ? -1 : 1;
+    factor *= Number.isNaN(parseInt(number)) ? 1 : parseInt(number);
+
+    switch (unit) {
+      case 's':
+        return timestamp + factor * 1000;
+      case 'm':
+        return timestamp + factor * 60 * 1000;
+      case 'h':
+        return timestamp + factor * 60 * 60 * 1000;
+      case 'd':
+        return new Date(timestamp).setDate(new Date(timestamp).getDate() + factor)
+      case 'w':
+        return new Date(timestamp).setDate(new Date(timestamp).getDate() + factor * 7);
+      case 'M':
+        return new Date(timestamp).setMonth(new Date(timestamp).getMonth() + factor);
+      case 'y':
+        return new Date(timestamp).setFullYear(new Date(timestamp).getFullYear() + factor);
+      default:
+        if(variables[unit]) {
+          const variableValue = getVariableValue(unit, variables);
+          return timestamp + factor * variableValue;
+        }
+        else {
+          console.error(`Invalid unit expression, unit ${unit} is not defined in the variables`);
+          return timestamp;
+        }
+    }
+  }
+
+  const getVariableValue = (unit: string, variables: { [key: string]: number | string }): number => {
+    if(variables[unit]) {
+      const variableValue = parseInt(variables[unit].toString());
+      if (typeof variables[unit] === 'number') {
+        return variableValue;
+      } else if (!isNaN(variableValue)) {
+        return variableValue;
+      } else {
+        const variableExpression = variables[unit].toString();
+        const variableValue = applyFormula(variableExpression, variables);
+        if (!isNaN(variableValue)) {
+          return variableValue;
+        } else {
+          console.error(`Invalid unit expression, unit ${unit} is not defined in the variables`);
+          return 0;
+        }
+      }
+    }
+    else {
+      console.error(`Invalid unit expression, unit ${unit} is not defined in the variables`);
+      return 0;
+    }
+  }
+
+  const applySpecial = (timestamp: number, special: string): number => {
+    switch (special) {
+      case "h":
+        return new Date(timestamp).setMinutes(0, 0, 0);
+      case "d":
+        return new Date(timestamp).setHours(0, 0, 0, 0);
+      case "w":
+        const date = new Date(timestamp);
+        const day = date.getDay();
+        const diff = date.getDate() - day + (day === 0 ? -6 : 1);
+        return new Date(date.setDate(diff)).setHours(0, 0, 0, 0);
+      case "M":
+        const dateM = new Date(timestamp);
+        return new Date(dateM.setMonth(dateM.getMonth(), 1)).setHours(0, 0, 0, 0);
+      case "y":
+        const dateY = new Date(timestamp);
+        return new Date(dateY.setMonth(0, 1)).setHours(0, 0, 0, 0);
+      default:
+        break;
+    }
+    return timestamp;
+  }
+
+  return {
+    convert
+  }
+}
