f3268e04870bb8ce381f512fb9362efd83610d93
qschroter <qschroter@gpi-experts.com>
2024-12-24 10:03:08 +0100

    add reset deviceOrganisation when location is updated

--- src/core/foundation-core-services/composables/services/useLocations.ts
+++ src/core/foundation-core-services/composables/services/useLocations.ts
@@ -3,11 +3,30 @@ import { ComposableFactory, ServiceFactory } from "@dative-gpi/bones-ui/core";
 
 import { LOCATIONS_URL, LOCATION_URL } from "../../config/urls";
 
+import { resetDeviceOrganisations } from "./useDeviceOrganisations";
+
 const LocationServiceFactory = new ServiceFactory<LocationDetailsDTO, LocationDetails>("location", LocationDetails)
   .createComplete<LocationInfos, LocationInfosDTO, CreateLocationDTO, UpdateLocationDTO, LocationFilters>(LOCATIONS_URL, LOCATION_URL, LocationInfos);
+
+const updateLocation = ComposableFactory.update(LocationServiceFactory);
 
 export const useLocation = ComposableFactory.get(LocationServiceFactory);
 export const useLocations = ComposableFactory.getMany(LocationServiceFactory);
 export const useCreateLocation = ComposableFactory.create(LocationServiceFactory);
-export const useUpdateLocation = ComposableFactory.update(LocationServiceFactory);
 export const useRemoveLocation = ComposableFactory.remove(LocationServiceFactory);
+
+export const useUpdateLocation = () => {
+  const { update, updated, updating } = updateLocation();
+
+  const actualUpdate = async (...params: Parameters<typeof update>) => {
+    const result = await update(...params);
+    resetDeviceOrganisations();
+    return result;
+  }
+
+  return {
+    update: actualUpdate,
+    updated,
+    updating
+  }
+}
